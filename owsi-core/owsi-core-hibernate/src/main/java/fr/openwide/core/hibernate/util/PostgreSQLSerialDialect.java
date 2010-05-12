d10d573acf46c7e9890dd28512f80a4e6814faf5
Julien Gonzalez <julien.gonzalez@openwide.fr>
2010-05-12 08:55:28 +0000

    Import initial du projet "owsi-core-hibernate"
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@54 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-hibernate/src/main/java/fr/openwide/core/hibernate/util/PostgreSQLSerialDialect.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2009-2010 Open Wide
+ * Contact: contact@openwide.fr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.openwide.core.hibernate.util;
+
+import org.hibernate.dialect.PostgreSQLDialect;
+
+/**
+ * <p>Surcharge du dialecte PostgreSQL qui permet de gérer les séquences par table plutôt que d'avoir une seule séquence
+ * globale pour tous les objets.</p>
+ * 
+ * <p>Les séquences sont gérées via des types serial et ce dialect suit donc les conventions de nommage de séquence de
+ * PostgreSQL.</p>
+ * 
+ * <p>Hibernate est assez peu finaud sur la gestion des séquences PostgreSQL.</p>
+ * 
+ * @author Open Wide
+ */
+public class PostgreSQLSerialDialect extends PostgreSQLDialect {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Class getNativeIdentifierGeneratorClass() {
+		return PostgreSQLSequenceGenerator.class;
+	}
+
+}
