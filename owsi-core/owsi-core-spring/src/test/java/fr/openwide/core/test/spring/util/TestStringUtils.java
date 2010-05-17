aa05a8b365e37fdf24d03b4981ddc223819bf695
Julien Gonzalez <julien.gonzalez@openwide.fr>
2010-05-17 12:28:48 +0000

    Ajout des test unitaires pour la classe fr.openwide.core.spring.util.StringUtils.java
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@75 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-spring/src/test/java/fr/openwide/core/test/spring/util/TestStringUtils.java
@@ -0,0 +1,106 @@
+package fr.openwide.core.test.spring.util;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import fr.openwide.core.spring.util.StringUtils;
+
+public class TestStringUtils {
+
+	@Test
+	public void testSplitAsList() {
+		String str1 = "test1-test2-test3";
+		List<String> list1 = StringUtils.splitAsList(str1, "-");
+		assertEquals("test1", list1.get(0));
+		assertEquals("test2", list1.get(1));
+		assertEquals("test3", list1.get(2));
+
+		String str2 = "test1,=+-test2";
+		List<String> list2 = StringUtils.splitAsList(str2, "+");
+		assertEquals("test1,=", list2.get(0));
+		assertEquals("-test2", list2.get(1));
+
+		String str3 = "";
+		List<String> list3 = StringUtils.splitAsList(str3, "");
+		assertTrue(list3.isEmpty());
+
+		String str4 = null;
+		List<String> list4 = StringUtils.splitAsList(str4, "");
+		assertTrue(list4.isEmpty());
+
+		String str5 = "test test test";
+		List<String> list5 = StringUtils.splitAsList(str5, null);
+		assertEquals(1, list5.size());
+		assertEquals(str5, list5.get(0));
+	}
+
+	@Test
+	public void testCleanString() {
+		String cleanStr;
+
+		String str_maj = "ABCDEFG";
+		cleanStr = StringUtils.cleanString(str_maj);
+		assertEquals("abcdefg", cleanStr);
+
+		String str_accent = "éèàçù";
+		cleanStr = StringUtils.cleanString(str_accent);
+		assertEquals("eeacu", cleanStr);
+
+		String str_dashes = "---------";
+		cleanStr = StringUtils.cleanString(str_dashes);
+		assertEquals("", cleanStr);
+
+		String str_spaces = "         ";
+		cleanStr = StringUtils.cleanString(str_spaces);
+		assertEquals("", cleanStr);
+
+		String str_regEx = "[’'`«»\n\r\t\":;,\\.!¡\\?¿&|°_%\\\\©®€²³\\+\\*÷×/%<>()^\\[\\]…–]";
+		cleanStr = StringUtils.cleanString(str_regEx);
+		assertEquals("", cleanStr);
+
+		String str_trimDash = " --test-test--";
+		cleanStr = StringUtils.cleanString(str_trimDash);
+		assertEquals("test-test", cleanStr);
+
+		String str_null = null;
+		cleanStr = StringUtils.cleanString(str_null);
+		assertNull(cleanStr);
+	}
+
+	@Test
+	public void testRemoveAccents() {
+		String cleanStr;
+
+		String str1 = "À Á Â Ã Ä Å";
+		cleanStr = StringUtils.removeAccents(str1);
+		assertEquals("A A A A A A", cleanStr);
+
+		String str2 = "tëßt  TÊST";
+		cleanStr = StringUtils.removeAccents(str2);
+		assertEquals("tesst  TEST", cleanStr);
+
+		String str3 = "";
+		cleanStr = StringUtils.removeAccents(str3);
+		assertEquals("", cleanStr);
+	}
+	
+	@Test
+	public void testCompare() {
+		String str1 = "testtest";
+		String str2 = "test test";
+		String str3 = "test test";
+		
+		assertTrue(StringUtils.compare(str1, str2) > 0);
+		assertTrue(StringUtils.compare(str2, str1) < 0);
+		assertTrue(StringUtils.compare(str2, str3) == 0);
+		assertTrue(StringUtils.compare(str3, "") > 0);
+		assertTrue(StringUtils.compare(str3, "") > 0);
+		assertTrue(StringUtils.compare("", "") == 0);
+		assertTrue(StringUtils.compare("", null) > 0);
+		assertTrue(StringUtils.compare(null, "") < 0 );
+		assertTrue(StringUtils.compare(null, null) == 0);
+	}
+}
