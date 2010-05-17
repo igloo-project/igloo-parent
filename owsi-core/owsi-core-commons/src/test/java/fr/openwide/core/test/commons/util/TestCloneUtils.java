cc8df64f647334e99fa2a869c582a003f3c881b2
Julien Gonzalez <julien.gonzalez@openwide.fr>
2010-05-17 09:26:02 +0000

    Ajout des test unitaires pour owsi-core-hibernate et owsi-core-commons
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@72 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-commons/src/test/java/fr/openwide/core/test/commons/util/TestCloneUtils.java
@@ -0,0 +1,49 @@
+package fr.openwide.core.test.commons.util;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.Test;
+
+import fr.openwide.core.commons.util.CloneUtils;
+
+public class TestCloneUtils {
+
+	@Test
+	public void testCloneDate() {
+		Calendar calendar = new GregorianCalendar();
+		calendar.set(2010, 05, 24, 17, 0, 15);
+
+		Date date1 = new Date(calendar.getTimeInMillis());
+
+		Date date2 = CloneUtils.clone(date1);
+
+		assertNotSame(date1, date2);
+		assertEquals(date1.getTime(), date2.getTime());
+
+		Date date3 = null;
+
+		Date date4 = CloneUtils.clone(date3);
+
+		assertNull(date4);
+	}
+
+	@Test
+	public void testCloneArray() {
+		String[] strArray1 = { "Chaine1", "Chaine2", "Chaine3" };
+
+		String[] strArray2 = CloneUtils.clone(strArray1);
+
+		assertNotSame(strArray1, strArray2);
+		assertArrayEquals(strArray1, strArray2);
+
+		String[] strArray3 = null;
+
+		String[] strArray4 = CloneUtils.clone(strArray3);
+
+		assertNull(strArray4);
+	}
+}
