70cb3dba014c7a401b8cc793291903481d651258
Julien Gonzalez <julien.gonzalez@openwide.fr>
2010-05-17 13:10:15 +0000

    remontée de l'example hibernate depuis "src/test/java" vers "src/main/java"
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@77 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi/owsi-hibernate/src/main/java/fr/openwide/hibernate/example/business/person/dao/PersonDao.java
@@ -0,0 +1,8 @@
+package fr.openwide.hibernate.example.business.person.dao;
+
+import fr.openwide.core.hibernate.business.generic.dao.GenericEntityDao;
+import fr.openwide.hibernate.example.business.person.model.Person;
+
+public interface PersonDao extends GenericEntityDao<Person> {
+
+}
---
+++ owsi/owsi-hibernate/src/main/java/fr/openwide/hibernate/example/business/util/service/ServiceExceptionServiceImpl.java
@@ -0,0 +1,54 @@
+package fr.openwide.hibernate.example.business.util.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.openwide.core.hibernate.exception.SecurityServiceException;
+import fr.openwide.core.hibernate.exception.ServiceException;
+import fr.openwide.hibernate.example.business.company.model.Company;
+import fr.openwide.hibernate.example.business.company.service.CompanyService;
+
+
+/**
+ * Classe utilitaire permettant de générer des exceptions et de vérifier
+ * leur bonne prise en compte par le transaction manager
+ * 
+ * @author Open Wide
+ */
+@Service
+public class ServiceExceptionServiceImpl {
+	
+	@Autowired
+	private CompanyService companyService;
+
+	public void dontThrow() throws ServiceException, SecurityServiceException {
+		Company company = new Company("Company Test");
+		companyService.create(company);
+	}
+
+	public void throwServiceException() throws ServiceException, SecurityServiceException {
+		Company company = new Company("Company Test");
+		companyService.create(company);
+		throw new ServiceException();
+	}
+
+	public void throwServiceInheritedException() throws ServiceException, SecurityServiceException {
+		Company company = new Company("Company Test");
+		companyService.create(company);
+		throw new MyException() ;
+	}
+
+	public void throwUncheckedException() throws ServiceException, SecurityServiceException {
+		Company company = new Company("Company Test");
+		companyService.create(company);
+		throw new IllegalStateException();
+	}
+
+	public long size() {
+		return companyService.count();
+	}
+	
+	private class MyException extends ServiceException {
+		private static final long serialVersionUID = -6408089837070550022L;
+	}
+}
