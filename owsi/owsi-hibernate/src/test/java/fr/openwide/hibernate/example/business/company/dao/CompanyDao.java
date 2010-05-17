d7ee99922c2a029ee661885ec56e782e58e448b7
Julien Gonzalez <julien.gonzalez@openwide.fr>
2010-05-17 13:11:53 +0000

    suppression des packages de l'exemple dans "src/test/java"
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@78 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi/owsi-hibernate/src/test/java/fr/openwide/hibernate/example/business/company/dao/CompanyDao.java
+++
@@ -1,8 +0,0 @@
-package fr.openwide.hibernate.example.business.company.dao;
-
-import fr.openwide.core.hibernate.business.generic.dao.GenericEntityDao;
-import fr.openwide.hibernate.example.business.company.model.Company;
-
-public interface CompanyDao extends GenericEntityDao<Company> {
-
-}
