1787044f59d9a70d041819b049e00ca312bd1a4f
Julien Gonzalez <julien.gonzalez@openwide.fr>
2010-05-17 09:26:00 +0000

    Ajout des test unitaires pour owsi-core-hibernate et owsi-core-commons
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@71 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-hibernate/src/test/java/fr/openwide/core/test/hibernate/example/business/person/dao/PersonDaoImpl.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2009-2010 Open Wide
+ * Contact: contact@openwide.fr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.openwide.core.test.hibernate.example.business.person.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import fr.openwide.core.hibernate.business.generic.dao.GenericEntityDaoImpl;
+import fr.openwide.core.test.hibernate.example.business.person.model.Person;
+
+@Repository("personDao")
+public class PersonDaoImpl extends GenericEntityDaoImpl<Person> implements PersonDao {
+
+	@Autowired
+	public PersonDaoImpl(SessionFactory sessionFactory) {
+		super(sessionFactory);
+	}
+}
