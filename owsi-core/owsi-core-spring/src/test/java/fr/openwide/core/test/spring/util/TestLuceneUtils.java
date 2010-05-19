e4fd1a8e5fc7b62050743df7a4992fbe3a6fb4a8
Guillaume Smet <guillaume.smet@gmail.com>
2010-05-19 15:36:58 +0000

    ajout de tests sur LuceneUtils et ajustements mineurs
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@92 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-spring/src/test/java/fr/openwide/core/test/spring/util/TestLuceneUtils.java
@@ -0,0 +1,33 @@
+package fr.openwide.core.test.spring.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import fr.openwide.core.spring.util.lucene.search.LuceneUtils;
+
+public class TestLuceneUtils {
+
+	@Test
+	public void testGetAutocompleteQuery() {
+		assertEquals("", LuceneUtils.getAutocompleteQuery(null));
+		assertEquals("", LuceneUtils.getAutocompleteQuery(""));
+		assertEquals("alfresc*", LuceneUtils.getAutocompleteQuery("alfresc"));
+		assertEquals("alfresco-sha*", LuceneUtils.getAutocompleteQuery("alfresco-sha"));
+		assertEquals("alfresco-sha1*", LuceneUtils.getAutocompleteQuery("alfresco-sha1"));
+		assertEquals("alfresco sha1*", LuceneUtils.getAutocompleteQuery("alfresco sha1"));
+		assertEquals("t es t*", LuceneUtils.getAutocompleteQuery("t' -_es**t"));
+	}
+	
+	@Test
+	public void testGetSimilarityQuery() {
+		assertEquals("", LuceneUtils.getSimilarityQuery(null, 0.7f));
+		assertEquals("", LuceneUtils.getSimilarityQuery("", 0.7f));
+		assertEquals("alfresc~0.7", LuceneUtils.getSimilarityQuery("alfresc", 0.7f));
+		assertEquals("alfresco-sha~0.7", LuceneUtils.getSimilarityQuery("alfresco-sha", 0.7f));
+		assertEquals("alfresco-sha1~0.7", LuceneUtils.getSimilarityQuery("alfresco-sha1", 0.7f));
+		assertEquals("alfresco~0.7 sha1~0.7", LuceneUtils.getAutocompleteQuery("alfresco sha1"));
+		assertEquals("t~0.7 es~0.7 t~0.7", LuceneUtils.getSimilarityQuery("t' -_es**t", 0.7f));
+	}
+	
+}
