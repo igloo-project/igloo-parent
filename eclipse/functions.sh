4941c21afebceda9e14f37a3d7a69d1e7b582591
Julien Gonzalez <julien.gonzalez@openwide.fr>
2010-07-28 08:36:11 +0000

    Modifications : * on enlève le SVNKit dans l'éclipse JEE * on ajoute le library path dans eclipse.ini s'il n'est pas déjà configuré
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@179 d3474844-eb8e-4abc-b058-2b321fed648b

--- eclipse/functions.sh
+++ eclipse/functions.sh
@@ -5,8 +5,21 @@ function install_plugins() {
 	features="$2"
 	repositories="$3"
 
+	correct_library_path="-Djava.library.path=/usr/lib:/usr/lib/jni"
+
 	${path_eclipse}/eclipse -nosplash \
 		-application org.eclipse.equinox.p2.director \
 		-repository "${repositories}" \
 		-installIU "${features}"
+	
+	library_path=`grep -r "java.library.path" ${path_eclipse}/eclipse.ini`
+
+	echo ${library_path}
+	
+	if [ "${library_path}" != "${correct_library_path}" ]
+	then 
+		echo "${correct_library_path}" >> ${path_eclipse}/eclipse.ini
+	else
+		echo "Le java.library.path est bien configuré!"
+	fi
 }
