93bcbcc8a1f9fb1032a2340cdb92c83ff1c5ce9f
Guillaume Smet <guillaume.smet@gmail.com>
2010-09-29 17:27:22 +0000

    suppression d'un message inutile
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@200 d3474844-eb8e-4abc-b058-2b321fed648b

--- eclipse/functions.sh
+++ eclipse/functions.sh
@@ -14,10 +14,7 @@ function install_plugins() {
 	
 	library_path=`grep -r "java.library.path" ${path_eclipse}/eclipse.ini`
 	
-	if [ "${library_path}" != "${correct_library_path}" ]
-	then 
+	if [ "${library_path}" != "${correct_library_path}" ]; then 
 		echo "${correct_library_path}" >> ${path_eclipse}/eclipse.ini
-	else
-		echo "Le java.library.path est bien configuré!"
 	fi
 }
