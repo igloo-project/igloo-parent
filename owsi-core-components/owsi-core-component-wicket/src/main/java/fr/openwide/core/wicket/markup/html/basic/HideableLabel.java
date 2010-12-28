4478abbd2b2ca046480f618abb0edf2a42c9a88d
Guillaume Smet <guillaume.smet@gmail.com>
2010-12-28 13:05:30 +0000

    factorisation d'un certain nombre de composants Wicket
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@260 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/basic/HideableLabel.java
@@ -0,0 +1,23 @@
+package fr.openwide.core.wicket.markup.html.basic;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+import org.springframework.util.StringUtils;
+
+public class HideableLabel extends Label {
+
+	private static final long serialVersionUID = 1L;
+
+	public HideableLabel(String id, IModel<?> model) {
+		super(id, model);
+	}
+
+	public HideableLabel(String id, String label) {
+		super(id, label);
+	}
+
+	public boolean isVisible() {
+		return StringUtils.hasText(getDefaultModelObjectAsString());
+	}
+
+}
