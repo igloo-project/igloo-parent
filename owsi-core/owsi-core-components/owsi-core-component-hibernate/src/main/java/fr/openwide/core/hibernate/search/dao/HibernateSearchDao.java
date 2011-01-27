9edeb2f3a1e21f157ab0256248ec45618d6fc097
Guillaume Smet <guillaume.smet@gmail.com>
2011-01-27 16:36:21 +0000

    mise en place de méthodes de recherche dans HibernateSearchService amélioration de la construction des requêtes autocomplete close #18
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@275 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-hibernate/src/main/java/fr/openwide/core/hibernate/search/dao/HibernateSearchDao.java
+++ owsi-core/owsi-core-components/owsi-core-component-hibernate/src/main/java/fr/openwide/core/hibernate/search/dao/HibernateSearchDao.java
@@ -1,9 +1,17 @@
 package fr.openwide.core.hibernate.search.dao;
 
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+
 import fr.openwide.core.hibernate.exception.ServiceException;
 
 
 public interface HibernateSearchDao {
+
+	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern) throws ServiceException;
+
+	<T> List<T> search(List<Class<? extends T>> classes, String[] fields, String searchPattern, Analyzer analyzer) throws ServiceException;
 
 	void reindexAll() throws ServiceException;
 	
