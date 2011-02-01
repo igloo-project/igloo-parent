efb290b1d959986b8b7bfe725ee4888637d39727
Laurent Almeras <lalmeras@gmail.com>
2011-02-01 23:15:33 +0000

    préparation de l'arbre jpa
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@282 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/hibernate/business/generic/dao/GenericEntityDaoImpl.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright (C) 2009-2010 Open Wide
+ * Contact: contact@openwide.fr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.openwide.core.hibernate.business.generic.dao;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.dao.DataAccessException;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import fr.openwide.core.hibernate.business.generic.model.GenericEntity;
+import fr.openwide.core.hibernate.business.generic.util.GenericEntityUtils;
+
+/**
+ * <p>Implémentation de {@link GenericEntityDao}</p>
+ *
+ * @author Open Wide
+ *
+ * @param <T> type de l'entité
+ */
+public abstract class GenericEntityDaoImpl<K extends Serializable & Comparable<K>, E extends GenericEntity<K, E>>
+		extends HibernateDaoSupport implements GenericEntityDao<K, E> {
+	
+	protected static final String SQL_LIKE_WILDCARD = "%";
+	
+	/**
+	 * Classe de l'entité, déterminé à partir des paramètres generics.
+	 */
+	private Class<E> objectClass;
+	
+	/**
+	 * Constructeur.
+	 *
+	 * @param sessionFactory session factory Hibernate injectée par Spring
+	 */
+	@SuppressWarnings("unchecked")
+	public GenericEntityDaoImpl(SessionFactory sessionFactory) {
+		setSessionFactory(sessionFactory);
+		
+		this.objectClass = (Class<E>) GenericEntityUtils.getGenericEntityClassFromComponentDefinition(getClass());
+	}
+	
+	/**
+	 * Retourne la classe de l'entité.
+	 * 
+	 * @return classe de l'entité
+	 */
+	protected final Class<E> getObjectClass() {
+		return objectClass;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public E getEntity(Class<? extends E> clazz, K id) {
+		return (E) getSession().get(clazz, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public E getById(K id) {
+		return (E) getSession().get(getObjectClass(), id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public E getByField(String fieldName, Object fieldValue) {
+		return (E) getSession().createCriteria(getObjectClass()).add(Restrictions.eq(fieldName, fieldValue)).uniqueResult();
+	}
+	
+	@Override
+	public void update(E entity) {
+		getSession().update(entity);
+	}
+	
+	@Override
+	public void save(E entity) {
+		getSession().save(entity);
+	}
+	
+	@Override
+	public void delete(E entity) {
+		getSession().delete(entity);
+	}
+	
+	@Override
+	public E refresh(E entity) {
+		getSession().refresh(entity);
+		
+		return entity;
+	}
+	
+	@Override
+	public void flush() {
+		getSession().flush();
+	}
+	
+	@Override
+	public List<E> list() {
+		return list(getObjectClass(), null, null, null, null);
+	}
+	
+	@Override
+	public List<E> listByField(String fieldName, Object fieldValue) {
+		Criterion filter = Restrictions.eq(fieldName, fieldValue);
+		return list(getObjectClass(), filter, null, null, null);
+	}
+	
+	/**
+	 * Retourne une liste d'entités correspondant aux paramètres. N'a pas vocation à être appelée directement.
+	 * 
+	 * @param objectClass classe de l'entité
+	 * @param filter filtre Hibernate
+	 * @param order ordre de tri
+	 * @param limit limit
+	 * @param offset offset
+	 * @return liste d'entités
+	 */
+	@SuppressWarnings("unchecked")
+	public <T extends E> List<T> list(Class<T> objectClass, Criterion filter, Order order, Integer limit, Integer offset) {
+		List<T> entities = new ArrayList<T>();
+		try {
+			Criteria criteria = buildCriteria(objectClass, null, filter, order, limit, offset);
+			
+			entities = criteria.list();
+			
+			if(order == null) {
+				Collections.sort(entities);
+			}
+			
+			return entities;
+		} catch(DataAccessException e) {
+			return entities;
+		}
+	}
+	
+	@Override
+	public Long count() {
+		return count(getObjectClass(), null, null, null, null);
+	}
+	
+	@Override
+	public Long countByField(String fieldName, Object fieldValue) {
+		Criterion filter = Restrictions.eq(fieldName, fieldValue);
+		return count(getObjectClass(), filter, null, null, null);
+	}
+	
+	/**
+	 * Compte le nombre d'entités correspondant aux paramètres. N'a pas vocation à être appelée directement.
+	 * 
+	 * @param objectClass classe de l'entité
+	 * @param filter filtre Hibernate
+	 * @param order ordre de tri
+	 * @param limit limit
+	 * @param offset offset
+	 * @return nombre d'entités
+	 */
+	public Long count(Class<? extends E> objectClass, Criterion filter, Order order, Integer limit, Integer offset) {
+		Criteria criteria = buildCriteria(objectClass, Projections.rowCount(), filter, order, limit, offset);
+		
+		Long count = (Long) criteria.uniqueResult();
+		
+		return count;
+	}
+	
+	/**
+	 * Construit un criteria Hibernate à partir de l'ensemble des paramètres.
+	 * 
+	 * @param objectClass classe de l'entité
+	 * @param projection projection (au sens de l'API Criteria Hibernate)
+	 * @param filter filtre Hibernate
+	 * @param order ordre de tri
+	 * @param limit limit
+	 * @param offset offset
+	 * @return criteria
+	 */
+	protected Criteria buildCriteria(Class<? extends E> objectClass, Projection projection, Criterion filter, Order order, Integer limit, Integer offset) {
+		Criteria criteria = getSession().createCriteria(objectClass);
+		if(projection != null) {
+			criteria.setProjection(projection);
+		}
+		if(filter != null) {
+			criteria.add(filter);
+		}
+		if(limit != null) {
+			criteria.setMaxResults(limit);
+		}
+		if(offset != null) {
+			criteria.setFirstResult(offset);
+		}
+		if(order != null) {
+			criteria.addOrder(order);
+		}
+		return criteria;
+	}
+	
+}
---
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/hibernate/search/dao/HibernateSearchDaoImpl.java
@@ -0,0 +1,85 @@
+package fr.openwide.core.hibernate.search.dao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.queryParser.MultiFieldQueryParser;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser.Operator;
+import org.apache.lucene.util.Version;
+import org.hibernate.CacheMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.Search;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import fr.openwide.core.hibernate.exception.ServiceException;
+
+@Repository("hibernateSearchDao")
+public class HibernateSearchDaoImpl extends HibernateDaoSupport implements HibernateSearchDao {
+	
+	@Autowired
+	public HibernateSearchDaoImpl(SessionFactory sessionFactory) {
+		setSessionFactory(sessionFactory);
+	}
+	
+	@Override
+	public <T> List<T> search(Class<T> clazz, String[] fields, String searchPattern) throws ServiceException {
+		List<Class<? extends T>> classes = new ArrayList<Class<? extends T>>(1);
+		classes.add(clazz);
+		
+		return search(classes, fields, searchPattern, Search.getFullTextSession(getSession()).getSearchFactory().getAnalyzer(clazz));
+	}
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T> List<T> search(List<Class<? extends T>> classes, String[] fields, String searchPattern, Analyzer analyzer) throws ServiceException {
+		if (!StringUtils.hasText(searchPattern)) {
+			return Collections.emptyList();
+		}
+		
+		try {
+			FullTextSession fullTextSession = Search.getFullTextSession(getSession());
+			
+			MultiFieldQueryParser parser = getMultiFieldQueryParser(fullTextSession, fields, MultiFieldQueryParser.AND_OPERATOR, analyzer);
+			
+			org.apache.lucene.search.Query luceneQuery = parser.parse(searchPattern);
+			org.hibernate.Query hibernateQuery = fullTextSession.createFullTextQuery(luceneQuery, classes.toArray(new Class<?>[0]));
+			
+			return (List<T>) hibernateQuery.list();
+		} catch(ParseException e) {
+			throw new ServiceException(String.format("Error parsing request: %1$s", searchPattern), e);
+		} catch (Exception e) {
+			throw new ServiceException(String.format("Error executing search: %1$s for classes: %2$s", searchPattern, classes), e);
+		}
+	}
+
+	@Override
+	public void reindexAll() throws ServiceException {
+		try {
+			FullTextSession fullTextSession = Search.getFullTextSession(getSession());
+			
+			fullTextSession.createIndexer()
+					.batchSizeToLoadObjects(30)
+					.threadsForSubsequentFetching(8)
+					.threadsToLoadObjects(4)
+					.cacheMode(CacheMode.NORMAL)
+					.startAndWait();
+		} catch (Exception e) {
+			throw new ServiceException(e);
+		}
+	}
+	
+	private MultiFieldQueryParser getMultiFieldQueryParser(FullTextSession fullTextSession, String[] fields, Operator defaultOperator, Analyzer analyzer) {
+		MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_30, fields, analyzer);
+		parser.setDefaultOperator(defaultOperator);
+		
+		return parser;
+	}
+	
+}
