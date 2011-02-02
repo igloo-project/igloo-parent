6154833e138148324fb9d5cd1af02cb9e52f1aab
Guillaume Smet <guillaume.smet@gmail.com>
2011-02-02 19:01:59 +0000

    travail de factorisation sur les composants Wicket
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@307 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/GenericEntityModel.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2009-2011 Open Wide
+ * Contact: contact@openwide.fr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.openwide.core.wicket.more.model;
+
+import java.io.Serializable;
+
+import org.apache.wicket.injection.web.InjectorHolder;
+import org.apache.wicket.model.LoadableDetachableModel;
+
+import fr.openwide.core.hibernate.business.generic.model.GenericEntity;
+import fr.openwide.core.hibernate.business.generic.service.GenericEntityService;
+
+public class GenericEntityModel<K extends Serializable & Comparable<K>, E extends GenericEntity<K, E>> extends LoadableDetachableModel<E> {
+	
+	private static final long serialVersionUID = 1L;
+
+	private GenericEntityService<K, E> service;
+	
+	protected K id;
+	
+	public GenericEntityModel(E entity) {
+		super(entity);
+		InjectorHolder.getInjector().inject(this);
+		
+		id = entity.getId();
+	}
+
+	@Override
+	protected E load() {
+		return service.getById(id);
+	}
+	
+	protected K getId() {
+		return id;
+	}
+	
+	protected void setService(GenericEntityService<K, E> service) {
+		this.service = service;
+	}
+
+}
