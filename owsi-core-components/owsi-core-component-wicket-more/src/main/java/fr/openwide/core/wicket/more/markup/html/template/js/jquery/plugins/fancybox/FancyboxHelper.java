6e5ea4cffac1ffadc6a52487eb3555d9e57befa2
Guillaume Smet <guillaume.smet@gmail.com>
2011-02-14 18:30:31 +0000

    factorisation d'un certain nombre d'éléments communs
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@328 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/fancybox/FancyboxHelper.java
@@ -0,0 +1,16 @@
+package fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.fancybox;
+
+public final class FancyboxHelper {
+	
+	public static String getResize() {
+		return "$.fancybox.resize()";
+	}
+	
+	public static String getClose() {
+		return "$.fancybox.close()";
+	}
+	
+	private FancyboxHelper() {
+	}
+
+}
