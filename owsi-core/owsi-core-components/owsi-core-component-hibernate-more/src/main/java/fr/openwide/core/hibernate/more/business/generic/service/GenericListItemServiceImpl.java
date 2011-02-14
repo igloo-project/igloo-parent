02a49c74342938baaca997cdda04d7617aec5f88
Guillaume Smet <guillaume.smet@gmail.com>
2011-02-14 13:09:38 +0000

    ajout d'un élément GenericListItem permettant de gérer des listes de valeur
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@318 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/business/generic/service/GenericListItemServiceImpl.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2009-2011 Open Wide
+ * Contact: contact@openwide.fr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.openwide.core.hibernate.more.business.generic.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.openwide.core.hibernate.more.business.generic.dao.GenericListItemDao;
+import fr.openwide.core.hibernate.more.business.generic.model.GenericListItem;
+import fr.openwide.core.hibernate.more.business.generic.model.GenericListItemBinding;
+
+@Service("genericListItemService")
+public class GenericListItemServiceImpl implements GenericListItemService {
+	
+	@SuppressWarnings("rawtypes")
+	private static final GenericListItemBinding GENERIC_LIST_ITEM_BINDING =
+			new GenericListItemBinding();
+	
+	protected GenericListItemDao genericListItemDao;
+	
+	@Autowired
+	public GenericListItemServiceImpl(GenericListItemDao governisListItemDao) {
+		super();
+		this.genericListItemDao = governisListItemDao;
+	}
+	
+	@Override
+	public <E extends GenericListItem<?>> E getById(Class<E> clazz, Integer id) {
+		return genericListItemDao.getById(clazz, id);
+	}
+	
+	@Override
+	public <E extends GenericListItem<?>> void create(E entity) {
+		genericListItemDao.save(entity);
+	}
+
+	@Override
+	public <E extends GenericListItem<?>> void update(E entity) {
+		genericListItemDao.update(entity);
+	}
+
+	@Override
+	public <E extends GenericListItem<?>> void delete(E entity) {
+		genericListItemDao.delete(entity);
+	}
+
+	@Override
+	public <E extends GenericListItem<? super E>> List<E> list(Class<E> clazz) {
+		return genericListItemDao.list(clazz);
+	}
+	
+	@Override
+	public <E extends GenericListItem<? super E>> List<E> listEnabled(Class<E> clazz) {
+		
+		
+		return genericListItemDao.listByField(clazz, GENERIC_LIST_ITEM_BINDING.enabled().getPath(), true);
+	}
+
+	@Override
+	public <E extends GenericListItem<?>> long count(Class<E> clazz) {
+		return genericListItemDao.count(clazz);
+	}
+
+}
