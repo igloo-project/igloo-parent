bfc1ee6692f5e514c202e24341c8487d899288e9
Guillaume Smet <guillaume.smet@gmail.com>
2011-02-14 18:27:14 +0000

    on évite de faire un comparator générique
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@327 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/business/generic/util/GenericListItemComparator.java
+++ owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/business/generic/util/GenericListItemComparator.java
@@ -5,10 +5,16 @@ import java.util.Comparator;
 import fr.openwide.core.hibernate.business.generic.model.GenericEntity;
 import fr.openwide.core.hibernate.more.business.generic.model.GenericListItem;
 
-public class GenericListItemComparator<E extends GenericListItem<E>> implements Comparator<E> {
-
+public class GenericListItemComparator implements Comparator<GenericListItem<?>> {
+	
+	public static final Comparator<GenericListItem<?>> INSTANCE = new GenericListItemComparator();
+	
 	@Override
-	public int compare(E o1, E o2) {
+	public int compare(GenericListItem<?> o1, GenericListItem<?> o2) {
+		if (!o1.getClass().equals(o2.getClass())) {
+			throw new IllegalArgumentException("Compared objects must have the same type.");
+		}
+		
 		// on trie en priorité sur la position, puis sur le libellé et enfin par l'identifiant
 		int order = 0;
 		if (o1.getPosition() != null) {
@@ -18,7 +24,7 @@ public class GenericListItemComparator<E extends GenericListItem<E>> implements 
 			order = GenericEntity.DEFAULT_STRING_COLLATOR.compare(o1.getLabel(), o2.getLabel());
 		}
 		if (order == 0) {
-			order = o1.compareTo(o2);
+			order = o1.getId().compareTo(o2.getId());
 		}
 		return order;
 	}
