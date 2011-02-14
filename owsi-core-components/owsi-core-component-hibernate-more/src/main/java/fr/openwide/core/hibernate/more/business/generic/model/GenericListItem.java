867a3c05225f0cb9728ef7282f2611263865c9a7
Guillaume Smet <guillaume.smet@gmail.com>
2011-02-14 13:09:38 +0000

    ajout d'un élément GenericListItem permettant de gérer des listes de valeur
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@318 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/business/generic/model/GenericListItem.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2009-2011 Open Wide
+ * Contact: contact@openwide.fr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.openwide.core.hibernate.more.business.generic.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import org.bindgen.Bindable;
+
+import fr.openwide.core.hibernate.business.generic.model.GenericEntity;
+
+@MappedSuperclass
+@Bindable
+public abstract class GenericListItem<E extends GenericListItem<E>> extends GenericEntity<Integer, E> {
+	
+	private static final long serialVersionUID = -6270832991786371463L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String label;
+	
+	private String shortLabel;
+	
+	@Column(nullable = false)
+	private Integer position;
+
+	@Column(nullable = false)
+	private boolean enabled = true;
+	
+	protected GenericListItem() {
+	}
+	
+	public GenericListItem(String label, Integer position) {
+		super();
+		
+		this.label= label;
+		this.position = position;
+	}
+	
+	@Override
+	public Integer getId() {
+		return id;
+	}
+	
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setShortLabel(String shortLabel) {
+		this.shortLabel = shortLabel;
+	}
+
+	public String getShortLabel() {
+		return shortLabel;
+	}
+
+	public void setPosition(Integer order) {
+		this.position = order;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public int compareTo(E o) {
+		if (this == o) {
+			return 0;
+		}
+		
+		int result = 0;
+		if (this.getPosition() != null) {
+			if (o.getPosition() != null) {
+				result = this.getPosition().compareTo(o.getPosition());
+			} else {
+				result = 1;
+			}
+		} else if (o.getPosition() != null) {
+			result = -1;
+		} else {
+			result = 0;
+		}
+		
+		if (result == 0 && this.getLabel() != null && o.getLabel() != null) {
+			result = DEFAULT_STRING_COLLATOR.compare(this.getLabel(), o.getLabel());
+		}
+		
+		return result;
+	}
+
+	@Override
+	public String getDisplayName() {
+		return getLabel();
+	}
+
+	@Override
+	public String getNameForToString() {
+		return getLabel();
+	}
+}
