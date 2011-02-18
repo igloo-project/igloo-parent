5c4168fe75048d90f7a5fe4b06f2f00df57c69f9
Guillaume Smet <guillaume.smet@gmail.com>
2011-02-18 15:20:19 +0000

    factorisation de la gestion des fileStores
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@344 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/business/file/model/FileStore.java
@@ -0,0 +1,19 @@
+package fr.openwide.core.hibernate.more.business.file.model;
+
+import java.io.File;
+
+import fr.openwide.core.hibernate.exception.SecurityServiceException;
+import fr.openwide.core.hibernate.exception.ServiceException;
+
+public interface FileStore {
+	
+	String getKey();
+	
+	void check() throws IllegalStateException;
+
+	void addFile(File file, String fileKey, String extension)
+			throws ServiceException, SecurityServiceException;
+
+	File getFile(String fileKey, String extension);
+
+}
---
+++ owsi-core/owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/business/file/model/SimpleFileStoreImpl.java
@@ -0,0 +1,87 @@
+package fr.openwide.core.hibernate.more.business.file.model;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
+import de.schlichtherle.io.FileOutputStream;
+import fr.openwide.core.hibernate.exception.SecurityServiceException;
+import fr.openwide.core.hibernate.exception.ServiceException;
+
+
+public class SimpleFileStoreImpl implements FileStore {
+	
+	private String key;
+	
+	private String rootDirectoryPath;
+	
+	private boolean writable;
+	
+	public SimpleFileStoreImpl(String key, String rootDirectoryPath, boolean writable) {
+		this.key = key;
+		this.rootDirectoryPath = rootDirectoryPath;
+		this.writable = writable;
+	}
+	
+	@Override
+	public String getKey() {
+		return key;
+	}
+	
+	@Override
+	public void addFile(File file, String fileKey, String extension) throws ServiceException, SecurityServiceException {
+		de.schlichtherle.io.FileInputStream fileInputStream = null;
+		try {
+			// Attention le fichier peut être contenu dans un zip d'où cette
+			// manipulation spécifique.
+			fileInputStream = new de.schlichtherle.io.FileInputStream(file);
+			IOUtils.copy(fileInputStream,
+					new FileOutputStream(new File(getFilePath(fileKey, extension))));
+		} catch (Exception e) {
+			throw new ServiceException(e);
+		} finally {
+			if (fileInputStream != null) {
+				try {
+					fileInputStream.close();
+				} catch (IOException e) {
+					throw new ServiceException(e);
+				}
+			}
+		}
+	}
+	
+	@Override
+	public File getFile(String fileKey, String extension) {
+		return new File(getFilePath(fileKey, extension));
+	}
+	
+	@Override
+	public void check() throws IllegalStateException {
+		File directory = new File(rootDirectoryPath);
+		if (!directory.isDirectory()) {
+			try {
+				FileUtils.forceMkdir(directory);
+			} catch (Exception e) {
+				throw new IllegalStateException("The directory " + rootDirectoryPath + " does not exist and we are unable to create it.");
+			}
+		}
+		if (!directory.canRead()) {
+			throw new IllegalStateException("The directory " + rootDirectoryPath + " exists but is not readable.");
+		}
+		if (writable && !directory.canWrite()) {
+			throw new IllegalStateException("The directory " + rootDirectoryPath + " exists but should be writable and is not.");
+		}
+	}
+	
+	protected String getFilePath(String fileKey, String extension) {
+		return FilenameUtils.concat(rootDirectoryPath, fileKey + "." + extension);
+	}
+	
+	protected String getRootDirectoryPath() {
+		return rootDirectoryPath;
+	}
+
+}
