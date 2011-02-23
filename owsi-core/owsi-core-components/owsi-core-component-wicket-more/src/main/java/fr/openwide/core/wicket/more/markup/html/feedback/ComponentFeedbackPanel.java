e7a4a13eef5f81e4935ad5e2895ca71aa64f6944
Laurent Almeras <lalmeras@gmail.com>
2011-02-23 18:20:45 +0000

    factorisation du feedbackpanel
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@357 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/feedback/ComponentFeedbackPanel.java
@@ -0,0 +1,45 @@
+package fr.openwide.core.wicket.more.markup.html.feedback;
+
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.feedback.FeedbackMessage;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * Panneau de feedback qui contient l'ensemble des feedback relatifs à l'élément
+ * « container » passé au constructeur, ou à ses enfants.
+ * 
+ * Si aucun message ne doit être affiché, le panel est caché.
+ * 
+ * Seuls les niveaux INFO et ERROR sont gérés.
+ *
+ */
+public class ComponentFeedbackPanel extends Panel {
+	private static final long serialVersionUID = 6100876122634391019L;
+	
+	public ComponentFeedbackPanel(String id, MarkupContainer container) {
+		super(id);
+		
+		FeedbackPanel errorFeedbackPanel = new FeedbackPanel("error-feedback-panel", new LevelComponentFeedbackMessageFilter(container, FeedbackMessage.ERROR)) {
+			private static final long serialVersionUID = 7521787479719533147L;
+
+			public boolean isVisible() {
+				return anyMessage();
+			}
+		};
+		errorFeedbackPanel.setEnabled(true);
+		errorFeedbackPanel.setOutputMarkupId(true);
+		
+		FeedbackPanel successFeedbackPanel = new FeedbackPanel("success-feedback-panel", new LevelComponentFeedbackMessageFilter(container, FeedbackMessage.INFO)) {
+			private static final long serialVersionUID = -3133860085898074341L;
+
+			public boolean isVisible() {
+				return anyMessage();
+			}
+		};
+		successFeedbackPanel.setEnabled(true);
+		successFeedbackPanel.setOutputMarkupId(true);
+		add(errorFeedbackPanel, successFeedbackPanel);
+	}
+
+}
