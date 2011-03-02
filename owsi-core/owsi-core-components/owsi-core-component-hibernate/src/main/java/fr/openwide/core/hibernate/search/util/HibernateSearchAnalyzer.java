2554f0488374ebb26ae83466ce03fab19c307eee
Guillaume Smet <guillaume.smet@gmail.com>
2011-03-02 17:40:24 +0000

    ajout d'une classe contenant les clés pour nommer les analyzers Hibernate Search
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@369 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-hibernate/src/main/java/fr/openwide/core/hibernate/search/util/HibernateSearchAnalyzer.java
@@ -0,0 +1,12 @@
+package fr.openwide.core.hibernate.search.util;
+
+public class HibernateSearchAnalyzer {
+	
+	public static final String TEXT = "text";
+
+	public static final String KEYWORD = "keyword";
+
+	private HibernateSearchAnalyzer() {
+	}
+
+}
