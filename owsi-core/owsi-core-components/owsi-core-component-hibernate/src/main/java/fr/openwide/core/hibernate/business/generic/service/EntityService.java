3f55845f887fd04e5a54949f548bb5dbf56a8967
Guillaume Smet <guillaume.smet@gmail.com>
2011-03-09 17:50:36 +0000

    ajout d'un service EntityService et utilisation dans les modèles afin de simplifier tout cela
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@410 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-hibernate/src/main/java/fr/openwide/core/hibernate/business/generic/service/EntityService.java
@@ -0,0 +1,11 @@
+package fr.openwide.core.hibernate.business.generic.service;
+
+import java.io.Serializable;
+
+import fr.openwide.core.hibernate.business.generic.model.GenericEntity;
+
+public interface EntityService {
+	
+	<K extends Serializable & Comparable<K>, E extends GenericEntity<K, E>> E getEntity(Class<E> clazz, K id);
+
+}
