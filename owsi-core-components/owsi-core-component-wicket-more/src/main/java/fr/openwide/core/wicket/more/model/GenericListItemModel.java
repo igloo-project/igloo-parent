053728df8fdbb67d9df4acf37badf8a3c4378750
Guillaume Smet <guillaume.smet@gmail.com>
2011-03-09 17:50:36 +0000

    ajout d'un service EntityService et utilisation dans les modèles afin de simplifier tout cela
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@410 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/GenericListItemModel.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/GenericListItemModel.java
@@ -16,55 +16,14 @@
  */
 package fr.openwide.core.wicket.more.model;
 
-import org.apache.wicket.injection.web.InjectorHolder;
-import org.apache.wicket.model.LoadableDetachableModel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-
 import fr.openwide.core.hibernate.more.business.generic.model.GenericListItem;
-import fr.openwide.core.hibernate.more.business.generic.service.GenericListItemService;
 
-public class GenericListItemModel<E extends GenericListItem<E>> extends LoadableDetachableModel<E> {
+public class GenericListItemModel<E extends GenericListItem<E>> extends GenericEntityModel<Integer, E> {
 	
 	private static final long serialVersionUID = 2483082448801965501L;
-
-	private Class<E> clazz;
 	
-	private Integer id;
-	
-	@SpringBean
-	private GenericListItemService genericListItemService;
-	
-	@SuppressWarnings("unchecked")
 	public GenericListItemModel(E listItem) {
 		super(listItem);
-		InjectorHolder.getInjector().inject(this);
-		
-		if (listItem != null) {
-			clazz = (Class<E>) listItem.getClass();
-			id = listItem.getId();
-		}
-	}
-
-	@Override
-	protected E load() {
-		E listItem = null;
-		if (clazz != null && id != null) {
-			listItem = genericListItemService.getById(clazz, id);
-		}
-		return listItem;
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public void setObject(E listItem) {
-		if (listItem != null) {
-			clazz = (Class<E>) listItem.getClass();
-			id = listItem.getId();
-		} else {
-			id = null;
-			clazz = null;
-		}
-		super.setObject(listItem);
 	}
 
 }
