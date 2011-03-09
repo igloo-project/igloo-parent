942de034022e9beebb0b9a47891fd835b41ba901
Guillaume Smet <guillaume.smet@gmail.com>
2011-03-09 01:06:23 +0000

    il faut également implémenter StringBridge pour que les bridges fonctionnent quand on construit les requêtes de recherche
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk@401 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/search/bridge/GenericListItemIdFieldBridge.java
+++ owsi-core/owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/search/bridge/GenericListItemIdFieldBridge.java
@@ -3,25 +3,29 @@ package fr.openwide.core.hibernate.more.search.bridge;
 import org.apache.lucene.document.Document;
 import org.hibernate.search.bridge.FieldBridge;
 import org.hibernate.search.bridge.LuceneOptions;
+import org.hibernate.search.bridge.StringBridge;
 
 import fr.openwide.core.hibernate.more.business.generic.model.GenericListItem;
 
-public class GenericListItemIdFieldBridge implements FieldBridge {
-	
-	private static final String ID_SUFFIX = "_id";
+public class GenericListItemIdFieldBridge implements FieldBridge, StringBridge {
 	
 	@Override
 	public void set(String name, Object value, Document document, LuceneOptions luceneOptions) {
 		if (!(value instanceof GenericListItem)) {
 			throw new IllegalArgumentException("This FieldBridge only supports GenericListItem properties.");
 		}
-		GenericListItem<?> listitem = (GenericListItem<?>) value;
+		GenericListItem<?> listItem = (GenericListItem<?>) value;
 		
-		luceneOptions.addFieldToDocument(getFieldName(name), listitem.getId().toString(), document);
+		luceneOptions.addFieldToDocument(name, listItem.getId().toString(), document);
 	}
 	
-	public static String getFieldName(String name) {
-		return name + ID_SUFFIX;
+	@Override
+	public String objectToString(Object object) {
+		if (!(object instanceof GenericListItem)) {
+			throw new IllegalArgumentException("This FieldBridge only supports GenericListItem properties.");
+		}
+		GenericListItem<?> listItem = (GenericListItem<?>) object;
+		return listItem.getId().toString();
 	}
-
+	
 }
