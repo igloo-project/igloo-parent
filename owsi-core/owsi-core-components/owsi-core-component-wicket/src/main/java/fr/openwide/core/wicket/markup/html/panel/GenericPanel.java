1db4011b26a883737a87812c0f9657476f6c9ac3
Guillaume Smet <guillaume.smet@gmail.com>
2011-03-10 17:22:08 +0000

    suppression du final sur setModel ajout d'un constructeur avec l'id seulement
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@422 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/panel/GenericPanel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/panel/GenericPanel.java
@@ -7,11 +7,15 @@ public class GenericPanel<T> extends Panel {
 
 	private static final long serialVersionUID = 7229728038574137817L;
 
+	public GenericPanel(String id) {
+		super(id);
+	}
+	
 	public GenericPanel(String id, IModel<? extends T> model) {
 		super(id, model);
 	}
 	
-	public final void setModel(IModel<? extends T> model) {
+	public void setModel(IModel<? extends T> model) {
 		super.setDefaultModel(model);
 	}
 	
