b039757c5403a5f70ccd85eaa1b0cb4cb2efa702
Guillaume Smet <guillaume.smet@gmail.com>
2011-03-16 14:10:55 +0000

    ajout de l'ensemble des options Tipsy
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@435 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/tipsy/Tipsy.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/tipsy/Tipsy.java
@@ -7,13 +7,29 @@ import org.odlabs.wiquery.core.options.Options;
 
 public class Tipsy implements ChainableStatement, Serializable {
 	private static final long serialVersionUID = -2090948335676368020L;
-
+	
+	private Integer delayIn;
+	
+	private Integer delayOut;
+	
 	private String title;
 	
+	private String fallback;
+	
+	private TipsyOptionGravity gravity;
+
+	private Boolean html;
+
 	private Boolean fade;
 	
 	private Boolean live;
+	
+	private Integer offset;
 
+	private Float opacity;
+	
+	private TipsyOptionTrigger trigger;
+	
 	@Override
 	public String chainLabel() {
 		return "tipsy";
@@ -22,18 +38,59 @@ public class Tipsy implements ChainableStatement, Serializable {
 	@Override
 	public CharSequence[] statementArgs() {
 		Options options = new Options();
-		if (fade != null) {
-			options.put("fade", fade);
+		if (delayIn != null) {
+			options.put("delayIn", delayIn);
+		}
+		if (delayOut != null) {
+			options.put("delayOut", delayOut);
 		}
 		if (title != null) {
 			options.put("title", title);
 		}
+		if (fallback != null) {
+			options.put("fallback", fallback);
+		}
+		if (gravity != null) {
+			options.put("gravity", gravity.getValue());
+		}
+		if (html != null) {
+			options.put("html", html);
+		}
+		if (fade != null) {
+			options.put("fade", fade);
+		}
 		if (live != null) {
 			options.put("live", live);
 		}
+		if (offset != null) {
+			options.put("offset", offset);
+		}
+		if (opacity != null) {
+			options.put("opacity", opacity);
+		}
+		if (trigger != null) {
+			options.put("trigger", trigger.getValue());
+		}
+		
 		CharSequence[] args = new CharSequence[1];
 		args[0] = options.getJavaScriptOptions();
 		return args;
+	}
+
+	public Integer getDelayIn() {
+		return delayIn;
+	}
+
+	public void setDelayIn(Integer delayIn) {
+		this.delayIn = delayIn;
+	}
+
+	public Integer getDelayOut() {
+		return delayOut;
+	}
+
+	public void setDelayOut(Integer delayOut) {
+		this.delayOut = delayOut;
 	}
 
 	public String getTitle() {
@@ -42,6 +99,30 @@ public class Tipsy implements ChainableStatement, Serializable {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getFallback() {
+		return fallback;
+	}
+
+	public void setFallback(String fallback) {
+		this.fallback = fallback;
+	}
+
+	public TipsyOptionGravity getGravity() {
+		return gravity;
+	}
+
+	public void setGravity(TipsyOptionGravity gravity) {
+		this.gravity = gravity;
+	}
+
+	public Boolean getHtml() {
+		return html;
+	}
+
+	public void setHtml(Boolean html) {
+		this.html = html;
 	}
 
 	public Boolean getFade() {
@@ -60,4 +141,28 @@ public class Tipsy implements ChainableStatement, Serializable {
 		this.live = live;
 	}
 
+	public Integer getOffset() {
+		return offset;
+	}
+
+	public void setOffset(Integer offset) {
+		this.offset = offset;
+	}
+
+	public Float getOpacity() {
+		return opacity;
+	}
+
+	public void setOpacity(Float opacity) {
+		this.opacity = opacity;
+	}
+
+	public TipsyOptionTrigger getTrigger() {
+		return trigger;
+	}
+
+	public void setTrigger(TipsyOptionTrigger trigger) {
+		this.trigger = trigger;
+	}
+	
 }
