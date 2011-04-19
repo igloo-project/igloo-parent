d5db487e9ad8f2304b97df81f95b26071862c73b
Laurent Almeras <lalmeras@gmail.com>
2011-04-19 19:27:34 +0000

    préparation d'un jpa-more
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@493 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/hibernate/more/business/generic/model/GenericListItemBindingInterface.java
@@ -0,0 +1,18 @@
+package fr.openwide.core.hibernate.more.business.generic.model;
+
+import org.bindgen.Bindable;
+
+@Bindable
+public interface GenericListItemBindingInterface {
+	
+	public Integer getId();
+	
+	public String getLabel();
+	
+	public String getShortLabel();
+	
+	public Integer getPosition();
+	
+	public boolean isEnabled();
+	
+}
