6dcc4dd0a08d90c9c5e300fa32a9df7ef30bba86
Guillaume Smet <guillaume.smet@gmail.com>
2011-04-20 10:13:22 +0000

    suppression des modules Hibernate
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@511 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-hibernate-more/src/main/java/fr/openwide/core/hibernate/more/business/parameter/dao/ParameterDao.java
+++
@@ -1,8 +0,0 @@
-package fr.openwide.core.hibernate.more.business.parameter.dao;
-
-import fr.openwide.core.hibernate.business.generic.dao.GenericEntityDao;
-import fr.openwide.core.hibernate.more.business.parameter.model.Parameter;
-
-public interface ParameterDao extends GenericEntityDao<Integer, Parameter> {
-
-}
