30e1105c736e162041b2b627b97d201d2f787f76
Guillaume Smet <guillaume.smet@gmail.com>
2011-05-27 16:55:10 +0000

    nouvelle gestion des breadcrumbs
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@571 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/SimpleBreadCrumbElementPanel.java
@@ -0,0 +1,24 @@
+package fr.openwide.core.wicket.more.markup.html.template.component;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+
+import fr.openwide.core.wicket.markup.html.panel.GenericPanel;
+import fr.openwide.core.wicket.more.markup.html.template.model.BreadCrumbElement;
+
+public class SimpleBreadCrumbElementPanel extends GenericPanel<BreadCrumbElement> {
+
+	private static final long serialVersionUID = 5385792712763242343L;
+
+	public SimpleBreadCrumbElementPanel(String id, IModel<BreadCrumbElement> model, boolean isSeparatorVisible) {
+		super(id, model);
+		
+		WebMarkupContainer separator = new WebMarkupContainer("separator");
+		separator.setVisible(isSeparatorVisible);
+		add(separator);
+		
+		add(new Label("breadCrumbElementLabel", getString(getModelObject().getLabelKey(), null, getModelObject().getLabelKey())));
+	}
+
+}
