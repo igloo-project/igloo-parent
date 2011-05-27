a6ffd6ad94ed8431911502d224979ca33045db30
Guillaume Smet <guillaume.smet@gmail.com>
2011-05-27 17:10:14 +0000

    utilisation de ResourceModel à la place de getString()
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@572 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/SimpleBreadCrumbElementPanel.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/SimpleBreadCrumbElementPanel.java
@@ -3,6 +3,7 @@ package fr.openwide.core.wicket.more.markup.html.template.component;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
 
 import fr.openwide.core.wicket.markup.html.panel.GenericPanel;
 import fr.openwide.core.wicket.more.markup.html.template.model.BreadCrumbElement;
@@ -18,7 +19,7 @@ public class SimpleBreadCrumbElementPanel extends GenericPanel<BreadCrumbElement
 		separator.setVisible(isSeparatorVisible);
 		add(separator);
 		
-		add(new Label("breadCrumbElementLabel", getString(getModelObject().getLabelKey(), null, getModelObject().getLabelKey())));
+		add(new Label("breadCrumbElementLabel", new ResourceModel(getModelObject().getLabelKey(), getModelObject().getLabelKey())));
 	}
 
 }
