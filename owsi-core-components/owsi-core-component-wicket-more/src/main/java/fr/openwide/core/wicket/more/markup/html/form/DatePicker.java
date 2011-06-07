2cf7da4bd4d270506c8e2b2635814800647932a2
Guillaume Smet <guillaume.smet@gmail.com>
2011-06-07 15:38:59 +0000

    ajout de la capacité à activer/désactiver l'autocomplete automatique du navigateur sur les DatePicker par défaut, on le désactive car ne sert à rien et est perturbant
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@581 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/DatePicker.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/DatePicker.java
@@ -2,6 +2,7 @@ package fr.openwide.core.wicket.more.markup.html.form;
 
 import java.util.Date;
 
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.util.convert.IConverter;
 
@@ -11,14 +12,25 @@ import fr.openwide.core.wicket.more.util.convert.converters.PatternDateConverter
 public class DatePicker extends org.odlabs.wiquery.ui.datepicker.DatePicker<Date> {
 
 	private static final long serialVersionUID = 8051575483617364457L;
+	
+	private static final String AUTOCOMPLETE_ATTRIBUTE = "autocomplete";
+	
+	private static final String AUTOCOMPLETE_ATTRIBUTE_OFF_VALUE = "off";
 
 	private IConverter converter;
 
 	private DatePattern datePattern;
+	
+	private boolean isAutocompleteActive;
 
 	public DatePicker(String id, IModel<Date> model, DatePattern datePattern) {
+		this(id, model, datePattern, false);
+	}
+	
+	public DatePicker(String id, IModel<Date> model, DatePattern datePattern, boolean isAutocompleteActive) {
 		super(id, model, Date.class);
 		this.datePattern = datePattern;
+		this.isAutocompleteActive = isAutocompleteActive;
 	}
 	
 	public void onInitialize() {
@@ -42,4 +54,13 @@ public class DatePicker extends org.odlabs.wiquery.ui.datepicker.DatePicker<Date
 		converter = null;
 	}
 
+	@Override
+	protected void onComponentTag(ComponentTag tag) {
+		super.onComponentTag(tag);
+		
+		if (!isAutocompleteActive) {
+			tag.put(AUTOCOMPLETE_ATTRIBUTE, AUTOCOMPLETE_ATTRIBUTE_OFF_VALUE);
+		}
+	}
+	
 }
