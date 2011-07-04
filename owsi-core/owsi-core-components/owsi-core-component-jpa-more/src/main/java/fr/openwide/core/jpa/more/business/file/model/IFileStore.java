45a29812cd6f9f5ece7212c83b35568d4533a37c
Laurent Almeras <lalmeras@gmail.com>
2011-07-04 15:15:22 +0000

    élargissement de l'api filestore vers inputstream
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@620 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/file/model/IFileStore.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/file/model/IFileStore.java
@@ -1,6 +1,7 @@
 package fr.openwide.core.jpa.more.business.file.model;
 
 import java.io.File;
+import java.io.InputStream;
 
 import fr.openwide.core.jpa.exception.SecurityServiceException;
 import fr.openwide.core.jpa.exception.ServiceException;
@@ -18,5 +19,7 @@ public interface IFileStore {
 	void removeFile(String fileKey, String extension);
 
 	void addFile(byte[] content, String fileKey, String extension) throws ServiceException, SecurityServiceException;
+
+	void addFile(InputStream inputStream, String fileKey, String extension) throws ServiceException, SecurityServiceException;
 
 }
