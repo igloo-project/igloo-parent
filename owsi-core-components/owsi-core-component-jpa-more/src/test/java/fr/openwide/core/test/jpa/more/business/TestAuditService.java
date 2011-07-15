2c39752b4cc0aadaac061cba9c4488512b6f593c
Julien Gonzalez <julien.gonzalez@openwide.fr>
2011-07-15 09:23:39 +0000

    Les tests sur le service d'Audit est complété
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@635 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa-more/src/test/java/fr/openwide/core/test/jpa/more/business/TestAuditService.java
+++ owsi-core-components/owsi-core-component-jpa-more/src/test/java/fr/openwide/core/test/jpa/more/business/TestAuditService.java
@@ -26,48 +26,190 @@ public class TestAuditService extends AbstractJpaMoreTestCase {
 		testEntityService.create(object);
 		TestEntity secondaryObject = new TestEntity("secondaryObject");
 		testEntityService.create(secondaryObject);
-		
-		MockAuditFeature feature = auditService.getAuditFeatureByEnum(MockAuditFeatureEnum.TEST_FEATURE_1);
-		
-		MockAuditAction action = auditService.getAuditActionByEnum(MockAuditActionEnum.TEST_ACTION_1);
-		
-		MockAudit audit1 = new MockAudit(
+
+		MockAuditFeature feature = auditService
+				.getAuditFeatureByEnum(MockAuditFeatureEnum.TEST_FEATURE_1);
+
+		MockAuditAction action = auditService
+				.getAuditActionByEnum(MockAuditActionEnum.TEST_ACTION_1);
+
+		MockAudit audit = new MockAudit("service", "method", context, subject,
+				feature, action, "message", object, secondaryObject);
+
+		auditService.create(audit);
+
+		List<MockAudit> audits = auditService.list();
+
+		Assert.assertNotNull(audit.getId());
+
+		Assert.assertEquals(audit.getService(), "service");
+		Assert.assertEquals(audit.getMethod(), "method");
+		Assert.assertEquals(1, audits.size());
+		Assert.assertTrue(audits.contains(audit));
+
+		Assert.assertEquals(feature, audit.getFeature());
+		Assert.assertEquals(action, audit.getAction());
+
+		Assert.assertEquals(context.getId(), audit.getContextId());
+		Assert.assertEquals(context.getLabel(), audit.getContextDisplayName());
+		Assert.assertEquals(TestEntity.class.getName(), audit.getContextClass());
+
+		Assert.assertEquals(subject.getId(), audit.getSubjectId());
+		Assert.assertEquals(subject.getLabel(), audit.getSubjectDisplayName());
+		Assert.assertEquals(TestEntity.class.getName(), audit.getSubjectClass());
+
+		Assert.assertEquals(object.getId(), audit.getObjectId());
+		Assert.assertEquals(object.getLabel(), audit.getObjectDisplayName());
+		Assert.assertEquals(TestEntity.class.getName(), audit.getObjectClass());
+
+		Assert.assertEquals(secondaryObject.getId(),
+				audit.getSecondaryObjectId());
+		Assert.assertEquals(secondaryObject.getLabel(),
+				audit.getSecondaryObjectDisplayName());
+		Assert.assertEquals(TestEntity.class.getName(),
+				audit.getSecondaryObjectClass());
+	}
+
+	@Test
+	public void testGetters() throws ServiceException, SecurityServiceException {
+		TestEntity subject = new TestEntity("Mr. Franck Black");
+		testEntityService.create(subject);
+		TestEntity context = new TestEntity("context");
+		testEntityService.create(context);
+		TestEntity object = new TestEntity("object");
+		testEntityService.create(object);
+		TestEntity secondaryObject = new TestEntity("secondaryObject");
+		testEntityService.create(secondaryObject);
+
+		MockAudit audit = new MockAudit(
 				"service",
 				"method",
 				context,
 				subject,
-				feature,
-				action,
+				auditService
+						.getAuditFeatureByEnum(MockAuditFeatureEnum.TEST_FEATURE_1),
+				auditService
+						.getAuditActionByEnum(MockAuditActionEnum.TEST_ACTION_1),
+				"message", object, secondaryObject);
+
+		Assert.assertEquals(auditService.getContextEntity(audit), context);
+		Assert.assertEquals(auditService.getSubjectEntity(audit), subject);
+		Assert.assertEquals(auditService.getObjectEntity(audit), object);
+		Assert.assertEquals(auditService.getSecondaryObjectEntity(audit),
+				secondaryObject);
+		Assert.assertEquals(auditService.getContextDisplayName(audit),
+				context.getDisplayName());
+		Assert.assertEquals(auditService.getSubjectDisplayName(audit),
+				subject.getDisplayName());
+		Assert.assertEquals(auditService.getObjectDisplayName(audit),
+				object.getDisplayName());
+		Assert.assertEquals(auditService.getSecondaryObjectDisplayName(audit),
+				secondaryObject.getDisplayName());
+
+		testEntityService.delete(context);
+		testEntityService.delete(subject);
+		testEntityService.delete(object);
+		testEntityService.delete(secondaryObject);
+
+		Assert.assertEquals(auditService.getContextDisplayName(audit),
+				context.getDisplayName());
+		Assert.assertEquals(auditService.getSubjectDisplayName(audit),
+				subject.getDisplayName());
+		Assert.assertEquals(auditService.getObjectDisplayName(audit),
+				object.getDisplayName());
+		Assert.assertEquals(auditService.getSecondaryObjectDisplayName(audit),
+				secondaryObject.getDisplayName());
+	}
+
+	@Test
+	public void testFilters() throws ServiceException, SecurityServiceException {
+		TestEntity subject1 = new TestEntity("subject1");
+		testEntityService.create(subject1);
+		TestEntity subject2 = new TestEntity("subject2");
+		testEntityService.create(subject2);
+		TestEntity subject3 = new TestEntity("subject3");
+		testEntityService.create(subject3);
+		
+		TestEntity context1 = new TestEntity("context1");
+		testEntityService.create(context1);
+		TestEntity context2 = new TestEntity("context2");
+		testEntityService.create(context2);
+		
+		TestEntity object1 = new TestEntity("object1");
+		testEntityService.create(object1);
+		TestEntity object2 = new TestEntity("object2");
+		testEntityService.create(object2);
+		
+		TestEntity secondaryObject = new TestEntity("secondaryObject");
+		testEntityService.create(secondaryObject);
+		
+		MockAudit audit1 = new MockAudit(
+				"service",
+				"method",
+				context2,
+				subject1,
+				auditService.getAuditFeatureByEnum(MockAuditFeatureEnum.TEST_FEATURE_1),
+				auditService.getAuditActionByEnum(MockAuditActionEnum.TEST_ACTION_1),
 				"message",
-				object,
+				object1,
+				null
+		);
+		MockAudit audit2 = new MockAudit(
+				"service",
+				"method",
+				context2,
+				subject1,
+				auditService.getAuditFeatureByEnum(MockAuditFeatureEnum.TEST_FEATURE_1),
+				auditService.getAuditActionByEnum(MockAuditActionEnum.TEST_ACTION_1),
+				"message",
+				object1,
+				null
+		);
+		MockAudit audit3 = new MockAudit(
+				"service",
+				"method",
+				context1,
+				subject3,
+				auditService.getAuditFeatureByEnum(MockAuditFeatureEnum.TEST_FEATURE_2),
+				auditService.getAuditActionByEnum(MockAuditActionEnum.TEST_ACTION_2),
+				"message",
+				object1,
 				secondaryObject
+		);
+		MockAudit audit4 = new MockAudit(
+				"service",
+				"method",
+				context1,
+				subject2,
+				auditService.getAuditFeatureByEnum(MockAuditFeatureEnum.TEST_FEATURE_3),
+				auditService.getAuditActionByEnum(MockAuditActionEnum.TEST_ACTION_3),
+				"message",
+				object2,
+				null
 		);
 		
 		auditService.create(audit1);
+		auditService.create(audit2);
+		auditService.create(audit3);
+		auditService.create(audit4);
 		
-		List<MockAudit> audits = auditService.list();
+		List<MockAudit> audits;
 		
-		Assert.assertNotNull(audit1.getId());
-		Assert.assertEquals(1, audits.size());
+		audits = auditService.listBySubject(subject1);
+		Assert.assertEquals(2, audits.size());
 		Assert.assertTrue(audits.contains(audit1));
+		Assert.assertTrue(audits.contains(audit2));
 		
-		Assert.assertEquals(feature, audit1.getFeature());
-		Assert.assertEquals(action, audit1.getAction());
+		audits = auditService.listByContextOrObject(context1);
+		Assert.assertEquals(2, audits.size());
+		Assert.assertTrue(audits.contains(audit3));
+		Assert.assertTrue(audits.contains(audit4));
 		
-		Assert.assertEquals(context.getId(), audit1.getContextId());
-		Assert.assertEquals(context.getLabel(), audit1.getContextDisplayName());
-		Assert.assertEquals(TestEntity.class.getName(), audit1.getContextClass());
+		audits = auditService.listByContextOrObject(object2);
+		Assert.assertEquals(1, audits.size());
+		Assert.assertTrue(audits.contains(audit4));
 		
-		Assert.assertEquals(subject.getId(), audit1.getSubjectId());
-		Assert.assertEquals(subject.getLabel(), audit1.getSubjectDisplayName());
-		Assert.assertEquals(TestEntity.class.getName(), audit1.getSubjectClass());
-		
-		Assert.assertEquals(object.getId(), audit1.getObjectId());
-		Assert.assertEquals(object.getLabel(), audit1.getObjectDisplayName());
-		Assert.assertEquals(TestEntity.class.getName(), audit1.getObjectClass());
-		
-		Assert.assertEquals(secondaryObject.getId(), audit1.getSecondaryObjectId());
-		Assert.assertEquals(secondaryObject.getLabel(), audit1.getSecondaryObjectDisplayName());
-		Assert.assertEquals(TestEntity.class.getName(), audit1.getSecondaryObjectClass());
+		audits = auditService.listByContextOrObject(secondaryObject);
+		Assert.assertEquals(0, audits.size());
 	}
 }
