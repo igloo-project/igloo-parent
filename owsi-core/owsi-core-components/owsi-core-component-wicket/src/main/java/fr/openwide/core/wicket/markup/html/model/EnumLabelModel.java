93b36fd000e23b4380e3efb399845eaded84e84c
Laurent Almeras <lalmeras@gmail.com>
2011-08-25 16:35:17 +0000

    constructeurs un peu plus utiles
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@666 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/model/EnumLabelModel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/model/EnumLabelModel.java
@@ -2,6 +2,7 @@ package fr.openwide.core.wicket.markup.html.model;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.model.ComponentModel;
+import org.apache.wicket.model.IModel;
 
 /**
  * Give translation for a given enum. Reosurce key is built as in EnumLabel.
@@ -12,15 +13,28 @@ public class EnumLabelModel<E extends Enum<?>> extends ComponentModel<String> {
 
 	private static final long serialVersionUID = -7290192562513833670L;
 
-	private E enumValue;
+	private IModel<E> enumValueModel;
 
-	public EnumLabelModel(E enumValue) {
+	private IModel<String> nullKeyModel;
+
+	public EnumLabelModel(IModel<E> enumValueModel) {
+		this(enumValueModel, null);
+	}
+
+	public EnumLabelModel(IModel<E> enumValueModel, IModel<String> nullKeyModel) {
 		super();
-		this.enumValue = enumValue;
+		this.enumValueModel = enumValueModel;
+		this.nullKeyModel = nullKeyModel;
 	}
 	
 	protected String getObject(Component component) {
-		return component.getString(enumValue.getClass().getSimpleName() + "." + enumValue.name());
+		if (enumValueModel != null && enumValueModel.getObject() != null) {
+			return component.getString(enumValueModel.getClass().getSimpleName() + "." + enumValueModel.getObject().name());
+		} else if (nullKeyModel != null && nullKeyModel.getObject() != null) {
+			return component.getString(nullKeyModel.getObject());
+		} else {
+			return "";
+		}
 	}
 
 }
