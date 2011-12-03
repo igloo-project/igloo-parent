c3e08ef07d7f744ae419979969c79531181e83cc
Guillaume Smet <guillaume.smet@gmail.com>
2011-12-03 10:10:36 +0000

    si jamais la page n'est pas instanciable pour des raisons d'autorisation, on évite de faire un lien dessus et on met juste le libellé
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.5-MAINTENANCE@784 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/BreadCrumbListView.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/BreadCrumbListView.java
@@ -2,6 +2,7 @@ package fr.openwide.core.wicket.more.markup.html.template.component;
 
 import java.util.List;
 
+import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
@@ -18,7 +19,9 @@ public class BreadCrumbListView extends ListView<BreadCrumbElement> {
 
 	@Override
 	protected void populateItem(ListItem<BreadCrumbElement> item) {
-		if (item.getModelObject().getPageClass() != null) {
+		if (item.getModelObject().getPageClass() != null && 
+				Session.get().getAuthorizationStrategy().isInstantiationAuthorized(item.getModelObject().getPageClass())
+				) {
 			item.add(new LinkBreadCrumbElementPanel("breadCrumbElement", item.getModel(), item.getIndex() > 0));
 		} else {
 			item.add(new SimpleBreadCrumbElementPanel("breadCrumbElement", item.getModel(), item.getIndex() > 0));
