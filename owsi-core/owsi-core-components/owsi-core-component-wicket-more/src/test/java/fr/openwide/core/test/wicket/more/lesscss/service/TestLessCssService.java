f5465364c871f22c7413d47af37715f2425ba1af
Guillaume Smet <guillaume.smet@gmail.com>
2011-12-21 23:36:52 +0000

    nouvelle implémentation des ressources LessCSS en isolant le service des dépendances Wicket et en rendant le cache désactivable ajout d'un test unitaire pour valider le bon fonctionnement de LessCSS
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@820 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/test/java/fr/openwide/core/test/wicket/more/lesscss/service/TestLessCssService.java
@@ -0,0 +1,45 @@
+package fr.openwide.core.test.wicket.more.lesscss.service;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+
+import fr.openwide.core.test.wicket.more.AbstractWicketMoreTestCase;
+import fr.openwide.core.test.wicket.more.lesscss.service.resource.TestLessCssServiceResourceScope;
+import fr.openwide.core.wicket.more.lesscss.model.CssStylesheetInformation;
+import fr.openwide.core.wicket.more.lesscss.service.ILessCssService;
+
+public class TestLessCssService extends AbstractWicketMoreTestCase {
+	
+	@Autowired
+	private ILessCssService lessCssService;
+	
+	@Test
+	public void testGetCompiledStylesheet() throws Exception {
+		InputStream is = null;
+		try {
+			ClassPathResource stylesheetResource = new ClassPathResource("style.less", TestLessCssServiceResourceScope.class);
+			is = stylesheetResource.getInputStream();
+			
+			String rawSource = IOUtils.toString(is);
+			
+			CssStylesheetInformation compiledStylesheet = lessCssService.getCompiledStylesheet(
+					TestLessCssServiceResourceScope.class,
+					"style.less",
+					new CssStylesheetInformation(rawSource, stylesheetResource.lastModified()),
+					true);
+			
+			Assert.assertEquals(".test2 {\n  color: #eeeeee;\n}\n.test {\n  color: #cccccc;\n}\n", compiledStylesheet.getSource());
+			Assert.assertTrue(compiledStylesheet.getLastModifiedTime() > 1324508163000l);
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+	}
+
+}
