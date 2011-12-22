44792859e95867a59916d5cce9ef413be271a8e4
Julien Gonzalez <julien.gonzalez@openwide.fr>
2011-12-22 13:43:38 +0000

    JPA : nettoyage des 'packagesToScan'. Désormais plus besoin de PackagesToScanPersistenceUnitPostProcessor, ni de persistence.xml. Les packages à scanner, pour trouver les @Entity, se mettent directement dans la conf de l'entityManagerFactory.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@831 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa-security/src/test/java/fr/openwide/core/test/jpa/security/business/TestPersonService.java
+++ owsi-core-components/owsi-core-component-jpa-security/src/test/java/fr/openwide/core/test/jpa/security/business/TestPersonService.java
@@ -2,9 +2,6 @@ package fr.openwide.core.test.jpa.security.business;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.junit.Test;
 
 import fr.openwide.core.jpa.exception.SecurityServiceException;
@@ -47,88 +44,5 @@ public class TestPersonService extends AbstractJpaSecurityTestCase {
 		mockPersonService.update(person2);
 		
 		assertEquals(2, person2.getAuthorities().size());
-	}
-	
-	@Test
-	// TODO : déplacer dans core-jpa
-	public void testSearch() throws ServiceException, SecurityServiceException {
-		MockPerson person1 = createMockPerson("login1", "firstName1", "lastName1");
-		MockPerson person2 = createMockPerson("login2", "firstName2", "lastName2");
-		
-		mockPersonService.flush();
-		
-		List<MockPerson> persons;
-		
-		persons = mockPersonService.search("login1");
-		
-		assertEquals(1, persons.size());
-		assertEquals(person1, persons.get(0));
-		
-		persons = mockPersonService.search("firstName1");
-		
-		assertEquals(1, persons.size());
-		assertEquals(person1, persons.get(0));
-		
-		persons = mockPersonService.search("lastName2");
-		
-		assertEquals(1, persons.size());
-		assertEquals(person2, persons.get(0));
-	}
-	
-	@Test
-	// TODO : déplacer dans core-jpa
-	public void testSearchAutocomplete() throws ServiceException, SecurityServiceException {
-		MockPerson person1 = createMockPerson("login1t", "firstName1", "lastName1");
-		MockPerson person2 = createMockPerson("login2te", "firstName2", "lastName2");
-		MockPerson person3 = createMockPerson("login3test", "firstName31 firstName32", "last-name3");
-		
-		List<MockPerson> persons;
-		
-		persons = mockPersonService.searchAutocomplete("login1");
-		
-		assertEquals(1, persons.size());
-		assertEquals(person1, persons.get(0));
-		
-		persons = mockPersonService.searchAutocomplete("login");
-		
-		assertEquals(3, persons.size());
-		
-		persons = mockPersonService.searchAutocomplete("firstName");
-		
-		assertEquals(3, persons.size());
-		
-		persons = mockPersonService.searchAutocomplete("lastName");
-		Collections.sort(persons);
-		
-		assertEquals(2, persons.size());
-		assertEquals(person1, persons.get(0));
-		assertEquals(person2, persons.get(1));
-		
-		persons = mockPersonService.searchAutocomplete("Name2");
-		
-		/*
-		 * Cette recherche retourne 0 résultats à cause de la configuration de
-		 * l'analyzer fullText dans la classe Parameter.
-		 */
-		assertEquals(0, persons.size());
-		
-		persons = mockPersonService.searchAutocomplete("firstName31 firstName32");
-		
-		assertEquals(1, persons.size());
-		assertEquals(person3, persons.get(0));
-		
-		persons = mockPersonService.searchAutocomplete("last-Na");
-		Collections.sort(persons);
-		
-		assertEquals(1, persons.size());
-		assertEquals(person3, persons.get(0));
-		
-		persons = mockPersonService.searchAutocomplete("last");
-		Collections.sort(persons);
-		
-		assertEquals(3, persons.size());
-		assertEquals(person1, persons.get(0));
-		assertEquals(person2, persons.get(1));
-		assertEquals(person3, persons.get(2));
 	}
 }
