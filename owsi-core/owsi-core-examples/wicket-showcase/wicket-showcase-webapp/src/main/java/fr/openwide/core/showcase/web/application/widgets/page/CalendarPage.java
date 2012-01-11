1c4bdc9582b9b6d2ffb4ae41da58b32e64185071
Victor Gallet <victor.gallet@openwide.fr>
2012-01-11 15:57:14 +0000

    Widget CalendarPage.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@918 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/widgets/page/CalendarPage.java
+++ owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/widgets/page/CalendarPage.java
@@ -1,15 +1,52 @@
 package fr.openwide.core.showcase.web.application.widgets.page;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import fr.openwide.core.showcase.web.application.util.template.MainTemplate;
+import fr.openwide.core.wicket.more.markup.html.basic.DateLabel;
+import fr.openwide.core.wicket.more.markup.html.form.DatePicker;
+import fr.openwide.core.wicket.more.util.DatePattern;
 
-public class CalendarPage extends MainTemplate {
+public class CalendarPage extends WidgetsMainPage {
 	private static final long serialVersionUID = -3963117430192776716L;
 
+	private IModel<Date> dateModel;
+	
 	public CalendarPage(PageParameters parameters) {
 		super(parameters);
+		
+		Form<Void> form = new Form<Void>("form");
+		add(form);
+		
+		dateModel = new Model<Date>(Calendar.getInstance().getTime());
+		
+		final DatePicker datePicker = new DatePicker("datePicker", dateModel, DatePattern.SHORT_DATE);
+		form.add(datePicker);
+		
+		final DateLabel dateLabel = new DateLabel("dateLabel", dateModel, DatePattern.SHORT_DATE);
+		dateLabel.setOutputMarkupId(true);
+		form.add(dateLabel);
+		
+		datePicker.add(new AjaxFormSubmitBehavior(form, "onchange") {
+			private static final long serialVersionUID = -5972628703079302821L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target) {
+				target.addComponent(dateLabel);
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target) {
+			}
+		});
 	}
 	
 	@Override
@@ -20,5 +57,14 @@ public class CalendarPage extends MainTemplate {
 	@Override
 	protected Class<? extends WebPage> getSecondMenuPage() {
 		return CalendarPage.class;
+	}
+
+	@Override
+	protected void detachModel() {
+		super.detachModel();
+		
+		if(dateModel != null) {
+			dateModel.detach();
+		}
 	}
 }
--- owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/widgets/page/FancyboxPage.java
+++ owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/widgets/page/FancyboxPage.java
@@ -3,9 +3,7 @@ package fr.openwide.core.showcase.web.application.widgets.page;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-import fr.openwide.core.showcase.web.application.util.template.MainTemplate;
-
-public class FancyboxPage extends MainTemplate {
+public class FancyboxPage extends WidgetsMainPage {
 	private static final long serialVersionUID = -4802009584951257187L;
 
 	public FancyboxPage(PageParameters parameters) {
