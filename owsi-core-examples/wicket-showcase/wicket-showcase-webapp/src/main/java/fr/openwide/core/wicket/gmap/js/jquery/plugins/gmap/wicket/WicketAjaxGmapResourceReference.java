d93e7603a61085cb6d486a4ecb3d82828dabd049
Victor Gallet <victor.gallet@openwide.fr>
2012-01-23 14:47:28 +0000

    Suite à l'ajout dans OWSI-CORE de JsonJavascriptResourceReference, suppression du paquet json et correction de la dépendance.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@968 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/wicket/gmap/js/jquery/plugins/gmap/wicket/WicketAjaxGmapResourceReference.java
+++ owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/wicket/gmap/js/jquery/plugins/gmap/wicket/WicketAjaxGmapResourceReference.java
@@ -3,7 +3,7 @@ package fr.openwide.core.wicket.gmap.js.jquery.plugins.gmap.wicket;
 import org.apache.wicket.resource.dependencies.AbstractResourceDependentResourceReference;
 import org.odlabs.wiquery.core.resources.WiQueryJavaScriptResourceReference;
 
-import fr.openwide.core.wicket.gmap.js.jquery.plugins.json.JsonResourceReference;
+import fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.json.JsonJavascriptResourceReference;
 
 public class WicketAjaxGmapResourceReference extends WiQueryJavaScriptResourceReference {
 	private static final long serialVersionUID = 8028339323586140527L;
@@ -21,7 +21,7 @@ public class WicketAjaxGmapResourceReference extends WiQueryJavaScriptResourceRe
 	public AbstractResourceDependentResourceReference[] getDependentResourceReferences() {
 		return new AbstractResourceDependentResourceReference[] {
 			WicketAjaxResourceReference.INSTANCE,
-			JsonResourceReference.INSTANCE
+			JsonJavascriptResourceReference.INSTANCE
 		};
 	}
 }
