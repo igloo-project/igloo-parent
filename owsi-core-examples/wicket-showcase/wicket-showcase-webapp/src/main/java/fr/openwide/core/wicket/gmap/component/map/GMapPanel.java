135f85c176d8ea46f3d29a75808f6b44eee33485
Victor Gallet <victor.gallet@openwide.fr>
2012-01-27 08:31:42 +0000

    Ajout de la représentation du décodage de la polyline sur la carte.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@979 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/wicket/gmap/component/map/GMapPanel.java
+++ owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/wicket/gmap/component/map/GMapPanel.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.request.Request;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.PropertyNamingStrategy;
+import org.odlabs.wiquery.core.behavior.WiQueryAbstractAjaxBehavior;
+import org.odlabs.wiquery.core.javascript.JsStatement;
 
 import com.google.code.geocoder.model.GeocoderResult;
 import com.google.code.geocoder.model.LatLng;
@@ -20,6 +24,7 @@ import fr.openwide.core.wicket.gmap.api.directions.CoreDirectionsResult;
 import fr.openwide.core.wicket.gmap.api.directions.GDirectionsStatus;
 import fr.openwide.core.wicket.gmap.api.directions.GPolyline;
 import fr.openwide.core.wicket.gmap.component.GMapHeaderContributor;
+import fr.openwide.core.wicket.gmap.js.jquery.plugins.gmap.gmarker.GMarkerOptions;
 import fr.openwide.core.wicket.gmap.js.jquery.plugins.gmap.map.GMapBehavior;
 import fr.openwide.core.wicket.gmap.js.jquery.plugins.gmap.map.GMapOptions;
 
@@ -32,6 +37,8 @@ public class GMapPanel extends Panel {
 		OBJECT_MAPPER.setPropertyNamingStrategy(new PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy());
 		OBJECT_MAPPER.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
+	
+	private Button decodedPolyline;
 	
 	private AbstractDefaultAjaxBehavior geocoderResultAjax;
 	private AbstractDefaultAjaxBehavior directionsResultAjax;
@@ -51,6 +58,7 @@ public class GMapPanel extends Panel {
 	
 	public GMapPanel(String id, Locale region, Locale locale, GMapOptions options) {
 		super(id);
+		setOutputMarkupId(true);
 		
 		this.options = options;
 		
@@ -82,7 +90,27 @@ public class GMapPanel extends Panel {
 				try {
 					directionsResult = OBJECT_MAPPER.readValue(result, CoreDirectionsResult.class);
 					if (GDirectionsStatus.OK.getValue().equals(directionsResult.getStatus())) {
-						List<LatLng> points = GPolyline.decodePolyline(directionsResult.getPolyline());
+						final List<LatLng> points = GPolyline.decodePolyline(directionsResult.getPolyline());
+						
+						Page page = GMapPanel.this.getPage();
+						page.add(new WiQueryAbstractAjaxBehavior(){
+							private static final long serialVersionUID = 7382629548949052911L;
+
+							@Override
+							public JsStatement statement() {
+								JsStatement statement = new JsStatement().$(GMapPanel.this);
+								for(LatLng point : points) {
+									statement.chain(new GMarkerOptions(point.toString(), point, GMapPanel.this));
+								}
+								return statement;
+							}
+
+							@Override
+							protected void respond(AjaxRequestTarget target) {
+							}
+						});
+						
+						target.add(page);
 					}
 				} catch (IOException e) {
 					new RuntimeException(e);
@@ -93,6 +121,9 @@ public class GMapPanel extends Panel {
 		
 		// Behavior d'initialisation de la carte
 		add(new GMapBehavior(this.options));
+		
+		decodedPolyline = new Button("decodedPolyline");
+		add(decodedPolyline);
 	}
 	
 	public AbstractDefaultAjaxBehavior getDirectionsResultAjax() {
