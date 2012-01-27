e39d2d5e981700018d58301ffb4e0db20f53e911
Guillaume Smet <guillaume.smet@gmail.com>
2012-01-27 10:57:41 +0000

    ce service doit également être transactionnel
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@982 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/business/generic/service/IEntityService.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/business/generic/service/IEntityService.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 import fr.openwide.core.jpa.business.generic.model.GenericEntity;
 
-public interface IEntityService {
+public interface IEntityService extends ITransactionalAspectAwareService {
 	
 	<K extends Serializable & Comparable<K>, E extends GenericEntity<K, ?>> E getEntity(Class<E> clazz, K id);
 
