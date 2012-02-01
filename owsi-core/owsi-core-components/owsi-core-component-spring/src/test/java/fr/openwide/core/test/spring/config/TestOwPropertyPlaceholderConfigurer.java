5194890211393d9d09fb64c06004ddc714477e09
Laurent Almeras <lalmeras@gmail.com>
2012-02-01 08:25:58 +0000

    re-écriture du CorePropertyPlaceholderConfigurer en héritant du nouveau PropertySourcesPlaceholderConfigurer ; permet :   - de supprimer un appel deprecated   - de mieux gérer les placeholders   - de se placer sur la nouvelle API de référence
    Mise à jour des tests de manière à être plus dans l'esprit Spring + ajout d'un nouveau test sur l'utilisation de placeholders imbriqués (on utilise au moins ça pour le switch de moteur de base de données)
    
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@995 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-spring/src/test/java/fr/openwide/core/test/spring/config/TestOwPropertyPlaceholderConfigurer.java
+++ owsi-core/owsi-core-components/owsi-core-component-spring/src/test/java/fr/openwide/core/test/spring/config/TestOwPropertyPlaceholderConfigurer.java
@@ -5,16 +5,31 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({
+	DependencyInjectionTestExecutionListener.class
+})
+@ContextConfiguration(locations = {
+	"classpath:test/testOwPropertyPlaceholderConfigurer-context.xml"
+})
 public class TestOwPropertyPlaceholderConfigurer {
+
+	@Autowired
+	private OwsiCoreTestConfigurer configurer;
 
 	@Test
 	public void testPlaceholder() throws IOException {
-		OwsiCoreTestConfigurer configurer = new OwsiCoreTestConfigurer();
-		
 		assertEquals("value1", configurer.getProperty1());
 		assertEquals("value2", configurer.getProperty2());
 		assertEquals("value1-value2", configurer.getProperty3());
+		assertEquals("value3", configurer.getProperty4());
 	}
 	
 }
