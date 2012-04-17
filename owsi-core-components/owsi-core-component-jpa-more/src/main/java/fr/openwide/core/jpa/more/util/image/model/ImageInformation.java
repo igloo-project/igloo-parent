eed6f5c0f604042acb0f623e31d523942f7733a5
Guillaume Smet <guillaume.smet@gmail.com>
2012-04-17 21:56:25 +0000

    wrapper un peu plus pratique pour récupérer la taille d'une image
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1085 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/util/image/model/ImageInformation.java
@@ -0,0 +1,28 @@
+package fr.openwide.core.jpa.more.util.image.model;
+
+public class ImageInformation {
+	
+	private int width;
+	
+	private int height;
+	
+	public ImageInformation() {
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+}
