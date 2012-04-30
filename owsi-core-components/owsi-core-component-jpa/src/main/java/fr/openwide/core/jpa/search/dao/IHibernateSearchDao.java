5e42e380fd5a7c8b2542494eff59812114aad89a
Laurent Almeras <lalmeras@gmail.com>
2012-04-30 16:13:53 +0000

    ajout d'une API limit offset
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1115 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/search/dao/IHibernateSearchDao.java
+++ owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/search/dao/IHibernateSearchDao.java
@@ -11,19 +11,36 @@ public interface IHibernateSearchDao {
 
 	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern) throws ServiceException;
 
+	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern, Integer limit, Integer offset) throws ServiceException;
+
+	<T> List<T> search(List<Class<? extends T>> classes, String[] fields, String searchPattern, String analyzerName,
+			Integer limit, Integer offset) throws ServiceException;
+
 	<T> List<T> search(List<Class<? extends T>> classes, String[] fields, String searchPattern, String analyzerName) throws ServiceException;
 
 	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern, String analyzerName) throws ServiceException;
+
+	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern, String analyzerName, Integer limit, Integer offset)
+			throws ServiceException;
 
 	void reindexAll() throws ServiceException;
 
 	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern, String analyzerName, Query additionalLuceneQuery)
 			throws ServiceException;
 
+	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern, String analyzerName, Query additionalLuceneQuery,
+			Integer limit, Integer offset) throws ServiceException;
+
 	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern, Query additionalLuceneQuery)
+			throws ServiceException;
+
+	<T> List<T> search(Class<T> clazz, String[] fields, String searchPattern, Query additionalLuceneQuery, Integer limit, Integer offset)
 			throws ServiceException;
 
 	<T> List<T> search(List<Class<? extends T>> classes, String[] fields, String searchPattern, String analyzerName,
 			Query additionalLuceneQuery) throws ServiceException;
+
+	<T> List<T> search(List<Class<? extends T>> classes, String[] fields, String searchPattern, String analyzerName,
+			Query additionalLuceneQuery, Integer limit, Integer offset) throws ServiceException;
 	
 }
