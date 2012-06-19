1eda6cb3a0946ae6ad68a2d3f1ea2440ddc1e8c3
Laurent Almeras <lalmeras@gmail.com>
2012-06-19 15:48:21 +0000

    méthodes pour la consultation des exécutions
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1166 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/execution/dao/AbstractExecutionDaoImpl.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/execution/dao/AbstractExecutionDaoImpl.java
@@ -1,13 +1,17 @@
 package fr.openwide.core.jpa.more.business.execution.dao;
 
+import java.util.Date;
 import java.util.List;
 
+import com.mysema.query.BooleanBuilder;
 import com.mysema.query.jpa.impl.JPAQuery;
 import com.mysema.query.types.Path;
 import com.mysema.query.types.path.EntityPathBase;
 
 import fr.openwide.core.jpa.business.generic.dao.GenericEntityDaoImpl;
 import fr.openwide.core.jpa.more.business.execution.model.AbstractExecution;
+import fr.openwide.core.jpa.more.business.execution.model.ExecutionStatus;
+import fr.openwide.core.jpa.more.business.execution.model.IExecutionType;
 import fr.openwide.core.jpa.more.business.execution.model.QAbstractExecution;
 
 public abstract class AbstractExecutionDaoImpl<E extends AbstractExecution<E, ?>>
@@ -22,5 +26,37 @@ public abstract class AbstractExecutionDaoImpl<E extends AbstractExecution<E, ?>
 			.from(qAbstractExecution)
 			.orderBy(qAbstractExecution.startDate.desc())
 			.list(path);
+	}
+
+	@Override
+	public List<E> listOrderedByDateTypeStatus(Date startDate, Date endDate, IExecutionType executionType, ExecutionStatus executionStatus,
+			Integer limit, Integer offset) {
+		Path<E> path = new EntityPathBase<E>(getObjectClass(), QAbstractExecution.abstractExecution.getMetadata());
+		QAbstractExecution qAbstractExecution = new QAbstractExecution(path);
+		
+		BooleanBuilder builder = new BooleanBuilder();
+		if (startDate != null) {
+			builder.and(qAbstractExecution.startDate.gt(startDate));
+		}
+		if (endDate != null) {
+			builder.and(qAbstractExecution.endDate.lt(endDate));
+		}
+		if (executionType != null) {
+			builder.and(qAbstractExecution.executionType.eq(executionType));
+		}
+		if (executionStatus != null) {
+			builder.and(qAbstractExecution.executionStatus.eq(executionStatus));
+		}
+		
+		JPAQuery jpaQuery = new JPAQuery(getEntityManager())
+			.from(qAbstractExecution)
+			.orderBy(qAbstractExecution.startDate.desc());
+		if (limit != null) {
+			jpaQuery.limit(limit);
+		}
+		if (offset != null) {
+			jpaQuery.offset(offset);
+		}
+		return jpaQuery.list(path);
 	}
 }
