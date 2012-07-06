a9340de426fd4e90a731bba9336c8ce2383a967a
Laurent Almeras <lalmeras@gmail.com>
2012-07-06 13:21:34 +0000

    prise en compte du hbm2ddl / import_files
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1223 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/spring/config/AbstractConfiguredJpaMoreConfig.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/spring/config/AbstractConfiguredJpaMoreConfig.java
@@ -3,12 +3,13 @@ package fr.openwide.core.jpa.more.spring.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import fr.openwide.core.jpa.config.spring.DefaultJpaConfig;
 import fr.openwide.core.jpa.config.spring.JpaConfigUtils;
 
-
+@Import(DefaultJpaConfig.class)
 public abstract class AbstractConfiguredJpaMoreConfig extends AbstractJpaMoreConfig {
 
 	@Autowired
--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/test/java/fr/openwide/core/test/jpa/more/spring/config/JpaMoreTestJpaConfig.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/test/java/fr/openwide/core/test/jpa/more/spring/config/JpaMoreTestJpaConfig.java
@@ -2,16 +2,13 @@ package fr.openwide.core.test.jpa.more.spring.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 
-import fr.openwide.core.jpa.config.spring.DefaultJpaConfig;
 import fr.openwide.core.jpa.config.spring.provider.JpaPackageScanProvider;
 import fr.openwide.core.jpa.more.spring.config.AbstractConfiguredJpaMoreConfig;
 import fr.openwide.core.test.jpa.more.business.JpaMoreTestBusinessPackage;
 
 @Configuration
 @EnableAspectJAutoProxy
-@Import(DefaultJpaConfig.class)
 public class JpaMoreTestJpaConfig extends AbstractConfiguredJpaMoreConfig {
 
 	@Override
