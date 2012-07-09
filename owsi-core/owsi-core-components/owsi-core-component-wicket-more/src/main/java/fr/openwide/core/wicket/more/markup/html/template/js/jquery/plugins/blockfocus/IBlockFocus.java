d94ad9923938bd2b1c89e53849c3d79f1481fdcf
Laurent Almeras <lalmeras@gmail.com>
2012-07-09 13:31:52 +0000

    on laisse ces éléments en spécifique
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1247 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/blockfocus/IBlockFocus.java
+++
@@ -1,9 +0,0 @@
-package fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.blockfocus;
-
-import java.io.Serializable;
-
-import org.odlabs.wiquery.core.javascript.ChainableStatement;
-
-public interface IBlockFocus extends Serializable, ChainableStatement {
-
-}
--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/gmap/api/gmarker/IGMarkerImageTermination.java
+++
@@ -1,7 +0,0 @@
-package fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.gmap.api.gmarker;
-
-public interface IGMarkerImageTermination {
-	
-	GMarkerImage create();
-	
-}
