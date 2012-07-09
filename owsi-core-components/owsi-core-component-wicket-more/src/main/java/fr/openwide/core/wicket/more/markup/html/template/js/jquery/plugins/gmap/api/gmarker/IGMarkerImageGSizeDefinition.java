c648f706407e6cfc1aedd35f871a6636871137ae
Laurent Almeras <lalmeras@gmail.com>
2012-07-09 13:31:52 +0000

    on laisse ces éléments en spécifique
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1247 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/gmap/api/gmarker/IGMarkerImageGSizeDefinition.java
+++
@@ -1,7 +0,0 @@
-package fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.gmap.api.gmarker;
-
-public interface IGMarkerImageGSizeDefinition {
-	
-	IGMarkerImageTermination scaledSize(int width, int height);
-
-}
--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/more/IMore.java
+++
@@ -1,9 +0,0 @@
-package fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.more;
-
-import java.io.Serializable;
-
-import org.odlabs.wiquery.core.javascript.ChainableStatement;
-
-public interface IMore extends ChainableStatement, Serializable {
-
-}
