2d3b956a64e1cec976bdbd1a18ebaa1973c08223
Laurent Almeras <lalmeras@gmail.com>
2012-07-10 08:46:42 +0000

    bean abstrait pour la configuration webapp + service lesscss
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1267 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/config/spring/AbstractWebappConfig.java
@@ -0,0 +1,15 @@
+package fr.openwide.core.wicket.more.config.spring;
+
+import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(LessCssConfig.class)
+public abstract class AbstractWebappConfig {
+
+	@Bean
+	public abstract WebApplication application();
+
+}
