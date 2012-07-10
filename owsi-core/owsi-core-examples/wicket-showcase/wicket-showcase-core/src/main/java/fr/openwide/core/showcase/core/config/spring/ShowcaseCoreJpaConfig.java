af44f32d4f6a1d3e08593e2bc88babc3b3c0b06a
Laurent Almeras <lalmeras@gmail.com>
2012-07-10 17:26:01 +0000

    rollback modifications inutiles
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1284 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-core/src/main/java/fr/openwide/core/showcase/core/config/spring/ShowcaseCoreJpaConfig.java
+++ owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-core/src/main/java/fr/openwide/core/showcase/core/config/spring/ShowcaseCoreJpaConfig.java
@@ -1,15 +1,10 @@
 package fr.openwide.core.showcase.core.config.spring;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import fr.openwide.core.jpa.config.spring.provider.JpaPackageScanProvider;
-import fr.openwide.core.jpa.more.business.JpaMoreBusinessPackage;
-import fr.openwide.core.jpa.security.business.JpaSecurityBusinessPackage;
 import fr.openwide.core.jpa.security.config.spring.AbstractConfiguredJpaSecurityJpaConfig;
 import fr.openwide.core.showcase.core.business.ShowcaseCoreBusinessPackage;
 
@@ -24,28 +19,6 @@ public class ShowcaseCoreJpaConfig extends AbstractConfiguredJpaSecurityJpaConfi
 	@Bean
 	public JpaPackageScanProvider applicationJpaPackageScanProvider() {
 		return new JpaPackageScanProvider(ShowcaseCoreBusinessPackage.class.getPackage());
-	}
-
-	@Override
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-		return super.entityManagerFactory();
-	}
-
-	@Override
-	@Bean
-	public DataSource dataSource() {
-		return super.dataSource();
-	}
-
-	@Bean
-	public JpaPackageScanProvider jpaSecurityPackageScanProvider() {
-		return new JpaPackageScanProvider(JpaSecurityBusinessPackage.class.getPackage());
-	}
-
-	@Bean
-	public JpaPackageScanProvider jpaMorePackageScanProvider() {
-		return new JpaPackageScanProvider(JpaMoreBusinessPackage.class.getPackage());
 	}
 
 }
