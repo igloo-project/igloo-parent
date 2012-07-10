e4949fe5d2dfc607fe05b5cb2922d1ddcabfe1a1
Laurent Almeras <lalmeras@gmail.com>
2012-07-10 17:42:25 +0000

    le filtre est hyper-important ; il ne faut pas charger les @Configuration n'importe comment
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1285 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/config/spring/ShowcaseWebappConfig.java
+++ owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/config/spring/ShowcaseWebappConfig.java
@@ -3,6 +3,7 @@ package fr.openwide.core.showcase.web.application.config.spring;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -19,7 +20,8 @@ import fr.openwide.core.wicket.more.config.spring.AbstractWebappConfig;
 @ComponentScan(
 		basePackageClasses = {
 				ShowcaseWebPackage.class
-		}
+		},
+		excludeFilters = @Filter(Configuration.class)
 )
 public class ShowcaseWebappConfig extends AbstractWebappConfig {
 
