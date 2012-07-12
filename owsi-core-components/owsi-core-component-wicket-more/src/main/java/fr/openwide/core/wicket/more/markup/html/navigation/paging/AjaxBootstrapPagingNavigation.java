e3bdef75aa6fc9756c5fe59696f5f08f468f0296
Renaud Joly <joly.renaud@gmail.com>
2012-07-12 17:55:06 +0000

    Pagination ajax.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1339 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/navigation/paging/AjaxBootstrapPagingNavigation.java
@@ -0,0 +1,20 @@
+package fr.openwide.core.wicket.more.markup.html.navigation.paging;
+
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.navigation.paging.IPageable;
+import org.apache.wicket.markup.html.navigation.paging.IPagingLabelProvider;
+
+public class AjaxBootstrapPagingNavigation extends BootstrapPagingNavigation {
+
+	private static final long serialVersionUID = -8839242459796577683L;
+	
+	public AjaxBootstrapPagingNavigation(String id, IPageable pageable, IPagingLabelProvider labelProvider) {
+		super(id, pageable, labelProvider);
+	}
+	
+	@Override
+	protected Link<?> newPagingNavigationLink(String id, IPageable pageable, int pageIndex) {
+		return new AjaxBootstrapPagingNavigationLink(id, pageable, pageIndex).setAutoEnable(false);
+	}
+
+}
--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/navigation/paging/HideableAjaxPagingNavigator.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/navigation/paging/HideableAjaxPagingNavigator.java
@@ -1,10 +1,14 @@
 package fr.openwide.core.wicket.more.markup.html.navigation.paging;
 
-import org.apache.wicket.ajax.markup.html.navigation.paging.AjaxPagingNavigator;
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.navigation.paging.IPageable;
 import org.apache.wicket.markup.html.navigation.paging.IPagingLabelProvider;
+import org.apache.wicket.markup.repeater.AbstractRepeater;
 
-public class HideableAjaxPagingNavigator extends AjaxPagingNavigator {
+public class HideableAjaxPagingNavigator extends HideablePagingNavigator {
 	private static final long serialVersionUID = -4406782762372796027L;
 	
 	public HideableAjaxPagingNavigator(String id, IPageable pageable) {
@@ -13,11 +17,42 @@ public class HideableAjaxPagingNavigator extends AjaxPagingNavigator {
 	
 	public HideableAjaxPagingNavigator(String id, IPageable pageable, IPagingLabelProvider labelProvider) {
 		super(id, pageable, labelProvider);
+		setOutputMarkupId(true);
 	}
 	
 	@Override
 	public boolean isVisible() {
 		IPageable pageable = getPageable();
 		return (pageable.getPageCount() > 1);
+	}
+	
+	@Override
+	protected Link<?> newPagingNavigationLink(String id, IPageable pageable, int pageNumber) {
+		return new AjaxBootstrapPagingNavigationLink(id, pageable, pageNumber);
+	}
+	
+	@Override
+	protected BootstrapPagingNavigation newNavigation(final String id, final IPageable pageable, final IPagingLabelProvider labelProvider) {
+		return new AjaxBootstrapPagingNavigation(id, pageable, labelProvider);
+	}
+	
+	protected void onAjaxEvent(AjaxRequestTarget target) {
+		// update the container (parent) of the pageable, this assumes that
+		// the pageable is a component, and that it is a child of a web
+		// markup container.
+		
+		Component container = ((Component)getPageable());
+		// no need for a nullcheck as there is bound to be a non-repeater
+		// somewhere higher in the hierarchy
+		while (container instanceof AbstractRepeater) {
+			container = container.getParent();
+		}
+		target.add(container);
+		
+		// in case the navigator is not contained by the container, we have
+		// to add it to the response
+		if (((MarkupContainer)container).contains(this, true) == false) {
+			target.add(this);
+		}
 	}
 }
