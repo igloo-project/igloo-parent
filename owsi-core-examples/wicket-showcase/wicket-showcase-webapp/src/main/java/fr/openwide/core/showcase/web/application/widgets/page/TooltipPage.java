401e791698993cc969d2cbfac1d806805da03477
Renaud Joly <joly.renaud@gmail.com>
2012-07-12 16:45:07 +0000

    Ajout d'exemples de popover avec les tooltips.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1337 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/widgets/page/TooltipPage.java
+++ owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/widgets/page/TooltipPage.java
@@ -1,9 +1,16 @@
 package fr.openwide.core.showcase.web.application.widgets.page;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import fr.openwide.core.wicket.behavior.ClassAttributeAppender;
+import fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.bootstrap.popover.BootstrapPopoverBehavior;
+import fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.bootstrap.popover.BootstrapPopoverOptions;
+import fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.bootstrap.popover.PopoverTrigger;
 import fr.openwide.core.wicket.more.markup.html.template.model.BreadCrumbElement;
 
 public class TooltipPage extends WidgetsTemplate {
@@ -14,6 +21,35 @@ public class TooltipPage extends WidgetsTemplate {
 		super(parameters);
 		
 		addBreadCrumbElement(new BreadCrumbElement(new ResourceModel("widgets.menu.tooltip"), TooltipPage.class));
+		
+		// Popover
+		WebMarkupContainer someInformation = new WebMarkupContainer("someInfomration");
+		someInformation.setOutputMarkupId(true);
+		add(someInformation);
+		
+		Label someLabelDefault = new Label("someLabelDefault", new ResourceModel("widgets.popover.someLabel.default"));
+		BootstrapPopoverOptions popoverOptions = new BootstrapPopoverOptions();
+		popoverOptions.setTrigger(PopoverTrigger.MANUAL);
+		popoverOptions.setTitleText(new ResourceModel("widgets.popover.someInformation.title").getObject());
+		popoverOptions.setContentComponent(someInformation);
+		someLabelDefault.add(new BootstrapPopoverBehavior(popoverOptions));
+		someLabelDefault.add(new ClassAttributeAppender(Model.of("popover-btn")));
+		add(someLabelDefault);
+		
+		Label someLabelLeft = new Label("someLabelLeft", new ResourceModel("widgets.popover.someLabel.left"));
+		someLabelLeft.add(new BootstrapPopoverBehavior(popoverOptions));
+		someLabelLeft.add(new ClassAttributeAppender(Model.of("popover-btn")));
+		add(someLabelLeft);
+		
+		Label someLabelTop = new Label("someLabelTop", new ResourceModel("widgets.popover.someLabel.top"));
+		someLabelTop.add(new BootstrapPopoverBehavior(popoverOptions));
+		someLabelTop.add(new ClassAttributeAppender(Model.of("popover-btn")));
+		add(someLabelTop);
+		
+		Label someLabelBottom = new Label("someLabelBottom", new ResourceModel("widgets.popover.someLabel.bottom"));
+		someLabelBottom.add(new BootstrapPopoverBehavior(popoverOptions));
+		someLabelBottom.add(new ClassAttributeAppender(Model.of("popover-btn")));
+		add(someLabelBottom);
 	}
 	
 	@Override
