404ef80b891904144e009d049f1c17819a5ef898
Guillaume Smet <guillaume.smet@gmail.com>
2012-08-19 15:19:12 +0000

    mise en place d'une dépendance pour la partie web de Spring Security correction de LoginSuccessPage pour coller avec le nouveau mode de fonctionnement de l'authentification (en fait, elle ne servait plus du tout et c'est pour ça qu'on n'a pas tilté qu'elle ne fonctionnait plus correctement)
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1447 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/security/page/LoginSuccessPage.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/security/page/LoginSuccessPage.java
@@ -1,21 +1,33 @@
 package fr.openwide.core.wicket.more.security.page;
 
 import org.apache.wicket.request.flow.RedirectToUrlException;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
+import fr.openwide.core.spring.util.StringUtils;
 import fr.openwide.core.wicket.more.AbstractCoreSession;
 import fr.openwide.core.wicket.more.markup.html.CoreWebPage;
+import fr.openwide.core.wicket.more.request.cycle.RequestCycleUtils;
 
 public class LoginSuccessPage extends CoreWebPage {
 	
 	private static final long serialVersionUID = -875304387617628398L;
+	
+	private static final String SPRING_SECURITY_SAVED_REQUEST = "SPRING_SECURITY_SAVED_REQUEST";
 
 	public LoginSuccessPage() {
-		AbstractCoreSession.get().signIn("", "");
+		AbstractCoreSession<?> session = AbstractCoreSession.get();
 		
-		String redirect = AbstractCoreSession.get().getRedirectUrl();
-		
-		if (redirect != null) {
-			throw new RedirectToUrlException(redirect);
+		String redirectUrl = null;
+		if (StringUtils.hasText(session.getRedirectUrl())) {
+			redirectUrl = session.getRedirectUrl();
+		} else {
+			Object savedRequest = RequestCycleUtils.getCurrentContainerRequest().getSession().getAttribute(SPRING_SECURITY_SAVED_REQUEST);
+			if (savedRequest instanceof SavedRequest) {
+				redirectUrl = ((SavedRequest) savedRequest).getRedirectUrl();
+			}
+		}
+		if (StringUtils.hasText(redirectUrl)) {
+			throw new RedirectToUrlException(redirectUrl);
 		} else {
 			redirect(this.getApplication().getHomePage());
 		}
