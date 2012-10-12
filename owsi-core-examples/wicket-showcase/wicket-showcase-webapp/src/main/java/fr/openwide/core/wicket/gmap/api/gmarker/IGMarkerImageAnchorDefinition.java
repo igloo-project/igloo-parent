9621a5e40bd84376f9e53c9604ccb826fa758002
Julien Gonzalez <julien.gonzalez@openwide.fr>
2012-10-12 14:23:18 +0000

    Migration OWSI-Core vers Wicket 6.1.1, Wicket 6.0.0 : tout ne marche pas encore mais au moins ça compile.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1622 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/wicket/gmap/api/gmarker/IGMarkerImageAnchorDefinition.java
+++
@@ -1,7 +0,0 @@
-package fr.openwide.core.wicket.gmap.api.gmarker;
-
-public interface IGMarkerImageAnchorDefinition {
-
-	IGMarkerImageGSizeDefinition anchor(float x, float y);
-	
-}
--- owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/wicket/gmap/api/gmarker/IGMarkerImageGSizeDefinition.java
+++
@@ -1,7 +0,0 @@
-package fr.openwide.core.wicket.gmap.api.gmarker;
-
-public interface IGMarkerImageGSizeDefinition {
-	
-	IGMarkerImageTermination scaledSize(int width, int height);
-
-}
