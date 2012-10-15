e4d4b7040ebe97c4216437ec7b2555c0e61fbfee
Julien Gonzalez <julien.gonzalez@openwide.fr>
2012-10-15 12:43:42 +0000

    Showcase : Lifting de la page de description des utilisateurs.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1634 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/portfolio/component/UserProfilePanel.java
+++ owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/portfolio/component/UserProfilePanel.java
@@ -1,38 +1,33 @@
 package fr.openwide.core.showcase.web.application.portfolio.component;
 
-import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.ResourceModel;
 import org.odlabs.wiquery.core.events.MouseEvent;
 
 import fr.openwide.core.showcase.core.business.user.model.User;
 import fr.openwide.core.showcase.core.business.user.model.UserBinding;
 import fr.openwide.core.showcase.web.application.portfolio.form.EditUserPopupPanel;
-import fr.openwide.core.wicket.markup.html.link.EmailLink;
 import fr.openwide.core.wicket.markup.html.panel.GenericPanel;
 import fr.openwide.core.wicket.more.markup.html.basic.DateLabel;
-import fr.openwide.core.wicket.more.markup.html.image.BooleanImage;
+import fr.openwide.core.wicket.more.markup.html.image.BooleanGlyphicon;
+import fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.emailobfuscator.ObfuscatedEmailLink;
 import fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.modal.behavior.AjaxOpenModalBehavior;
 import fr.openwide.core.wicket.more.model.BindingModel;
 import fr.openwide.core.wicket.more.util.DatePattern;
 
-public class UserIdentityCardPanel extends GenericPanel<User> {
+public class UserProfilePanel extends GenericPanel<User> {
 	private static final long serialVersionUID = 646894189220818498L;
 	
 	private static final UserBinding USER_BINDING = new UserBinding();
 	
-	public UserIdentityCardPanel(String id, IModel<User> userModel) {
+	public UserProfilePanel(String id, IModel<User> userModel) {
 		super(id, userModel);
 		
-		add(new Label("title", BindingModel.of(userModel, USER_BINDING.fullName())));
-		
-		add(new Label("fullName", BindingModel.of(userModel, USER_BINDING.fullName())));
 		add(new Label("userName", BindingModel.of(userModel, USER_BINDING.userName())));
 		
-		add(new EmailLink("emailLink", BindingModel.of(userModel, USER_BINDING.email())));
+		add(new ObfuscatedEmailLink("emailLink", BindingModel.of(userModel, USER_BINDING.email()), true));
 		
 		add(new Label("phoneNumber", BindingModel.of(userModel, USER_BINDING.phoneNumber())));
 		add(new Label("gsmNumber", BindingModel.of(userModel, USER_BINDING.gsmNumber())));
@@ -43,14 +38,7 @@ public class UserIdentityCardPanel extends GenericPanel<User> {
 		add(new DateLabel("lastLoginDate", BindingModel.of(userModel, USER_BINDING.lastLoginDate()),
 				DatePattern.SHORT_DATETIME));
 		
-		BooleanImage activeImage = new BooleanImage("active", BindingModel.of(userModel, USER_BINDING.active()));
-		
-		boolean active = getModelObject().isActive();
-		String tipsyProperty = active ? "common.active" : "common.inactive";
-		
-		activeImage.add(new AttributeModifier("alt", new ResourceModel(tipsyProperty, "")));
-		activeImage.add(new AttributeModifier("data-tooltip", new ResourceModel(tipsyProperty, "")));
-		add(activeImage);
+		add(new BooleanGlyphicon("active", BindingModel.of(userModel, USER_BINDING.active())));
 		
 		// Edit user popup panel
 		EditUserPopupPanel userEditPopupPanel = new EditUserPopupPanel("userEditPopupPanel", userModel);
