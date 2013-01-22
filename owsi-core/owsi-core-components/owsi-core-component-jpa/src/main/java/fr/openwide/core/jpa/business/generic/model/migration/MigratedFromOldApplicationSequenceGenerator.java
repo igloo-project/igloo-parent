e4317c4b69e1a2b25c9af22f87a156cb81e44277
Guillaume Smet <guillaume.smet@gmail.com>
2013-01-22 11:17:23 +0000

    factorisation des outils permettant de migrer les ids d'une ancienne application vers une nouvelle
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1853 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/business/generic/model/migration/MigratedFromOldApplicationSequenceGenerator.java
@@ -0,0 +1,36 @@
+package fr.openwide.core.jpa.business.generic.model.migration;
+
+import java.io.Serializable;
+
+import org.hibernate.engine.spi.SessionImplementor;
+
+import fr.openwide.core.jpa.util.PostgreSQLSequenceGenerator;
+
+/**
+ * Sequence generator used to define the new id from the id used in an old application.
+ * 
+ * <pre>
+ * {@code
+ * @Id
+ * @GeneratedValue(generator = "Ticket_id")
+ * @GenericGenerator(name = "Ticket_id", strategy = "fr.openwide.core.jpa.business.generic.model.migration.MigratedFromOldApplicationSequenceGenerator")
+ * @DocumentId
+ * private Long id;
+ * }
+ * </pre>
+ */
+public class MigratedFromOldApplicationSequenceGenerator extends PostgreSQLSequenceGenerator {
+	
+	@Override
+	public Serializable generate(SessionImplementor session, Object obj) {
+		if (obj instanceof MigratedFromOldApplicationGenericEntity) {
+			Serializable oldApplicationId = ((MigratedFromOldApplicationGenericEntity<?, ?>) obj).getOldApplicationId();
+			if (oldApplicationId != null) {
+				return oldApplicationId;
+			}
+		}
+		
+		return super.generate(session, obj);
+	}
+
+}
