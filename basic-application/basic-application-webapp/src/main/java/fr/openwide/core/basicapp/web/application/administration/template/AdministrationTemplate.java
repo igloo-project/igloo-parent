ab9f27f9f256500be8e93de1cd10a1ad28b02168
Guillaume Smet <guillaume.smet@gmail.com>
2013-01-23 17:58:55 +0000

    on verrouille l'accès à l'admin...
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@1869 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/administration/template/AdministrationTemplate.java
+++ basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/administration/template/AdministrationTemplate.java
@@ -2,6 +2,7 @@ package fr.openwide.core.basicapp.web.application.administration.template;
 
 import java.util.List;
 
+import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -11,9 +12,11 @@ import com.google.common.collect.Lists;
 import fr.openwide.core.basicapp.web.application.administration.page.AdministrationUserGroupPortfolioPage;
 import fr.openwide.core.basicapp.web.application.administration.page.AdministrationUserPortfolioPage;
 import fr.openwide.core.basicapp.web.application.common.template.MainTemplate;
+import fr.openwide.core.jpa.security.business.authority.util.CoreAuthorityConstants;
 import fr.openwide.core.wicket.more.markup.html.template.model.BreadCrumbElement;
 import fr.openwide.core.wicket.more.markup.html.template.model.NavigationMenuItem;
 
+@AuthorizeInstantiation(CoreAuthorityConstants.ROLE_ADMIN)
 public abstract class AdministrationTemplate extends MainTemplate {
 
 	private static final long serialVersionUID = -5571981353426833725L;
