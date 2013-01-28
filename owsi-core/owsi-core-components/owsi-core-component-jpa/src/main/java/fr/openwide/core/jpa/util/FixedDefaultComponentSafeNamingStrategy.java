d086f711447a69f2583d9d96bd8aa0cbe580786a
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-01-28 18:00:26 +0000

    FixedDefaultComponentSafeNamingStrategy : application du bugfix partout où c'est nécessaire.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1900 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/util/FixedDefaultComponentSafeNamingStrategy.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/util/FixedDefaultComponentSafeNamingStrategy.java
@@ -8,10 +8,28 @@ import org.hibernate.cfg.DefaultComponentSafeNamingStrategy;
 public class FixedDefaultComponentSafeNamingStrategy extends DefaultComponentSafeNamingStrategy {
 
 	private static final long serialVersionUID = -2382165043375385406L;
+	
+	protected static String fixPropertyName(String propertyName) {
+		return propertyName.replace(".collection&&element.", ".");
+	}
 
 	@Override
 	public String propertyToColumnName(String propertyName) {
-		return super.propertyToColumnName(propertyName.replace(".collection&&element.", "."));
+		return super.propertyToColumnName(fixPropertyName(propertyName));
+	}
+	
+	@Override
+	public String collectionTableName(String ownerEntity, String ownerEntityTable, String associatedEntity,
+			String associatedEntityTable, String propertyName) {
+		return super.collectionTableName(ownerEntity, ownerEntityTable, associatedEntity, associatedEntityTable,
+				fixPropertyName(propertyName));
+	}
+	
+	@Override
+	public String foreignKeyColumnName(String propertyName, String propertyEntityName, String propertyTableName,
+			String referencedColumnName) {
+		return super.foreignKeyColumnName(fixPropertyName(propertyName),
+				propertyEntityName, propertyTableName, referencedColumnName);
 	}
 
 }
