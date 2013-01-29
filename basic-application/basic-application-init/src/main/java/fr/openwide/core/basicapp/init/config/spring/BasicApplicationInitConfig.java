82ec96d3663145aef8292cfdd697878584bbd65e
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-01-29 15:12:49 +0000

    Archetype : désactivation de l'indexation au fil de l'eau dans la partie initialisation.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@1908 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-init/src/main/java/fr/openwide/core/basicapp/init/config/spring/BasicApplicationInitConfig.java
+++ basic-application/basic-application-init/src/main/java/fr/openwide/core/basicapp/init/config/spring/BasicApplicationInitConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Import;
 
 import fr.openwide.core.basicapp.core.config.spring.BasicApplicationCoreCommonConfig;
 import fr.openwide.core.basicapp.init.BasicApplicationInitPackage;
+import fr.openwide.core.spring.config.spring.annotation.ConfigurationLocations;
 
 @Configuration
 @Import({
 	BasicApplicationCoreCommonConfig.class
 })
+@ConfigurationLocations(locations = "classpath:configuration-init.properties")
 @ComponentScan(
 		basePackageClasses = {
 			BasicApplicationInitPackage.class
