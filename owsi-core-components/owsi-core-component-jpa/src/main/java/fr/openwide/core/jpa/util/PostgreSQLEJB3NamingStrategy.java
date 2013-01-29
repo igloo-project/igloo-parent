2a602f585cec591ee647cf57a125578b8ac9284a
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-01-29 13:11:16 +0000

    Ajout de NamingStategies permettant de tronquer automatiquement les noms de colonne/table/etc, pour éviter les problèmes avec PostgreSQL en particulier qui tronque silencieusement les noms de plus de 64 caractères.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1903 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/util/PostgreSQLEJB3NamingStrategy.java
@@ -0,0 +1,16 @@
+package fr.openwide.core.jpa.util;
+
+import org.hibernate.cfg.EJB3NamingStrategy;
+import org.hibernate.cfg.NamingStrategy;
+
+/**
+ * @see TruncatingNamingStrategyWrapper
+ */
+public class PostgreSQLEJB3NamingStrategy extends TruncatingNamingStrategyWrapper {
+	public static final NamingStrategy INSTANCE = new PostgreSQLEJB3NamingStrategy();
+
+	public PostgreSQLEJB3NamingStrategy() {
+		super(EJB3NamingStrategy.INSTANCE, 64);
+	}
+
+}
