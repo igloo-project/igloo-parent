d462988069de6691bf2a9beb4f7698e0218a9439
Renaud Joly <joly.renaud@gmail.com>
2013-02-01 09:24:48 +0000

    Ajustements DatePicker :  * affichage de la barre des boutons (Aujourd'hui et Fermer) par défaut  * bouton Aujourd'hui met à jour le champ avec la date du jour  * synchronisation des champs date dans le showcase
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1925 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/DatePickerOverrideJavaScriptResourceReference.java
@@ -0,0 +1,34 @@
+package fr.openwide.core.wicket.more.markup.html.form;
+
+import java.util.List;
+
+import org.apache.wicket.markup.head.HeaderItem;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.odlabs.wiquery.ui.datepicker.DatePickerJavaScriptResourceReference;
+
+import com.google.common.collect.Lists;
+
+import fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.util.AbstractCoreJQueryPluginResourceReference;
+
+public class DatePickerOverrideJavaScriptResourceReference extends AbstractCoreJQueryPluginResourceReference {
+	
+	private static final long serialVersionUID = -1451228678288396852L;
+	
+	private static final DatePickerOverrideJavaScriptResourceReference INSTANCE = new DatePickerOverrideJavaScriptResourceReference();
+
+	private DatePickerOverrideJavaScriptResourceReference() {
+		super(DatePickerOverrideJavaScriptResourceReference.class, "jquery.datePickerOverride.js");
+	}
+	
+	@Override
+	protected Iterable<? extends HeaderItem> getPluginDependencies() {
+		List<HeaderItem> dependencies = Lists.newArrayList();
+		dependencies.add(JavaScriptHeaderItem.forReference(DatePickerJavaScriptResourceReference.get()));
+		return dependencies;
+	}
+
+	public static DatePickerOverrideJavaScriptResourceReference get() {
+		return INSTANCE;
+	}
+
+}
