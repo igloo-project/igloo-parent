c40b353768562111afb2460d47e3a2a2642a3571
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-02-01 11:13:05 +0000

    Ajout d'avertissements concernant le SetToListWrapperModel.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@1927 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/SetToListWrapperModel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/SetToListWrapperModel.java
@@ -2,7 +2,9 @@ package fr.openwide.core.wicket.more.model;
 
 import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
 
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -10,10 +12,16 @@ import org.apache.wicket.model.Model;
 import com.google.common.collect.ImmutableList;
 
 /**
- * A wrapper on Set models to display them in Wicket components, such as ListView.
- * <br>
- * As this model brutally copies the elements to a new list from the underlying set, it may expose performance
- * issues if used extensively or on very large sets.
+ * A wrapper on Set models to display them in Wicket components, such as
+ * ListView. <br>
+ * WARNING : depending on the implementation of the underlying set, the index of
+ * items in the returned list may not be the same on each call to getObject() if
+ * the set has been reloaded, even with the same content. To avoid any problem,
+ * either use {@link SortedSet SortedSets} or, if using this model in a
+ * {@link ListView}, override {@link ListView#getListItemModel()}. <br>
+ * As this model brutally copies the elements to a new list from the underlying
+ * set, it may expose performance issues if used extensively or on very large
+ * sets.
  */
 public class SetToListWrapperModel<T> extends AbstractReadOnlyModel<List<T>> {
 
