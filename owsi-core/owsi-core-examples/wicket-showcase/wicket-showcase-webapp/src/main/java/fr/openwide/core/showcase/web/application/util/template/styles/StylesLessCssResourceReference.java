0ff5629de7b9a3d788963d7e835db437af2a3f65
Guillaume Smet <guillaume.smet@gmail.com>
2013-04-02 17:16:21 +0000

    ajout de la dépendance à la CSS jquery ui pour être sûr de la charger avant mise en cohérence des noms des feuilles de styles entre le showcase et l'archetype
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2064 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/util/template/styles/StylesLessCssResourceReference.java
@@ -0,0 +1,33 @@
+package fr.openwide.core.showcase.web.application.util.template.styles;
+
+import java.util.List;
+
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.HeaderItem;
+
+import com.google.common.collect.Lists;
+
+import fr.openwide.core.wicket.more.lesscss.LessCssResourceReference;
+import fr.openwide.core.wicket.more.markup.html.template.css.jqueryui.JQueryUiCssResourceReference;
+
+public class StylesLessCssResourceReference extends LessCssResourceReference {
+
+	private static final long serialVersionUID = 4656765761895221782L;
+
+	private static final StylesLessCssResourceReference INSTANCE = new StylesLessCssResourceReference();
+
+	private StylesLessCssResourceReference() {
+		super(StylesLessCssResourceReference.class, "styles.less");
+	}
+	
+	@Override
+	public Iterable<? extends HeaderItem> getDependencies() {
+		List<HeaderItem> dependencies = Lists.newArrayListWithExpectedSize(1);
+		dependencies.add(CssHeaderItem.forReference(JQueryUiCssResourceReference.get()));
+		return dependencies;
+	}
+	
+	public static StylesLessCssResourceReference get() {
+		return INSTANCE;
+	}
+}
