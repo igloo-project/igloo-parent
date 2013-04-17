421e7a4960bbc59aa2cb63188f4e4b8f5e848cb1
Julien Gonzalez <julien.gonzalez@openwide.fr>
2013-04-17 14:11:33 +0000

    Pas besoin de @Bindable la dessus.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2090 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/upgrade/model/IDataUpgrade.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/upgrade/model/IDataUpgrade.java
@@ -1,11 +1,8 @@
 package fr.openwide.core.jpa.more.business.upgrade.model;
 
-import org.bindgen.Bindable;
-
 import fr.openwide.core.jpa.exception.SecurityServiceException;
 import fr.openwide.core.jpa.exception.ServiceException;
 
-@Bindable
 public interface IDataUpgrade {
 	
 	/**
