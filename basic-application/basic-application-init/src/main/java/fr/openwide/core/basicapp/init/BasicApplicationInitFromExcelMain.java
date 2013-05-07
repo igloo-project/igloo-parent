6b0bc5564c9635021378382883e33c6f791dd41f
Guillaume Smet <guillaume.smet@gmail.com>
2013-05-07 08:58:49 +0000

    fermeture propre du contexte Spring avant de quitter
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@2109 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-init/src/main/java/fr/openwide/core/basicapp/init/BasicApplicationInitFromExcelMain.java
+++ basic-application/basic-application-init/src/main/java/fr/openwide/core/basicapp/init/BasicApplicationInitFromExcelMain.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import fr.openwide.core.basicapp.init.config.spring.BasicApplicationInitConfig;
@@ -18,20 +18,26 @@ public final class BasicApplicationInitFromExcelMain {
 	private static final Logger LOGGER = LoggerFactory.getLogger(BasicApplicationInitFromExcelMain.class);
 
 	public static void main(String[] args) throws ServiceException, SecurityServiceException, IOException {
-		ApplicationContext context = new AnnotationConfigApplicationContext(BasicApplicationInitConfig.class);
-		
-		SpringContextWrapper contextWrapper = context.getBean("springContextWrapper",
-				SpringContextWrapper.class);
-		
-		contextWrapper.openEntityManager();
-		contextWrapper.importDirectory(new File("src/main/resources/init"));
-		
-		contextWrapper.reindexAll();
-		contextWrapper.closeEntityManager();
-		
-		LOGGER.info("Initialization complete");
-		
-		System.exit(0);
+		ConfigurableApplicationContext context = null;
+		try {
+			context = new AnnotationConfigApplicationContext(BasicApplicationInitConfig.class);
+			
+			SpringContextWrapper contextWrapper = context.getBean("springContextWrapper",
+					SpringContextWrapper.class);
+			
+			contextWrapper.openEntityManager();
+			contextWrapper.importDirectory(new File("src/main/resources/init"));
+			
+			contextWrapper.reindexAll();
+			contextWrapper.closeEntityManager();
+			
+			LOGGER.info("Initialization complete");
+		} finally {
+			if (context != null) {
+				context.close();
+			}
+			System.exit(0);
+		}
 	}
 	
 	private BasicApplicationInitFromExcelMain() {
