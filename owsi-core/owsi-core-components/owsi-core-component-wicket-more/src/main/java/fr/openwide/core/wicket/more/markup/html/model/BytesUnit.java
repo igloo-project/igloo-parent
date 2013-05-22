5d7b296cce3234c77ac850b9df4bf44e2a14772c
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-05-22 15:40:16 +0000

    Ajout d'utilitaires liés aux tailles de fichiers.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2132 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/model/BytesUnit.java
@@ -0,0 +1,38 @@
+package fr.openwide.core.wicket.more.markup.html.model;
+
+import org.apache.wicket.util.lang.Bytes;
+
+public enum BytesUnit {
+	BYTE {
+		@Override public double toDouble(Bytes bytesValue) { return bytesValue.bytes(); }
+		@Override public Bytes fromDouble(double doubleValue) { return Bytes.bytes(doubleValue); }
+		@Override public Bytes fromLong(long longValue) { return Bytes.bytes(longValue); }
+	},
+	KILOBYTE {
+		@Override public double toDouble(Bytes bytesValue) { return bytesValue.kilobytes(); }
+		@Override public Bytes fromDouble(double doubleValue) { return Bytes.kilobytes(doubleValue); }
+		@Override public Bytes fromLong(long longValue) { return Bytes.kilobytes(longValue); }
+	},
+	MEGABYTE {
+		@Override public double toDouble(Bytes bytesValue) { return bytesValue.megabytes(); }
+		@Override public Bytes fromDouble(double doubleValue) { return Bytes.megabytes(doubleValue); }
+		@Override public Bytes fromLong(long longValue) { return Bytes.megabytes(longValue); }
+	},
+	GIGABYTE {
+		@Override public double toDouble(Bytes bytesValue) { return bytesValue.gigabytes(); }
+		@Override public Bytes fromDouble(double doubleValue) { return Bytes.gigabytes(doubleValue); }
+		@Override public Bytes fromLong(long longValue) { return Bytes.gigabytes(longValue); }
+	},
+	TERABYTE {
+		@Override public double toDouble(Bytes bytesValue) { return bytesValue.terabytes(); }
+		@Override public Bytes fromDouble(double doubleValue) { return Bytes.gigabytes(doubleValue); }
+		@Override public Bytes fromLong(long longValue) { return Bytes.gigabytes(longValue); }
+	};
+	
+	public abstract double toDouble(Bytes bytesValue);
+	
+	public abstract Bytes fromDouble(double doubleValue);
+	
+	public abstract Bytes fromLong(long longValue);
+
+}
