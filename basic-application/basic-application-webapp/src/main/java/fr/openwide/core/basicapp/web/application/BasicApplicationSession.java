f3da18a7c1b5d2dd9d343d257c51da27d39c88e0
Stephane Gleizes <stephane.gleizes@openwide.fr>
2013-06-06 09:29:09 +0000

    Suppression de la surcharge inutile de la méthode authenticate.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@2175 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/BasicApplicationSession.java
+++ basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/BasicApplicationSession.java
@@ -5,9 +5,6 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.Request;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import fr.openwide.core.basicapp.core.business.user.model.User;
 import fr.openwide.core.wicket.more.AbstractCoreSession;
@@ -25,15 +22,6 @@ public class BasicApplicationSession extends AbstractCoreSession<User> {
 	
 	public static BasicApplicationSession get() {
 		return (BasicApplicationSession) Session.get();
-	}
-	
-	@Override
-	public boolean authenticate(String username, String password) {
-		Authentication authentication = authenticationManager
-				.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		
-		return super.authenticate(username, password);
 	}
 	
 	public User getUser() {
