5ac5fef03f0db0de05bf62f7fd56caa85f699303
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-06-21 09:29:55 +0000

    Ajout de divers composants basés sur Select2 (remontés depuis Iota-Intranet).
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2205 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/util/convert/converters/LongIdGenericEntityConverter.java
@@ -0,0 +1,63 @@
+package fr.openwide.core.wicket.more.util.convert.converters;
+
+import java.util.Locale;
+
+import org.apache.wicket.injection.Injector;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.convert.converter.AbstractConverter;
+
+import fr.openwide.core.jpa.business.generic.model.GenericEntity;
+import fr.openwide.core.jpa.business.generic.service.IEntityService;
+import fr.openwide.core.spring.util.StringUtils;
+
+public class LongIdGenericEntityConverter<E extends GenericEntity<Long, ?>> extends AbstractConverter<E> {
+	
+	private static final long serialVersionUID = -6119059653808653326L;
+
+	private final Class<E> targetType;
+	
+	@SpringBean
+	private IEntityService entityService;
+
+	public LongIdGenericEntityConverter(Class<E> targetType) {
+		Injector.get().inject(this);
+		this.targetType = targetType;
+	}
+	
+	@Override
+	public String convertToString(E value, Locale locale) {
+		if (value == null) {
+			return null;
+		}
+		return convertToString(value.getId());
+	}
+
+	@Override
+	public E convertToObject(String value, Locale locale) {
+		if (!StringUtils.hasText(value)) {
+			return null;
+		}
+		return entityService.getEntity(targetType, convertToId(value, locale));
+	}
+	
+	private String convertToString(Long value) {
+		if (value == null) {
+			return null;
+		}
+		return String.valueOf(value);
+	}
+	
+	private Long convertToId(String value, Locale locale) {
+		try {
+			return Long.valueOf(value);
+		} catch (NumberFormatException e) {
+			throw newConversionException(e.getMessage(), value, locale); // NOSONAR : the Wicket API forces us to lose stacktrace
+		}
+	}
+
+	@Override
+	protected Class<E> getTargetType() {
+		return targetType;
+	}
+
+}
