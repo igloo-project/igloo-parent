61afb6067b422695fa55319c7e716cc9aa39da81
Guillaume Smet <guillaume.smet@gmail.com>
2013-07-15 08:14:59 +0000

    plus besoin de déclarer le getUser dans les PermissionEvaluator on le laisse final pour le moment mais on enlèvera sans doute le final par la suite
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2254 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa-security/src/test/java/fr/openwide/core/test/jpa/security/service/TestCorePermissionEvaluator.java
+++ owsi-core-components/owsi-core-component-jpa-security/src/test/java/fr/openwide/core/test/jpa/security/service/TestCorePermissionEvaluator.java
@@ -1,37 +1,15 @@
 package fr.openwide.core.test.jpa.security.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.acls.model.Permission;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import fr.openwide.core.jpa.security.service.AbstractCorePermissionEvaluator;
 import fr.openwide.core.test.jpa.security.business.person.model.MockPerson;
-import fr.openwide.core.test.jpa.security.business.person.service.IMockPersonService;
 
 public class TestCorePermissionEvaluator extends AbstractCorePermissionEvaluator<MockPerson> {
-	
-	@Autowired
-	private IMockPersonService personService;
 
 	@Override
 	protected boolean hasPermission(MockPerson user, Object targetDomainObject, Permission permission) {
 		return true;
-	}
-
-	@Override
-	protected MockPerson getUser(Authentication authentication) {
-		if (authentication == null) {
-			return null;
-		}
-
-		if (authentication.getPrincipal() instanceof UserDetails) {
-			UserDetails details = (UserDetails) authentication.getPrincipal();
-			String userName = details.getUsername();
-			return personService.getByUserName(userName);
-		}
-
-		return null;
 	}
 
 }
