cb31d573ce01736857300b795b4b6b184c472454
Guillaume Smet <guillaume.smet@gmail.com>
2013-07-16 12:51:11 +0000

    un peu de nettoyage et de mise en cohérence
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2262 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/business/authority/util/CoreAuthorityConstants.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/business/authority/util/CoreAuthorityConstants.java
@@ -1,5 +1,8 @@
 package fr.openwide.core.jpa.security.business.authority.util;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public class CoreAuthorityConstants {
 
 	public static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
@@ -11,6 +14,9 @@ public class CoreAuthorityConstants {
 	 */
 	public static final String ROLE_SYSTEM = "ROLE_SYSTEM";
 	public static final String RUN_AS_SYSTEM = "RUN_AS_SYSTEM";
+	
+	public static final GrantedAuthority AUTHORITY_SYSTEM = new SimpleGrantedAuthority(ROLE_SYSTEM);
+	public static final GrantedAuthority AUTHORITY_ADMIN = new SimpleGrantedAuthority(ROLE_ADMIN);
 	
 	protected CoreAuthorityConstants() {
 	}
