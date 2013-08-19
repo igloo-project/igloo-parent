7f067f8f6c678bc9af801fa8dc281912cba91d76
Guillaume Smet <guillaume.smet@gmail.com>
2013-08-19 18:20:35 +0000

    on allège le pattern du Configurer comme discuté avec Yoann il y a un moment
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@2349 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/config/application/BasicApplicationConfigurer.java
+++ basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/config/application/BasicApplicationConfigurer.java
@@ -4,11 +4,9 @@ import fr.openwide.core.spring.config.CoreConfigurer;
 
 public class BasicApplicationConfigurer extends CoreConfigurer {
 
-	private static final String PORTFOLIO_ITEMS_PER_PAGE = "portfolio.itemsPerPage";
-	
 	private static final Integer PORTFOLIO_ITEMS_PER_PAGE_DEFAULT_VALUE = 20;
 
 	public int getPortfolioItemsPerPage() {
-		return getPropertyAsInteger(PORTFOLIO_ITEMS_PER_PAGE, PORTFOLIO_ITEMS_PER_PAGE_DEFAULT_VALUE);
+		return getPropertyAsInteger("portfolio.itemsPerPage", PORTFOLIO_ITEMS_PER_PAGE_DEFAULT_VALUE);
 	}
 }
