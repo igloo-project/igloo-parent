c058657c1cf4e13ae69ab00ebdfb0b30c467f9a0
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-08-20 08:41:45 +0000

    Gestion de IComponentAssignedModel dans IReadOnlyModel.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2354 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/ReadOnlyModel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/ReadOnlyModel.java
@@ -2,15 +2,18 @@ package fr.openwide.core.wicket.more.model;
 
 import java.io.Serializable;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IComponentAssignedModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.IWrapModel;
 import org.apache.wicket.model.Model;
 
-public class ReadOnlyModel<T> extends AbstractReadOnlyModel<T> {
+public class ReadOnlyModel<T> extends AbstractReadOnlyModel<T> implements IComponentAssignedModel<T> {
 
 	private static final long serialVersionUID = -6272545665317639093L;
 	
-	private final IModel<? extends T> wrappedModel;
+	private final IModel<? extends T> readModel;
 	
 	public static <T> ReadOnlyModel<T> of(IModel<? extends T> model) {
 		return new ReadOnlyModel<T>(model);
@@ -20,20 +23,46 @@ public class ReadOnlyModel<T> extends AbstractReadOnlyModel<T> {
 		return new ReadOnlyModel<T>(Model.of(object));
 	}
 
-	protected ReadOnlyModel(IModel<? extends T> wrappedModel) {
+	protected ReadOnlyModel(IModel<? extends T> readModel) {
 		super();
-		this.wrappedModel = wrappedModel;
+		this.readModel = readModel;
 	}
 
 	@Override
 	public T getObject() {
-		return wrappedModel.getObject();
+		return readModel.getObject();
 	}
 	
 	@Override
 	public void detach() {
 		super.detach();
-		wrappedModel.detach();
+		readModel.detach();
+	}
+	
+	@Override
+	public IWrapModel<T> wrapOnAssignment(Component component) {
+		return new WrapModel(component);
+	}
+	
+	private class WrapModel extends ReadOnlyModel<T> implements IWrapModel<T> {
+		private static final long serialVersionUID = 7996314523359141428L;
+		
+		protected WrapModel(Component component) {
+			super(wrap(readModel, component));
+		}
+		
+		@Override
+		public IModel<?> getWrappedModel() {
+			return ReadOnlyModel.this;
+		}
+	}
+	
+	private static <T> IModel<? extends T> wrap(IModel<? extends T> model, Component component) {
+		if (model instanceof IComponentAssignedModel) {
+			return ((IComponentAssignedModel<? extends T>)model).wrapOnAssignment(component);
+		} else {
+			return model;
+		}
 	}
 
 }
