cf4e72b0e0a2f58d932ccc751642b9b684d07ba8
Guillaume Smet <guillaume.smet@gmail.com>
2013-08-28 13:30:03 +0000

    faisons en sorte qu'il puisse prendre plus d'un paramètre, ça va être pratique
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2417 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/builder/impl/CoreLinkDescriptorBuilderMandatoryParameterValidator.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/builder/impl/CoreLinkDescriptorBuilderMandatoryParameterValidator.java
@@ -1,7 +1,11 @@
 package fr.openwide.core.wicket.more.link.descriptor.builder.impl;
 
+import java.util.List;
+
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.lang.Args;
+
+import com.google.common.collect.Lists;
 
 import fr.openwide.core.wicket.more.link.descriptor.parameter.validator.ILinkParameterValidator;
 import fr.openwide.core.wicket.more.link.descriptor.parameter.validator.LinkParameterValidationErrorCollector;
@@ -10,17 +14,24 @@ public class CoreLinkDescriptorBuilderMandatoryParameterValidator implements ILi
 	
 	private static final long serialVersionUID = 7015800524943994171L;
 	
-	private final String parameterName;
+	private final List<String> parameterNames = Lists.newArrayListWithExpectedSize(1);
+	
+	public CoreLinkDescriptorBuilderMandatoryParameterValidator(String... parameterNames) {
+		this(Lists.newArrayList(parameterNames));
+	}
 
-	public CoreLinkDescriptorBuilderMandatoryParameterValidator(String parameterName) {
-		Args.notNull(parameterName, "parameterName");
-		this.parameterName = parameterName;
+	public CoreLinkDescriptorBuilderMandatoryParameterValidator(List<String> parameterNames) {
+		Args.notNull(parameterNames, "parameterNames");
+		Args.notEmpty(parameterNames, "parameterNames");
+		this.parameterNames.addAll(parameterNames);
 	}
 
 	@Override
 	public void validate(PageParameters parameters, LinkParameterValidationErrorCollector collector) {
-		if (parameters.get(parameterName).isNull()) {
-			collector.addError(String.format("Mandatory parameter '%s' was null.", parameterName));
+		for (String parameterName : parameterNames) {
+			if (parameters.get(parameterName).isNull()) {
+				collector.addError(String.format("Mandatory parameter '%s' was null.", parameterName));
+			}
 		}
 	}
 
