9f597d4fcf6a9aec4fb1fc7934a730fcd7bc12b1
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-08-29 11:44:33 +0000

    Corrections de commentaires et de casse sur le AbstractGenericEntityComparator.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2424 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/business/generic/util/AbstractGenericEntityComparator.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/business/generic/util/AbstractGenericEntityComparator.java
@@ -12,11 +12,11 @@ import fr.openwide.core.jpa.business.generic.model.GenericEntity;
 
 /**
  * A {@link GenericEntity} comparator that compares IDs.
- * <p>The {@link #GenericEntityComparator() default implementation}, returned by {@link #get()}, mimics the behavior of {@link GenericEntity#compareTo(GenericEntity)},
+ * <p>The {@link #AbstractGenericEntityComparator() default implementation} mimics the behavior of {@link GenericEntity#compareTo(GenericEntity)},
  * except that it is null-safe, both regarding entities and regarding their IDs. It assumes a null entity is <em>more</em>
  * than a non-null one, and that an entity with a null ID is <em>more</em> than an entity with a non-null ID. This behavior is consistent with
  * {@link GenericEntity#isNew()}: an entity without an id is new, hence more recent than an entity with an id, which is not new.
- * <p>This comparator is consistent with equals.
+ * <p>This comparator is consistent with equals as long as the {@link #equalsNotNullObjects(GenericEntity, GenericEntity)} method is not overridden.
  * <p><strong>WARNING:</strong> trying to compare two not-null entities with null IDs which are different according to
  * {@link #equalsNotNullObjects(GenericEntity, GenericEntity)} will result in an {@link IllegalArgumentException}
  * being thrown, since such objects are inherently incomparable. This behavior may be overridden in subclasses by
@@ -26,7 +26,7 @@ public abstract class AbstractGenericEntityComparator<K extends Comparable<K> & 
 
 	private static final long serialVersionUID = -5933751018161012653L;
 	
-	private final Comparator<? super K> KEY_COMPARATOR;
+	private final Comparator<? super K> keyComparator;
 
 	public AbstractGenericEntityComparator() {
 		this(false, Ordering.natural().nullsLast());
@@ -34,14 +34,14 @@ public abstract class AbstractGenericEntityComparator<K extends Comparable<K> & 
 
 	/**
 	 * @param nullIsLow Whether a null entity is lower than a non-null entity. This only applies to the entity object, not to its id.
-	 * @param keyComparator The key comparator.
+	 * @param keyComparator The {@link GenericEntity#getId() key} comparator.
 	 * 		Must be null-safe in order for this comparator to be null-safe.
 	 * 		Must be serializable in order for this comparator to be serializable.
 	 */
 	public AbstractGenericEntityComparator(boolean nullIsLow, Comparator<? super K> keyComparator) {
 		super(nullIsLow);
 		Assert.notNull(keyComparator);
-		this.KEY_COMPARATOR = keyComparator;
+		this.keyComparator = keyComparator;
 	}
 	
 	@Override
@@ -59,7 +59,7 @@ public abstract class AbstractGenericEntityComparator<K extends Comparable<K> & 
 		if (leftId == null && rightId == null) {
 			throw new IllegalArgumentException("Cannot compare two different entities with null IDs");
 		}
-		return KEY_COMPARATOR.compare(leftId, rightId);
+		return keyComparator.compare(leftId, rightId);
 	}
 
 }
