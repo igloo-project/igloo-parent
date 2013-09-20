48b946a93bd8bacc450c2bc69e266f5874d9b3e9
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-09-20 15:03:43 +0000

    Ajout de CommonParameters.TIMESTAMP.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2448 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/parameter/CommonParameters.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/parameter/CommonParameters.java
@@ -3,8 +3,10 @@ package fr.openwide.core.wicket.more.link.descriptor.parameter;
 public final class CommonParameters {
 	
 	public static final String ID = "id";
-	
 	public static final String NATURAL_ID = "naturalId";
+	
+	/** Used as an anti-cache parameter. */
+	public static final String TIMESTAMP = "t";
 	
 	private CommonParameters() {
 	}
