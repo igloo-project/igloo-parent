3633a95144454eaa5b560227d48c5b6c464639a3
Guillaume Smet <guillaume.smet@gmail.com>
2013-10-09 16:18:25 +0000

    ajustements sur la génération du titre de la page : principalement pour reporter les corrections faites dans Sitra et pour permettre d'avoir un chemin inversé facilement
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2468 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/AbstractWebPageTemplate.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/AbstractWebPageTemplate.java
@@ -7,10 +7,12 @@ import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.model.IComponentAssignedModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
 import fr.openwide.core.wicket.behavior.ClassAttributeAppender;
@@ -21,11 +23,19 @@ public abstract class AbstractWebPageTemplate extends CoreWebPage {
 
 	private static final long serialVersionUID = -5598937641577320345L;
 
-	private static final String META_TITLE_SEPARATOR = " › ";
+	private static final String DEFAULT_HEAD_PAGE_TITLE_SEPARATOR = " › ";
+	
+	private static final String DEFAULT_HEAD_PAGE_TITLE_SEPARATOR_REVERSE = " ‹ ";
 	
 	protected List<BreadCrumbElement> headPageTitleElements = Lists.newArrayList();
 	
 	protected List<BreadCrumbElement> breadCrumbElements = Lists.newArrayList();
+	
+	protected boolean headPageTitleReversed = false;
+	
+	protected String headPageTitleSeparator = DEFAULT_HEAD_PAGE_TITLE_SEPARATOR;
+	
+	protected String headPageTitleSeparatorReverse = DEFAULT_HEAD_PAGE_TITLE_SEPARATOR_REVERSE;
 	
 	public AbstractWebPageTemplate(PageParameters parameters) {
 		super(parameters);
@@ -94,22 +104,82 @@ public abstract class AbstractWebPageTemplate extends CoreWebPage {
 		this.headPageTitleElements.add(breadCrumbElement);
 	}
 	
+	protected void setHeadPageTitleReversed(boolean headPageTitleReversed) {
+		this.headPageTitleReversed = headPageTitleReversed;
+	}
+	
+	protected void setHeadPageTitleSeparator(String headPageTitleSeparator) {
+		this.headPageTitleSeparator = headPageTitleSeparator;
+	}
+	
+	protected void setHeadPageTitleSeparatorReverse(String headPageTitleSeparatorReverse) {
+		this.headPageTitleSeparatorReverse = headPageTitleSeparatorReverse;
+	}
+	
+	/**
+	 * This method is public so that Wicket's PropertyModel can find it.
+	 */
 	public String getHeadPageTitle() {
-		StringBuilder sb = new StringBuilder(getLocalizer().getString(getRootPageTitleLabelKey(), this));
+		StringBuilder sb = new StringBuilder();
+		
+		if (!headPageTitleReversed) {
+			sb.append(getLocalizer().getString(getRootPageTitleLabelKey(), this));
+		}
+		
 		if (headPageTitleElements.size() > 0) {
-			for (BreadCrumbElement pageTitleElement : headPageTitleElements) {
-				sb.append(META_TITLE_SEPARATOR);
-				sb.append(pageTitleElement.getLabel());
+			List<BreadCrumbElement> orderedHeadPageTitleElements;
+			String separator;
+			
+			if (headPageTitleReversed) {
+				orderedHeadPageTitleElements = ImmutableList.copyOf(headPageTitleElements).reverse();
+				separator = headPageTitleSeparatorReverse;
+			} else {
+				orderedHeadPageTitleElements = ImmutableList.copyOf(headPageTitleElements);
+				separator = headPageTitleSeparator;
+			}
+			for (BreadCrumbElement pageTitleElement : orderedHeadPageTitleElements) {
+				IModel<String> titleElementModel = pageTitleElement.getLabelModel();
+				if (titleElementModel instanceof IComponentAssignedModel) {
+					titleElementModel = wrap(titleElementModel);
+				}
+				if (sb.length() > 0) {
+					sb.append(separator);
+				}
+				sb.append(titleElementModel.getObject());
 			}
 		} else {
 			boolean oneElementBreadcrumb = (breadCrumbElements.size() == 1);
 			
-			for (BreadCrumbElement breadCrumbElement : breadCrumbElements) {
+			List<BreadCrumbElement> orderedBreadCrumbElements;
+			String separator;
+			
+			if (headPageTitleReversed) {
+				orderedBreadCrumbElements = ImmutableList.copyOf(breadCrumbElements).reverse();
+				separator = headPageTitleSeparatorReverse;
+			} else {
+				orderedBreadCrumbElements = ImmutableList.copyOf(breadCrumbElements);
+				separator = headPageTitleSeparator;
+			}
+			
+			for (BreadCrumbElement breadCrumbElement : orderedBreadCrumbElements) {
 				if (oneElementBreadcrumb || !getApplication().getHomePage().equals(breadCrumbElement.getPageClass())) {
-					sb.append(META_TITLE_SEPARATOR);
-					sb.append(breadCrumbElement.getLabel());
+					IModel<String> breadcrumbElementModel = breadCrumbElement.getLabelModel();
+					if (breadcrumbElementModel instanceof IComponentAssignedModel) {
+						breadcrumbElementModel = wrap(breadcrumbElementModel);
+					}
+					if (sb.length() > 0) {
+						sb.append(separator);
+					}
+					sb.append(breadcrumbElementModel.getObject());
 				}
 			}
+		}
+		
+		if (headPageTitleReversed) {
+			if (sb.length() > 0) {
+				sb.append(headPageTitleSeparatorReverse);
+			}
+			sb.append(getLocalizer().getString(getRootPageTitleLabelKey(), this));
 		}
 		
 		return sb.toString();
