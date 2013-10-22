eb8d49ebf1bc85bb500454dbed001c386cd42c36
Guillaume Smet <guillaume.smet@gmail.com>
2013-10-22 07:27:33 +0000

    BatchReport aurait dû être Serializable
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2489 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/report/BatchReportItem.java
+++ owsi-core/owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/report/BatchReportItem.java
@@ -1,10 +1,13 @@
 package fr.openwide.core.commons.util.report;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import fr.openwide.core.commons.util.CloneUtils;
 
-public class BatchReportItem {
+public class BatchReportItem implements Serializable {
+
+	private static final long serialVersionUID = -8250079955023459814L;
 
 	private String message;
 
