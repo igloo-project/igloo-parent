5f364a79d062e953cb6acd5ea035320a4a8cd62c
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-10-22 14:16:08 +0000

    LinkDescriptors : relâchement des contraintes sur le LinkDescriptorBuilder. On peut désormais écrire .map().map().validator().map().validator().
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2490 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/builder/state/IParameterMappingState.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/builder/state/IParameterMappingState.java
@@ -4,11 +4,15 @@ import org.apache.wicket.model.IModel;
 
 import fr.openwide.core.wicket.more.link.descriptor.ILinkDescriptor;
 import fr.openwide.core.wicket.more.link.descriptor.parameter.mapping.ILinkParameterMappingEntry;
+import fr.openwide.core.wicket.more.link.descriptor.parameter.validator.ILinkParameterValidator;
 
 public interface IParameterMappingState<L extends ILinkDescriptor> extends IValidatorState<L> {
 
 	<T> IAddedParameterMappingState<L> map(String parameterName, IModel<T> valueModel, Class<T> valueType);
 	
 	IAddedParameterMappingState<L> map(ILinkParameterMappingEntry parameterMappingEntry);
+	
+	@Override
+	IParameterMappingState<L> validator(ILinkParameterValidator validator);
 
 }
