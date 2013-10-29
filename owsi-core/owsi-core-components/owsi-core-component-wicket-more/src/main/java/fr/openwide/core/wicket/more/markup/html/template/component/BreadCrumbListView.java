471971a90815e154125208455bbafacb4627f2a0
Guillaume Smet <guillaume.smet@gmail.com>
2013-10-29 15:57:22 +0000

    ajustements sur les breadcrumbs
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2510 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/BreadCrumbListView.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/BreadCrumbListView.java
@@ -2,11 +2,13 @@ package fr.openwide.core.wicket.more.markup.html.template.component;
 
 import java.util.List;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 
+import fr.openwide.core.wicket.more.markup.html.basic.EnclosureBehavior;
 import fr.openwide.core.wicket.more.markup.html.template.model.BreadCrumbElement;
 
 public class BreadCrumbListView extends ListView<BreadCrumbElement> {
@@ -23,7 +25,10 @@ public class BreadCrumbListView extends ListView<BreadCrumbElement> {
 		separator.setVisible(item.getIndex() > 0);
 		item.add(separator);
 		
-		item.add(item.getModelObject().component("breadCrumbElement"));
+		Component breadCrumbLink = item.getModelObject().component("breadCrumbElement");
+		item.add(breadCrumbLink);
+		
+		item.add(new EnclosureBehavior().component(breadCrumbLink));
 	}
 	
 }
--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/LinkDescriptorBreadCrumbElementPanel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/component/LinkDescriptorBreadCrumbElementPanel.java
@@ -3,19 +3,21 @@ package fr.openwide.core.wicket.more.markup.html.template.component;
 import org.apache.wicket.markup.html.link.Link;
 
 import fr.openwide.core.wicket.markup.html.panel.GenericPanel;
+import fr.openwide.core.wicket.more.markup.html.basic.EnclosureBehavior;
 import fr.openwide.core.wicket.more.markup.html.template.model.BreadCrumbElement;
 
 public class LinkDescriptorBreadCrumbElementPanel extends GenericPanel<String> {
 
 	private static final long serialVersionUID = 5385792712763242343L;
-
+	
 	public LinkDescriptorBreadCrumbElementPanel(String id, BreadCrumbElement breadCrumbElement) {
 		super(id, breadCrumbElement.getLabelModel());
 		
-		Link<Void> link = breadCrumbElement.getLinkDescriptor().link("breadCrumbElementLink").setAutoHideIfInvalid(true);
-		link.setBody(getModel());
+		Link<Void> breadCrumbLink = breadCrumbElement.getLinkDescriptor().link("breadCrumbElementLink").setAutoHideIfInvalid(true);
+		breadCrumbLink.setBody(getModel());
+		add(breadCrumbLink);
 		
-		add(link);
+		add(new EnclosureBehavior().component(breadCrumbLink));
 	}
-
+	
 }
