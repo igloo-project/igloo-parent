cd23ad12d07f55ca17208f6f05a96115b806d235
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-11-18 09:39:47 +0000

    Support des feuilles de style CSS (côté serveur) pour les notifications mail : gestion de la spécificité des sélecteurs et, en passant (ça ne coûte rien), du modificateur !important.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2564 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/notification/service/PhlocCssHtmlNotificationCssRegistry.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/notification/service/PhlocCssHtmlNotificationCssRegistry.java
@@ -1,43 +1,51 @@
 package fr.openwide.core.wicket.more.notification.service;
 
+import java.text.Collator;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.wicket.markup.ComponentTag;
 import org.springframework.util.Assert;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Ordering;
 import com.phloc.css.CCSS;
 import com.phloc.css.ECSSVersion;
 import com.phloc.css.ICSSWriterSettings;
 import com.phloc.css.decl.CSSDeclaration;
 import com.phloc.css.decl.CSSSelector;
+import com.phloc.css.decl.CSSSelectorSimpleMember;
 import com.phloc.css.decl.CSSStyleRule;
 import com.phloc.css.decl.CascadingStyleSheet;
+import com.phloc.css.decl.ICSSSelectorMember;
 import com.phloc.css.writer.CSSWriterSettings;
 
-import fr.openwide.core.commons.util.ordering.AbstractNullSafeComparator;
+import fr.openwide.core.commons.util.ordering.SerializableCollator;
 import fr.openwide.core.wicket.more.notification.service.IHtmlNotificationCssService.IHtmlNotificationCssRegistry;
+import fr.openwide.core.wicket.more.notification.service.impl.CssDeclarationPrecedence;
+import fr.openwide.core.wicket.more.notification.service.impl.CssSelectorSpecificity;
 import fr.openwide.core.wicket.more.notification.service.impl.PhlocCssMatchableComponentTag;
 
 public class PhlocCssHtmlNotificationCssRegistry implements IHtmlNotificationCssRegistry {
+	private static Comparator<String> CSS_PROPERTY_NAME_COLLATOR;
+	static {
+		SerializableCollator collator = new SerializableCollator(Locale.ROOT);
+		collator.setStrength(Collator.SECONDARY);
+		CSS_PROPERTY_NAME_COLLATOR = collator;
+	}
 	
 	private static final ICSSWriterSettings STYLE_ATTRIBUTE_WRITER_SETTINGS;
 	static {
 		CSSWriterSettings settings = new CSSWriterSettings(ECSSVersion.CSS30, true);
 		STYLE_ATTRIBUTE_WRITER_SETTINGS = settings;
 	}
-	
-	private static final Comparator<CSSDeclaration> CSS_DECLARATION_PROPERTY_COMPARATOR = new AbstractNullSafeComparator<CSSDeclaration>() {
-		private static final long serialVersionUID = 1L;
-		@Override
-		protected int compareNotNullObjects(CSSDeclaration left, CSSDeclaration right) {
-			return left.getProperty().compareTo(right.getProperty());
-		}
-	};
 
 	private final CascadingStyleSheet styleSheet;
 	
@@ -50,17 +58,23 @@ public class PhlocCssHtmlNotificationCssRegistry implements IHtmlNotificationCss
 	@Override
 	public String getStyle(ComponentTag tag) {
 		PhlocCssMatchableComponentTag matchableTag = new PhlocCssMatchableComponentTag(tag);
-		Map<CSSStyleRule, Collection<CSSSelector>> matchedRules = getMatchingRules(matchableTag);
+		Map<CSSStyleRule, CssSelectorSpecificity> matchedRules = getMatchedRules(matchableTag);
 		Collection<CSSDeclaration> declarations = mergeRules(matchedRules);
 		return buildString(declarations);
 	}
 
-	private Map<CSSStyleRule, Collection<CSSSelector>> getMatchingRules(PhlocCssMatchableComponentTag matchableTag) {
-		Map<CSSStyleRule, Collection<CSSSelector>> matchedRules = Maps.newHashMap();
+	private Map<CSSStyleRule, CssSelectorSpecificity> getMatchedRules(PhlocCssMatchableComponentTag matchableTag) {
+		Map<CSSStyleRule, CssSelectorSpecificity> matchedRules = Maps.newHashMap();
 		for (CSSStyleRule rule : styleSheet.getAllStyleRules()) {
 			Collection<CSSSelector> matchedSelectors = getMatchedSelectors(matchableTag, rule);
-			if (!matchedSelectors.isEmpty()) {
-				matchedRules.put(rule, matchedSelectors);
+			Collection<CssSelectorSpecificity> matchedSelectorsSpecificities = Collections2.transform(matchedSelectors, new Function<CSSSelector, CssSelectorSpecificity>() {
+				@Override
+				public CssSelectorSpecificity apply(CSSSelector input) {
+					return computeSpecificity(input);
+				}
+			});
+			if (!matchedSelectorsSpecificities.isEmpty()) {
+				matchedRules.put(rule, Ordering.natural().max(matchedSelectorsSpecificities));
 			}
 		}
 		return matchedRules;
@@ -76,33 +90,57 @@ public class PhlocCssHtmlNotificationCssRegistry implements IHtmlNotificationCss
 		return matchedSelectors;
 	}
 
-	private Collection<CSSDeclaration> mergeRules(Map<CSSStyleRule, Collection<CSSSelector>> matchedRules) {
-		Map<CSSDeclaration, Integer> mergedDeclarations = Maps.newTreeMap(CSS_DECLARATION_PROPERTY_COMPARATOR);
+	private Collection<CSSDeclaration> mergeRules(Map<CSSStyleRule, CssSelectorSpecificity> matchedRules) {
+		// Maps property names to the elected CSS declaration and its precedence
 		
-		for (Map.Entry<CSSStyleRule, Collection<CSSSelector>> entry : matchedRules.entrySet()) {
-			int score = 0;
-			// Compute max. score
-			for (CSSSelector selector : entry.getValue()) {
-				int selectorScore = computeScore(selector);
-				if (selectorScore > score) {
-					score = selectorScore;
-				}
-			}
+		Map<String, Pair<CSSDeclaration, CssDeclarationPrecedence>> mergedDeclarations = Maps.newTreeMap(CSS_PROPERTY_NAME_COLLATOR);
+		
+		for (Map.Entry<CSSStyleRule, CssSelectorSpecificity> entry : matchedRules.entrySet()) {
+			final CssSelectorSpecificity specificity = entry.getValue();
+			
 			// Merge declarations if the score is high enough
-			for (CSSDeclaration declaration : entry.getKey().getAllDeclarations()) {
-				Integer currentScore = mergedDeclarations.get(declaration);
-				if (currentScore == null || score >= currentScore) {
-					mergedDeclarations.put(declaration, score);
+			for (CSSDeclaration currentDeclaration : entry.getKey().getAllDeclarations()) {
+				String propertyName = currentDeclaration.getProperty();
+				CssDeclarationPrecedence currentDeclarationPrecedence = new CssDeclarationPrecedence(currentDeclaration.isImportant(), specificity);
+				
+				Pair<CSSDeclaration, CssDeclarationPrecedence> electedDeclaration = mergedDeclarations.get(propertyName);
+				if (electedDeclaration == null || electedDeclaration.getRight().compareTo(currentDeclarationPrecedence) <= 0) {
+					mergedDeclarations.put(propertyName, Pair.of(currentDeclaration, currentDeclarationPrecedence));
 				}
 			}
 		}
-		return mergedDeclarations.keySet();
+		
+		return Collections2.transform(mergedDeclarations.values(), new Function<Pair<CSSDeclaration, CssDeclarationPrecedence>, CSSDeclaration>() {
+			@Override
+			public CSSDeclaration apply(Pair<CSSDeclaration, CssDeclarationPrecedence> input) {
+				return input.getLeft();
+			}
+		});
 	}
 	
-	private int computeScore(CSSSelector selector) {
-		// TODO compute scores for each selector, so that we only return one declaration per property when merging the rules
-		// (e.g. not 2 'background-color' declarations)
-		return 0;
+	private CssSelectorSpecificity computeSpecificity(CSSSelector selector) {
+		int idSelectors = 0;
+		int classAndPseudoClassSelectors = 0;
+		int typeSelectorsAndPseudoElements = 0;
+		
+		for (ICSSSelectorMember member : selector.getAllMembers()) {
+			if (member instanceof CSSSelectorSimpleMember && !((CSSSelectorSimpleMember) member).isPseudo()) {
+				CSSSelectorSimpleMember simpleMember = (CSSSelectorSimpleMember) member;
+				if (simpleMember.isClass()) {
+					++classAndPseudoClassSelectors;
+				} else if (simpleMember.isElementName()) {
+					++typeSelectorsAndPseudoElements;
+				} else if (simpleMember.isHash()) {
+					++idSelectors;
+				} else {
+					throw new IllegalStateException("Unexpected type for simple selector member " + simpleMember);
+				}
+			} else {
+				throw new IllegalStateException("Unexpected type for selector member " + member);
+			}
+		}
+		
+		return new CssSelectorSpecificity(idSelectors, classAndPseudoClassSelectors, typeSelectorsAndPseudoElements);
 	}
 
 	private String buildString(Collection<CSSDeclaration> declarations) {
