4d7dedce268634b8cbfa4318c84144d39a011c89
Renaud Joly <joly.renaud@gmail.com>
2013-11-19 17:30:11 +0000

    Feedback : possibilité de le positionner en bas avec data-placement="bottom".
    See #129.
    
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2585 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/alert/jquery.alert.js
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/alert/jquery.alert.js
@@ -6,10 +6,15 @@
 	$.fn.alert = {
 		show: function(selector, autohideDelay) {
 			var $selector = $(selector);
+			var placement = $selector.data("placement");
 			var $panel = $(".alert-global-panel", $selector);
 			$panel.off("click.alert");
 			if ($(".alert-info, .alert-warning, .alert-error, .alert-success" , $panel).size() > 0) {
-				$panel.animate({ top: 0 }, 500);
+				if (placement == "bottom") {
+					$panel.animate({ bottom: 0 }, 500);
+				} else {
+					$panel.animate({ top: 0 }, 500);
+				}
 				
 				if ($(".alert-info, .alert-warning, .alert-error", $panel).size() == 0) {
 					$panel.on("click.alert", function(event) { $.fn.alert.close(event); });
@@ -17,16 +22,25 @@
 						autohideDelay = 5000;
 					}
 					if (autohideDelay >= 0) {
-						$panel.delay(autohideDelay).animate({ top: -$panel.outerHeight() });
+						if (placement == "bottom") {
+							$panel.delay(autohideDelay).animate({ bottom: -$panel.outerHeight() });
+						} else {
+							$panel.delay(autohideDelay).animate({ top: -$panel.outerHeight() });
+						}
 					}
 				}
 			}
 		},
 		hide: function(selector) {
 			var $selector = $(selector);
+			var placement = $selector.data("placement");
 			var $panel = $(".alert-global-panel", $selector);
 			if ($(".alert-info, .alert-warning, .alert-error, .alert-success" , $panel).size() > 0) {
-				$panel.css({ top: -$panel.outerHeight() });
+				if (placement == "bottom") {
+					$panel.css({ bottom: -$panel.outerHeight() });
+				} else {
+					$panel.css({ top: -$panel.outerHeight() });
+				}
 			}
 		},
 		reset: function(selector, autohideDelay) {
@@ -36,8 +50,13 @@
 		},
 		close: function(event) {
 			var $panel = $(event.target).closest(".alert-global-panel");
+			var placement = $(event.target).closest(".animated-global-feedback").data("placement");
 			if ($(".alert-info, .alert-warning, .alert-error, .alert-success" , $panel).size() > 0) {
-				$panel.stop(true).animate({ top: -$panel.outerHeight() });
+				if (placement == "bottom") {
+					$panel.stop(true).animate({ bottom: -$panel.outerHeight() });
+				} else {
+					$panel.stop(true).animate({ top: -$panel.outerHeight() });
+				}
 			}
 			event.preventDefault();
 		}
