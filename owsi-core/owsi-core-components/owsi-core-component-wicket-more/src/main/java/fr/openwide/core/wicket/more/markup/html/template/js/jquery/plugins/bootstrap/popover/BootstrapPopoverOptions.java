eb1884ee5d532e0d9607661a2c9d61144ba5571b
Renaud Joly <joly.renaud@gmail.com>
2013-11-20 09:39:34 +0000

    Popover : ajout d'un bouton de fermeture.
    Voir https://projects.openwide.fr/services/svn/trac/sitra/ticket/1139
    
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2588 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/bootstrap/popover/BootstrapPopoverOptions.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/bootstrap/popover/BootstrapPopoverOptions.java
@@ -14,6 +14,12 @@ public class BootstrapPopoverOptions extends Options {
 
 	private static final long serialVersionUID = 680573363463468690L;
 
+	// OWSI-Core options
+	private Component component;
+
+	private boolean addCloseButton = true;
+
+	// Bootstrap popover options
 	private Boolean animation;
 
 	private Boolean html;
@@ -77,6 +83,25 @@ public class BootstrapPopoverOptions extends Options {
 		return super.getJavaScriptOptions();
 	}
 
+	// OWSI-Core options
+	public Component getComponent() {
+		return component;
+	}
+
+	public void setComponent(Component component) {
+		component.setOutputMarkupId(true);
+		this.component = component;
+	}
+
+	public boolean isAddCloseButton() {
+		return addCloseButton;
+	}
+
+	public void setAddCloseButton(boolean addCloseButton) {
+		this.addCloseButton = addCloseButton;
+	}
+
+	// Bootstrap popover options
 	public Boolean getAnimation() {
 		return animation;
 	}
@@ -117,12 +142,38 @@ public class BootstrapPopoverOptions extends Options {
 		this.trigger = trigger;
 	}
 
+	protected void setTitleFunction(final JsStatement titleComponentStatement) {
+		setTitleFunction(new JsScope() {
+			private static final long serialVersionUID = 1L;
+			@Override
+			protected void execute(JsScopeContext scopeContext) {
+				scopeContext.append("var titleComponent = " + titleComponentStatement.render(false) + ";");
+				if (addCloseButton && component != null) {
+					scopeContext.append("titleComponent.append($('<a class=\"close\">&times;</a>').on('click', function(e) { "
+							+ new JsStatement().$(component).chain("popover", "'toggle'").render() + " e.preventDefault();"
+							+ " }));");
+				}
+				scopeContext.append("return titleComponent;");
+			}
+		});
+	}
+
 	public String getTitleText() {
 		return titleText;
 	}
 
+	/**
+	 * <strong>Attention</strong> : utiliser {@link #setComponent(Component)} avant {@link #setTitleText(String)}
+	 * si on veut ajouter un bouton de fermeture.
+	 */
 	public void setTitleText(String titleText) {
-		this.titleText = titleText;
+		// Si on veut ajouter un bouton de fermeture, on doit passer par une fonction,
+		// sinon on passe par l'option titleText classique.
+		if (addCloseButton && component != null) {
+			setTitleFunction(new JsStatement().$(null, "<span></span>").chain("html", JsUtils.quotes(titleText)));
+		} else {
+			this.titleText = titleText;
+		}
 	}
 
 	public JsScope getTitleFunction() {
@@ -133,15 +184,13 @@ public class BootstrapPopoverOptions extends Options {
 		this.titleFunction = titleFunction;
 	}
 
+	/**
+	 * <strong>Attention</strong> : utiliser {@link #setComponent(Component)} avant {@link #setTitleComponent(Component)}
+	 * si on veut ajouter un bouton de fermeture.
+	 */
 	public void setTitleComponent(final Component titleComponent) {
 		titleComponent.setOutputMarkupId(true);
-		setTitleFunction(new JsScope() {
-			private static final long serialVersionUID = 1L;
-			@Override
-			protected void execute(JsScopeContext scopeContext) {
-				scopeContext.append("return " + new JsStatement().$(titleComponent).chain("html").render());
-			}
-		});
+		setTitleFunction(new JsStatement().$(titleComponent).chain("html"));
 	}
 
 	public String getContentText() {
