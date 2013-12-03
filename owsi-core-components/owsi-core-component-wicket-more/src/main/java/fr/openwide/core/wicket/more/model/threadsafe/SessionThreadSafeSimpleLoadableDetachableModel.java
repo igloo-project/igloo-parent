c3152eeff675c36e3e426dbb55b37c00593c0b49
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-12-03 15:54:29 +0000

    Travail sur les SessionThreadSafe*LoadableDetachableModel pour simplifier les usages les plus courants.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2626 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/threadsafe/SessionThreadSafeSimpleLoadableDetachableModel.java
@@ -0,0 +1,63 @@
+package fr.openwide.core.wicket.more.model.threadsafe;
+
+import org.apache.wicket.Session;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+
+import fr.openwide.core.wicket.more.model.threadsafe.impl.AbstractThreadSafeLoadableDetachableModel;
+import fr.openwide.core.wicket.more.model.threadsafe.impl.LoadableDetachableModelThreadContext;
+
+/**
+ * An implementation of {@link LoadableDetachableModel} that is thread-safe, and may thus be used in multiple request cycles at the same time.
+ * <p>When in doubt, use this class as a basis for your thread-safe loadable/detachable model.
+ * <p>This class should be used when :
+ * <ul>
+ * 	<li>the model is stored in a global object, such as the {@link Session wicket session}
+ * 	<li>the serializable state of this loadable/detachable model is empty (no non-transient member) or not directly related to the model object (for instance, it is contained in a member whose type is {@link IModel}).
+ * </ul>
+ *
+ * @param <T> The type of the model object.
+ */
+public abstract class SessionThreadSafeSimpleLoadableDetachableModel<T>
+		extends AbstractThreadSafeLoadableDetachableModel<T, SessionThreadSafeSimpleLoadableDetachableModel<T>.ThreadContextImpl> {
+	
+	private static final long serialVersionUID = 6859907414385876596L;
+
+	protected class ThreadContextImpl extends LoadableDetachableModelThreadContext<T> { }
+	
+	public SessionThreadSafeSimpleLoadableDetachableModel() { }
+
+	public SessionThreadSafeSimpleLoadableDetachableModel(T object) {
+		setObject(object);
+	}
+	
+	@Override
+	protected final ThreadContextImpl newThreadContext() {
+		return new ThreadContextImpl();
+	}
+	
+	@Override
+	protected final void onSetObject(ThreadContextImpl threadContext) {
+		// No need to override this in subclasses
+	}
+	
+	@Override
+	protected final T load(ThreadContextImpl threadContext) {
+		return load();
+	}
+
+	/**
+	 * Loads the model object value from the implementation-defined data source.
+	 */
+	protected abstract T load();
+	
+	@Override
+	protected final void onDetach(ThreadContextImpl threadContext) {
+		onDetach();
+	}
+
+	protected void onDetach() {
+		// Does nothing by default
+	}
+
+}
