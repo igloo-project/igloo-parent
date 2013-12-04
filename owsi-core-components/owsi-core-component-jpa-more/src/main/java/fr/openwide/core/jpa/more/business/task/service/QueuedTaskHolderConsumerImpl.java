5c8787b86e4845ef945c2296e2b55864ead5fc8e
Guillaume Smet <guillaume.smet@gmail.com>
2013-12-04 23:39:18 +0000

    injection nommée de l'ObjectMapper
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2638 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/task/service/QueuedTaskHolderConsumerImpl.java
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/task/service/QueuedTaskHolderConsumerImpl.java
@@ -8,6 +8,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -32,6 +33,7 @@ public class QueuedTaskHolderConsumerImpl implements IQueuedTaskHolderConsumer {
 	private IQueuedTaskHolderService queuedTaskHolderService;
 
 	@Autowired
+	@Qualifier("queuedTaskHolderObjectMapper")
 	private ObjectMapper queuedTaskHolderObjectMapper;
 
 	@Autowired
