e0282c9c3446ca6a630fe84e0b71c6a845c991e2
Yoann Rodiere <yoann.rodiere@openwide.fr>
2013-12-04 08:37:49 +0000

    SessionThreadSafeModel : ajout de doc.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2631 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/threadsafe/SessionThreadSafeModel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/threadsafe/SessionThreadSafeModel.java
@@ -3,8 +3,20 @@ package fr.openwide.core.wicket.more.model.threadsafe;
 import java.io.Serializable;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.apache.wicket.Session;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
+/**
+ * An alternative implementation of {@link Model} that is thread-safe, and may thus be used in multiple request cycles at the same time.
+ * <p>This class should be used when :
+ * <ul>
+ * 	<li>the model is stored in a global object, such as the {@link Session wicket session}
+ * 	<li>the model need not maintain a cache (on contrary to loadable/detachable models such as {@link SessionThreadSafeSimpleLoadableDetachableModel} or {@link SessionThreadSafeDerivedSerializableStateLoadableDetachableModel}).
+ * </ul>
+ * <p><strong>WARNING:</strong> Access to members of the model object is obviously not secured by this model in any way, so writing to the model
+ * object is <strong>NOT</strong> thread-safe, unless the object implements thread safety.
+ */
 public class SessionThreadSafeModel<T extends Serializable> implements IModel<T> {
 	
 	private static final long serialVersionUID = 4954375223101982404L;
