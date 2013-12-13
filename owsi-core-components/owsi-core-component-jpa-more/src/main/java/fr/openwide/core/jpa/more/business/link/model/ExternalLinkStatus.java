90884c2fab0604e5a33eaaa2efc612884c84563b
Guillaume Smet <guillaume.smet@gmail.com>
2013-12-13 11:00:24 +0000

    ajustements sur la gestion des liens externes
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2664 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/link/model/ExternalLinkStatus.java
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/link/model/ExternalLinkStatus.java
@@ -3,5 +3,6 @@ package fr.openwide.core.jpa.more.business.link.model;
 public enum ExternalLinkStatus {
 	ONLINE,
 	OFFLINE,
-	DEAD_LINK
+	DEAD_LINK,
+	DELETED // Hibernate has a lot of problem to cascade the removal so let's introduce a specific status for that
 }
