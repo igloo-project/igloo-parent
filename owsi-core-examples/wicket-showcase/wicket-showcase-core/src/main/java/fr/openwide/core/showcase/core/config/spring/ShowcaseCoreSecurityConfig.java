4bde9655adf00648387ff59b8285ad05f6dd5d6e
Renaud Joly <joly.renaud@gmail.com>
2013-12-16 09:51:37 +0000

    suppression d'injections qui causaient des dépendances circulaires et empêchaient l'initialisation du transactionnel
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2676 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-examples/wicket-showcase/wicket-showcase-core/src/main/java/fr/openwide/core/showcase/core/config/spring/ShowcaseCoreSecurityConfig.java
+++ owsi-core-examples/wicket-showcase/wicket-showcase-core/src/main/java/fr/openwide/core/showcase/core/config/spring/ShowcaseCoreSecurityConfig.java
@@ -1,6 +1,5 @@
 package fr.openwide.core.showcase.core.config.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -9,19 +8,11 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import fr.openwide.core.jpa.security.config.spring.AbstractJpaSecuritySecuredConfig;
 import fr.openwide.core.jpa.security.service.AuthenticationUserNameComparison;
 import fr.openwide.core.jpa.security.service.ICorePermissionEvaluator;
-import fr.openwide.core.jpa.security.service.ISecurityService;
-import fr.openwide.core.showcase.core.business.user.service.IUserService;
 import fr.openwide.core.showcase.core.security.service.ShowcasePermissionEvaluator;
 
 @Configuration
 public class ShowcaseCoreSecurityConfig extends AbstractJpaSecuritySecuredConfig {
 	
-	@Autowired
-	private ISecurityService securityService;
-
-	@Autowired
-	private IUserService userService;
-
 	@Bean
 	@Override
 	public AuthenticationUserNameComparison authenticationUserNameComparison() {
