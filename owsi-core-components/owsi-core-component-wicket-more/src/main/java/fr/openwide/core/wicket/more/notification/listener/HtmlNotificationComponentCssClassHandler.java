f8a820900583313e4b4f2bd64f2941eeb282b1cd
Julien Gonzalez <julien.gonzalez@openwide.fr>
2014-01-10 18:14:08 +0000

    Notification HTML : un behavior n'était pas serializable, ce qui faisait une vilaine stackTrace dans les log.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2718 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/notification/listener/HtmlNotificationComponentCssClassHandler.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/notification/listener/HtmlNotificationComponentCssClassHandler.java
@@ -2,18 +2,12 @@ package fr.openwide.core.wicket.more.notification.listener;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.application.IComponentInitializationListener;
-import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.injection.Injector;
-import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.springframework.util.StringUtils;
 
-import fr.openwide.core.jpa.exception.ServiceException;
 import fr.openwide.core.wicket.more.notification.service.IHtmlNotificationCssService;
 
 public class HtmlNotificationComponentCssClassHandler implements IComponentInitializationListener {
-	
-	private static final String CSS_STYLE_ATTRIBUTE = "style";
 
 	@SpringBean
 	private IHtmlNotificationCssService cssService;
@@ -25,25 +19,7 @@ public class HtmlNotificationComponentCssClassHandler implements IComponentIniti
 	@Override
 	public void onInitialize(Component component) {
 		if (cssService.hasRegistry(component.getVariation())) {
-			component.add(new Behavior() {
-				private static final long serialVersionUID = 1L;
-				
-				@Override
-				public void onComponentTag(Component component, ComponentTag tag) {
-					try {
-						IHtmlNotificationCssService.IHtmlNotificationCssRegistry cssRegistry = cssService.getRegistry(component.getVariation());
-						if (cssRegistry != null) {
-							String appendedStyle = cssRegistry.getStyle(tag);
-							if (StringUtils.hasText(appendedStyle)) {
-								tag.append(CSS_STYLE_ATTRIBUTE, appendedStyle, "");
-							}
-						}
-					} catch (ServiceException e) {
-						throw new IllegalStateException(e);
-					}
-				}
-			});
+			component.add(new HtmlNotificationComponentCssClassHandlerBehavior(cssService));
 		}
 	}
-
 }
