49ab6070734fbf7bb158a20370a123f4dd45bfcb
Guillaume Smet <guillaume.smet@gmail.com>
2014-01-15 16:33:16 +0000

    ajustements pour permettre d'avoir plusieurs pages possibles pour le retour de navigation
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2727 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/impl/DynamicBookmarkablePageInstanceLink.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/impl/DynamicBookmarkablePageInstanceLink.java
@@ -2,9 +2,6 @@ package fr.openwide.core.wicket.more.link.descriptor.impl;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.Session;
-import org.apache.wicket.core.request.handler.PageProvider;
-import org.apache.wicket.core.request.handler.RenderPageRequestHandler;
-import org.apache.wicket.model.IModel;
 
 import fr.openwide.core.wicket.more.link.descriptor.AbstractDynamicBookmarkableLink;
 import fr.openwide.core.wicket.more.link.descriptor.LinkInvalidTargetRuntimeException;
@@ -14,52 +11,32 @@ public class DynamicBookmarkablePageInstanceLink extends AbstractDynamicBookmark
 	
 	private static final long serialVersionUID = 3375563675399910552L;
 	
-	private final IModel<? extends Page> pageInstanceModel;
+	private final CorePageInstanceLinkGenerator pageInstanceLinkGenerator;
 
-	private final IModel<? extends Class<? extends Page>> expectedPageClassModel;
-	
-	public DynamicBookmarkablePageInstanceLink(String wicketId,
-			IModel<? extends Page> pageInstanceModel,
-			IModel<? extends Class<? extends Page>> expectedPageClassModel) {
+	public DynamicBookmarkablePageInstanceLink(String wicketId, CorePageInstanceLinkGenerator pageInstanceLinkGenerator) {
 		super(wicketId);
-		this.pageInstanceModel = pageInstanceModel;
-		this.expectedPageClassModel = expectedPageClassModel;
-	}
-
-	protected final Page getPageInstance() {
-		return pageInstanceModel.getObject();
+		this.pageInstanceLinkGenerator = pageInstanceLinkGenerator;
 	}
 
 	@Override
 	protected boolean isValid() {
-		Page pageInstance = getPageInstance();
-		Class<? extends Page> expectedPageClass = expectedPageClassModel.getObject();
-		return getPageInstance() != null
-				&& (expectedPageClass == null || expectedPageClass.isAssignableFrom(pageInstance.getClass()))
-				&& Session.get().getAuthorizationStrategy().isActionAuthorized(pageInstance, Page.RENDER);
+		return pageInstanceLinkGenerator.isAccessible();
 	}
 
 	@Override
 	protected CharSequence getRelativeURL() throws LinkInvalidTargetRuntimeException,
 			LinkParameterValidationRuntimeException {
-		Page pageInstance = getPageInstance();
-		if (pageInstance == null) {
-			throw new LinkInvalidTargetRuntimeException("The target page instance of a link of type '" + getClass() + "' was null when trying to render the URL.");
-		}
-		Class<? extends Page> expectedPageClass = expectedPageClassModel.getObject();
-		if (expectedPageClass != null && !expectedPageClass.isAssignableFrom(pageInstance.getClass())) {
-			throw new LinkInvalidTargetRuntimeException("The target page instance '" + pageInstance + "' had unexpected type when trying to render the URL : got " + pageInstance.getClass().getName() + ", expected " + expectedPageClass.getName());
-		}
+		Page pageInstance = pageInstanceLinkGenerator.getValidPageInstance();
 		if (!Session.get().getAuthorizationStrategy().isActionAuthorized(pageInstance, Page.RENDER)) {
 			throw new LinkInvalidTargetRuntimeException("The rendering of the target page '" + pageInstance + "' was not authorized when trying to render the URL.");
 		}
-		return urlFor(new RenderPageRequestHandler(new PageProvider(pageInstance)));
+		return pageInstanceLinkGenerator.relativeUrl();
 	}
 	
 	@Override
 	protected void onDetach() {
 		super.onDetach();
-		pageInstanceModel.detach();
+		pageInstanceLinkGenerator.detach();
 	}
 
 }
