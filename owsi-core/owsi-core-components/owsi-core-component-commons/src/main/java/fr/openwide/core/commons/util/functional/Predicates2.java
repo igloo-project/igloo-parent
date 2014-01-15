82ac18ab483ad0ed02aa4eaa7e121cfbe82ca877
Guillaume Smet <guillaume.smet@gmail.com>
2014-01-15 15:34:10 +0000

    ajout d'un Predicate pour le hasText (on utilise en dessous commons lang3 mais on a un fonctionnement équivalent au hasText de Spring qu'on utilise plus)
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2726 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/functional/Predicates2.java
+++ owsi-core/owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/functional/Predicates2.java
@@ -3,6 +3,8 @@ package fr.openwide.core.commons.util.functional;
 import java.io.Serializable;
 import java.util.Collection;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.google.common.base.Predicate;
 
 public final class Predicates2 {
@@ -15,6 +17,10 @@ public final class Predicates2 {
 	
 	public static <T extends Collection<?>> Predicate<T> notEmpty() {
 		return CollectionPredicate.NOT_EMPTY.withNarrowedType();
+	}
+	
+	public static Predicate<String> hasText() {
+		return StringPredicate.HAS_TEXT;
 	}
 
 	private enum CollectionPredicate implements Predicate<Collection<?>>, Serializable {
@@ -34,6 +40,15 @@ public final class Predicates2 {
 		@SuppressWarnings("unchecked") // these Collection predicates work for any T that extends Collection<?>
 		<T extends Collection<?>> Predicate<T> withNarrowedType() {
 			return (Predicate<T>) this;
+		}
+	}
+	
+	private enum StringPredicate implements Predicate<String>, Serializable {
+		HAS_TEXT {
+			@Override
+			public boolean apply(String input) {
+				return StringUtils.isNotBlank(input);
+			}
 		}
 	}
 
