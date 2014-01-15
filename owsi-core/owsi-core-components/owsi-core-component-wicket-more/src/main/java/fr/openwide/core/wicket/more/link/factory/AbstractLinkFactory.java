e50a60abb92b2194b6e496e4ecbcf9a75e546806
Guillaume Smet <guillaume.smet@gmail.com>
2014-01-15 16:50:58 +0000

    on va plutôt utiliser des Collection, ça évitera d'avoir des problèmes de varargs
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2728 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/factory/AbstractLinkFactory.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/factory/AbstractLinkFactory.java
@@ -1,5 +1,8 @@
 package fr.openwide.core.wicket.more.link.factory;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import org.apache.wicket.Page;
 import org.apache.wicket.model.IModel;
 
@@ -15,17 +18,23 @@ public abstract class AbstractLinkFactory {
 		return new LinkDescriptorBuilder();
 	}
 	
-	public IPageLinkGenerator linkGenerator(Page pageInstance, Class<? extends Page> expectedClass,
-			Class<? extends Page>... otherExpectedClasses) {
-		return linkGenerator(PageModel.of(pageInstance), expectedClass, otherExpectedClasses);
+	public IPageLinkGenerator linkGenerator(Page pageInstance, Class<? extends Page> expectedClass) {
+		return linkGenerator(pageInstance, Collections.singleton(expectedClass));
+	}
+	
+	public IPageLinkGenerator linkGenerator(IModel<? extends Page> pageInstanceModel, Class<? extends Page> expectedClass) {
+		return linkGenerator(pageInstanceModel, Collections.singleton(expectedClass));
+	}
+	
+	public IPageLinkGenerator linkGenerator(Page pageInstance, Collection<? extends Class<? extends Page>> expectedClasses) {
+		return linkGenerator(PageModel.of(pageInstance), expectedClasses);
 	}
 	
 	public IPageLinkGenerator linkGenerator(IModel<? extends Page> pageInstanceModel,
-			Class<? extends Page> expectedClass,
-			Class<? extends Page>... otherExpectedClasses) {
-		IPageInstanceState<?> builder = builder().pageInstance(pageInstanceModel).validate(expectedClass);
-		for (Class<? extends Page> otherExpectedClass : otherExpectedClasses) {
-			builder.validate(otherExpectedClass);
+			Collection<? extends Class<? extends Page>> expectedClasses) {
+		IPageInstanceState<?> builder = builder().pageInstance(pageInstanceModel);
+		for (Class<? extends Page> expectedClass : expectedClasses) {
+			builder.validate(expectedClass);
 		}
 		return builder.build();
 	}
