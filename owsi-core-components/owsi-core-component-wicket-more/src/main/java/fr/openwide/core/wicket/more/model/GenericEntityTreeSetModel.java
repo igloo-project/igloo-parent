a0de14af53a4438fcf29ab8d5491564618ca8a64
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-01-17 13:16:20 +0000

    Corrections sur Suppliers2 et GenericEntityTreeSetModel.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2751 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/GenericEntityTreeSetModel.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/model/GenericEntityTreeSetModel.java
@@ -1,10 +1,13 @@
 package fr.openwide.core.wicket.more.model;
 
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
-import com.google.common.collect.Sets;
+import com.google.common.base.Supplier;
 
+import fr.openwide.core.commons.util.functional.Suppliers2;
 import fr.openwide.core.jpa.business.generic.model.GenericEntity;
 
 public class GenericEntityTreeSetModel<K extends Serializable & Comparable<K>, E extends GenericEntity<K, ?>>
@@ -14,16 +17,24 @@ public class GenericEntityTreeSetModel<K extends Serializable & Comparable<K>, E
 
 	public static <K extends Serializable & Comparable<K>, E extends GenericEntity<K, ?>>
 			GenericEntityTreeSetModel<K, E> of(Class<E> clazz) {
-		return new GenericEntityTreeSetModel<K, E>(clazz);
+		return new GenericEntityTreeSetModel<K, E>(clazz, Suppliers2.<E>treeSet());
 	}
 
-	public GenericEntityTreeSetModel(Class<E> clazz) {
+	public static <K extends Serializable & Comparable<K>, E extends GenericEntity<K, ?>>
+			GenericEntityTreeSetModel<K, E> of(Class<E> clazz, Comparator<? super E> comparator) {
+		return new GenericEntityTreeSetModel<K, E>(clazz, Suppliers2.treeSet(comparator));
+	}
+	
+	private final Supplier<? extends TreeSet<E>> newCollectionSupplier;
+
+	public GenericEntityTreeSetModel(Class<E> clazz, Supplier<? extends TreeSet<E>> newCollectionSupplier) {
 		super(clazz);
+		this.newCollectionSupplier = newCollectionSupplier;
 	}
 
 	@Override
 	protected SortedSet<E> createEntityCollection() {
-		return Sets.newTreeSet();
+		return newCollectionSupplier.get();
 	}
 
 }
