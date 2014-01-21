abeddd8e92cdbbe9b42f8f880cba52150bedb5fb
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-01-21 13:34:51 +0000

    Gestion des collection null dans les CollectionViews.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2759 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/collection/AbstractGenericCollectionView.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/collection/AbstractGenericCollectionView.java
@@ -1,9 +1,14 @@
 package fr.openwide.core.wicket.more.markup.html.collection;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 
 import org.apache.wicket.markup.repeater.RefreshingView;
+import org.apache.wicket.markup.repeater.util.ModelIteratorAdapter;
 import org.apache.wicket.model.IModel;
+
+import com.google.common.collect.Iterators;
 
 public abstract class AbstractGenericCollectionView<T, C extends Collection<? extends T>>
 		extends RefreshingView<T> { // Does not implement IGenericComponent<C> in order to allow using IModel<? extends C>, not only IModel<C>
@@ -23,5 +28,26 @@ public abstract class AbstractGenericCollectionView<T, C extends Collection<? ex
 	public C getModelObject() {
 		return (C) getDefaultModelObject();
 	}
+	
+	/**
+	 * Note: if you wish to override this, and overriding {@link #getModel(T)} is not enough, you're better off extending {@link RefreshingView} directly.
+	 */
+	@Override
+	protected final Iterator<IModel<T>> getItemModels() {
+		C collection = getModelObject();
+		if (collection == null) {
+			return Iterators.emptyIterator();
+		}
+		
+		Collection<T> collectionWithoutTypeWildcard = Collections.unmodifiableCollection(getModelObject());
+		return new ModelIteratorAdapter<T>(collectionWithoutTypeWildcard.iterator()) {
+			@Override
+			protected IModel<T> model(T object) {
+				return AbstractGenericCollectionView.this.getItemModel(object);
+			}
+		};
+	}
+	
+	protected abstract IModel<T> getItemModel(T object);
 
 }
