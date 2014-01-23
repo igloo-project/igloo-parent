81b66d5e6c756b1d67c249c285ee8ab84e0de11a
Guillaume Smet <guillaume.smet@gmail.com>
2014-01-23 13:12:24 +0000

    refonte de l'injection de l'Interceptor géré par Spring (mail en cours sur la liste hibernate-dev pour voir si on peut rendre cela plus solide/pratique) normalement, tout est fonctionnel maintenant \o/
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2790 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/hibernate/ejb/InterceptorAwareHibernatePersistenceProvider.java
@@ -0,0 +1,60 @@
+package fr.openwide.core.jpa.hibernate.ejb;
+
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.spi.PersistenceUnitInfo;
+
+import org.hibernate.Interceptor;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+import org.hibernate.jpa.boot.internal.PersistenceUnitInfoDescriptor;
+import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
+import org.hibernate.jpa.boot.spi.PersistenceUnitDescriptor;
+import org.hibernate.service.ServiceRegistry;
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Inject an Hibernate Interceptor managed by Spring in the SessionFactory.
+ * 
+ * The original idea came from the following blog:
+ * http://blog.krecan.net/2009/01
+ * /24/spring-managed-hibernate-interceptor-in-jpa/ but have been completely
+ * overhauled with the ORM 4.3 upgrade.
+ */
+public class InterceptorAwareHibernatePersistenceProvider extends HibernatePersistenceProvider {
+
+	private static final Logger log = Logger.getLogger(InterceptorAwareHibernatePersistenceProvider.class);
+
+	@Autowired
+	private Interceptor interceptor;
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public EntityManagerFactory createContainerEntityManagerFactory(PersistenceUnitInfo info, Map properties) {
+		log.tracef("Starting createContainerEntityManagerFactory : %s", info.getPersistenceUnitName());
+
+		return getEntityManagerFactoryBuilder(new PersistenceUnitInfoDescriptor(info), properties, null).build();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public EntityManagerFactoryBuilder getEntityManagerFactoryBuilder(
+			PersistenceUnitDescriptor persistenceUnitDescriptor, Map integration, ClassLoader providedClassLoader) {
+		return new EntityManagerFactoryBuilderImpl(persistenceUnitDescriptor, integration, providedClassLoader) {
+			@Override
+			public Configuration buildHibernateConfiguration(ServiceRegistry serviceRegistry) {
+				Configuration configuration = super.buildHibernateConfiguration(serviceRegistry);
+				if (InterceptorAwareHibernatePersistenceProvider.this.interceptor != null) {
+					if (configuration.getInterceptor() != null) {
+						log.error("The persistence provider was already configured with an interceptor: we override it.");
+					}
+					configuration.setInterceptor(InterceptorAwareHibernatePersistenceProvider.this.interceptor);
+				}
+				return configuration;
+			}
+		};
+	}
+
+}
