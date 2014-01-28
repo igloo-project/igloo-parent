491a9a69fe9837ad2875794205704e18d01bbadc
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-01-28 14:15:19 +0000

    Correction de SerializableFunction, qui héritait de Predicate...
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@2816 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/functional/SerializableFunction.java
+++ owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/functional/SerializableFunction.java
@@ -2,11 +2,11 @@ package fr.openwide.core.commons.util.functional;
 
 import java.io.Serializable;
 
-import com.google.common.base.Predicate;
+import com.google.common.base.Function;
 
 /**
  * A simple interface enabling declaration of anonymous serializable functions.
  */
-public interface SerializableFunction<F, T> extends Predicate<T>, Serializable {
+public interface SerializableFunction<F, T> extends Function<F, T>, Serializable {
 
 }
