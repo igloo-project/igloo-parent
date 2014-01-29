4499466a874fb729615810a225a91c216bb7d5f2
Guillaume Smet <guillaume.smet@gmail.com>
2014-01-29 02:46:04 +0000

    on raccourcit un peu le nom des paramètres
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2817 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/parameter/CommonParameters.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/parameter/CommonParameters.java
@@ -3,8 +3,8 @@ package fr.openwide.core.wicket.more.link.descriptor.parameter;
 public final class CommonParameters {
 	
 	public static final String ID = "id";
-	public static final String SOURCE_PAGE_ID = "sourcePageId";
-	public static final String NATURAL_ID = "naturalId";
+	public static final String SOURCE_PAGE_ID = "spid";
+	public static final String NATURAL_ID = "nid";
 	
 	/** Used as an anti-cache parameter. */
 	public static final String TIMESTAMP = "t";
