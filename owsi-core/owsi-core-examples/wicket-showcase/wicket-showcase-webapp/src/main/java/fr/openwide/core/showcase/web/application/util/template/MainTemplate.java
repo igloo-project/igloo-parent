734f6a0b54f8d2eca741ca200979eefb1fa1ee49
Guillaume Smet <guillaume.smet@gmail.com>
2014-01-30 16:52:25 +0000

    correction comportement breadcrumb
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2829 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/util/template/MainTemplate.java
+++ owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-webapp/src/main/java/fr/openwide/core/showcase/web/application/util/template/MainTemplate.java
@@ -14,6 +14,7 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
@@ -171,7 +172,7 @@ public abstract class MainTemplate extends AbstractWebPageTemplate {
 	
 	@Override
 	protected Component createBodyBreadCrumb(String wicketId) {
-		return new BodyBreadCrumbPanel(wicketId, bodyBreadCrumbPrependedElementsModel, breadCrumbElementsModel)
+		return new BodyBreadCrumbPanel(wicketId, bodyBreadCrumbPrependedElementsModel, breadCrumbElementsModel, Model.of(""))
 				.setTrailingSeparator(true);
 	}
 	
