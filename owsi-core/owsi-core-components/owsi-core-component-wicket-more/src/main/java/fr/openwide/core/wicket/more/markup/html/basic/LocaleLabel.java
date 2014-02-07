1ab409fb476a305bb46b29f5307e8ac671c24272
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-02-07 15:08:05 +0000

    Remontée de diverses classes (BlankLink, GenericBlankLink, LocaleDropDownChoice, LocaleLabel + déplacement de certains converters dans le package dédié.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2867 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/basic/LocaleLabel.java
@@ -0,0 +1,29 @@
+package fr.openwide.core.wicket.more.markup.html.basic;
+
+import java.util.Locale;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.IConverter;
+
+import fr.openwide.core.wicket.markup.html.basic.GenericLabel;
+import fr.openwide.core.wicket.more.util.convert.converters.HumanReadableLocaleConverter;
+
+public class LocaleLabel extends GenericLabel<Locale> {
+
+	private static final long serialVersionUID = 8270747942276592901L;
+
+	public LocaleLabel(String id, IModel<Locale> model) {
+		super(id, model);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public <C> IConverter<C> getConverter(Class<C> type) {
+		if (Locale.class.isAssignableFrom(type)) {
+			return (IConverter<C>) HumanReadableLocaleConverter.get();
+		} else {
+			return super.getConverter(type);
+		}
+	}
+
+}
