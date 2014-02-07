7a4456d4a85e72024b2f079d41a74c11d804fafd
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-02-07 18:08:50 +0000

    Virgules en fin de ligne. (non, ça n'a pas été fait à la main)
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@2877 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/notification/component/AbstractHtmlNotificationPanel.java
+++ basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/notification/component/AbstractHtmlNotificationPanel.java
@@ -36,24 +36,24 @@ public abstract class AbstractHtmlNotificationPanel<T> extends GenericPanel<T> {
 	protected void addTopProperty(MarkupContainer table, String headerComponentId, Component dataComponent) {
 		table.add(
 				createHeaderComponent(headerComponentId)
-						.add(new ClassAttributeAppender(CLASS_TABLE_TOP_LEFT))
-				, dataComponent
+						.add(new ClassAttributeAppender(CLASS_TABLE_TOP_LEFT)),
+				dataComponent
 						.add(new ClassAttributeAppender(CLASS_TABLE_TOP_RIGHT))
 		);
 	}
 	
 	protected void addMiddleProperty(MarkupContainer table, String headerComponentId, Component dataComponent) {
 		table.add(
-				createHeaderComponent(headerComponentId)
-				, dataComponent
+				createHeaderComponent(headerComponentId),
+				dataComponent
 		);
 	}
 	
 	protected void addBottomProperty(MarkupContainer table, String headerComponentId, Component dataComponent) {
 		table.add(
 				createHeaderComponent(headerComponentId)
-						.add(new ClassAttributeAppender(CLASS_TABLE_BOTTOM_LEFT))
-				, dataComponent
+						.add(new ClassAttributeAppender(CLASS_TABLE_BOTTOM_LEFT)),
+				dataComponent
 						.add(new ClassAttributeAppender(CLASS_TABLE_BOTTOM_RIGHT))
 		);
 	}
