c1e50d3fc8c21eef5906e9a51c485013f8f6de85
Guillaume Smet <guillaume.smet@gmail.com>
2014-02-12 16:43:55 +0000

    report des modifications de FLA dans la branche 0.8 et on renomme la classe vu qu'on est dans le trunk et qu'on peut casser des trucs
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2893 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/flash/zeroclipboard/ZeroClipboardDataAttributeModifier.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/flash/zeroclipboard/ZeroClipboardDataAttributeModifier.java
@@ -1,12 +1,12 @@
 package fr.openwide.core.wicket.more.markup.html.template.flash.zeroclipboard;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
-import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
-public class ZeroClipboardDataAttributeAppender extends Behavior {
+public class ZeroClipboardDataAttributeModifier extends Behavior {
 
 	private static final long serialVersionUID = 2564881748188747597L;
 
@@ -18,11 +18,11 @@ public class ZeroClipboardDataAttributeAppender extends Behavior {
 
 	private IModel<String> dataClipboardTextModel;
 	
-	public ZeroClipboardDataAttributeAppender(IModel<String> dataClipboardTextModel) {
+	public ZeroClipboardDataAttributeModifier(IModel<String> dataClipboardTextModel) {
 		this(Model.of(true), dataClipboardTextModel);
 	}
 
-	public ZeroClipboardDataAttributeAppender(IModel<Boolean> dataClipboardModel, IModel<String> dataClipboardTextModel) {
+	public ZeroClipboardDataAttributeModifier(IModel<Boolean> dataClipboardModel, IModel<String> dataClipboardTextModel) {
 		this.dataClipboardModel = dataClipboardModel;
 		this.dataClipboardTextModel = dataClipboardTextModel;
 	}
@@ -30,8 +30,8 @@ public class ZeroClipboardDataAttributeAppender extends Behavior {
 	@Override
 	public void onConfigure(Component component) {
 		super.onConfigure(component);
-		component.add(new AttributeAppender(DATA_CLIPBOARD, dataClipboardModel));
-		component.add(new AttributeAppender(DATA_CLIPBOARD_TEXT, dataClipboardTextModel));
+		component.add(new AttributeModifier(DATA_CLIPBOARD, dataClipboardModel));
+		component.add(new AttributeModifier(DATA_CLIPBOARD_TEXT, dataClipboardTextModel));
 	}
 
 	@Override
