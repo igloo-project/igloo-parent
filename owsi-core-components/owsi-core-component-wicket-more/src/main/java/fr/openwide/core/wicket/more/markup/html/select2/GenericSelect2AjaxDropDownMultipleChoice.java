b60f66fba60b0b19ee3878d2421bbef2268287a3
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-02-13 18:05:17 +0000

    Ajout du hack "IDropDownChoiceWidth" à "GenericSelect2AjaxDropDownMultipleChoice".
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2913 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/select2/GenericSelect2AjaxDropDownMultipleChoice.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/select2/GenericSelect2AjaxDropDownMultipleChoice.java
@@ -2,27 +2,58 @@ package fr.openwide.core.wicket.more.markup.html.select2;
 
 import java.util.Collection;
 
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.util.lang.Args;
 import org.retzlaff.select2.ISelect2AjaxAdapter;
 import org.retzlaff.select2.Select2MultipleChoice;
 import org.retzlaff.select2.Select2Settings;
 
 import com.google.common.base.Supplier;
 
+import fr.openwide.core.wicket.more.markup.html.select2.util.DropDownChoiceWidth;
+import fr.openwide.core.wicket.more.markup.html.select2.util.IDropDownChoiceWidth;
 import fr.openwide.core.wicket.more.markup.html.select2.util.Select2Utils;
 import fr.openwide.core.wicket.more.model.ConcreteCollectionToCollectionWrapperModel;
 
 public class GenericSelect2AjaxDropDownMultipleChoice<T, C extends Collection<T>> extends Select2MultipleChoice<T> {
 
 	private static final long serialVersionUID = 6355575209286187233L;
+	
+	/**
+	 * Hack.
+	 * @see IDropDownChoiceWidth
+	 */
+	private IDropDownChoiceWidth width = DropDownChoiceWidth.NORMAL;
 
 	protected GenericSelect2AjaxDropDownMultipleChoice(String id, IModel<C> model, Supplier<? extends C> collectionSupplier, ISelect2AjaxAdapter<T> adapter) {
 		super(id, new ConcreteCollectionToCollectionWrapperModel<T, C>(model, collectionSupplier), adapter);
 		
 		fillSelect2Settings(getSettings());
 	}
+	
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+		
+		add(new AttributeAppender("style", new LoadableDetachableModel<String>() {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			protected String load() {
+				return "width: " + width.getWidth() + "px";
+			}
+		}));
+	}
 
 	protected void fillSelect2Settings(Select2Settings settings) {
 		Select2Utils.setDefaultAjaxSettings(settings);
+	}
+	
+	public GenericSelect2AjaxDropDownMultipleChoice<T, C> setWidth(IDropDownChoiceWidth width) {
+		Args.notNull(width, "width");
+		this.width = width;
+		return this;
 	}
 }
