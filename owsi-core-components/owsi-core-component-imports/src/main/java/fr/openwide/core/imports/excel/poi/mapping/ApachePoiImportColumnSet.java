767d0d7dd81952d19606185b9f4b9fe02d6f7b53
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-02-13 15:42:55 +0000

    Passage d'attributs en final.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2910 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-imports/src/main/java/fr/openwide/core/imports/excel/poi/mapping/ApachePoiImportColumnSet.java
+++ owsi-core-components/owsi-core-component-imports/src/main/java/fr/openwide/core/imports/excel/poi/mapping/ApachePoiImportColumnSet.java
@@ -1,5 +1,7 @@
 package fr.openwide.core.imports.excel.poi.mapping;
 
+import java.util.Comparator;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -12,11 +14,19 @@ import fr.openwide.core.imports.excel.poi.mapping.column.builder.ApachePoiColumn
 public class ApachePoiImportColumnSet extends AbstractExcelImportColumnSet<Sheet, Row, Cell, CellReference> {
 
 	public ApachePoiImportColumnSet() {
-		this(new ApachePoiColumnBuilder());
+		super(new ApachePoiColumnBuilder());
 	}
 	
 	public ApachePoiImportColumnSet(AbstractColumnBuilder<Sheet, Row, Cell, CellReference> builder) {
 		super(builder);
+	}
+	
+	public ApachePoiImportColumnSet(Comparator<? super String> defaultHeaderLabelCollator) {
+		super(new ApachePoiColumnBuilder(), defaultHeaderLabelCollator);
+	}
+	
+	public ApachePoiImportColumnSet(AbstractColumnBuilder<Sheet, Row, Cell, CellReference> builder, Comparator<? super String> defaultHeaderLabelCollator) {
+		super(builder, defaultHeaderLabelCollator);
 	}
 
 }
