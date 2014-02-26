ee34d2a807e6a05040233125194906a7b7e2d442
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-02-26 12:39:00 +0000

    ImportDataService : utilisation de clés de type String au lieu de Long. Ça ne change pas grand chose quand on veut utiliser des chiffres, et ça change VRAIMENT la vie lorsqu'on a besoin d'une codification qui a plus de sens.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2952 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/util/init/util/GenericEntityConverter.java
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/util/init/util/GenericEntityConverter.java
@@ -31,10 +31,10 @@ public class GenericEntityConverter implements GenericConverter {
 	
 	private Map<Class<?>, Class<?>> classMapping;
 	
-	private Map<String, Map<Long, GenericEntity<Long, ?>>> idsMapping;
+	private Map<String, Map<String, GenericEntity<Long, ?>>> idsMapping;
 	
 	public GenericEntityConverter(IImportDataDao importDataDao, Workbook workbook,
-			Map<Class<?>, Class<?>> classMapping, Map<String, Map<Long, GenericEntity<Long, ?>>> idsMapping) {
+			Map<Class<?>, Class<?>> classMapping, Map<String, Map<String, GenericEntity<Long, ?>>> idsMapping) {
 		super();
 		
 		this.importDataDao = importDataDao;
@@ -55,14 +55,16 @@ public class GenericEntityConverter implements GenericConverter {
 	@Override
 	public Object convert(Object source, TypeDescriptor sourceType, TypeDescriptor targetType) {
 		Object item = null;
-		
-		if (StringUtils.hasText((String) source)) {
-			item = convertFromDatabase(source, targetType);
+
+		String importId = StringUtils.trimWhitespace((String)source);
+		if (StringUtils.hasText(importId)) {
+			item = convertFromDatabase(importId, targetType);
 			
 			if (item == null) {
-				item = convertFromWorkbook(source, targetType);
+				item = convertFromWorkbook(importId, targetType);
 			}
 		}
+		
 		return item;
 	}
 	
@@ -70,10 +72,9 @@ public class GenericEntityConverter implements GenericConverter {
 		this.conversionService = conversionService;
 	}
 	
-	private Object convertFromDatabase(Object source, TypeDescriptor targetType) {
+	private Object convertFromDatabase(String importId, TypeDescriptor targetType) {
 		Class<GenericEntity<Long, ?>> clazz = getTargetClass(targetType);
 		
-		Long importId = Long.parseLong((String) source);
 		Long databaseId = getDatabaseId(clazz, importId);
 		
 		if (databaseId != null) {
@@ -82,16 +83,15 @@ public class GenericEntityConverter implements GenericConverter {
 		return null;
 	}
 	
-	private Object convertFromWorkbook(Object source, TypeDescriptor targetType) {
+	private Object convertFromWorkbook(String importId, TypeDescriptor targetType) {
 		Class<GenericEntity<Long, ?>> clazz = getTargetClass(targetType);
-		String importId = StringUtils.trimWhitespace((String) source);
 		
 		Sheet sheet = workbook.getSheet(clazz.getSimpleName());
 		if (sheet != null) {
 			Map<String, Object> itemData = getItemData(sheet, importId);
 			if (itemData != null) {
 				if (!idsMapping.containsKey(clazz.getName())) {
-					idsMapping.put(clazz.getName(), new HashMap<Long, GenericEntity<Long, ?>>());
+					idsMapping.put(clazz.getName(), new HashMap<String, GenericEntity<Long, ?>>());
 				}
 				
 				GenericEntity<Long, ?> item = BeanUtils.instantiateClass(clazz);
@@ -100,7 +100,7 @@ public class GenericEntityConverter implements GenericConverter {
 				wrapper.setConversionService(conversionService);
 				wrapper.setPropertyValues(new MutablePropertyValues(itemData), true);
 				
-				idsMapping.get(clazz.getName()).put(Long.parseLong(importId), item);
+				idsMapping.get(clazz.getName()).put(importId, item);
 				
 				return item;
 			}
@@ -120,7 +120,7 @@ public class GenericEntityConverter implements GenericConverter {
 		return clazz;
 	}
 	
-	private Long getDatabaseId(Class<GenericEntity<Long, ?>> clazz, Long importId) {
+	private Long getDatabaseId(Class<GenericEntity<Long, ?>> clazz, String importId) {
 		if (idsMapping.containsKey(clazz.getName()) && idsMapping.get(clazz.getName()).containsKey(importId)) {
 			return idsMapping.get(clazz.getName()).get(importId).getId();
 		} else {
