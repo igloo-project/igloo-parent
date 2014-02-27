bc6cdd979214cf90cb54710380f27e6b1aa5ad38
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-02-27 14:50:15 +0000

    On remonte FileUploadForm depuis IOTA, avec quelques ajouts.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@2956 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/FileUploadForm.java
@@ -0,0 +1,44 @@
+package fr.openwide.core.wicket.more.markup.html.form;
+
+import java.util.Map;
+
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.lang.Bytes;
+import org.apache.wicket.util.upload.FileUploadException;
+import org.apache.wicket.util.upload.FileUploadBase.SizeLimitExceededException;
+
+/**
+ * A form with a decent resource key for the file upload error message:
+ * <ul>
+ * <li><pre>FileUploadForm.uploadFailed</pre> when the upload failed
+ * <li><pre>FileUploadForm.uploadTooLarge</pre> when the uploaded file is too large (model: "maxSize" and "actualSize")
+ * <ul>
+ */
+public class FileUploadForm<T> extends Form<T> {
+	
+	private static final long serialVersionUID = 4537543017863239331L;
+	
+	private static final String UPLOAD_TOO_LARGE_RESOURCE_KEY = "uploadTooLarge";
+
+	private static final String UPLOAD_FAILED_RESOURCE_KEY = "uploadFailed";
+	
+	public FileUploadForm(String id, IModel<T> model) {
+		super(id, model);
+	}
+	
+	@Override
+	protected void onFileUploadException(final FileUploadException e, final Map<String, Object> model) {
+		if (e instanceof SizeLimitExceededException) {
+			final String defaultValue = "Upload must be less than " + getMaxSize();
+			model.put("actualSize", Bytes.bytes(((SizeLimitExceededException)e).getActualSize()));
+			String msg = getString(FileUploadForm.class.getSimpleName() + '.' + UPLOAD_TOO_LARGE_RESOURCE_KEY, Model.ofMap(model), defaultValue);
+			error(msg);
+		} else {
+			final String defaultValue = "Upload failed: " + e.getLocalizedMessage();
+			String msg = getString(FileUploadForm.class.getSimpleName() + '.' + UPLOAD_FAILED_RESOURCE_KEY, Model.ofMap(model), defaultValue);
+			error(msg);
+		}
+	}
+}
