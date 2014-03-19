db7534d89643bf08947aece3a97024b5b54bbb97
Guillaume Smet <guillaume.smet@gmail.com>
2014-03-19 14:50:12 +0000

    refonte de CountLabel pour éviter de faire un setDefaultModel et utiliser un peu mieux les paradygmes Wicket
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3027 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/basic/CountLabel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/basic/CountLabel.java
@@ -21,68 +21,67 @@ import org.apache.wicket.model.IModel;
 import fr.openwide.core.wicket.markup.html.model.CountMessageModel;
 
 /**
- * Displays a label with up to three different variations based on
- * whether a numeric model is zero, one, or greater than one. This is
- * incredibly useful for printing count messages where the phrasing needs
- * to change according to singular and plural rules, like this:
+ * Displays a label with up to three different variations based on whether a
+ * numeric model is zero, one, or greater than one. This is incredibly useful
+ * for printing count messages where the phrasing needs to change according to
+ * singular and plural rules, like this:
  * <ul>
  * <li>There are no friends in your network.</li>
  * <li>There is <u>one friend</u> in your network.</li>
  * <li>There are <u>5 friends</u> in your network.</li>
  * </ul>
  * <p>
- * This class is a simple Label wrapper around a CountMessageModel, which
- * does most of the work. Refer to the class documentation of
+ * This class is a simple Label wrapper around a CountMessageModel, which does
+ * most of the work. Refer to the class documentation of
  * {@link CountMessageModel} for more details.
  * 
  * @since 2.0
  */
-public class CountLabel extends Label
-{
-    private static final long serialVersionUID = -889821092752173878L;
+public class CountLabel extends Label {
+	private static final long serialVersionUID = -889821092752173878L;
 
-    private boolean hideIfZeroOrNull = false;
-    
-    private IModel<? extends Number> countModel;
+	private boolean hideIfZeroOrNull = false;
 
-    /**
-     * Constructs a label that will a display zero, singular or plural
-     * variation based on the value of the specified {@code count} model.
-     * 
-     * @param id The wicket:id of the label component. This is will also
-     *           be used as the message key to look up the message strings
-     *           in the localized properties file.
-     * 
-     * @param count A number that will be used to choose either the
-     *              <code>id.zero</code>, <code>id.one</code> or
-     *              <code>id.many</code> string from the properties file. It
-     *              will also be made available to the string as an
-     *              interpolated <code>${count}</code> variable.
-     */
-    public CountLabel(String id, String messageKey, IModel<? extends Number> countModel)
-    {
-        super(id);
-        this.countModel = countModel;
-        setDefaultModel(new CountMessageModel(messageKey, this, countModel));
-        setEscapeModelStrings(false);
-    }
-	
+	private IModel<? extends Number> countModel;
+
+	/**
+	 * Constructs a label that will a display zero, singular or plural variation
+	 * based on the value of the specified {@code count} model.
+	 * 
+	 * @param id
+	 *            The wicket:id of the label component. This is will also be
+	 *            used as the message key to look up the message strings in the
+	 *            localized properties file.
+	 * 
+	 * @param count
+	 *            A number that will be used to choose either the
+	 *            <code>id.zero</code>, <code>id.one</code> or
+	 *            <code>id.many</code> string from the properties file. It will
+	 *            also be made available to the string as an interpolated
+	 *            <code>${count}</code> variable.
+	 */
+	public CountLabel(String id, String messageKey, IModel<? extends Number> countModel) {
+		super(id, new CountMessageModel(messageKey, countModel));
+		this.countModel = countModel;
+		setEscapeModelStrings(false);
+	}
+
 	@Override
 	protected void onConfigure() {
 		super.onConfigure();
 		Number count = getCountModelObject();
 		setVisible(!hideIfZeroOrNull || (count != null && !count.equals(0L)));
 	}
-	
+
 	public CountLabel hideIfZeroOrNull() {
 		this.hideIfZeroOrNull = true;
 		return this;
 	}
-	
+
 	private Number getCountModelObject() {
 		return countModel != null ? countModel.getObject() : null;
 	}
-	
+
 	@Override
 	protected void onDetach() {
 		super.onDetach();
