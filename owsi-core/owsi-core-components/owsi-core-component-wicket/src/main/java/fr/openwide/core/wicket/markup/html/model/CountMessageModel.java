aab0ec0702660a566b4b9be16ce8cc93207afcec
Guillaume Smet <guillaume.smet@gmail.com>
2014-03-19 11:24:39 +0000

    réindentation avant ajustements
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3024 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/model/CountMessageModel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/model/CountMessageModel.java
@@ -22,28 +22,27 @@ import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.util.lang.Args;
 
 /**
- * Selects an appropriate localized string based on whether a count value
- * is zero, one or greater than one. This makes it easier to produce singluar
- * and plural phrases.
+ * Selects an appropriate localized string based on whether a count value is
+ * zero, one or greater than one. This makes it easier to produce singluar and
+ * plural phrases.
  * <p>
- * For example, consider the case where we would like to set one of these
- * three feedback messages. Notice how the phrasing is slightly different
- * depending on the number of items. Furthermore in the latter two examples
- * we would like to embed a hyperlink to a bookmarkable page.
+ * For example, consider the case where we would like to set one of these three
+ * feedback messages. Notice how the phrasing is slightly different depending on
+ * the number of items. Furthermore in the latter two examples we would like to
+ * embed a hyperlink to a bookmarkable page.
  * <ul>
  * <li>There are no friends in your network.</li>
  * <li>There is <u>one friend</u> in your network.</li>
  * <li>There are <u>5 friends</u> in your network.</li>
  * </ul>
  * <p>
- * Normally in Wicket you would accomplish this using tedious if/else
- * statements and string concatenation, conditionally visible
- * fragments/containers, or the incredibly arcane syntax of Java's
- * {@link java.text.ChoiceFormat ChoiceFormat} (and note that you'd have to
- * carefully escape the "&lt;" and "&gt;" if you want to include HTML markup
- * as we need for the hyperlinks in this example). All of these
- * approaches are clumsy, and it would be hard for a non-programmer to localize
- * or edit the strings.
+ * Normally in Wicket you would accomplish this using tedious if/else statements
+ * and string concatenation, conditionally visible fragments/containers, or the
+ * incredibly arcane syntax of Java's {@link java.text.ChoiceFormat
+ * ChoiceFormat} (and note that you'd have to carefully escape the "&lt;" and
+ * "&gt;" if you want to include HTML markup as we need for the hyperlinks in
+ * this example). All of these approaches are clumsy, and it would be hard for a
+ * non-programmer to localize or edit the strings.
  * <p>
  * You can now use this model in a label (also consider
  * {@link fiftyfive.wicket.basic.CountLabel CountLabel} as a shortcut for this
@@ -52,98 +51,89 @@ import org.apache.wicket.util.lang.Args;
  * 
  * @since 2.0
  */
-public class CountMessageModel extends AbstractReadOnlyModel<String>
-{
-    private static final long serialVersionUID = 3717960938783720989L;
-    
-    private StringResourceModel stringModel;
-    private IModel<? extends Number> countModel;
-    
-    /**
-     * Constructs a CountMessageModel that will choose the appropriate
-     * localized string from a properties file.
-     * 
-     * @param messageKey The key that will be consulted in the properties file.
-     *                   The key will have ".zero", ".one" or ".many" appended
-     *                   to it depending on the value of the {@code count}.
-     * @param component The Wicket component that will be used for finding
-     *                  the properties file. Usually this is the page or panel
-     *                  where you plan to use the model.
-     * @param count The number that will be used in the message. This will
-     *              be used to substitute any <code>${count}</code> expressions
-     *              in the message.
-     */
-    public CountMessageModel(String messageKey,
-                             Component component,
-                             IModel<? extends Number> count)
-    {
-        super();
-        Args.notNull(messageKey, "messageKey");
-        Args.notNull(count, "count");
-        
-        this.countModel = count;
-        this.stringModel = new StringResourceModel(
-            messageKey + "${resourceSuffix}",
-            component,
-            new AbstractReadOnlyModel<CountMessageModel>()
-            {
-                private static final long serialVersionUID = 1L;
+public class CountMessageModel extends AbstractReadOnlyModel<String> {
+	private static final long serialVersionUID = 3717960938783720989L;
 
-                @Override
-                public CountMessageModel getObject()
-                {
-                    return CountMessageModel.this;
-                }
-            }
-        );
-    }
-    
-    public CountMessageModel(String messageKey, IModel<? extends Number> count)
-    {
-        this(messageKey, null, count);
-    }
-    
-    /**
-     * Detaches the count model passed into the constructor and other
-     * internal state.
-     */
-    @Override
-    public void detach()
-    {
-        this.countModel.detach();
-        this.stringModel.detach();
-        super.detach();
-    }
+	private StringResourceModel stringModel;
+	private IModel<? extends Number> countModel;
 
-    /**
-     * Returns the message after all interpolation has been performed.
-     */
-    @Override
-    public String getObject()
-    {
-        return this.stringModel.getObject();
-    }
-    
-    /**
-     * Returns the current value of the count model. This is exposed as
-     * <code>${count}</code> within the message.
-     */
-    public int getCount()
-    {
-        Number num = this.countModel.getObject();
-        return num != null ? num.intValue() : 0;
-    }
-    
-    /**
-     * Returns either ".zero", ".one", or ".many" depending on the current
-     * value of the count model.
-     */
-    public String getResourceSuffix()
-    {
-        int count = getCount();
-        
-        if(0 == count) return ".zero";
-        if(1 == count) return ".one";
-        return ".many";
-    }
+	/**
+	 * Constructs a CountMessageModel that will choose the appropriate localized
+	 * string from a properties file.
+	 * 
+	 * @param messageKey
+	 *            The key that will be consulted in the properties file. The key
+	 *            will have ".zero", ".one" or ".many" appended to it depending
+	 *            on the value of the {@code count}.
+	 * @param component
+	 *            The Wicket component that will be used for finding the
+	 *            properties file. Usually this is the page or panel where you
+	 *            plan to use the model.
+	 * @param count
+	 *            The number that will be used in the message. This will be used
+	 *            to substitute any <code>${count}</code> expressions in the
+	 *            message.
+	 */
+	public CountMessageModel(String messageKey, Component component, IModel<? extends Number> count) {
+		super();
+		Args.notNull(messageKey, "messageKey");
+		Args.notNull(count, "count");
+
+		this.countModel = count;
+		this.stringModel = new StringResourceModel(messageKey + "${resourceSuffix}", component,
+				new AbstractReadOnlyModel<CountMessageModel>() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public CountMessageModel getObject() {
+						return CountMessageModel.this;
+					}
+				});
+	}
+
+	public CountMessageModel(String messageKey, IModel<? extends Number> count) {
+		this(messageKey, null, count);
+	}
+
+	/**
+	 * Detaches the count model passed into the constructor and other internal
+	 * state.
+	 */
+	@Override
+	public void detach() {
+		this.countModel.detach();
+		this.stringModel.detach();
+		super.detach();
+	}
+
+	/**
+	 * Returns the message after all interpolation has been performed.
+	 */
+	@Override
+	public String getObject() {
+		return this.stringModel.getObject();
+	}
+
+	/**
+	 * Returns the current value of the count model. This is exposed as
+	 * <code>${count}</code> within the message.
+	 */
+	public int getCount() {
+		Number num = this.countModel.getObject();
+		return num != null ? num.intValue() : 0;
+	}
+
+	/**
+	 * Returns either ".zero", ".one", or ".many" depending on the current value
+	 * of the count model.
+	 */
+	public String getResourceSuffix() {
+		int count = getCount();
+
+		if (0 == count)
+			return ".zero";
+		if (1 == count)
+			return ".one";
+		return ".many";
+	}
 }
