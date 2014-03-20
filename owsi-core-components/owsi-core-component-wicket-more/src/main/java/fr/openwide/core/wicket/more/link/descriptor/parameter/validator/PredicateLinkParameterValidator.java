1c479f2dad6d7c9930ab80044aac89404ffcdca6
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-03-20 14:29:08 +0000

    Ajout d'un PredicateLinkParameterValidator.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3040 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/parameter/validator/PredicateLinkParameterValidator.java
@@ -0,0 +1,39 @@
+package fr.openwide.core.wicket.more.link.descriptor.parameter.validator;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.google.common.base.Predicate;
+
+public class PredicateLinkParameterValidator<T> implements ILinkParameterValidator {
+	
+	private static final long serialVersionUID = 7015800524943994171L;
+	
+	private final Predicate<T> predicate;
+	
+	private final IModel<T> model;
+	
+	public PredicateLinkParameterValidator(Predicate<T> predicate, IModel<T> model) {
+		this.predicate = predicate;
+		this.model = model;
+	}
+
+	@Override
+	public void validateSerialized(PageParameters parameters, LinkParameterValidationErrorCollector collector) {
+		// Nothing to do
+	}
+
+	@Override
+	public void validateModel(LinkParameterValidationErrorCollector collector) {
+		T object = model.getObject();
+		if (!predicate.apply(object)) {
+			collector.addError(String.format("Predicate '%s' did not apply to model object '%s'.", predicate, object));
+		}
+	}
+
+	@Override
+	public void detach() {
+		model.detach();
+	}
+
+}
