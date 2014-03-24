3f5fc6c4d1673cead9d59a3d2d79e6aa2d3eecf4
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-03-24 18:21:31 +0000

    Ajout de la possibilité d'utiliser CommentOutModifier pour des commentaires conditionnels.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3054 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/behavior/CommentOutModifier.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/behavior/CommentOutModifier.java
@@ -2,6 +2,8 @@ package fr.openwide.core.wicket.behavior;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.request.Response;
+import org.springframework.util.StringUtils;
 
 /**
  * Taken from http://stackoverflow.com/a/3947013
@@ -9,14 +11,38 @@ import org.apache.wicket.behavior.Behavior;
 public class CommentOutModifier extends Behavior {
 
 	private static final long serialVersionUID = -534729382028439538L;
+	
+	private final String condition;
+	
+	public CommentOutModifier() {
+		this(null);
+	}
+	
+	/**
+	 * @param condition A condition. If non-null and non-empty, the comment will be an conditional comment. Useful for markup that
+	 * should only be displayed in Internet Explorer.
+	 */
+	public CommentOutModifier(String condition) {
+		this.condition = condition;
+	}
 
 	@Override
 	public void beforeRender(Component component) {
-		component.getResponse().write("<!--");
+		Response response = component.getResponse();
+		response.write("<!--");
+		if (StringUtils.hasText(condition)) {
+			response.write(
+					new StringBuilder("[if ").append(condition).append("]>").toString()
+			);
+		}
 	}
 
 	@Override
 	public void afterRender(Component component) {
-		component.getResponse().write("-->");
+		Response response = component.getResponse();
+		if (StringUtils.hasText(condition)) {
+			response.write("<![endif]");
+		}
+		response.write("-->");
 	}
 }
