5c3a98b6c5304b98c056b665aa520e3f5f00d59d
Guillaume Smet <guillaume.smet@gmail.com>
2014-03-26 10:36:19 +0000

    merge 3062 from trunk
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@3063 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/model/CountMessageModel.java
+++ owsi-core-components/owsi-core-component-wicket/src/main/java/fr/openwide/core/wicket/markup/html/model/CountMessageModel.java
@@ -103,8 +103,9 @@ public class CountMessageModel extends AbstractReadOnlyModel<String> implements 
 
 	@Override
 	public String getObject() {
-		// this shouldn't be called always wrapped!
-		return Application.get().getResourceSettings().getLocalizer().getString(getResourceKey(), null, CountMessageModel.this);
+		// this shouldn't be called in theory BUT a few classes extending this class are indeed using it.
+		return Application.get().getResourceSettings().getLocalizer().getString(getResourceKey(), null,
+				Model.of(getPropertySubstitutionBean()));
 	}
 
 	public int getCount() {
