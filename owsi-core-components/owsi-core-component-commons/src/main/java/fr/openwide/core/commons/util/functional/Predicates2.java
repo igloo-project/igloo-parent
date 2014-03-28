d155bb2ead0b9f953bae40bb9e4c8ff031d9613b
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-03-28 15:30:25 +0000

    Ajout des méthodes any et all dans Predicates2.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@3071 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/functional/Predicates2.java
+++ owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/functional/Predicates2.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
 
 public final class Predicates2 {
 
@@ -53,10 +54,6 @@ public final class Predicates2 {
 	public static <T extends Collection<?>> Predicate<T> notEmpty() {
 		return CollectionPredicate.NOT_EMPTY.withNarrowedType();
 	}
-	
-	public static Predicate<String> hasText() {
-		return StringPredicate.HAS_TEXT;
-	}
 
 	private enum CollectionPredicate implements Predicate<Collection<?>>, Serializable {
 		IS_EMPTY {
@@ -78,12 +75,76 @@ public final class Predicates2 {
 		}
 	}
 	
+	public static Predicate<String> hasText() {
+		return StringPredicate.HAS_TEXT;
+	}
+	
 	private enum StringPredicate implements Predicate<String>, Serializable {
 		HAS_TEXT {
 			@Override
 			public boolean apply(String input) {
 				return StringUtils.isNotBlank(input);
 			}
+		}
+	}
+
+	/**
+	 * @return A predicate returning true if the given iterable is non-null, is non-empty and has at least one element
+	 *         that satisfies {@code itemPredicate}.
+	 * @see Iterables#any(Iterable, Predicate)
+	 */
+	public static <T> Predicate<Iterable<? extends T>> any(Predicate<? super T> itemPredicate) {
+		return new IterableAnyPredicate<T>(itemPredicate);
+	}
+	
+	private static class IterableAnyPredicate<T> implements Predicate<Iterable<? extends T>>, Serializable {
+		private static final long serialVersionUID = -359783441767977199L;
+		
+		private final Predicate<? super T> itemPredicate;
+
+		public IterableAnyPredicate(Predicate<? super T> itemPredicate) {
+			super();
+			this.itemPredicate = itemPredicate;
+		}
+		
+		@Override
+		public boolean apply(Iterable<? extends T> input) {
+			return input != null && Iterables.any(input, itemPredicate);
+		}
+		
+		@Override
+		public String toString() {
+			return new StringBuilder().append("any(").append(itemPredicate).append(")").toString();
+		}
+	}
+	
+	/**
+	 * @return A predicate returning true if the given iterable is null, is empty or has only elements
+	 *         that satisfy {@code itemPredicate}.
+	 * @see Iterables#all(Iterable, Predicate)
+	 */
+	public static <T> Predicate<Iterable<? extends T>> all(Predicate<? super T> itemPredicate) {
+		return new IterableAllPredicate<T>(itemPredicate);
+	}
+	
+	private static class IterableAllPredicate<T> implements Predicate<Iterable<? extends T>>, Serializable {
+		private static final long serialVersionUID = -359783441767977199L;
+		
+		private final Predicate<? super T> itemPredicate;
+
+		public IterableAllPredicate(Predicate<? super T> itemPredicate) {
+			super();
+			this.itemPredicate = itemPredicate;
+		}
+		
+		@Override
+		public boolean apply(Iterable<? extends T> input) {
+			return input == null || Iterables.all(input, itemPredicate);
+		}
+		
+		@Override
+		public String toString() {
+			return new StringBuilder().append("all(").append(itemPredicate).append(")").toString();
 		}
 	}
 
