f32bb0ee67f5b75de828556b490829594c1a0146
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-03-28 15:37:17 +0000

    Merge -c 3071 from branch 0.8-MAINTENANCE to trunk.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3072 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/functional/Predicates2.java
+++ owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/functional/Predicates2.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
 
 public final class Predicates2 {
 
@@ -106,6 +107,66 @@ public final class Predicates2 {
 		@Override
 		public boolean apply(T input) {
 			return comparator.compare(referenceValue, input) == 0;
+		}
+	}
+
+	/**
+	 * @return A predicate returning true if the given iterable is non-null, is non-empty and has at least one element
+	 *         that satisfies {@code itemPredicate}.
+	 * @see Iterables#any(Iterable, Predicate)
+	 */
+	public static <T> Predicate<Iterable<? extends T>> any(Predicate<? super T> itemPredicate) {
+		return new IterableAnyPredicate<T>(itemPredicate);
+	}
+	
+	private static class IterableAnyPredicate<T> implements Predicate<Iterable<? extends T>>, Serializable {
+		private static final long serialVersionUID = -359783441767977199L;
+		
+		private final Predicate<? super T> itemPredicate;
+
+		public IterableAnyPredicate(Predicate<? super T> itemPredicate) {
+			super();
+			this.itemPredicate = itemPredicate;
+		}
+		
+		@Override
+		public boolean apply(Iterable<? extends T> input) {
+			return input != null && Iterables.any(input, itemPredicate);
+		}
+		
+		@Override
+		public String toString() {
+			return new StringBuilder().append("any(").append(itemPredicate).append(")").toString();
+		}
+	}
+	
+	/**
+	 * @return A predicate returning true if the given iterable is null, is empty or has only elements
+	 *         that satisfy {@code itemPredicate}.
+	 * @see Iterables#all(Iterable, Predicate)
+	 */
+	public static <T> Predicate<Iterable<? extends T>> all(Predicate<? super T> itemPredicate) {
+		return new IterableAllPredicate<T>(itemPredicate);
+	}
+	
+	private static class IterableAllPredicate<T> implements Predicate<Iterable<? extends T>>, Serializable {
+		private static final long serialVersionUID = -359783441767977199L;
+		
+		private final Predicate<? super T> itemPredicate;
+
+		public IterableAllPredicate(Predicate<? super T> itemPredicate) {
+			super();
+			this.itemPredicate = itemPredicate;
+		}
+		
+		@Override
+		public boolean apply(Iterable<? extends T> input) {
+			return input == null || Iterables.all(input, itemPredicate);
+		}
+		
+		@Override
+		public String toString() {
+			return new StringBuilder().append("all(").append(itemPredicate).append(")").toString();
 		}
 	}
 
