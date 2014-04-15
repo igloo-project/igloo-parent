b2924b6548f4e91c3bbfdbf97080efc30852c6e7
Julien Gonzalez <julien.gonzalez@openwide.fr>
2014-04-15 09:18:12 +0000

    Unused import.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@3124 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-spring/src/main/java/fr/openwide/core/spring/config/CoreConfigurer.java
+++ owsi-core-components/owsi-core-component-spring/src/main/java/fr/openwide/core/spring/config/CoreConfigurer.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.LocaleUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
