a0346df852c992431a39aa90baef4b1c370e78c1
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-04-24 07:44:19 +0000

    Ajout de GenericEntityConverterChoiceRenderer.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3157 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/GenericEntityConverterChoiceRenderer.java
@@ -0,0 +1,36 @@
+package fr.openwide.core.wicket.more.markup.html.form;
+
+import java.util.Locale;
+
+import org.apache.wicket.Session;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.util.convert.IConverter;
+
+import fr.openwide.core.jpa.business.generic.model.GenericEntity;
+
+public class GenericEntityConverterChoiceRenderer<T extends GenericEntity<?, ?>> implements IChoiceRenderer<T> {
+	
+	private static final long serialVersionUID = -8949718525256769535L;
+
+	private final IConverter<? super T> converter;
+
+	public GenericEntityConverterChoiceRenderer(IConverter<? super T> converter) {
+		super();
+		this.converter = converter;
+	}
+
+	@Override
+	public Object getDisplayValue(T object) {
+		return converter.convertToString(object, getLocale());
+	}
+
+	@Override
+	public String getIdValue(T object, int index) {
+		return object == null ? null : String.valueOf(object.getId());
+	}
+	
+	protected Locale getLocale() {
+		return Session.get().getLocale();
+	}
+
+}
