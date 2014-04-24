aa5dee288cc0f1b4c686728befb1f312960a772d
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-04-24 07:44:19 +0000

    Ajout de GenericEntityConverterChoiceRenderer.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3157 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/select2/AbstractLongIdGenericEntitySelect2AjaxAdapter.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/select2/AbstractLongIdGenericEntitySelect2AjaxAdapter.java
@@ -1,8 +1,10 @@
 package fr.openwide.core.wicket.more.markup.html.select2;
 
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.util.convert.IConverter;
 
 import fr.openwide.core.jpa.business.generic.model.GenericEntity;
+import fr.openwide.core.wicket.more.markup.html.form.GenericEntityConverterChoiceRenderer;
 import fr.openwide.core.wicket.more.util.convert.converters.LongIdGenericEntityConverter;
 
 public abstract class AbstractLongIdGenericEntitySelect2AjaxAdapter<T extends GenericEntity<Long, ?>> extends AbstractChoiceRendererSelect2AjaxAdapter<T> {
@@ -10,6 +12,10 @@ public abstract class AbstractLongIdGenericEntitySelect2AjaxAdapter<T extends Ge
 	private static final long serialVersionUID = 7880845591046909801L;
 	
 	private final LongIdGenericEntityConverter<T> entityConverter;
+
+	public AbstractLongIdGenericEntitySelect2AjaxAdapter(Class<T> clazz, IConverter<? super T> converter) {
+		this(clazz, new GenericEntityConverterChoiceRenderer<>(converter));
+	}
 
 	public AbstractLongIdGenericEntitySelect2AjaxAdapter(Class<T> clazz, IChoiceRenderer<? super T> choiceRenderer) {
 		super(choiceRenderer);
