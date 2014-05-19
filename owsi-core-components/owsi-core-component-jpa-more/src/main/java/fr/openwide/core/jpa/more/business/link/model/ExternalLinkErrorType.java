7d73361d0faba69fafd482c99136876bef5486e3
Guillaume Smet <guillaume.smet@gmail.com>
2014-05-19 15:37:55 +0000

    backport : ajustement sur la vérification de liens : on utilise la librairie galimatias pour parser les URL et gérer correctement l'escaping et les IDN
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.8-MAINTENANCE@3236 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/link/model/ExternalLinkErrorType.java
@@ -0,0 +1,11 @@
+package fr.openwide.core.jpa.more.business.link.model;
+
+public enum ExternalLinkErrorType {
+
+	HTTP,
+	INVALID_IDN,
+	URI_SYNTAX,
+	MALFORMED_URL,
+	UNKNOWN_HTTPCLIENT_ERROR;
+	
+}
