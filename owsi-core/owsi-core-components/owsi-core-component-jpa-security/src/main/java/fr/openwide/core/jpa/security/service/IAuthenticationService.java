b0d4ce7d3b1acf1cd75793d2451c8ad047e29ad9
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-05-19 10:53:13 +0000

    Relachement de contraintes sur les services de permissions : on n'est pas obligé de travailler uniquement avec des GenericEntity.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3228 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/service/IAuthenticationService.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/service/IAuthenticationService.java
@@ -5,8 +5,6 @@ import java.util.Collection;
 import org.springframework.security.acls.model.Permission;
 import org.springframework.security.core.GrantedAuthority;
 
-import fr.openwide.core.jpa.business.generic.model.GenericEntity;
-
 public interface IAuthenticationService {
 
 	String getUserName();
@@ -23,7 +21,7 @@ public interface IAuthenticationService {
 	
 	boolean hasRole(String role);
 	
-	boolean hasPermission(GenericEntity<?, ?> entity, Permission permission);
+	boolean hasPermission(Object securedObject, Permission permission);
 	
 	boolean hasPermission(Permission permission);
 	
