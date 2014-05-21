ccf47098a3bff2fdbc692456a1d163b415d73d16
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-05-21 13:16:55 +0000

    Import excel : dissociation de l'eventHandler et du messageFormatter.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3253 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-imports/src/main/java/fr/openwide/core/imports/excel/event/LoggerExcelImportEventHandler.java
+++ owsi-core-components/owsi-core-component-imports/src/main/java/fr/openwide/core/imports/excel/event/LoggerExcelImportEventHandler.java
@@ -1,6 +1,7 @@
 package fr.openwide.core.imports.excel.event;
 
-import fr.openwide.core.commons.util.report.BatchReport;
+import org.slf4j.Logger;
+
 import fr.openwide.core.imports.excel.event.exception.ExcelImportContentException;
 import fr.openwide.core.imports.excel.event.exception.ExcelImportHeaderLabelMappingException;
 import fr.openwide.core.imports.excel.event.exception.ExcelImportMappingException;
@@ -9,31 +10,31 @@ import fr.openwide.core.imports.excel.event.formatter.RawExcelImportEventMessage
 import fr.openwide.core.imports.excel.location.ExcelImportLocation;
 
 /**
- * A event handler that will add errors to a {@link BatchReport} and delay exception throwing until {@link #checkNoErrorOccurred()} is called.
+ * A event handler that will add errors to a {@link Logger} and delay exception throwing until {@link #checkNoErrorOccurred()} is called.
  * <p><strong>CAUTION:</string> when using this event handler, {@link #checkNoErrorOccurred()} MUST be called before attempting any action based on the imported data.
  * Doing otherwise will result in corrupted data.
  */
-public class BatchReportExcelImportEventHandler implements IExcelImportEventHandler {
+public class LoggerExcelImportEventHandler implements IExcelImportEventHandler {
 	
 	private final IExcelImportEventMessageFormatter formatter;
 	
-	private final BatchReport batchReport;
+	private final Logger logger;
 	
 	private boolean hasError = false;
 	
-	public BatchReportExcelImportEventHandler(BatchReport batchReport) {
-		this(new RawExcelImportEventMessageFormatter(), batchReport);
+	public LoggerExcelImportEventHandler(Logger logger) {
+		this(new RawExcelImportEventMessageFormatter(), logger);
 	}
 	
-	public BatchReportExcelImportEventHandler(IExcelImportEventMessageFormatter formatter, BatchReport batchReport) {
+	public LoggerExcelImportEventHandler(IExcelImportEventMessageFormatter formatter, Logger logger) {
 		this.formatter = formatter;
-		this.batchReport = batchReport;
+		this.logger = logger;
 	}
 
 	@Override
 	public void headerLabelMappingError(String expectedHeaderLabel, int indexAmongMatchedColumns, ExcelImportLocation location) throws ExcelImportHeaderLabelMappingException {
 		this.hasError = true;
-		batchReport.error(formatter.formatHeaderLabelMappingErrorMessage(expectedHeaderLabel, indexAmongMatchedColumns, location));
+		logger.error(formatter.formatHeaderLabelMappingErrorMessage(expectedHeaderLabel, indexAmongMatchedColumns, location));
 		throw new ExcelImportHeaderLabelMappingException(
 				"Could not map column '" + expectedHeaderLabel + "' (index among matched columns : " + indexAmongMatchedColumns + ")",
 				expectedHeaderLabel, location
@@ -44,27 +45,27 @@ public class BatchReportExcelImportEventHandler implements IExcelImportEventHand
 	public void checkNoMappingErrorOccurred() throws ExcelImportMappingException {
 		if (hasError) {
 			this.hasError = false;
-			throw new ExcelImportMappingException("An error occurred; see the error report for more details");
+			throw new ExcelImportMappingException("An error occurred; see the logs for more details");
 		}
 	}
 
 	@Override
 	public void error(String error, ExcelImportLocation location) throws ExcelImportContentException {
 		this.hasError = true;
-		batchReport.error(formatter.formatErrorMessage(error, location));
+		logger.error(formatter.formatErrorMessage(error, location));
 	}
 
 	@Override
 	public void missingValue(String error, ExcelImportLocation location) throws ExcelImportContentException {
 		this.hasError = true;
-		batchReport.error(formatter.formatErrorMessage(error, location));
+		logger.error(formatter.formatErrorMessage(error, location));
 	}
 
 	@Override
 	public void checkNoErrorOccurred() throws ExcelImportContentException {
 		if (hasError) {
 			this.hasError = false;
-			throw new ExcelImportContentException("An error occurred; see the error report for more details");
+			throw new ExcelImportContentException("An error occurred; see the logs for more details");
 		}
 	}
 	
