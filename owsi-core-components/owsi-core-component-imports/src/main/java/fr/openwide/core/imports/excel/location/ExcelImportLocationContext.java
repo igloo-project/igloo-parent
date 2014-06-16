11b87d11bf96a5c90c89981b494d430322b1d99c
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-06-16 08:44:58 +0000

    Ajustements pour permettre de récupéer un ExcelImportLocation depuis un ExcelImportLocationContext (utile dans des cas très précis, comme lorsqu'on veut corriger les données d'une certaine cellule).
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3311 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-imports/src/main/java/fr/openwide/core/imports/excel/location/ExcelImportLocationContext.java
+++ owsi-core-components/owsi-core-component-imports/src/main/java/fr/openwide/core/imports/excel/location/ExcelImportLocationContext.java
@@ -1,11 +1,19 @@
 package fr.openwide.core.imports.excel.location;
 
 import fr.openwide.core.imports.excel.event.ExcelImportEvent;
+import fr.openwide.core.imports.excel.event.IExcelImportEventHandler;
 import fr.openwide.core.imports.excel.event.ExcelImportEvent.ExcelImportErrorEvent;
 import fr.openwide.core.imports.excel.event.ExcelImportEvent.ExcelImportInfoEvent;
 import fr.openwide.core.imports.excel.event.exception.ExcelImportContentException;
 
 public abstract class ExcelImportLocationContext {
+	
+	protected final IExcelImportEventHandler eventHandler;
+
+	protected ExcelImportLocationContext(IExcelImportEventHandler eventHandler) {
+		super();
+		this.eventHandler = eventHandler;
+	}
 
 	public final void fatal(String message, Object ... args) throws ExcelImportContentException {
 		event(ExcelImportEvent.FATAL, message, (Object[])args);
@@ -15,7 +23,9 @@ public abstract class ExcelImportLocationContext {
 		event(ExcelImportEvent.ERROR, message, (Object[])args);
 	}
 	
-	public abstract void event(ExcelImportErrorEvent event, String message, Object ... args) throws ExcelImportContentException;
+	public void event(ExcelImportErrorEvent event, String message, Object ... args) throws ExcelImportContentException {
+		eventHandler.event(event, getLocation(), message, (Object[])args);
+	}
 
 	public final void warn(String message, Object ... args) {
 		event(ExcelImportEvent.WARNING, message, (Object[])args);
@@ -25,6 +35,10 @@ public abstract class ExcelImportLocationContext {
 		event(ExcelImportEvent.INFO, message, (Object[])args);
 	}
 	
-	public abstract void event(ExcelImportInfoEvent event, String message, Object ... args);
+	public void event(ExcelImportInfoEvent event, String message, Object ... args) {
+		eventHandler.event(event, getLocation(), message, (Object[])args);
+	}
+	
+	public abstract ExcelImportLocation getLocation();
 
 }
