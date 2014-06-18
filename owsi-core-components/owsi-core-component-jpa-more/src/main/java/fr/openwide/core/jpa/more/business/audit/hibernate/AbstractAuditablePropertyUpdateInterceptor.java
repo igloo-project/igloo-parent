3c2562b435740ed2eb48493121e8d1e0a6f51234
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-06-18 09:37:35 +0000

    Merge -r 3315-3317 from branch 0.8-MAINTENANCE.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3318 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/audit/hibernate/AbstractAuditablePropertyUpdateInterceptor.java
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/audit/hibernate/AbstractAuditablePropertyUpdateInterceptor.java
@@ -11,9 +11,10 @@ import com.google.common.base.Objects;
 import fr.openwide.core.jpa.hibernate.interceptor.AbstractChainableInterceptor;
 import fr.openwide.core.jpa.more.business.audit.model.embeddable.AbstractAuditableProperty;
 
-public abstract class AuditablePropertyUpdateInterceptor extends AbstractChainableInterceptor {
-	
-	protected abstract boolean isIntercepted(Object entity);
+public abstract class AbstractAuditablePropertyUpdateInterceptor extends AbstractChainableInterceptor {
+
+	@Override
+	public abstract boolean applyTo(Object entity);
 	
 	protected void updateProperty(AbstractAuditableProperty<?> property) {
 		property.setLastEditDate(new Date());
@@ -23,14 +24,12 @@ public abstract class AuditablePropertyUpdateInterceptor extends AbstractChainab
 	public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
 		boolean stateChanged = false;
 		
-		if (isIntercepted(entity)) {
-			for (int i = 0 ; i < state.length ; ++i) {
-				Type type = types[i];
-				
-				if (AbstractAuditableProperty.class.isAssignableFrom(type.getReturnedClass())) {
-					stateChanged = true;
-					refreshAuditableProperty(state, i, type);
-				}
+		for (int i = 0 ; i < state.length ; ++i) {
+			Type type = types[i];
+			
+			if (AbstractAuditableProperty.class.isAssignableFrom(type.getReturnedClass())) {
+				stateChanged = true;
+				refreshAuditableProperty(state, i, type);
 			}
 		}
 		
@@ -43,17 +42,15 @@ public abstract class AuditablePropertyUpdateInterceptor extends AbstractChainab
 		String[] propertyNames, Type[] types) {
 		boolean stateChanged = false;
 
-		if (isIntercepted(entity)) {
-			for (int i = 0 ; i < currentState.length ; ++i) {
-				Type type = types[i];
-				Object currentProperty = currentState[i];
-				Object oldProperty = previousState[i];
-				
-				if (AbstractAuditableProperty.class.isAssignableFrom(type.getReturnedClass())
-						&& (currentProperty == null || !Objects.equal(currentProperty, oldProperty))) {
-					stateChanged = true;
-					refreshAuditableProperty(currentState, i, type);
-				}
+		for (int i = 0 ; i < currentState.length ; ++i) {
+			Type type = types[i];
+			Object currentProperty = currentState[i];
+			Object oldProperty = previousState[i];
+			
+			if (AbstractAuditableProperty.class.isAssignableFrom(type.getReturnedClass())
+					&& (currentProperty == null || !Objects.equal(currentProperty, oldProperty))) {
+				stateChanged = true;
+				refreshAuditableProperty(currentState, i, type);
 			}
 		}
 		
