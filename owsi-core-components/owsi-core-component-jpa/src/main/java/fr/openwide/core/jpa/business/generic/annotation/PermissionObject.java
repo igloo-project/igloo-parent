4c143b7c49ce73ecef85605a9dd4c3c32e39add9
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-06-27 17:05:17 +0000

    Ajout de l'annotation @PermissionObject, permettant de faire abstraction du nom des paramètres des services d'OWSI-Core lors de la configuration de la sécurité.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3337 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/business/generic/annotation/PermissionObject.java
@@ -0,0 +1,17 @@
+package fr.openwide.core.jpa.business.generic.annotation;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(PARAMETER)
+@Retention(RUNTIME)
+public @interface PermissionObject {
+	
+	String DEFAULT_PERMISSION_OBJECT_NAME = "permissionObject";
+	
+	String value() default DEFAULT_PERMISSION_OBJECT_NAME;
+
+}
