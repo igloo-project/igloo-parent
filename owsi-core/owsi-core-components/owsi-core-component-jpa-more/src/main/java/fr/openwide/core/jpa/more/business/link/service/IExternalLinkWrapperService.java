2f89bde6e0ffcc52e6416a2b7990555f0251fde9
Renaud Joly <joly.renaud@gmail.com>
2014-07-08 11:10:19 +0000

    Nettoyage imports.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3364 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/link/service/IExternalLinkWrapperService.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/link/service/IExternalLinkWrapperService.java
@@ -1,11 +1,7 @@
 package fr.openwide.core.jpa.more.business.link.service;
 
-import io.mola.galimatias.URL;
-
 import java.util.Collection;
 import java.util.List;
-
-import com.google.common.collect.Multimap;
 
 import fr.openwide.core.jpa.business.generic.service.IGenericEntityService;
 import fr.openwide.core.jpa.more.business.link.model.ExternalLinkWrapper;
