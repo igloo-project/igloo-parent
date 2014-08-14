9bbee56926d46c3a9ec9508203f1056567390cde
Florian Lacreuse <lacreuse.florian@gmail.com>
2014-08-14 08:02:39 +0000

    Ajout d'une méthode static notFound retournant un FileStoreResourceStream qui provoquera une ResourceStreamNotFoundException à la lecture.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3424 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/request/resource/FileStoreResourceStream.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/request/resource/FileStoreResourceStream.java
@@ -1,13 +1,25 @@
 package fr.openwide.core.wicket.more.request.resource;
 
 import java.io.File;
+import java.io.InputStream;
 
 import org.apache.wicket.util.lang.Args;
 import org.apache.wicket.util.resource.FileResourceStream;
+import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
 
 public class FileStoreResourceStream extends FileResourceStream {
 
 	private static final long serialVersionUID = -8027981055479218267L;
+	
+	public static FileStoreResourceStream notFound() {
+		return new FileStoreResourceStream(new File("")) {
+			private static final long serialVersionUID = 1L;
+			@Override 
+			public InputStream getInputStream() throws ResourceStreamNotFoundException {
+				throw new ResourceStreamNotFoundException();
+			}
+		};
+	}
 	
 	private String fileName;
 	
