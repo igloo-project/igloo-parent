f9b3894fe346c802a5d3729c2edccba52a450b24
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-09-02 14:31:59 +0000

    Suppression d'injections inutiles.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3484 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/parameter/validator/ConditionLinkParameterValidator.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/parameter/validator/ConditionLinkParameterValidator.java
@@ -2,10 +2,7 @@ package fr.openwide.core.wicket.more.link.descriptor.parameter.validator;
 
 import org.apache.wicket.injection.Injector;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.springframework.security.acls.domain.PermissionFactory;
 
-import fr.openwide.core.jpa.security.service.IAuthenticationService;
 import fr.openwide.core.wicket.more.condition.Condition;
 
 public class ConditionLinkParameterValidator implements ILinkParameterValidator {
@@ -13,12 +10,6 @@ public class ConditionLinkParameterValidator implements ILinkParameterValidator 
 	private static final long serialVersionUID = -6678335084190190566L;
 
 	private final Condition condition;
-	
-	@SpringBean
-	private IAuthenticationService authenticationService;
-	
-	@SpringBean
-	private PermissionFactory permissionFactory;
 	
 	public ConditionLinkParameterValidator(Condition condition) {
 		this.condition = condition;
