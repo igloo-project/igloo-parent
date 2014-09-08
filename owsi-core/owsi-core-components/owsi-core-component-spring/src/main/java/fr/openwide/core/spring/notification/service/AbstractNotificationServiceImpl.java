453e24ed2cfce071b81dc05138b072123ab72cec
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-09-08 16:28:56 +0000

    close #193 Notification mail : problème avec le préfixe de sujet. Provoquera peut-être des problèmes de compilation sur les projets qui font des new NotificationBuilder().
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3504 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-spring/src/main/java/fr/openwide/core/spring/notification/service/AbstractNotificationServiceImpl.java
+++ owsi-core/owsi-core-components/owsi-core-component-spring/src/main/java/fr/openwide/core/spring/notification/service/AbstractNotificationServiceImpl.java
@@ -3,16 +3,13 @@ package fr.openwide.core.spring.notification.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
-import fr.openwide.core.spring.util.SpringBeanUtils;
-
 public class AbstractNotificationServiceImpl {
 	
 	@Autowired
 	protected ApplicationContext applicationContext;
 
 	protected INotificationBuilderBaseState builder() {
-		INotificationBuilderBaseState notificationBuilder = NotificationBuilder.create();
-		SpringBeanUtils.autowireBean(applicationContext, notificationBuilder);
-		return notificationBuilder;
+		INotificationBuilderInitState notificationBuilder = NotificationBuilder.create();
+		return notificationBuilder.init(applicationContext);
 	}
 }
