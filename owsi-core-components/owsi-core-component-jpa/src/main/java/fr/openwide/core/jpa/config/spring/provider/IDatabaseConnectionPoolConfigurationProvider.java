7628ecc170187d452ce72def19f7333f1300e440
Florian Lacreuse <lacreuse.florian@gmail.com>
2014-09-12 13:15:58 +0000

    Ajustements sur la déclaration des datasources.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3522 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/config/spring/provider/IDatabaseConnectionPoolConfigurationProvider.java
@@ -0,0 +1,21 @@
+package fr.openwide.core.jpa.config.spring.provider;
+
+import java.sql.Driver;
+
+public interface IDatabaseConnectionPoolConfigurationProvider {
+
+	Class<Driver> getDriverClass();
+
+	String getUrl();
+
+	String getUser();
+
+	String getPassword();
+
+	int getMinIdle();
+
+	int getMaxPoolSize();
+
+	String getValidationQuery();
+
+}
