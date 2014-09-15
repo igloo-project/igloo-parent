fb768e2794432b3d19cc9939d85234e38b9ea6ef
Sebastien Dot <sebastien.dot@openwide.fr>
2014-09-15 08:25:07 +0000

    gsmet: correction du constructeur de ParameterServiceImpl
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@3524 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/business/parameter/service/ParameterServiceImpl.java
+++ basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/business/parameter/service/ParameterServiceImpl.java
@@ -2,7 +2,6 @@ package fr.openwide.core.basicapp.core.business.parameter.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import fr.openwide.core.jpa.more.business.parameter.dao.IParameterDao;
 import fr.openwide.core.jpa.more.business.parameter.service.AbstractParameterServiceImpl;
@@ -11,7 +10,7 @@ import fr.openwide.core.jpa.more.business.parameter.service.AbstractParameterSer
 public class ParameterServiceImpl extends AbstractParameterServiceImpl implements IParameterService {
 
 	@Autowired
-	public ParameterServiceImpl(IParameterDao dao, PlatformTransactionManager transactionManager) {
-		super(dao, transactionManager);
+	public ParameterServiceImpl(IParameterDao dao) {
+		super(dao);
 	}
 }
