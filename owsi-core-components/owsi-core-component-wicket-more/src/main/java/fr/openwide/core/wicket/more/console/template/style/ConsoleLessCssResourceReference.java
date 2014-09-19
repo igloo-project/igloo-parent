1d1e14819d1b799f5e0613e14a9f314366baa4fc
Florian Lacreuse <lacreuse.florian@gmail.com>
2014-09-19 08:10:05 +0000

    La console mérite aussi d'avoir de jolis select2.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3539 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/console/template/style/ConsoleLessCssResourceReference.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/console/template/style/ConsoleLessCssResourceReference.java
@@ -1,6 +1,15 @@
 package fr.openwide.core.wicket.more.console.template.style;
 
+import java.util.List;
+
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.HeaderItem;
+import org.retzlaff.select2.resource.Select2CssResourceReference;
+
+import com.google.common.collect.Lists;
+
 import fr.openwide.core.wicket.more.lesscss.LessCssResourceReference;
+import fr.openwide.core.wicket.more.markup.html.template.css.bootstrap2.jqueryui.JQueryUiCssResourceReference;
 
 public final class ConsoleLessCssResourceReference extends LessCssResourceReference {
 
@@ -10,6 +19,14 @@ public final class ConsoleLessCssResourceReference extends LessCssResourceRefere
 
 	private ConsoleLessCssResourceReference() {
 		super(ConsoleLessCssResourceReference.class, "console.less");
+	}
+
+	@Override
+	public Iterable<? extends HeaderItem> getDependencies() {
+		List<HeaderItem> dependencies = Lists.newArrayListWithExpectedSize(2);
+		dependencies.add(CssHeaderItem.forReference(JQueryUiCssResourceReference.get()));
+		dependencies.add(CssHeaderItem.forReference(Select2CssResourceReference.get()));
+		return dependencies;
 	}
 
 	public static ConsoleLessCssResourceReference get() {
