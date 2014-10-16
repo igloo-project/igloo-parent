d6b98f92e229ca790108d14459ac163aa28f6382
Renaud Joly <joly.renaud@gmail.com>
2014-10-16 07:59:47 +0000

    Liens externes : on utilise les status inactifs de manière cohérente.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3586 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/link/model/ExternalLinkStatus.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/link/model/ExternalLinkStatus.java
@@ -1,9 +1,15 @@
 package fr.openwide.core.jpa.more.business.link.model;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
 public enum ExternalLinkStatus {
 	ONLINE,
 	OFFLINE,
 	DEAD_LINK,
 	DELETED, // Hibernate has a lot of problem to cascade the removal so let's introduce a specific status for that
-	IGNORED
+	IGNORED;
+	
+	public static final List<ExternalLinkStatus> INACTIVES = ImmutableList.of(DEAD_LINK, DELETED, IGNORED);
 }
