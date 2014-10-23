a0e6cbb31aa2bfc24a972c68ed54f5a84fbe1b30
Yoann Rodiere <yoann.rodiere@openwide.fr>
2014-10-23 08:04:02 +0000

    Correction de AnyTldUrlValidator pour éviter d'imposer des caractères ASCII seulement dans l'URL. J'ai dû faire ça de façon moche, en recopiant une partie du code de la classe mère, mais ça a l'avantage de conserver un comportement quasiment identique, et ça nous évitera des surprises. On verra plus tard si on chamboule tout...
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3603 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-commons/src/test/java/fr/openwide/core/test/commons/util/validator/TestAnyTldDomainValidator.java
+++ owsi-core/owsi-core-components/owsi-core-component-commons/src/test/java/fr/openwide/core/test/commons/util/validator/TestAnyTldDomainValidator.java
@@ -1,26 +1,50 @@
 package fr.openwide.core.test.commons.util.validator;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import fr.openwide.core.commons.util.validator.AnyTldDomainValidator;
 
+@RunWith(Parameterized.class)
 public class TestAnyTldDomainValidator {
+	
+	@Parameters(name = "{index} - \"{0}\" expecting {1}")
+	public static Iterable<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+				{ "www.google.fr", true },
+				{ "google.fr", true },
+				{ "google9.fr", true },
+				{ "gîte-lasoldanelle.com", true },
+				{ "www.gîte-lasoldanelle.com", true },
+				{ "île-lasoldanelle.com", true },
+				{ "location_maechler.monsite-orange.fr", true },
+				{ "-test-plop.fr", false },
+				{ "test-plop-.fr", false },
+				{ "test-plop-.fr", false },
+				{ "test@-plop.fr", false },
+				{ "test,-plop.fr", false }
+		});
+	}
+	
+	@Parameter(0)
+	public String domain;
+	
+	@Parameter(1)
+	public boolean expectedValid;
 	
 	@Test
 	public void testValidation() {
-		Assert.assertTrue(AnyTldDomainValidator.getInstance().isValid("www.google.fr"));
-		Assert.assertTrue(AnyTldDomainValidator.getInstance().isValid("google.fr"));
-		Assert.assertTrue(AnyTldDomainValidator.getInstance().isValid("google9.fr"));
-		Assert.assertTrue(AnyTldDomainValidator.getInstance().isValid("gîte-lasoldanelle.com"));
-		Assert.assertTrue(AnyTldDomainValidator.getInstance().isValid("www.gîte-lasoldanelle.com"));
-		Assert.assertTrue(AnyTldDomainValidator.getInstance().isValid("île-lasoldanelle.com"));
-		Assert.assertTrue(AnyTldDomainValidator.getInstance().isValid("location_maechler.monsite-orange.fr"));
-		Assert.assertFalse(AnyTldDomainValidator.getInstance().isValid("-test-plop.fr"));
-		Assert.assertFalse(AnyTldDomainValidator.getInstance().isValid("test-plop-.fr"));
-		Assert.assertFalse(AnyTldDomainValidator.getInstance().isValid("test-plop-.fr"));
-		Assert.assertFalse(AnyTldDomainValidator.getInstance().isValid("test@-plop.fr"));
-		Assert.assertFalse(AnyTldDomainValidator.getInstance().isValid("test,-plop.fr"));
+		AnyTldDomainValidator validator = AnyTldDomainValidator.getInstance();
+		if (expectedValid) {
+			Assert.assertTrue(validator.isValid(domain));
+		} else {
+			Assert.assertFalse(validator.isValid(domain));
+		}
 	}
-
 }
