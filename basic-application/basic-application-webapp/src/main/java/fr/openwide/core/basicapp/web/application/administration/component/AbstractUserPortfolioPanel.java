acdbe0f08b967d90e08e8695ea11837d0e4ce26f
Florian Lacreuse <lacreuse.florian@gmail.com>
2014-10-31 11:24:08 +0000

    TechnicalUser / BasicUser + interfaces + début du modèle pour le nouveau système de gestion de MDP
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@3647 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/administration/component/AbstractUserPortfolioPanel.java
+++ basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/administration/component/AbstractUserPortfolioPanel.java
@@ -1,54 +1,17 @@
 package fr.openwide.core.basicapp.web.application.administration.component;
 
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.navigation.paging.IPageable;
-import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
-import org.apache.wicket.model.IModel;
 
 import fr.openwide.core.basicapp.core.business.user.model.User;
-import fr.openwide.core.wicket.more.markup.html.navigation.paging.HideablePagingNavigator;
-import fr.openwide.core.wicket.more.markup.repeater.data.OddEvenDataView;
+import fr.openwide.core.wicket.more.markup.html.list.PageablePortfolioPanel;
 
-public abstract class AbstractUserPortfolioPanel<U extends User> extends Panel {
+public abstract class AbstractUserPortfolioPanel<U extends User> extends PageablePortfolioPanel<U> {
 
 	private static final long serialVersionUID = 3233717910646663737L;
-	
-	private final DataView<U> dataView;
-	
-	public AbstractUserPortfolioPanel(String wicketId, IDataProvider<U> dataProvider, int itemsPerPage) {
-		super(wicketId);
-		
-		dataView = new OddEvenDataView<U>("item", dataProvider, itemsPerPage) {
-			private static final long serialVersionUID = 8487422965167269031L;
 
-			@Override
-			protected void populateItem(final Item<U> item) {
-				final IModel<U> itemModel = item.getModel();
-				
-				AbstractUserPortfolioPanel.this.populateItem(item, itemModel);
-			}
-		};
-		add(dataView);
-
-		add(new WebMarkupContainer("emptyList") {
-			private static final long serialVersionUID = 6700720373087584498L;
-
-			@Override
-			public boolean isVisible() {
-				return dataView.getDataProvider().size() == 0;
-			}
-		});
-		
-		add(new HideablePagingNavigator("pager", dataView));
+	public AbstractUserPortfolioPanel(String wicketId, IDataProvider<U> dataProvider, int itemsPerPage,
+			String countRessourceKey) {
+		super(wicketId, dataProvider, itemsPerPage, countRessourceKey);
 	}
-	
-	public IPageable getPageable() {
-		return dataView;
-	}
-
-	protected abstract void populateItem(final Item<U> item, final IModel<U> itemModel);
 
 }
--- basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/administration/component/UserPortfolioPanel.java
+++ basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/administration/component/UserPortfolioPanel.java
@@ -1,20 +1,26 @@
 package fr.openwide.core.basicapp.web.application.administration.component;
 
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import fr.openwide.core.basicapp.core.business.user.model.User;
 import fr.openwide.core.basicapp.core.business.user.service.IUserService;
 import fr.openwide.core.basicapp.core.util.binding.Bindings;
+import fr.openwide.core.basicapp.web.application.BasicApplicationSession;
 import fr.openwide.core.basicapp.web.application.administration.util.AdministrationTypeUser;
+import fr.openwide.core.jpa.exception.SecurityServiceException;
+import fr.openwide.core.jpa.exception.ServiceException;
 import fr.openwide.core.wicket.markup.html.link.EmailLink;
 import fr.openwide.core.wicket.more.link.descriptor.generator.IPageLinkGenerator;
 import fr.openwide.core.wicket.more.link.model.PageModel;
 import fr.openwide.core.wicket.more.markup.html.image.BooleanIcon;
 import fr.openwide.core.wicket.more.model.BindingModel;
+import fr.openwide.core.wicket.more.model.ReadOnlyModel;
 
 public class UserPortfolioPanel<U extends User> extends AbstractUserPortfolioPanel<U> {
 
@@ -26,23 +32,65 @@ public class UserPortfolioPanel<U extends User> extends AbstractUserPortfolioPan
 	private final AdministrationTypeUser<U> type;
 
 	public UserPortfolioPanel(String id, IDataProvider<U> dataProvider, AdministrationTypeUser<U> type, int itemsPerPage) {
-		super(id, dataProvider, itemsPerPage);
+		super(id, dataProvider, itemsPerPage, "administration.user.count");
 		this.type = type;
 	}
 
-	@Override
-	protected void populateItem(Item<U> item, IModel<U> userModel) {
-		item.add(
-				getPageLinkGenerator(item.getModel()).link("userNameLink").setBody(BindingModel.of(userModel, Bindings.user().userName())),
-				new Label("firstName", BindingModel.of(userModel, Bindings.user().firstName())),
-				new Label("lastName", BindingModel.of(userModel, Bindings.user().lastName())),
-				new BooleanIcon("active", BindingModel.of(userModel, Bindings.user().active())).hideIfNullOrFalse(),
-				new EmailLink("email", BindingModel.of(userModel, Bindings.user().email()))
-		);
-	}
-	
 	protected IPageLinkGenerator getPageLinkGenerator(IModel<U> userModel) {
 		return type.fiche(userModel, PageModel.of(getPage()));
+	}
+
+	@Override
+	protected void addItemColumns(Item<U> item, IModel<? extends U> itemModel) {
+		item.add(
+				getPageLinkGenerator(item.getModel()).link("userNameLink").setBody(BindingModel.of(itemModel, Bindings.user().userName())),
+				new Label("firstName", BindingModel.of(itemModel, Bindings.user().firstName())),
+				new Label("lastName", BindingModel.of(itemModel, Bindings.user().lastName())),
+				new BooleanIcon("active", BindingModel.of(itemModel, Bindings.user().active())).hideIfNullOrFalse(),
+				new EmailLink("email", BindingModel.of(itemModel, Bindings.user().email()))
+		);
+	}
+
+	@Override
+	protected boolean hasWritePermissionOn(IModel<? extends U> itemModel) {
+		return BasicApplicationSession.get().hasRoleAdmin();
+	}
+
+	@Override
+	protected void doDeleteItem(IModel<? extends U> itemModel) throws ServiceException, SecurityServiceException {
+		userService.delete(itemModel.getObject()); 
+	}
+	
+	@Override
+	protected IModel<String> getDeleteConfirmationTitleModel(IModel<? extends U> userModel) {
+		return new StringResourceModel("administration.user.delete.confirmation.title", null, new Object[] { userModel
+				.getObject().getFullName() });
+	}
+
+	@Override
+	protected IModel<String> getDeleteConfirmationTextModel(IModel<? extends U> userModel) {
+		return new StringResourceModel("administration.user.delete.confirmation.text", null, new Object[] { userModel
+				.getObject().getFullName() });
+	}
+
+	@Override
+	protected boolean isActionAvailable() {
+		return true;
+	}
+
+	@Override 
+	protected MarkupContainer getActionLink(String id, IModel<? extends U> userModel) { 
+		return getPageLinkGenerator(ReadOnlyModel.of(userModel)).link(id); 
+	}
+
+	@Override
+	protected boolean isDeleteAvailable() {
+		return BasicApplicationSession.get().hasRoleAdmin();
+	}
+
+	@Override
+	protected boolean isEditAvailable() {
+		return false;
 	}
 
 }
