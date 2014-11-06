5bbe15ed1752e171db97bd75dcafedfba548d67a
Florian Lacreuse <lacreuse.florian@gmail.com>
2014-11-06 09:14:11 +0000

    Gestion MDP : ajustements suite remarques YRO (DelegatedMarkupPanel -> Fragment)
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@3739 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/navigation/page/MaintenancePage.java
+++ basic-application/basic-application-webapp/src/main/java/fr/openwide/core/basicapp/web/application/navigation/page/MaintenancePage.java
@@ -1,12 +1,12 @@
 package fr.openwide.core.basicapp.web.application.navigation.page;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import fr.openwide.core.basicapp.web.application.common.template.ServiceTemplate;
-import fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.bootstrap.modal.component.DelegatedMarkupPanel;
 import fr.openwide.core.wicket.more.markup.html.template.model.BreadCrumbElement;
 
 public class MaintenancePage extends ServiceTemplate {
@@ -26,7 +26,7 @@ public class MaintenancePage extends ServiceTemplate {
 
 	@Override
 	protected Component getContentComponent(String wicketId) {
-		return new DelegatedMarkupPanel(wicketId, "contentFragment", getClass());
+		return new Fragment(wicketId, "contentFragment", this);
 	}
 
 	@Override
