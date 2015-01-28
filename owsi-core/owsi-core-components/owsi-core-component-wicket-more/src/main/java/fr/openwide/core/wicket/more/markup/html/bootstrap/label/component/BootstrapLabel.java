b5dc683adc4c5cdb518b5e80a4461583ae437c8c
Florian Lacreuse <lacreuse.florian@gmail.com>
2015-01-28 14:00:57 +0000

    BootstrapLabel caché si label vide.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3848 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/bootstrap/label/component/BootstrapLabel.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/bootstrap/label/component/BootstrapLabel.java
@@ -5,7 +5,10 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
 
+import fr.openwide.core.commons.util.functional.Predicates2;
 import fr.openwide.core.wicket.behavior.ClassAttributeAppender;
+import fr.openwide.core.wicket.more.markup.html.basic.ComponentBooleanProperty;
+import fr.openwide.core.wicket.more.markup.html.basic.EnclosureBehavior;
 import fr.openwide.core.wicket.more.markup.html.bootstrap.label.behavior.BootstrapColorBehavior;
 import fr.openwide.core.wicket.more.markup.html.bootstrap.label.renderer.BootstrapLabelRenderer;
 
@@ -16,13 +19,19 @@ public class BootstrapLabel<T> extends GenericPanel<T> {
 	public BootstrapLabel(String id, IModel<T> model, final BootstrapLabelRenderer<? super T> renderer) {
 		super(id, model);
 		
+		IModel<String> labelModel = renderer.asModel(model);
+		
 		add(
 				new WebMarkupContainer("icon")
 						.add(new ClassAttributeAppender(renderer.asIconCssClassModel(model))),
-				new Label("label", renderer.asModel(model))
+				new Label("label", labelModel)
 		);
 		
-		add(BootstrapColorBehavior.label(renderer.asColorModel(model)));
+		add(
+				BootstrapColorBehavior.label(renderer.asColorModel(model)),
+				new EnclosureBehavior(ComponentBooleanProperty.VISIBLE).model(Predicates2.hasText(), labelModel)
+		);
+		
 	}
 
 }
