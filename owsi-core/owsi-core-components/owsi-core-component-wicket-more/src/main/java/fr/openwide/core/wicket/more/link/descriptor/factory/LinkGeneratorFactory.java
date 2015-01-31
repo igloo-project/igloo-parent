4ff11e0cfb0c5f1ab1678b7de4f1b7f87583dce2
Yoann Rodiere <yoann.rodiere@openwide.fr>
2015-01-31 16:18:01 +0000

    Ajout de BindingLinkGeneratorFactory.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3859 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/factory/LinkGeneratorFactory.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/link/descriptor/factory/LinkGeneratorFactory.java
@@ -1,15 +1,18 @@
 package fr.openwide.core.wicket.more.link.descriptor.factory;
 
-import java.io.Serializable;
-
+import org.apache.wicket.model.IDetachable;
 import org.apache.wicket.model.IModel;
 
 import fr.openwide.core.wicket.more.link.descriptor.generator.ILinkGenerator;
 
-public abstract class LinkGeneratorFactory<T> implements Serializable {
+public abstract class LinkGeneratorFactory<T> implements IDetachable {
 
 	private static final long serialVersionUID = 7320937503075282012L;
 
 	public abstract ILinkGenerator create(IModel<T> model);
 
+	@Override
+	public void detach() {
+		// Rien par défaut
+	}
 }
