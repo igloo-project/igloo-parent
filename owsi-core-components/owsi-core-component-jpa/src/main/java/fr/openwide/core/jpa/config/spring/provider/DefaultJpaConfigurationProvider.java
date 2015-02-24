3995aed96a569adf599f344ec75b0d3cea9cf010
Yoann Rodiere <yoann.rodiere@openwide.fr>
2015-02-24 17:05:34 +0000

    Ajout du support de la propriété hibernate.search.indexing_strategy dans les options de config JPA. En passant, dépréciation de quelques méthodes à rallonge pour faciliter l'ajout du support de propriétés à l'avenir.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3892 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/config/spring/provider/DefaultJpaConfigurationProvider.java
+++ owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/config/spring/provider/DefaultJpaConfigurationProvider.java
@@ -10,7 +10,7 @@ import org.hibernate.dialect.Dialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
-public class DefaultJpaConfigurationProvider {
+public class DefaultJpaConfigurationProvider implements IJpaConfigurationProvider {
 
 	@Autowired
 	private List<JpaPackageScanProvider> jpaPackageScanProviders;
@@ -29,6 +29,9 @@ public class DefaultJpaConfigurationProvider {
 
 	@Value("${lucene.index.path}")
 	private String hibernateSearchIndexBase;
+	
+	@Value("${hibernate.search.indexing_strategy}")
+	private String hibernateSearchIndexingStrategy;
 
 	@Value("#{dataSource}")
 	private DataSource dataSource;
@@ -54,58 +57,77 @@ public class DefaultJpaConfigurationProvider {
 	@Value("${hibernate.create_empty_composites.enabled}")
 	private boolean createEmptyCompositesEnabled;
 
+	@Override
 	public List<JpaPackageScanProvider> getJpaPackageScanProviders() {
 		return jpaPackageScanProviders;
 	}
 
+	@Override
 	public Class<Dialect> getDialect() {
 		return dialect;
 	}
 
+	@Override
 	public String getHbm2Ddl() {
 		return hbm2Ddl;
 	}
 
+	@Override
 	public String getHbm2DdlImportFiles() {
 		return hbm2DdlImportFiles;
 	}
 
+	@Override
 	public Integer getDefaultBatchSize() {
 		return defaultBatchSize;
 	}
 
+	@Override
 	public String getHibernateSearchIndexBase() {
 		return hibernateSearchIndexBase;
 	}
+	
+	@Override
+	public String getHibernateSearchIndexingStrategy() {
+		return hibernateSearchIndexingStrategy;
+	}
 
+	@Override
 	public DataSource getDataSource() {
 		return dataSource;
 	}
 
+	@Override
 	public String getEhCacheConfiguration() {
 		return ehCacheConfiguration;
 	}
 
+	@Override
 	public boolean isEhCacheSingleton() {
 		return ehCacheSingleton;
 	}
 
+	@Override
 	public boolean isQueryCacheEnabled() {
 		return queryCacheEnabled;
 	}
 
+	@Override
 	public PersistenceProvider getPersistenceProvider() {
 		return persistenceProvider;
 	}
 
+	@Override
 	public String getValidationMode() {
 		return validationMode;
 	}
 	
+	@Override
 	public Class<NamingStrategy> getNamingStrategy() {
 		return namingStrategy;
 	}
 
+	@Override
 	public boolean isCreateEmptyCompositesEnabled() {
 		return createEmptyCompositesEnabled;
 	}
