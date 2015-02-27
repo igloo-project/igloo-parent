3e7feb5aba678bcd3f1d06da23c955a8e0af29e6
Sebastien Dot <sebastien.dot@openwide.fr>
2015-02-27 14:35:16 +0000

    Suppression des classes deprecated HumanReadableFileSizeLabelModel et HumanReadableBytesConverter (close #86)
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3906 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/model/BytesToLongModel.java
@@ -0,0 +1,47 @@
+package fr.openwide.core.wicket.more.markup.html.model;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.util.lang.Bytes;
+
+public class BytesToLongModel extends LoadableDetachableModel<Long> {
+
+	private static final long serialVersionUID = 3511276823849427019L;
+
+	private final IModel<Bytes> bytesModel;
+
+	protected BytesToLongModel(IModel<Bytes> bytesModel) {
+		super();
+		this.bytesModel = bytesModel;
+	}
+
+	@Override
+	protected Long load() {
+		if (bytesModel.getObject() == null) {
+			return null;
+		} else {
+			return Long.valueOf(bytesModel.getObject().bytes());
+		}
+	}
+
+	@Override
+	public void detach() {
+		bytesModel.detach();
+		super.detach();
+	}
+
+	@Override
+	public void setObject(Long object) {
+		if (object == null) {
+			bytesModel.setObject(null);
+		} else {
+			bytesModel.setObject(Bytes.bytes(object));
+		}
+		super.setObject(object);
+	}
+
+	public static final BytesToLongModel of(IModel<Bytes> bytesModel) {
+		return new BytesToLongModel(bytesModel);
+	}
+
+}
