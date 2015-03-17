38e0b9f959140eb6b7b4941c9429a5e4dcf76d9a
Florian Lacreuse <lacreuse.florian@gmail.com>
2015-03-17 09:59:55 +0000

    AjaxInputPrerequisiteBehavior + utilisation des ComponentBooleanProperty + AjaxInputPrerequisiteVisibilityAllowedBehavior
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3962 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/AjaxInputPrerequisiteVisibilityAllowedBehavior.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/AjaxInputPrerequisiteVisibilityAllowedBehavior.java
@@ -5,7 +5,7 @@ import org.apache.wicket.markup.html.form.FormComponent;
 import fr.openwide.core.wicket.more.markup.html.basic.ComponentBooleanProperty;
 
 /**
- * Makes sure that the attached component is disabled and that its model is null
+ * Makes sure that the attached component is hidden and that its model is null
  * so long that a given prerequisite component's input is null or invalid.<br>
  * This behavior effectively enables sequenced input (first the user must input
  * <code>prerequisiteField</code>'s content, then the attached component's
@@ -13,12 +13,12 @@ import fr.openwide.core.wicket.more.markup.html.basic.ComponentBooleanProperty;
  * 
  * @see AjaxInputPrerequisiteBehavior
  */
-public class AjaxInputPrerequisiteEnabledBehavior<T> extends AjaxInputPrerequisiteBehavior<T> {
+public class AjaxInputPrerequisiteVisibilityAllowedBehavior<T> extends AjaxInputPrerequisiteBehavior<T> {
 
-	private static final long serialVersionUID = 3907107358503401628L;
+	private static final long serialVersionUID = -745132571908314845L;
 
-	public AjaxInputPrerequisiteEnabledBehavior(FormComponent<T> prerequisiteField) {
-		super(prerequisiteField, ComponentBooleanProperty.ENABLE);
+	public AjaxInputPrerequisiteVisibilityAllowedBehavior(FormComponent<T> prerequisiteField) {
+		super(prerequisiteField, ComponentBooleanProperty.VISIBILITY_ALLOWED);
 	}
 
 }
