cf04eb019b68880dcebf93ff3e00f2a2b74beaa8
Thomas Bouchacot <thomas.bouchacot@openwide.fr>
2015-04-07 16:26:35 +0000

    Ajout d'un boolean dropdown single choice
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@3988 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/rendering/BooleanRenderer.java
@@ -0,0 +1,81 @@
+package fr.openwide.core.wicket.more.rendering;
+
+import java.util.Locale;
+
+import org.apache.wicket.Localizer;
+import org.apache.wicket.model.Model;
+
+import fr.openwide.core.spring.util.StringUtils;
+
+public class BooleanRenderer extends Renderer<Boolean> {
+
+	private static final long serialVersionUID = -6934415690685574154L;
+	
+	private static BooleanRenderer INSTANCE = new BooleanRenderer("common.boolean", null);
+	
+	public static BooleanRenderer get() {
+		return INSTANCE;
+	}
+	
+	public static BooleanRenderer withPrefix(String prefix) {
+		return with(prefix, null);
+	}
+	
+	public static BooleanRenderer withSuffix(String suffix) {
+		return with(null, suffix);
+	}
+	
+	public static BooleanRenderer with(String prefix, String suffix) {
+		return new BooleanRenderer(prefix, suffix);
+	}
+	
+	private String prefix = null;
+	
+	private String suffix = null;
+	
+	/**
+	 * @deprecated Use the static factory methods instead.
+	 */
+	@Deprecated
+	protected BooleanRenderer() { }
+
+	/**
+	 * @deprecated Use the static factory methods instead.
+	 */
+	@Deprecated
+	protected BooleanRenderer(String prefix, String suffix) {
+		this();
+		this.prefix = prefix;
+		this.suffix = suffix;
+	}
+
+	@Override
+	public String render(Boolean value, Locale locale) {
+		if (value == null) {
+			return null;
+		}
+		
+		StringBuilder key = new StringBuilder();
+		
+		if (StringUtils.hasText(prefix)) {
+			key.append(prefix).append(".");
+		}
+		
+		key.append(resourceKey(value));
+		
+		if (StringUtils.hasText(suffix)) {
+			key.append(".").append(suffix);
+		}
+		
+		return Localizer.get().getString(key.toString(), null, Model.of(value));
+	}
+
+	/**
+	 * @deprecated Do not override this.
+	 */
+	@Deprecated
+	protected String resourceKey(Boolean value) {
+		return value.toString();
+	}
+
+}
