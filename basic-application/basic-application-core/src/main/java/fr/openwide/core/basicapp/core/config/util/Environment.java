2619ede49dd9cad89a21f8ca77c0f32138c9d02d
Yoann Rodiere <yoann.rodiere@openwide.fr>
2015-04-13 17:35:00 +0000

    Passe Sonar.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/archetype@4004 d3474844-eb8e-4abc-b058-2b321fed648b

--- basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/config/util/Environment.java
+++ basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/config/util/Environment.java
@@ -1,8 +1,8 @@
 package fr.openwide.core.basicapp.core.config.util;
 
 public enum Environment {
-	development,
-	testing,
-	staging,
-	production
+	development, // NOSONAR
+	testing, // NOSONAR
+	staging, // NOSONAR
+	production // NOSONAR
 }
