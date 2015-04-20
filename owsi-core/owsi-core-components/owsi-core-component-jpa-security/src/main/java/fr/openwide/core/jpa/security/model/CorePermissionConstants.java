84ca3b7f36b9687e8e813e9cfc9c69b0b93e6bff
Florian Lacreuse <lacreuse.florian@gmail.com>
2015-04-20 14:11:51 +0000

    Ajout permission UPDATE
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@4026 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/model/CorePermissionConstants.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/model/CorePermissionConstants.java
@@ -6,6 +6,7 @@ public class CorePermissionConstants {
 	public static final String WRITE = "WRITE";
 	public static final String ADMINISTRATION = "ADMINISTRATION";
 	public static final String CREATE = "CREATE";
+	public static final String UPDATE = "UPDATE";
 	public static final String DELETE = "DELETE";
 	public static final String ALLOWED = "ALLOWED";
 	public static final String DENIED = "DENIED";
