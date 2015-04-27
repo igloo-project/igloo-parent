e94c82b126131b178ea855ba814d106dd07a1f60
Yoann Rodiere <yoann.rodiere@openwide.fr>
2015-04-27 08:09:13 +0000

    Ajout d'un FieldBridge pour les GenericEntityReference.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@4040 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/search/bridge/GenericEntityReferenceFieldBridge.java
@@ -0,0 +1,38 @@
+package fr.openwide.core.jpa.search.bridge;
+
+import org.apache.lucene.document.Document;
+import org.hibernate.search.bridge.FieldBridge;
+import org.hibernate.search.bridge.LuceneOptions;
+import org.hibernate.search.bridge.StringBridge;
+
+import fr.openwide.core.jpa.business.generic.model.GenericEntityReference;
+
+public class GenericEntityReferenceFieldBridge implements FieldBridge, StringBridge {
+	
+	@Override
+	public String objectToString(Object object) {
+		if (object == null) {
+			return null;
+		}
+		if (!(object instanceof GenericEntityReference)) {
+			throw new IllegalArgumentException("This FieldBridge only supports GenericEntityReference properties.");
+		}
+		GenericEntityReference<?, ?> entityReference = (GenericEntityReference<?, ?>) object;
+		Object id = entityReference.getEntityId();
+		Class<?> clazz = entityReference.getEntityClass();
+		if (clazz == null) {
+			return null;
+		} else {
+			return clazz.getCanonicalName() + "|" + id.toString();
+		}
+	}
+	
+	@Override
+	public void set(String name, Object value, Document document, LuceneOptions luceneOptions) {
+		if (value == null) {
+			return;
+		}
+		luceneOptions.addFieldToDocument(name, objectToString(value), document);
+	}
+
+}
