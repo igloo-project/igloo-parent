91c2023fd6d517e4d6eb5b19d0e9e1ca8fab7cde
Yoann Rodiere <yoann.rodiere@openwide.fr>
2015-04-29 14:30:53 +0000

    Meilleure gestion des exceptions dans TransactionSynchronizationTaksAdapter ; en particulier, en cas d'erreur "beforeCommit", on veut faire un rollback...
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/trunk/owsi-core@4057 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/util/transaction/exception/TransactionSynchronizationException.java
@@ -0,0 +1,17 @@
+package fr.openwide.core.jpa.more.util.transaction.exception;
+
+import org.springframework.core.NestedRuntimeException;
+
+public class TransactionSynchronizationException extends NestedRuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public TransactionSynchronizationException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public TransactionSynchronizationException(String msg) {
+		super(msg);
+	}
+
+}
