50ba3af3baf0cfc822644bcbcccb900ee687fb57
Renaud Joly <joly.renaud@gmail.com>
2015-05-27 09:51:27 +0000

    Export Excel : capacité à rendre une enum avec suffixe et/ou préfixe de clé de trad.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.9-MAINTENANCE@4143 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/export/excel/AbstractSimpleExcelTableExport.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/export/excel/AbstractSimpleExcelTableExport.java
@@ -10,14 +10,13 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.wicket.Application;
 import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.util.lang.Classes;
 
 import com.google.common.base.Joiner;
 
 import fr.openwide.core.export.excel.AbstractExcelTableExport;
-import fr.openwide.core.wicket.markup.html.model.CountMessageModel;
+import fr.openwide.core.spring.util.StringUtils;
 
 /**
  * Apporte quelques configurations de style ainsi que le support de l'extraction de traduction via wicket.
@@ -71,7 +70,28 @@ public abstract class AbstractSimpleExcelTableExport extends AbstractExcelTableE
 	}
 
 	protected String localize(Enum<?> enumValue) {
-		return enumValue == null ? null : localize(Classes.simpleName(enumValue.getDeclaringClass()) + "." + enumValue.name());
+		return localize(enumValue, null, null);
+	}
+	
+	protected <E> String localize(Enum<?> enumValue, String prefix, String suffix) {
+		if (enumValue == null) {
+			return null;
+		}
+		
+		StringBuilder key = new StringBuilder();
+		
+		if (StringUtils.hasText(prefix)) {
+			key.append(prefix).append(".");
+		}
+		
+		key.append(Classes.simpleName(enumValue.getDeclaringClass()))
+				.append(".").append(enumValue.name());
+		
+		if (StringUtils.hasText(suffix)) {
+			key.append(".").append(suffix);
+		}
+		
+		return localize(key.toString());
 	}
 	
 	protected <E> String localize(Class<E> clazz, E value) {
