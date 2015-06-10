37f812ddb8f49d548aebba522af0227cd5edbf2f
Julien Benichou <julien.benichou.0@gmail.com>
2015-06-10 09:17:28 +0000

    Implémentation de méthodes pour se logger avec un utilisateur (utile pour les crons par exemple)
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.9-MAINTENANCE@4152 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/business/person/util/UserUtils.java
@@ -0,0 +1,10 @@
+package fr.openwide.core.jpa.security.business.person.util;
+
+public class UserUtils {
+
+	public static final String SYSTEM_FAFCEA_USERNAME = "system-fafcea";
+
+	private UserUtils() {
+	}
+
+}
