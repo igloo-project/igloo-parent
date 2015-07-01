eef132d87631759fa3a26f036960675a1f8309bb
Yoann Rodiere <yoann.rodiere@openwide.fr>
2015-07-01 10:09:34 +0000

    DatePicker : ajout de la possibilité de préciser des formats alternatifs (à utiliser dans le convertToObject en cas d'erreur) ou "préemptifs" (à utiliser en priorité dans le convertToObject si possible).
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.9-MAINTENANCE@4196 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/util/convert/converters/CascadingConverter.java
@@ -0,0 +1,61 @@
+package fr.openwide.core.wicket.more.util.convert.converters;
+
+import java.util.Locale;
+
+import org.apache.wicket.util.convert.ConversionException;
+import org.apache.wicket.util.convert.IConverter;
+
+import com.google.common.collect.ImmutableList;
+
+public class CascadingConverter<T> implements IConverter<T> {
+	
+	private static final long serialVersionUID = 4943347630093315725L;
+	
+	private final Iterable<? extends IConverter<? extends T>> preemptiveConverters;
+
+	private final IConverter<T> mainConverter;
+	
+	private final Iterable<? extends IConverter<? extends T>> alternativeConverters;
+
+	public CascadingConverter(IConverter<T> mainConverter,
+			Iterable<? extends IConverter<? extends T>> preemptiveConverters,
+			Iterable<? extends IConverter<? extends T>> alternativeConverters) {
+		super();
+		this.preemptiveConverters = ImmutableList.copyOf(preemptiveConverters);
+		this.mainConverter = mainConverter;
+		this.alternativeConverters = ImmutableList.copyOf(alternativeConverters);
+	}
+
+	@Override
+	public T convertToObject(String value, Locale locale) throws ConversionException {
+		// First try the preemptive converters
+		for (IConverter<? extends T> preemptiveConverter : preemptiveConverters) {
+			try {
+				return preemptiveConverter.convertToObject(value, locale);
+			} catch (ConversionException ignored) {
+			}
+		}
+
+		// If they didn't work, ignore the errors and try the main converter
+		try {
+			return mainConverter.convertToObject(value, locale);
+		} catch (ConversionException e) {
+			// If the main converter didn't work, try an alternative converter
+			for (IConverter<? extends T> alternativeConverter : alternativeConverters) {
+				try {
+					return alternativeConverter.convertToObject(value, locale);
+				} catch (ConversionException ignored) {
+				}
+			}
+			// If no alternative converter managed to convert, just throw the original exception.
+			throw e;
+		}
+	}
+
+	@Override
+	public String convertToString(T value, Locale locale) {
+		// Just use the main converter.
+		return mainConverter.convertToString(value, locale);
+	}
+
+}
