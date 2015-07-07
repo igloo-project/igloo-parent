430c48af341e9d66d6c0159a1943ea4aefc29061
Renaud Joly <joly.renaud@gmail.com>
2015-07-07 06:58:44 +0000

    SearchQuery :  * création d'une AbstractSearchQuery ;  * premier jet pour l'implémentation JPA ;  * ajustements sur la mécanique Hibernate Search.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.9-MAINTENANCE@4205 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/search/query/AbstractSearchQuery.java
@@ -0,0 +1,43 @@
+package fr.openwide.core.jpa.more.business.search.query;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import fr.openwide.core.jpa.more.business.sort.ISort;
+import fr.openwide.core.jpa.more.business.sort.ISort.SortOrder;
+
+public abstract class AbstractSearchQuery<T, S extends ISort<?>> implements ISearchQuery<T, S> /* NOT Serializable */ {
+	
+	@PersistenceContext
+	protected EntityManager entityManager;
+	
+	protected List<S> defaultSorts;
+	protected Map<S, SortOrder> sortMap;
+	
+	@SafeVarargs
+	protected AbstractSearchQuery(S ... defaultSorts) {
+		this.defaultSorts = ImmutableList.copyOf(defaultSorts);
+	}
+	
+	// Sort
+	@Override
+	public ISearchQuery<T, S> sort(Map<S, SortOrder> sortMap) {
+		this.sortMap = ImmutableMap.copyOf(sortMap);
+		return this;
+	}
+	
+	// List and count
+	@Override
+	@Transactional(readOnly = true)
+	public List<T> fullList() {
+		return list(0L, Integer.MAX_VALUE);
+	}
+}
--- owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/sort/SortUtils.java
+++ owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/sort/SortUtils.java
@@ -15,6 +15,8 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Ordering;
+import com.mysema.query.types.OrderSpecifier;
+import com.mysema.query.types.expr.ComparableExpressionBase;
 
 import fr.openwide.core.jpa.more.business.sort.ISort.NullSortValue;
 import fr.openwide.core.jpa.more.business.sort.ISort.SortNull;
@@ -24,6 +26,22 @@ public final class SortUtils {
 	
 	private SortUtils() { }
 	
+	// Common
+	private static boolean isReverse(ISort<?> sort, SortOrder order) {
+		return SortOrder.DESC == sort.getDefaultOrder().asDefaultFor(order);
+	}
+
+	public static <S, T extends ISort<S>> List<S> collectSortFields(Map<T, SortOrder> sortsMap) {
+		List<S> sortFields = Lists.newArrayList();
+		if (sortsMap != null) {
+			for (Entry<T, SortOrder> sortOrderEntry : sortsMap.entrySet()) {
+				sortFields.addAll(sortOrderEntry.getKey().getSortFields(sortOrderEntry.getValue()));
+			}
+		}
+		return sortFields;
+	}
+	
+	// Hibernate Search
 	@SafeVarargs
 	public static <T extends ISort<?>> Map<T, SortOrder> composite(T first, T ... rest) {
 		return composite(Lists.asList(first, rest));
@@ -62,20 +80,6 @@ public final class SortUtils {
 		return Sorting.getStringSortField(fieldName, isReverse(sort, order),
 				sortNull.isLast(defaultedOrder), sortNull.isFirst(defaultedOrder));
 	}
-	
-	private static boolean isReverse(ISort<SortField> sort, SortOrder order) {
-		return SortOrder.DESC == sort.getDefaultOrder().asDefaultFor(order);
-	}
-
-	public static <T extends ISort<SortField>> List<SortField> collectSortFields(Map<T, SortOrder> sortsMap) {
-		List<SortField> sortFields = Lists.newArrayList();
-		if (sortsMap != null) {
-			for (Entry<T, SortOrder> sortOrderEntry : sortsMap.entrySet()) {
-				sortFields.addAll(sortOrderEntry.getKey().getSortFields(sortOrderEntry.getValue()));
-			}
-		}
-		return sortFields;
-	}
 
 	@SafeVarargs
 	public static <T extends ISort<SortField>> Sort getLuceneSortWithDefaults(
@@ -110,6 +114,31 @@ public final class SortUtils {
 			sortFields.addAll(defaultSort.getSortFields(defaultSort.getDefaultOrder()));
 		}
 		return Ordering.compound(sortFields);
+	}
+	
+	// JPA
+	public static <T extends Comparable<?>> OrderSpecifier<T> orderSpecifier(ISort<OrderSpecifier<?>> sort,
+			SortOrder order, ComparableExpressionBase<T> expressionBase) {
+		if (isReverse(sort, order)) {
+			return expressionBase.desc();
+		} else {
+			return expressionBase.asc();
+		}
+	}
+
+	@SafeVarargs
+	public static <T extends ISort<OrderSpecifier<?>>> List<OrderSpecifier<?>> getOrderSpecifierWithDefaults(
+			Map<T, SortOrder> sortsMap, T firstDefaultSort, T ... otherDefaultSorts) {
+		return getOrderSpecifierWithDefaults(sortsMap, Lists.asList(firstDefaultSort, otherDefaultSorts));
+	}
+
+	public static <T extends ISort<OrderSpecifier<?>>> List<OrderSpecifier<?>> getOrderSpecifierWithDefaults(
+				Map<T, SortOrder> sortsMap, List<T> list) {
+		List<OrderSpecifier<?>> sortFields = collectSortFields(sortsMap);
+		for (T defaultSort : list) {
+			sortFields.addAll(defaultSort.getSortFields(defaultSort.getDefaultOrder()));
+		}
+		return sortFields;
 	}
 
 }
