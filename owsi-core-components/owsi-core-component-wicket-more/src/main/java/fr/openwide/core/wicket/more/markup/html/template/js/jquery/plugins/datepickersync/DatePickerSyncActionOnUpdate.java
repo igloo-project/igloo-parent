781e586c2b67fd8cc17335e2fc746d6338caead4
Laurent Almeras <lalmeras@gmail.com>
2015-07-09 16:19:02 +0000

    enhancement - ajout d'une configuration sur le datepickersync qui permette de prendre en compte dynamiquement les modifications dateMin / dateMax si la date n'est plus dans les bornes (soit rien - par défaut, NOTHING, soit on vide la date - EMPTY, soit on modifie la date pour la placer dans l'intervale - UPDATE)
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.9-MAINTENANCE@4210 d3474844-eb8e-4abc-b058-2b321fed648b

---
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/template/js/jquery/plugins/datepickersync/DatePickerSyncActionOnUpdate.java
@@ -0,0 +1,23 @@
+package fr.openwide.core.wicket.more.markup.html.template.js.jquery.plugins.datepickersync;
+
+public enum DatePickerSyncActionOnUpdate {
+
+	/**
+	 * Pas de modification de la date si les dates suivantes et précédentes sont incohérentes avec la date du champ.
+	 * Toutefois, au moment du clic dans le champ, les mécanismes dateMin / dateMax peuvent mettre à jour le champ.
+	 */
+	NOTHING,
+
+	/**
+	 * Si les dates suivantes ou précédentes ne sont pas cohérentes avec la date courante, alors la date courante est
+	 * modifiée par la date mini ou maxi (en fonction du champ modifié, précédent ou suivant)
+	 */
+	UPDATE,
+
+	/**
+	 * Si les dates suivantes ou précédentes ne sont pas cohérentes avec la date courante, alors la date courante est
+	 * vidée
+	 */
+	EMPTY;
+
+}
