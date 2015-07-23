309c971fdde64c67adbd67ed5f8d363284f0a7ce
Florian Lacreuse <lacreuse.florian@gmail.com>
2015-07-23 09:04:40 +0000

    DataTableBuilder : ajout d'une factory pour le CoreDataTable.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.9-MAINTENANCE@4215 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/repeater/data/table/builder/IDataTableFactory.java
+++ owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/repeater/data/table/builder/IDataTableFactory.java
@@ -4,12 +4,13 @@ import java.util.Map;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.util.io.IClusterable;
 
 import fr.openwide.core.jpa.more.business.sort.ISort;
 import fr.openwide.core.wicket.more.condition.Condition;
 import fr.openwide.core.wicket.more.markup.html.repeater.data.table.CoreDataTable;
 
-public interface IDataTableFactory<T, S extends ISort<?>> {
+public interface IDataTableFactory<T, S extends ISort<?>> extends IClusterable {
 
 	public CoreDataTable<T, S> build(String id, Map<IColumn<T, S>, Condition> columns, IDataProvider<T> dataProvider, long rowsPerPage);
 
