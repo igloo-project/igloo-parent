f760097efcabffaecad6b71bd8bee2cf8711eecc
Yoann Rodiere <yoann.rodiere@openwide.fr>
2015-08-24 15:10:00 +0000

    Correction de la configuration par défaut de OWSI-Core lorsqu'on utilise les annotation @PreAuthorized, etc. : on active la reconnaissance des annotations @PermissionObject par défaut.
    git-svn-id: svn+ssh://svn.projects.openwide.fr/svnroot/owsi-tools/branches/owsi-core-0.9-MAINTENANCE@4265 d3474844-eb8e-4abc-b058-2b321fed648b

--- owsi-core/owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/config/spring/AbstractJpaSecuritySecuredConfig.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-security/src/main/java/fr/openwide/core/jpa/security/config/spring/AbstractJpaSecuritySecuredConfig.java
@@ -3,6 +3,15 @@ package fr.openwide.core.jpa.security.config.spring;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.core.parameters.AnnotationParameterNameDiscoverer;
+import org.springframework.security.core.parameters.DefaultSecurityParameterNameDiscoverer;
+
+import com.google.common.collect.ImmutableList;
+
+import fr.openwide.core.jpa.business.generic.annotation.PermissionObject;
+import fr.openwide.core.jpa.security.access.expression.method.CoreMethodSecurityExpressionHandler;
+import fr.openwide.core.jpa.security.service.ICorePermissionEvaluator;
 
 /**
  * Par rapport à son parent, cette classe active la protection via les
@@ -14,5 +23,20 @@ import org.springframework.security.access.annotation.Secured;
 @ImportResource("classpath:spring/owsi-core-component-jpa-security-context.xml")
 // définition des proxys Secured
 public abstract class AbstractJpaSecuritySecuredConfig extends AbstractJpaSecurityConfig {
+	
+	@Override
+	public MethodSecurityExpressionHandler expressionHandler(ICorePermissionEvaluator corePermissionEvaluator) {
+		CoreMethodSecurityExpressionHandler methodSecurityExpressionHandler = new CoreMethodSecurityExpressionHandler();
+		methodSecurityExpressionHandler.setCorePermissionEvaluator(corePermissionEvaluator);
+		
+		// Discover parameter name using the @PermissionObject annotation, too
+		methodSecurityExpressionHandler.setParameterNameDiscoverer(new DefaultSecurityParameterNameDiscoverer(
+				ImmutableList.of(
+						new AnnotationParameterNameDiscoverer(PermissionObject.class.getName())
+				)
+		));
+		
+		return methodSecurityExpressionHandler;
+	}
 
 }
