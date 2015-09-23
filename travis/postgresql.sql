70fac6d743be6bcea900e660278ca07664b80839
Guillaume Smet <guillaume.smet@gmail.com>
2015-09-23 18:32:03 +0200

    Add travis configuration

---
+++ travis/postgresql.sql
@@ -0,0 +1,7 @@
+CREATE USER basic_application_test WITH PASSWORD 'basic_application_test';
+CREATE USER owsicore_test WITH PASSWORD 'owsicore_test';
+CREATE USER owsiexample_test WITH PASSWORD 'owsiexample_test';
+
+CREATE DATABASE basic_application_test WITH OWNER basic_application_test;
+CREATE DATABASE owsicore_test WITH OWNER owsicore_test;
+CREATE DATABASE owsiexample_test WITH OWNER owsiexample_test;
