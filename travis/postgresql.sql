b0a69f421419fac406184124e49018d57268e4d9
Guillaume Smet <guillaume.smet@gmail.com>
2015-10-13 16:36:31 +0200

    Add travis configuration to maintenance branch

---
+++ travis/postgresql.sql
@@ -0,0 +1,5 @@
+CREATE USER owsicore_test WITH PASSWORD 'owsicore_test';
+CREATE USER owsiexample_test WITH PASSWORD 'owsiexample_test';
+
+CREATE DATABASE owsicore_test WITH OWNER owsicore_test;
+CREATE DATABASE owsiexample_test WITH OWNER owsiexample_test;
