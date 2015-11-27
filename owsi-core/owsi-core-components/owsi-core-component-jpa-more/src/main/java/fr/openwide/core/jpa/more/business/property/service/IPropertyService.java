5a945cb763ea355aaba31a8c1be4c2e4eb55c32e
flacreuse <florian.lacreuse@openwide.fr>
2015-11-27 18:20:30 +0100

    Adding Property object.

--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/property/service/IPropertyService.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/property/service/IPropertyService.java
@@ -1,12 +1,11 @@
 package fr.openwide.core.jpa.more.business.property.service;
 
-import fr.openwide.core.jpa.business.generic.service.ITransactionalAspectAwareService;
 import fr.openwide.core.jpa.exception.SecurityServiceException;
 import fr.openwide.core.jpa.exception.ServiceException;
 import fr.openwide.core.jpa.more.business.property.model.MutablePropertyId;
 import fr.openwide.core.jpa.more.business.property.model.PropertyId;
 
-public interface IPropertyService extends ITransactionalAspectAwareService {
+public interface IPropertyService {
 
 	<T> T get(PropertyId<T> propertyId);
 
