28f336dadca53211290e1f6aae1668c2de7b5e27
Yoann Rodière <yoann.rodiere@openwide.fr>
2015-11-30 09:27:18 +0100

    Fixed a comment.

--- owsi-core/owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/rendering/IRenderer.java
+++ owsi-core/owsi-core-components/owsi-core-component-commons/src/main/java/fr/openwide/core/commons/util/rendering/IRenderer.java
@@ -4,8 +4,8 @@ import java.util.Locale;
 
 /**
  * An object that can convert an object of type {@code T} to a String for a given {@link Locale}.
- * <p>Note: you probably want to extend WicketRenderer instead of implementing this. This class is primarily intended
- * to serve as a wicket-independant base.
+ * <p>Note: you probably want to extend fr.openwide.core.wicket.more.rendering.Renderer&lt;T&gt; instead
+ * of implementing this. This class is primarily intended to serve as a wicket-independant base.
  */
 public interface IRenderer<T> {
 	
