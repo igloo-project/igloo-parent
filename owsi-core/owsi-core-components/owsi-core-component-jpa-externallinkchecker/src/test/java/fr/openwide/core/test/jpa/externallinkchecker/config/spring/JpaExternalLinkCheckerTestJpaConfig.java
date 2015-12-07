516409771c01da869c3254069d4addc78c6705f5
Guillaume Smet <guillaume.smet@gmail.com>
2015-12-07 14:44:09 +0100

    Declare the interceptor (even if it's not very useful in the tests atm)

--- owsi-core/owsi-core-components/owsi-core-component-jpa-externallinkchecker/src/test/java/fr/openwide/core/test/jpa/externallinkchecker/config/spring/JpaExternalLinkCheckerTestJpaConfig.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-externallinkchecker/src/test/java/fr/openwide/core/test/jpa/externallinkchecker/config/spring/JpaExternalLinkCheckerTestJpaConfig.java
@@ -1,11 +1,15 @@
 package fr.openwide.core.test.jpa.externallinkchecker.config.spring;
 
+import org.hibernate.Interceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import fr.openwide.core.jpa.config.spring.AbstractConfiguredJpaConfig;
 import fr.openwide.core.jpa.config.spring.provider.JpaPackageScanProvider;
 import fr.openwide.core.jpa.externallinkchecker.business.JpaExternalLinkCheckerBusinessPackage;
+import fr.openwide.core.jpa.externallinkchecker.business.interceptor.ExternalLinkWrapperInterceptor;
+import fr.openwide.core.jpa.hibernate.interceptor.ChainedInterceptor;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -14,6 +18,12 @@ public class JpaExternalLinkCheckerTestJpaConfig extends AbstractConfiguredJpaCo
 	@Override
 	public JpaPackageScanProvider applicationJpaPackageScanProvider() {
 		return new JpaPackageScanProvider(JpaExternalLinkCheckerBusinessPackage.class.getPackage());
+	}
+	
+	@Bean
+	public Interceptor interceptor() {
+		return new ChainedInterceptor()
+				.add(new ExternalLinkWrapperInterceptor());
 	}
 
 }
