ecd1d55caa37963c41563403995de2e09661d7c8
flacreuse <florian.lacreuse@openwide.fr>
2015-12-16 15:25:28 +0100

    Adding property service.

--- owsi-core/owsi-core-components/owsi-core-component-jpa-externallinkchecker/src/test/java/fr/openwide/core/test/jpa/externallinkchecker/config/spring/JpaExternalLinkCheckerTestApplicationPropertyConfig.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-externallinkchecker/src/test/java/fr/openwide/core/test/jpa/externallinkchecker/config/spring/JpaExternalLinkCheckerTestApplicationPropertyConfig.java
@@ -1,10 +1,14 @@
 package fr.openwide.core.test.jpa.externallinkchecker.config.spring;
 
+import org.springframework.context.annotation.Import;
+
+import fr.openwide.core.jpa.externallinkchecker.config.spring.JpaExternalLinkCheckerApplicationPropertyRegistryConfig;
 import fr.openwide.core.jpa.more.business.parameter.dao.ParameterDaoImpl;
 import fr.openwide.core.spring.config.spring.AbstractApplicationPropertyConfig;
 import fr.openwide.core.spring.property.dao.IMutablePropertyDao;
 import fr.openwide.core.spring.property.service.IPropertyRegistry;
 
+@Import(JpaExternalLinkCheckerApplicationPropertyRegistryConfig.class)
 public class JpaExternalLinkCheckerTestApplicationPropertyConfig extends AbstractApplicationPropertyConfig {
 
 	@Override
