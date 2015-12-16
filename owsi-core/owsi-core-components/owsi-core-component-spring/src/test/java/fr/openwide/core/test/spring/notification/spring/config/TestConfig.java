f3075cfb0db91a53e79240fe53cdb4623cd1096b
flacreuse <florian.lacreuse@openwide.fr>
2015-12-16 14:12:36 +0100

    Adding property service. (mistakenly deleted)

--- owsi-core/owsi-core-components/owsi-core-component-spring/src/test/java/fr/openwide/core/test/spring/notification/spring/config/TestConfig.java
+++ owsi-core/owsi-core-components/owsi-core-component-spring/src/test/java/fr/openwide/core/test/spring/notification/spring/config/TestConfig.java
@@ -1,9 +1,13 @@
 package fr.openwide.core.test.spring.notification.spring.config;
 
+import java.net.MalformedURLException;
+
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import fr.openwide.core.spring.config.CoreConfigurer;
 import fr.openwide.core.spring.config.spring.annotation.ApplicationConfigurerBeanFactoryPostProcessor;
 import fr.openwide.core.spring.config.spring.annotation.ApplicationDescription;
 import fr.openwide.core.spring.config.spring.annotation.ConfigurationLocations;
@@ -13,7 +17,12 @@ import fr.openwide.core.spring.config.spring.annotation.ConfigurationLocations;
 @ConfigurationLocations(locations = {"classpath:notification-test.properties", "classpath:notification-test-${user}.properties"})
 @Import({ NotificationTestConfig.class })
 public class TestConfig {
-	
+
+	@Bean(name = { "configurer" })
+	public static CoreConfigurer environment(ConfigurableApplicationContext context) throws MalformedURLException {
+		return new CoreConfigurer();
+	}
+
 	@Bean
 	public static ApplicationConfigurerBeanFactoryPostProcessor applicationConfigurer() {
 		return new ApplicationConfigurerBeanFactoryPostProcessor();
