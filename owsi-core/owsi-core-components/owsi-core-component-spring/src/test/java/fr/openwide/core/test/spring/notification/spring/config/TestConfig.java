eeb24635d8ea94c1d68eed0397b7f2be431092a5
Guillaume Smet <guillaume.smet@gmail.com>
2015-12-22 15:51:22 +0100

    Extract the property infrastructure from the Configurer

--- owsi-core/owsi-core-components/owsi-core-component-spring/src/test/java/fr/openwide/core/test/spring/notification/spring/config/TestConfig.java
+++ owsi-core/owsi-core-components/owsi-core-component-spring/src/test/java/fr/openwide/core/test/spring/notification/spring/config/TestConfig.java
@@ -1,13 +1,12 @@
 package fr.openwide.core.test.spring.notification.spring.config;
 
-import java.net.MalformedURLException;
-
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import fr.openwide.core.spring.config.CoreConfigurer;
+import fr.openwide.core.spring.config.CorePropertyPlaceholderConfigurer;
 import fr.openwide.core.spring.config.spring.annotation.ApplicationConfigurerBeanFactoryPostProcessor;
 import fr.openwide.core.spring.config.spring.annotation.ApplicationDescription;
 import fr.openwide.core.spring.config.spring.annotation.ConfigurationLocations;
@@ -19,13 +18,18 @@ import fr.openwide.core.spring.config.spring.annotation.ConfigurationLocations;
 public class TestConfig {
 
 	@Bean(name = { "configurer" })
-	public static CoreConfigurer environment(ConfigurableApplicationContext context) throws MalformedURLException {
+	public static CoreConfigurer configurer() {
 		return new CoreConfigurer();
 	}
 
 	@Bean
 	public static ApplicationConfigurerBeanFactoryPostProcessor applicationConfigurer() {
 		return new ApplicationConfigurerBeanFactoryPostProcessor();
+	}
+	
+	@Bean
+	public static CorePropertyPlaceholderConfigurer environment(ConfigurableApplicationContext context) {
+		return new CorePropertyPlaceholderConfigurer();
 	}
 
 }
