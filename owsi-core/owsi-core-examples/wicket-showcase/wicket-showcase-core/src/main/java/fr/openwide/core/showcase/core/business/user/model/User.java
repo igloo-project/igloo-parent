be552885155d2a94877d801ca5b2682fea3a13e6
Renaud Joly <renaud.joly@openwide.fr>
2015-12-29 14:46:48 +0100

    Showcase: sample for the new jQuery multivalued expand plugin.

--- owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-core/src/main/java/fr/openwide/core/showcase/core/business/user/model/User.java
+++ owsi-core/owsi-core-examples/wicket-showcase/wicket-showcase-core/src/main/java/fr/openwide/core/showcase/core/business/user/model/User.java
@@ -1,15 +1,25 @@
 package fr.openwide.core.showcase.core.business.user.model;
 
+import java.util.Collections;
+import java.util.SortedSet;
+
 import javax.persistence.Cacheable;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 import org.bindgen.Bindable;
+import org.hibernate.annotations.SortNatural;
 import org.hibernate.search.annotations.Analyzer;
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.annotations.SortableField;
 
+import com.google.common.collect.Sets;
+
+import fr.openwide.core.commons.util.collections.CollectionUtils;
 import fr.openwide.core.jpa.search.util.HibernateSearchAnalyzer;
 import fr.openwide.core.jpa.security.business.person.model.GenericSimpleUser;
 
@@ -23,12 +33,24 @@ public class User extends GenericSimpleUser<User, UserGroup> {
 	@Column(nullable = false)
 	private Integer position;
 	
+	@ElementCollection
+	@SortNatural
+	private SortedSet<String> tags = Sets.newTreeSet();
+	
 	public Integer getPosition() {
 		return position;
 	}
 	
 	public void setPosition(Integer position) {
 		this.position = position;
+	}
+	
+	public SortedSet<String> getTags() {
+		return Collections.unmodifiableSortedSet(tags);
+	}
+	
+	public void setTags(SortedSet<String> tags) {
+		CollectionUtils.replaceAll(this.tags, tags);
 	}
 	
 	@Field(analyzer = @Analyzer(definition = HibernateSearchAnalyzer.TEXT_SORT))
