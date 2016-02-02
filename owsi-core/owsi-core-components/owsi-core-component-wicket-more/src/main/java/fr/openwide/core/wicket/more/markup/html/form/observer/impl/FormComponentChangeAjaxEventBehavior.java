7c536927e033d9077eeadb25f62fbda5d12d8f65
Yoann Rodière <yoann.rodiere@openwide.fr>
2016-02-02 17:58:00 +0100

    Revert "Simplified FormComponentChangeAjaxEventBehavior thanks to new Wicket 7 features"
    As from Wicket 7.2.0, the radio buttons seem to be no longer submitted correctly, same as Wicket 6.X...
    
    This reverts commit d73d2634ef491bab1d3ef08bdf0fe78edee60d3d and commit 595644cc0fbb2268c0b89c7d933a5fc2eeb8908a
    
    Conflicts:
    	owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/observer/impl/FormComponentChangeAjaxEventBehavior.java

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/observer/impl/FormComponentChangeAjaxEventBehavior.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/observer/impl/FormComponentChangeAjaxEventBehavior.java
@@ -14,12 +14,16 @@ import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
 import org.apache.wicket.ajax.attributes.AjaxRequestAttributes.Method;
 import org.apache.wicket.ajax.json.JSONException;
 import org.apache.wicket.ajax.json.JSONObject;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.form.CheckBoxMultipleChoice;
 import org.apache.wicket.markup.html.form.CheckGroup;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.RadioChoice;
 import org.apache.wicket.markup.html.form.RadioGroup;
+import org.apache.wicket.request.resource.ResourceReference;
+import org.apache.wicket.resource.JQueryPluginResourceReference;
 import org.wicketstuff.wiquery.core.events.MouseEvent;
 import org.wicketstuff.wiquery.core.events.StateEvent;
 import org.wicketstuff.wiquery.core.javascript.JsStatement;
@@ -52,6 +56,11 @@ public class FormComponentChangeAjaxEventBehavior extends AjaxEventBehavior impl
 			return ajaxEventBehaviors.iterator().next();
 		}
 	}
+	
+	static final ResourceReference CHOICE_JS = new JQueryPluginResourceReference(
+			FormComponentChangeAjaxEventBehavior.class,
+			"jquery.FormComponentChangeAjaxEventBehavior.Choice.js"
+	);
 	
 	private static final MetaDataKey<Boolean> IS_SUBMITTED_USING_THIS_BEHAVIOR = new MetaDataKey<Boolean>() {
 		private static final long serialVersionUID = 1L;
@@ -94,6 +103,14 @@ public class FormComponentChangeAjaxEventBehavior extends AjaxEventBehavior impl
 		return (FormComponent<?>)getComponent();
 	}
 	
+	@Override
+	public void renderHead(Component component, IHeaderResponse response) {
+		super.renderHead(component, response);
+		if (choice) {
+			response.render(JavaScriptHeaderItem.forReference(FormComponentChangeAjaxEventBehavior.CHOICE_JS));
+		}
+	}
+
 	// Due to the fact that, for choice components, events are attached to the form and not to the component itself,
 	// we must remove the handlers on ajax refreshes.
 	// Thus we need a unique event name, so that we can call $('#formId').off('click.my.unique.namespace')
@@ -130,8 +147,10 @@ public class FormComponentChangeAjaxEventBehavior extends AjaxEventBehavior impl
 			// For explanations, see: getUniqueEventName(), getCallbackScript(), postprocessConfiguration()
 			attributes.setEventNames(getUniqueEventName());
 			
-			// Note: from Wicket 7 on, choice (radio/checkbox) values are correctly submitted by Wicket.
-			// No need to add an extra DynamicParameter like we used to.
+			// Copied from AjaxFormChoiceComponentUpdatingBehavior
+			attributes.getDynamicExtraParameters().add(
+					String.format("return OWSI.FormComponentChangeAjaxEventBehavior.Choice.getInputValues('%s', attrs);", getFormComponent().getInputName())
+			);
 		}
 	}
 	
