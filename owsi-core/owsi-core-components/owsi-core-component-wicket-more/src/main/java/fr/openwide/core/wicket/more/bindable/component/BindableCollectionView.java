13a4424f8780291a9dd8604f79fff037343e1a76
Yoann Rodière <yoann.rodiere@openwide.fr>
2016-05-17 15:47:09 +0200

    Introduced collection/map views for specific model types
    Which is especially useful when your model is a IBindableCollectionModel.

---
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/bindable/component/BindableCollectionView.java
@@ -0,0 +1,33 @@
+package fr.openwide.core.wicket.more.bindable.component;
+
+import fr.openwide.core.wicket.more.bindable.model.IBindableCollectionModel;
+import fr.openwide.core.wicket.more.bindable.model.IBindableModel;
+import fr.openwide.core.wicket.more.markup.repeater.collection.SpecificModelCollectionView;
+
+/**
+ * An easy-to-use shorthand for {@code SpecificModelCollectionView<T, IBindableModel<T>>}.
+ * 
+ * <p>This class is only useful if you want to use an {@link IBindableModel} for your item models.
+ * If you want to use a custom subtype of yours, go with {@link SpecificModelCollectionView} directly.
+ * 
+ * @author yrodiere
+ *
+ * @param <T>
+ * 
+ * @see SpecificModelCollectionView
+ */
+public abstract class BindableCollectionView<T> extends SpecificModelCollectionView<T, IBindableModel<T>> {
+	
+	private static final long serialVersionUID = 1L;
+
+	public BindableCollectionView(String id, IBindableCollectionModel<T, ?> collectionModel) {
+		super(id, collectionModel);
+	}
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public IBindableCollectionModel<T, ?> getModel() {
+		return (IBindableCollectionModel<T, ?>) super.getModel();
+	}
+
+}
