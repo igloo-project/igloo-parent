4c183887a4fe86760b1bf02f9a7b2ee54fc511e8
Yoann Rodière <yoann.rodiere@openwide.fr>
2016-05-30 16:53:24 +0200

    Added FieldPathType, for correct FieldPath serialization using Hibernate

--- basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/business/history/model/HistoryLog.java
+++ basic-application/basic-application-core/src/main/java/fr/openwide/core/basicapp/core/business/history/model/HistoryLog.java
@@ -6,9 +6,12 @@ import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 
 import org.bindgen.Bindable;
+import org.hibernate.annotations.TypeDef;
 import org.hibernate.search.annotations.Indexed;
 
 import fr.openwide.core.basicapp.core.business.history.model.atomic.HistoryEventType;
+import fr.openwide.core.commons.util.fieldpath.FieldPath;
+import fr.openwide.core.jpa.more.business.history.hibernate.FieldPathType;
 import fr.openwide.core.jpa.more.business.history.model.AbstractHistoryLog;
 import fr.openwide.core.jpa.more.business.history.model.embeddable.HistoryValue;
 
@@ -16,6 +19,7 @@ import fr.openwide.core.jpa.more.business.history.model.embeddable.HistoryValue;
 @Bindable
 @Cacheable
 @Indexed
+@TypeDef(defaultForType = FieldPath.class, typeClass = FieldPathType.class)
 public class HistoryLog extends AbstractHistoryLog<HistoryLog, HistoryEventType, HistoryDifference> {
 
 	private static final long serialVersionUID = -8557932643510393694L;
