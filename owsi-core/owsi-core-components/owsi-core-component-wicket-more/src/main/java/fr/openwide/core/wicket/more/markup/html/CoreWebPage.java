322705e2236d2a0b54a75d32a2cd5b7a58337107
Yoann Rodière <yoann.rodiere@openwide.fr>
2016-06-01 12:18:29 +0200

    Deprecated some obsolete methods in CoreWebPage

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/CoreWebPage.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/CoreWebPage.java
@@ -9,6 +9,9 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.flow.RedirectToUrlException;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import fr.openwide.core.wicket.more.link.descriptor.generator.IPageLinkGenerator;
+import fr.openwide.core.wicket.more.markup.html.basic.EnclosureBehavior;
+
 public abstract class CoreWebPage extends WebPage {
 
 	private static final long serialVersionUID = 607411189304353902L;
@@ -24,22 +27,51 @@ public abstract class CoreWebPage extends WebPage {
 		super(parameters);
 	}
 	
+	/**
+	 * @deprecated This hides the exception throwing, which makes dead code harder to spot. Just throw a
+	 * {@link RestartResponseException} yourself. Note that if you're using a {@link IPageLinkGenerator}, it
+	 * can instantiate the exception for you.
+	 */
+	@Deprecated
 	public final void redirect(final Class<? extends Page> clazz) {
 		throw new RestartResponseException(clazz);
 	}
 
+	/**
+	 * @deprecated This hides the exception throwing, which makes dead code harder to spot. Just throw a
+	 * {@link RestartResponseException} yourself. Note that if you're using a {@link IPageLinkGenerator}, it
+	 * can instantiate the exception for you.
+	 */
+	@Deprecated
 	public final void redirect(final Class<? extends Page> clazz, PageParameters parameters) {
 		throw new RestartResponseException(clazz, parameters);
 	}
 
+	/**
+	 * @deprecated This hides the exception throwing, which makes dead code harder to spot. Just throw a
+	 * {@link RestartResponseException} yourself. Note that if you're using a {@link IPageLinkGenerator}, it
+	 * can instantiate the exception for you.
+	 */
+	@Deprecated
 	public final void redirect(final Page page) {
 		throw new RestartResponseException(page);
 	}
 	
+	/**
+	 * @deprecated This hides the exception throwing, which makes dead code harder to spot. Just throw a
+	 * {@link RedirectToUrlException} yourself. Note that if you're using a {@link IPageLinkGenerator}, it
+	 * can instantiate the exception for you.
+	 */
+	@Deprecated
 	public final void redirect(String url) {
 		throw new RedirectToUrlException(url);
 	}
 	
+	/**
+	 * @deprecated Just use {@link Component#setVisible(boolean)} or {@link Component#setVisibilityAllowed(boolean)}, or
+	 * (better) add an {@link EnclosureBehavior} to manage the component's visibility declaratively.
+	 */
+	@Deprecated
 	protected Component visible(Component component, boolean visible) {
 		component.setVisible(visible);
 		return component;
