549a81f83275e85080578044c5ed78a5ce9e06be
Yoann Rodière <yoann.rodiere@openwide.fr>
2016-06-23 12:02:26 +0200

    Added generic Lucene Score sorts.

--- owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/generic/model/search/GenericListItemSort.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/generic/model/search/GenericListItemSort.java
@@ -6,26 +6,30 @@ import org.apache.lucene.search.SortField;
 
 import com.google.common.collect.ImmutableList;
 
-import fr.openwide.core.jpa.business.generic.model.GenericEntity;
 import fr.openwide.core.jpa.more.business.generic.model.GenericListItem;
 import fr.openwide.core.jpa.more.business.sort.ISort;
 import fr.openwide.core.jpa.more.business.sort.SortUtils;
 
 public enum GenericListItemSort implements ISort<SortField> {
-	
-	ID {
+
+	SCORE {
 		@Override
 		public List<SortField> getSortFields(SortOrder sortOrder) {
-			return ImmutableList.of(
-					SortUtils.luceneSortField(
-							this, sortOrder, SortField.Type.LONG,
-							GenericEntity.ID_SORT
-					)
-			);
+			return GenericEntitySort.SCORE.getSortFields(sortOrder);
 		}
 		@Override
 		public SortOrder getDefaultOrder() {
-			return SortOrder.DESC;
+			return GenericEntitySort.SCORE.getDefaultOrder();
+		}
+	},
+	ID {
+		@Override
+		public List<SortField> getSortFields(SortOrder sortOrder) {
+			return GenericEntitySort.ID.getSortFields(sortOrder);
+		}
+		@Override
+		public SortOrder getDefaultOrder() {
+			return GenericEntitySort.ID.getDefaultOrder();
 		}
 	},
 	LABEL {
