2af755dca652dc4b909b8bc1464d70cb26827571
Yoann Rodière <yoann.rodiere@openwide.fr>
2016-08-02 11:13:23 +0200

    Allow defining AbstractHibernateSearchSearchQuery's default result
    i.e. the result to be returned when no criterion has been set.

---
+++ owsi-core/owsi-core-components/owsi-core-component-jpa-more/src/main/java/fr/openwide/core/jpa/more/business/search/query/SearchQueryDefaultResult.java
@@ -0,0 +1,17 @@
+package fr.openwide.core.jpa.more.business.search.query;
+
+/**
+ * Refers to the default behavior of a search query when no criterion has been set.
+ */
+public enum SearchQueryDefaultResult {
+	
+	/**
+	 * Do not return anything.
+	 */
+	NOTHING,
+	/**
+	 * Return every item.
+	 */
+	EVERYTHING;
+
+}
