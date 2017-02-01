5452f7f683344a2663bf52e5c72527e86107ef97
Julien Gonzalez <julien.gonzalez@openwide.fr>
2017-02-01 15:21:41 +0100

    Prerequisite behavior must check if attached components are visible

--- owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/AbstractAjaxInputPrerequisiteBehavior.java
+++ owsi-core/owsi-core-components/owsi-core-component-wicket-more/src/main/java/fr/openwide/core/wicket/more/markup/html/form/AbstractAjaxInputPrerequisiteBehavior.java
@@ -28,6 +28,7 @@ import fr.openwide.core.wicket.more.condition.Condition;
 import fr.openwide.core.wicket.more.markup.html.form.observer.IFormComponentChangeObserver;
 import fr.openwide.core.wicket.more.markup.html.form.observer.impl.FormComponentChangeAjaxEventBehavior;
 import fr.openwide.core.wicket.more.util.model.Detachables;
+import fr.openwide.core.wicket.more.util.visit.VisitFilters;
 
 /**
  * Performs abstract actions on the attached component according to the actual, client-side content of a given {@link FormComponent}.<br>
@@ -325,7 +326,10 @@ public abstract class AbstractAjaxInputPrerequisiteBehavior<T> extends Behavior 
 		T convertedInput = getPrerequisiteFieldConvertedInput();
 		
 		for (Component attachedComponent : attachedComponents) {
-			target.add(getAjaxTarget(attachedComponent));
+			if (VisitFilters.renderedComponents().visitObject(attachedComponent)) {
+				target.add(getAjaxTarget(attachedComponent));
+			}
+			
 			boolean hasReset = false;
 			if (resetAttachedModelPredicate.apply(convertedInput)) {
 				attachedComponent.setDefaultModelObject(null);
