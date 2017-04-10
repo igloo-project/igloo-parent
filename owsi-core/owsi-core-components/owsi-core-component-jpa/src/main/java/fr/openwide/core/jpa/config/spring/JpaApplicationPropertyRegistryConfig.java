24518704ee5bd1eaf039ba101aeb68f6e4326478
Margot Piva <margot.piva@smile.fr>
2017-04-10 17:09:42 +0200

    elasticsearch · delete index before reindexing with elasticsearch

--- owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/config/spring/JpaApplicationPropertyRegistryConfig.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/config/spring/JpaApplicationPropertyRegistryConfig.java
@@ -2,6 +2,8 @@ package fr.openwide.core.jpa.config.spring;
 
 import static fr.openwide.core.jpa.property.JpaPropertyIds.HIBERNATE_SEARCH_REINDEX_BATCH_SIZE;
 import static fr.openwide.core.jpa.property.JpaPropertyIds.HIBERNATE_SEARCH_REINDEX_LOAD_THREADS;
+import static fr.openwide.core.jpa.property.JpaPropertyIds.HIBERNATE_SEARCH_INDEXMANAGER;
+import static fr.openwide.core.jpa.property.JpaPropertyIds.HIBERNATE_SEARCH_ELASTICSEARCH_HOST;
 import static fr.openwide.core.jpa.property.JpaPropertyIds.LUCENE_BOOLEAN_QUERY_MAX_CLAUSE_COUNT;
 
 import org.apache.lucene.search.BooleanQuery;
@@ -32,6 +34,8 @@ public class JpaApplicationPropertyRegistryConfig extends AbstractApplicationPro
 		
 		registry.registerInteger(HIBERNATE_SEARCH_REINDEX_BATCH_SIZE, 25);
 		registry.registerInteger(HIBERNATE_SEARCH_REINDEX_LOAD_THREADS, 8);
+		registry.registerString(HIBERNATE_SEARCH_INDEXMANAGER, "lucene");
+		registry.registerString(HIBERNATE_SEARCH_ELASTICSEARCH_HOST, "http://127.0.0.1:9220");
 	}
 
 }
--- owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/property/JpaPropertyIds.java
+++ owsi-core/owsi-core-components/owsi-core-component-jpa/src/main/java/fr/openwide/core/jpa/property/JpaPropertyIds.java
@@ -9,7 +9,6 @@ public final class JpaPropertyIds extends AbstractPropertyIds {
 	 * Mutable Properties
 	 */
 	
-	// None
 	
 	/*
 	 * Immutable Properties
@@ -19,5 +18,9 @@ public final class JpaPropertyIds extends AbstractPropertyIds {
 	
 	public static final ImmutablePropertyId<Integer> HIBERNATE_SEARCH_REINDEX_BATCH_SIZE = immutable("hibernate.search.reindex.batchSize");
 	public static final ImmutablePropertyId<Integer> HIBERNATE_SEARCH_REINDEX_LOAD_THREADS = immutable("hibernate.search.reindex.loadThreads");
+	
+	public static final ImmutablePropertyId<String> HIBERNATE_SEARCH_INDEXMANAGER = immutable("hibernate.search.default.indexmanager");
+	public static final ImmutablePropertyId<String> HIBERNATE_SEARCH_ELASTICSEARCH_HOST = immutable("hibernate.search.default.elasticsearch.host");
+	
 	
 }
