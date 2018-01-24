ada2930b73f148a3109380ba99a89022f3197089
Florian Lacreuse <florian.lacreuse@kobalt-si.fr>
2018-01-24 16:27:54 +0100

    Migration to Boostrap 4 (WIP)

--- igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/html/sort/SortIconStyle.java
+++ igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/html/sort/SortIconStyle.java
@@ -5,34 +5,34 @@ public enum SortIconStyle implements ISortIconStyle {
 	DEFAULT {
 		@Override
 		public String getAscIconCssClasses() {
-			return "icon-sort-by-attributes fa fa-sort-amount-asc";
+			return "icon-sort-by-attributes fa fa-sort-amount-asc fa-sort-amount-up";
 		}
 
 		@Override
 		public String getDescIconCssClasses() {
-			return "icon-sort-by-attributes-alt fa fa-sort-amount-desc";
+			return "icon-sort-by-attributes-alt fa fa-sort-amount-desc fa-sort-amount-down";
 		}
 	},
 	ALPHABET {
 		@Override
 		public String getAscIconCssClasses() {
-			return "icon-sort-by-alphabet fa fa-sort-alpha-asc";
+			return "icon-sort-by-alphabet fa fa-sort-alpha-asc fa-sort-alpha-up";
 		}
 		
 		@Override
 		public String getDescIconCssClasses() {
-			return "icon-sort-by-alphabet-alt fa fa-sort-alpha-desc";
+			return "icon-sort-by-alphabet-alt fa fa-sort-alpha-desc fa-sort-alpha-down";
 		}
 	},
 	NUMERIC {
 		@Override
 		public String getAscIconCssClasses() {
-			return "icon-sort-by-order fa fa-sort-numeric-asc";
+			return "icon-sort-by-order fa fa-sort-numeric-asc fa-sort-numeric-up";
 		}
 		
 		@Override
 		public String getDescIconCssClasses() {
-			return "icon-sort-by-order-alt fa fa-sort-numeric-desc";
+			return "icon-sort-by-order-alt fa fa-sort-numeric-desc fa-sort-numeric-down";
 		}
 	};
 
