8ec07564fb38f73c9548b01db77dbb593d7e5810
Julien Gonzalez <julien.gonzalez@kobalt-si.fr>
2018-01-30 18:04:51 +0100

    Fix travis tests

--- travis/postgresql.sql
+++ travis/postgresql.sql
@@ -1,13 +1,13 @@
 CREATE USER basic_application_test WITH PASSWORD 'basic_application_test';
-CREATE USER owsicore_test WITH PASSWORD 'owsicore_test';
-CREATE USER owsiexample_test WITH PASSWORD 'owsiexample_test';
+CREATE USER igloo_test WITH PASSWORD 'igloo_test';
+CREATE USER iglooexample_test WITH PASSWORD 'iglooexample_test';
 
 CREATE DATABASE basic_application_test WITH OWNER basic_application_test;
-CREATE DATABASE owsicore_test WITH OWNER owsicore_test;
-CREATE DATABASE owsiexample_test WITH OWNER owsiexample_test;
+CREATE DATABASE igloo_test WITH OWNER igloo_test;
+CREATE DATABASE iglooexample_test WITH OWNER iglooexample_test;
 
-\c owsicore_test
-CREATE SCHEMA owsicore AUTHORIZATION owsicore_test;
+\c igloo_test
+CREATE SCHEMA igloo_test AUTHORIZATION igloo_test;
 
 \c basic_application_test
 CREATE SCHEMA basic_application_test AUTHORIZATION basic_application_test;
