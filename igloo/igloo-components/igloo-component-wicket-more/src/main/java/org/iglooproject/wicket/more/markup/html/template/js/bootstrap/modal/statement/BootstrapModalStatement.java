ee90116cc7bb7ef0349048abdc4acde2eb93d06b
Florian Lacreuse <florian.lacreuse@kobalt-si.fr>
2018-02-13 12:04:33 +0100

    Set up timeout delay to hide modal to be sure it's fully shown.

--- igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/html/template/js/bootstrap/modal/statement/BootstrapModalStatement.java
+++ igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/html/template/js/bootstrap/modal/statement/BootstrapModalStatement.java
@@ -5,6 +5,8 @@ import org.wicketstuff.wiquery.core.javascript.JsStatement;
 
 public final class BootstrapModalStatement {
 
+	private static final int HIDE_TIMEOUT_DELAY = 1000;
+
 	public static final JsStatement show(Component modal) {
 		return show(modal, null);
 	}
@@ -18,7 +20,14 @@ public final class BootstrapModalStatement {
 	}
 
 	public static final JsStatement hide(Component modal) {
-		return new JsStatement().$(modal).chain(BootstrapModal.hide()).append(";");
+		/*
+		 * Bootstrap modal js doesn't allow to close a modal while until it's fully opened.
+		 * We need to set up a timeout to delay the hide statement call.
+		 */
+		return new JsStatement()
+				.append("setTimeout(function() {")
+				.append(new JsStatement().$(modal).chain(BootstrapModal.hide()).append(";").render())
+				.append("}, " + HIDE_TIMEOUT_DELAY + ");");
 	}
 
 	private BootstrapModalStatement() {
