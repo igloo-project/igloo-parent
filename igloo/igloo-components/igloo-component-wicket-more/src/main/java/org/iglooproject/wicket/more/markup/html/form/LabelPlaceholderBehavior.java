93491490f9b5ccd59ecf02c3d013602aa698fee7
Florian Lacreuse <florian.lacreuse@kobalt-si.fr>
2018-02-13 16:19:57 +0100

    Fix label placeholder for select2 components.

--- igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/html/form/LabelPlaceholderBehavior.java
+++ igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/html/form/LabelPlaceholderBehavior.java
@@ -2,18 +2,29 @@ package org.iglooproject.wicket.more.markup.html.form;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
+import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.model.ComponentModel;
 import org.apache.wicket.model.IModel;
+import org.wicketstuff.select2.Select2Behavior;
 
-public class LabelPlaceholderBehavior extends AttributeModifier {
+public class LabelPlaceholderBehavior extends Behavior {
 
 	private static final long serialVersionUID = 7392345869664046823L;
 
 	private static final String PLACEHOLDER_ATTRIBUTE = "placeholder";
+	private static final String DATA_PLACEHOLDER_ATTRIBUTE = "data-placeholder";
 
 	public LabelPlaceholderBehavior() {
-		super(PLACEHOLDER_ATTRIBUTE, new LabelPlaceholderModel());
+		super();
+	}
+
+	@Override
+	public void bind(Component component) {
+		super.bind(component);
+		
+		boolean isSelect2Component = !component.getBehaviors(Select2Behavior.class).isEmpty();
+		component.add(new AttributeModifier(isSelect2Component ? DATA_PLACEHOLDER_ATTRIBUTE : PLACEHOLDER_ATTRIBUTE, new LabelPlaceholderModel()));
 	}
 
 	private static class LabelPlaceholderModel extends ComponentModel<String> {
