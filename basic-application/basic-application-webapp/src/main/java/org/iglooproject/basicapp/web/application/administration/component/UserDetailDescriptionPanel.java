f266bc4c17f8594f10dc801c563cab992563abb5
Florian Lacreuse <florian.lacreuse@kobalt-si.fr>
2018-02-21 14:40:31 +0100

    Migration to Boostrap 4 (WIP)

--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/administration/component/UserDetailDescriptionPanel.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/administration/component/UserDetailDescriptionPanel.java
@@ -1,63 +1,37 @@
 package org.iglooproject.basicapp.web.application.administration.component;
 
-import static org.iglooproject.commons.util.functional.Predicates2.isTrue;
-
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.iglooproject.basicapp.core.business.user.model.User;
-import org.iglooproject.basicapp.core.business.user.model.atomic.UserPasswordRecoveryRequestInitiator;
-import org.iglooproject.basicapp.core.business.user.model.atomic.UserPasswordRecoveryRequestType;
 import org.iglooproject.basicapp.core.business.user.service.IUserService;
-import org.iglooproject.basicapp.core.security.service.ISecurityManagementService;
 import org.iglooproject.basicapp.core.util.binding.Bindings;
-import org.iglooproject.basicapp.web.application.BasicApplicationSession;
 import org.iglooproject.basicapp.web.application.administration.form.AbstractUserPopup;
-import org.iglooproject.basicapp.web.application.administration.form.UserPasswordUpdatePopup;
 import org.iglooproject.basicapp.web.application.administration.form.UserPopup;
 import org.iglooproject.basicapp.web.application.common.typedescriptor.user.UserTypeDescriptor;
 import org.iglooproject.wicket.markup.html.basic.CoreLabel;
 import org.iglooproject.wicket.markup.html.link.EmailLink;
 import org.iglooproject.wicket.markup.html.panel.GenericPanel;
 import org.iglooproject.wicket.more.condition.Condition;
-import org.iglooproject.wicket.more.markup.html.action.AbstractAjaxAction;
 import org.iglooproject.wicket.more.markup.html.basic.DateLabel;
 import org.iglooproject.wicket.more.markup.html.basic.DefaultPlaceholderPanel;
-import org.iglooproject.wicket.more.markup.html.feedback.FeedbackUtils;
 import org.iglooproject.wicket.more.markup.html.image.BooleanIcon;
 import org.iglooproject.wicket.more.markup.html.link.BlankLink;
-import org.iglooproject.wicket.more.markup.html.template.js.bootstrap.confirm.component.AjaxConfirmLink;
 import org.iglooproject.wicket.more.markup.html.template.js.bootstrap.modal.behavior.AjaxModalOpenBehavior;
 import org.iglooproject.wicket.more.model.BindingModel;
 import org.iglooproject.wicket.more.util.DatePattern;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.wicketstuff.wiquery.core.events.MouseEvent;
 
 public class UserDetailDescriptionPanel<U extends User> extends GenericPanel<U> {
 
 	private static final long serialVersionUID = 8651898170121443991L;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailDescriptionPanel.class);
-
 	@SpringBean
 	private IUserService userService;
-
-	@SpringBean
-	private ISecurityManagementService securityManagementService;
 
 	public UserDetailDescriptionPanel(String id, final IModel<U> userModel, UserTypeDescriptor<U> typeDescriptor) {
 		super(id, userModel);
 		
 		AbstractUserPopup<U> editPopup = createEditPopup("editPopup", getModel(), typeDescriptor);
-		
-		UserPasswordUpdatePopup<U> passwordEditPopup = new UserPasswordUpdatePopup<>("passwordEditPopup", getModel());
-		
-		IModel<String> confirmationTextModel = new StringResourceModel("administration.user.action.disable.confirmation.content", userModel);
 		
 		IModel<String> emailModel = BindingModel.of(userModel, Bindings.user().email());
 		
@@ -65,100 +39,6 @@ public class UserDetailDescriptionPanel<U extends User> extends GenericPanel<U> 
 				editPopup,
 				new BlankLink("edit")
 						.add(new AjaxModalOpenBehavior(editPopup, MouseEvent.CLICK)),
-				
-				passwordEditPopup,
-				new BlankLink("passwordEdit")
-						.add(new AjaxModalOpenBehavior(passwordEditPopup, MouseEvent.CLICK))
-						.add(
-								Condition.predicate(
-										Model.of(securityManagementService.getOptions(getModelObject()).isPasswordAdminUpdateEnabled()),
-										isTrue()
-								).thenShow()
-						),
-				
-				AjaxConfirmLink.<U>build()
-						.title(new ResourceModel("administration.user.action.password.recovery.reset.confirmation.title"))
-						.content(new StringResourceModel("administration.user.action.password.recovery.reset.confirmation.content", userModel))
-						.confirm()
-						.onClick(new AbstractAjaxAction() {
-							private static final long serialVersionUID = 1L;
-							@Override
-							public void execute(AjaxRequestTarget target) {
-								try {
-									securityManagementService.initiatePasswordRecoveryRequest(userModel.getObject(),
-											UserPasswordRecoveryRequestType.RESET,
-											UserPasswordRecoveryRequestInitiator.ADMIN,
-											BasicApplicationSession.get().getUser()
-									);
-									getSession().success(getString("administration.user.action.password.recovery.reset.success"));
-									target.add(getPage());
-								} catch (Exception e) {
-									LOGGER.error("Error occured while sending a password recovery request", e);
-									getSession().error(getString("common.error.unexpected"));
-								}
-								FeedbackUtils.refreshFeedback(target, getPage());
-							}
-						})
-						.create("passwordReset", userModel)
-						.add(
-								Condition.predicate(
-										Model.of(securityManagementService.getOptions(getModelObject()).isPasswordAdminRecoveryEnabled()),
-										isTrue()
-								).thenShow()
-						),
-				
-				new Link<U>("enable", userModel) {
-					private static final long serialVersionUID = 1L;
-					@Override
-					public void onClick() {
-						try {
-							userService.setActive(getModelObject(), true);
-							getSession().success(getString("administration.user.action.enable.success"));
-						} catch (Exception e) {
-							LOGGER.error("Error occured while enabling user", e);
-							getSession().error(getString("common.error.unexpected"));
-						}
-					}
-					@Override
-					protected void onConfigure() {
-						super.onConfigure();
-						setVisible(!getModelObject().isActive());
-					}
-				},
-				
-				AjaxConfirmLink.<U>build()
-						.title(new ResourceModel("administration.user.action.disable.confirmation.title"))
-						.content(confirmationTextModel)
-						.confirm()
-						.onClick(
-								new AbstractAjaxAction() {
-									private static final long serialVersionUID = 1L;
-									@Override
-									public void execute(AjaxRequestTarget target) {
-										try {
-											userService.setActive(userModel.getObject(), false);
-											getSession().success(getString("administration.user.action.disable.success"));
-										} catch (Exception e) {
-											LOGGER.error("Error occured while disabling user", e);
-											getSession().error(getString("common.error.unexpected"));
-										}
-										target.add(getPage());
-										FeedbackUtils.refreshFeedback(target, getPage());
-									}
-								}
-						)
-						.create("disable", userModel)
-						.add(
-								new Condition() {
-									private static final long serialVersionUID = 1L;
-									@Override
-									public boolean applies() {
-										User displayedUser = getModelObject();
-										User currentUser = BasicApplicationSession.get().getUser();
-										return !displayedUser.equals(currentUser) && displayedUser.isActive();
-									}
-								}.thenShowInternal()
-						),
 				
 				new CoreLabel("username", BindingModel.of(userModel, Bindings.user().username()))
 						.showPlaceholder(),
--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/administration/template/AdministrationUserDetailTemplate.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/administration/template/AdministrationUserDetailTemplate.java
@@ -2,11 +2,23 @@ package org.iglooproject.basicapp.web.application.administration.template;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.Page;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.iglooproject.basicapp.core.business.user.model.User;
+import org.iglooproject.basicapp.core.business.user.model.atomic.UserPasswordRecoveryRequestInitiator;
+import org.iglooproject.basicapp.core.business.user.model.atomic.UserPasswordRecoveryRequestType;
+import org.iglooproject.basicapp.core.business.user.service.IUserService;
+import org.iglooproject.basicapp.core.security.service.ISecurityManagementService;
 import org.iglooproject.basicapp.core.util.binding.Bindings;
+import org.iglooproject.basicapp.web.application.BasicApplicationSession;
+import org.iglooproject.basicapp.web.application.administration.form.UserPasswordUpdatePopup;
 import org.iglooproject.basicapp.web.application.common.typedescriptor.user.UserTypeDescriptor;
 import org.iglooproject.basicapp.web.application.navigation.link.LinkFactory;
 import org.iglooproject.commons.util.functional.SerializableFunction;
@@ -17,16 +29,26 @@ import org.iglooproject.wicket.more.link.descriptor.builder.LinkDescriptorBuilde
 import org.iglooproject.wicket.more.link.descriptor.mapper.ITwoParameterLinkDescriptorMapper;
 import org.iglooproject.wicket.more.link.descriptor.parameter.CommonParameters;
 import org.iglooproject.wicket.more.link.model.PageModel;
+import org.iglooproject.wicket.more.markup.html.action.AbstractAjaxAction;
 import org.iglooproject.wicket.more.markup.html.factory.DetachableFactories;
+import org.iglooproject.wicket.more.markup.html.feedback.FeedbackUtils;
+import org.iglooproject.wicket.more.markup.html.link.BlankLink;
+import org.iglooproject.wicket.more.markup.html.template.js.bootstrap.confirm.component.AjaxConfirmLink;
+import org.iglooproject.wicket.more.markup.html.template.js.bootstrap.modal.behavior.AjaxModalOpenBehavior;
 import org.iglooproject.wicket.more.model.BindingModel;
 import org.iglooproject.wicket.more.model.GenericEntityModel;
 import org.iglooproject.wicket.more.model.ReadOnlyModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wicketstuff.wiquery.core.events.MouseEvent;
 
 import com.google.common.base.Function;
 
 public class AdministrationUserDetailTemplate<U extends User> extends AdministrationTemplate {
 
 	private static final long serialVersionUID = -550100874222819991L;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AdministrationUserDetailTemplate.class);
 
 	private static final Function<User, Class<? extends Page>> USER_TO_FICHE_CLASS_FUNCTION =
 			new SerializableFunction<User, Class<? extends Page>>() {
@@ -54,6 +76,12 @@ public class AdministrationUserDetailTemplate<U extends User> extends Administra
 				));
 	}
 	
+	@SpringBean
+	private IUserService userService;
+	
+	@SpringBean
+	private ISecurityManagementService securityManagementService;
+	
 	protected final UserTypeDescriptor<U> typeDescriptor;
 	
 	protected final IModel<U> userModel = new GenericEntityModel<Long, U>(null);
@@ -79,9 +107,101 @@ public class AdministrationUserDetailTemplate<U extends User> extends Administra
 				)
 				.link("backToSourcePage").hideIfInvalid();
 		
+		UserPasswordUpdatePopup<U> passwordEditPopup = new UserPasswordUpdatePopup<>("passwordEditPopup", userModel);
+		
 		add(
 				new CoreLabel("pageTitle", BindingModel.of(userModel, Bindings.user().fullName())),
 				
+				passwordEditPopup,
+				new BlankLink("passwordEdit")
+						.add(new AjaxModalOpenBehavior(passwordEditPopup, MouseEvent.CLICK))
+						.add(
+								Condition.isTrue(
+										Model.of(securityManagementService.getOptions(userModel.getObject()).isPasswordAdminUpdateEnabled())
+								).thenShow()
+						),
+				
+				AjaxConfirmLink.<U>build()
+						.title(new ResourceModel("administration.user.action.password.recovery.reset.confirmation.title"))
+						.content(new StringResourceModel("administration.user.action.password.recovery.reset.confirmation.content", userModel))
+						.confirm()
+						.onClick(new AbstractAjaxAction() {
+							private static final long serialVersionUID = 1L;
+							@Override
+							public void execute(AjaxRequestTarget target) {
+								try {
+									securityManagementService.initiatePasswordRecoveryRequest(userModel.getObject(),
+											UserPasswordRecoveryRequestType.RESET,
+											UserPasswordRecoveryRequestInitiator.ADMIN,
+											BasicApplicationSession.get().getUser()
+									);
+									getSession().success(getString("administration.user.action.password.recovery.reset.success"));
+									target.add(getPage());
+								} catch (Exception e) {
+									LOGGER.error("Error occured while sending a password recovery request", e);
+									getSession().error(getString("common.error.unexpected"));
+								}
+								FeedbackUtils.refreshFeedback(target, getPage());
+							}
+						})
+						.create("passwordReset", userModel)
+						.add(
+								Condition.isTrue(
+										Model.of(securityManagementService.getOptions(userModel.getObject()).isPasswordAdminRecoveryEnabled())
+								).thenShow()
+						),
+				
+				new AjaxLink<U>("enable", userModel) {
+					private static final long serialVersionUID = 1L;
+					@Override
+					public void onClick(AjaxRequestTarget target) {
+						try {
+							userService.setActive(getModelObject(), true);
+							getSession().success(getString("administration.user.action.enable.success"));
+							target.add(getPage());
+						} catch (Exception e) {
+							LOGGER.error("Error occured while enabling user", e);
+							getSession().error(getString("common.error.unexpected"));
+						}
+						FeedbackUtils.refreshFeedback(target, getPage());
+					}
+				}
+						.add(Condition.isFalse(BindingModel.of(userModel, Bindings.user().active())).thenShow()),
+				
+				AjaxConfirmLink.<U>build()
+						.title(new ResourceModel("administration.user.action.disable.confirmation.title"))
+						.content(new StringResourceModel("administration.user.action.disable.confirmation.content", userModel))
+						.confirm()
+						.onClick(
+								new AbstractAjaxAction() {
+									private static final long serialVersionUID = 1L;
+									@Override
+									public void execute(AjaxRequestTarget target) {
+										try {
+											userService.setActive(userModel.getObject(), false);
+											getSession().success(getString("administration.user.action.disable.success"));
+										} catch (Exception e) {
+											LOGGER.error("Error occured while disabling user", e);
+											getSession().error(getString("common.error.unexpected"));
+										}
+										target.add(getPage());
+										FeedbackUtils.refreshFeedback(target, getPage());
+									}
+								}
+						)
+						.create("disable", userModel)
+						.add(
+								new Condition() {
+									private static final long serialVersionUID = 1L;
+									@Override
+									public boolean applies() {
+										User user = userModel.getObject();
+										User currentUser = BasicApplicationSession.get().getUser();
+										return !user.equals(currentUser) && user.isActive();
+									}
+								}.thenShow()
+						),
+						
 				backToSourcePage,
 				typeDescriptor.administrationTypeDescriptor().list().link("backToList")
 						.add(Condition.componentVisible(backToSourcePage).thenHide())
