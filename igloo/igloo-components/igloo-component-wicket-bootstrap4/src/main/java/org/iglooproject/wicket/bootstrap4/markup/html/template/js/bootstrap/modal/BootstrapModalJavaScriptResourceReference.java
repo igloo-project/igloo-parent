ea1ceaa6b8ad7d936fa995daeed032cb8a92b4c2
Florian Lacreuse <florian.lacreuse@kobalt-si.fr>
2018-02-21 14:22:17 +0100

    Bootstrap Modal - remove transitioning check on hide.

--- igloo/igloo-components/igloo-component-wicket-bootstrap4/src/main/java/org/iglooproject/wicket/bootstrap4/markup/html/template/js/bootstrap/modal/BootstrapModalJavaScriptResourceReference.java
+++ igloo/igloo-components/igloo-component-wicket-bootstrap4/src/main/java/org/iglooproject/wicket/bootstrap4/markup/html/template/js/bootstrap/modal/BootstrapModalJavaScriptResourceReference.java
@@ -1,26 +1,15 @@
 package org.iglooproject.wicket.bootstrap4.markup.html.template.js.bootstrap.modal;
 
-import java.util.List;
-
-import org.apache.wicket.markup.head.HeaderItem;
 import org.iglooproject.wicket.more.markup.html.template.js.jquery.plugins.util.AbstractCoreJQueryPluginResourceReference;
 
-import com.google.common.collect.Lists;
-
 public final class BootstrapModalJavaScriptResourceReference extends AbstractCoreJQueryPluginResourceReference {
+
 	private static final long serialVersionUID = -8799742276479282371L;
-	
+
 	private static final BootstrapModalJavaScriptResourceReference INSTANCE = new BootstrapModalJavaScriptResourceReference();
 
 	private BootstrapModalJavaScriptResourceReference() {
-		super(BootstrapModalJavaScriptResourceReference.class, "modal.js");
-	}
-	
-	@Override
-	protected List<HeaderItem> getPluginDependencies() {
-		List<HeaderItem> dependencies = Lists.newArrayList();
-//		dependencies.add(JavaScriptHeaderItem.forReference(BootstrapModalManagerJavaScriptResourceReference.get()));
-		return dependencies;
+		super(BootstrapModalJavaScriptResourceReference.class, "modal-patch.js");
 	}
 
 	public static BootstrapModalJavaScriptResourceReference get() {
--- igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/html/template/js/bootstrap/modal/statement/BootstrapModalStatement.java
+++ igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/html/template/js/bootstrap/modal/statement/BootstrapModalStatement.java
@@ -5,8 +5,6 @@ import org.wicketstuff.wiquery.core.javascript.JsStatement;
 
 public final class BootstrapModalStatement {
 
-	private static final int HIDE_TIMEOUT_DELAY = 1000;
-
 	public static final JsStatement show(Component modal) {
 		return show(modal, null);
 	}
@@ -20,14 +18,7 @@ public final class BootstrapModalStatement {
 	}
 
 	public static final JsStatement hide(Component modal) {
-		/*
-		 * Bootstrap modal js doesn't allow to close a modal while until it's fully opened.
-		 * We need to set up a timeout to delay the hide statement call.
-		 */
-		return new JsStatement()
-				.append("setTimeout(function() {")
-				.append(new JsStatement().$(modal).chain(BootstrapModal.hide()).append(";").render())
-				.append("}, " + HIDE_TIMEOUT_DELAY + ");");
+		return new JsStatement().$(modal).chain(BootstrapModal.hide()).append(";");
 	}
 
 	private BootstrapModalStatement() {
