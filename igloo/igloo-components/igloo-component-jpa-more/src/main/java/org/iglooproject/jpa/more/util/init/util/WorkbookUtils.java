17253465c502708437f18d74178ec11a11971c79
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-03-05 19:26:34 +0100

    remove unused import

--- igloo/igloo-components/igloo-component-jpa-more/src/main/java/org/iglooproject/jpa/more/util/init/util/WorkbookUtils.java
+++ igloo/igloo-components/igloo-component-jpa-more/src/main/java/org/iglooproject/jpa/more/util/init/util/WorkbookUtils.java
@@ -9,7 +9,6 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import org.apache.commons.lang3.EnumUtils;
