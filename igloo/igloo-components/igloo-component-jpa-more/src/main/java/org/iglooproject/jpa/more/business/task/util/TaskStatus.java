45d0d0f2f87801a743506de80ba367d46f5f9ebb
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-03-06 12:00:25 +0100

    sonar: use immutable list for static final

--- igloo/igloo-components/igloo-component-jpa-more/src/main/java/org/iglooproject/jpa/more/business/task/util/TaskStatus.java
+++ igloo/igloo-components/igloo-component-jpa-more/src/main/java/org/iglooproject/jpa/more/business/task/util/TaskStatus.java
@@ -2,6 +2,7 @@ package org.iglooproject.jpa.more.business.task.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.google.common.collect.Lists;
@@ -15,23 +16,23 @@ public enum TaskStatus {
 	INTERRUPTED, // Utilisé pour indiquer que la tâche est interrompue par arrêt technique de la file ou du serveur (INTERRUPTED != FAILED)
 	CANCELLED;
 	
-	public static final List<TaskStatus> CONSUMABLE_TASK_STATUS = Lists.newArrayList(
-			TaskStatus.TO_RUN,
-			TaskStatus.RUNNING,
-			TaskStatus.FAILED,
-			TaskStatus.INTERRUPTED
-	);
+	public static final List<TaskStatus> CONSUMABLE_TASK_STATUS = Collections.unmodifiableList(Lists.newArrayList(
+					TaskStatus.TO_RUN,
+					TaskStatus.RUNNING,
+					TaskStatus.FAILED,
+					TaskStatus.INTERRUPTED
+	));
 
-	public static final List<TaskStatus> RELOADABLE_TASK_STATUS = Lists.newArrayList(
-			TaskStatus.CANCELLED,
-			TaskStatus.FAILED,
-			TaskStatus.INTERRUPTED
-	);
+	public static final List<TaskStatus> RELOADABLE_TASK_STATUS = Collections.unmodifiableList(Lists.newArrayList(
+					TaskStatus.CANCELLED,
+					TaskStatus.FAILED,
+					TaskStatus.INTERRUPTED
+	));
 	
-	public static final List<TaskStatus> CANCELLABLE_TASK_STATUS = Lists.newArrayList(
-			TaskStatus.FAILED,
-			TaskStatus.INTERRUPTED
-	);
+	public static final List<TaskStatus> CANCELLABLE_TASK_STATUS = Collections.unmodifiableList(Lists.newArrayList(
+					TaskStatus.FAILED,
+					TaskStatus.INTERRUPTED
+	));
 	
 	public static List<String> getValuesAsStringList() {
 		List<TaskStatus> taskStatusList = Arrays.asList(values());
