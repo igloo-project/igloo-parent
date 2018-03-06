7f9cea3a9e96f4c3d4e2567b002723b048aa6d88
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-03-06 16:35:49 +0100

    sonar: drop dead code

--- basic-application/basic-application-core/src/main/java/org/iglooproject/basicapp/core/business/upgrade/service/IDataUpgradeManager.java
+++ basic-application/basic-application-core/src/main/java/org/iglooproject/basicapp/core/business/upgrade/service/IDataUpgradeManager.java
@@ -4,9 +4,5 @@ import org.iglooproject.jpa.exception.SecurityServiceException;
 import org.iglooproject.jpa.exception.ServiceException;
 
 public interface IDataUpgradeManager {
-
-	public final static String DATA_UPGRADE_NAME_PREFIX = "dataUpgrade.";
-	public final static String DATA_UPGRADE_NAME_SUFFIX = ".autoPerform";
-
 	void autoPerformDataUpgrades() throws ServiceException, SecurityServiceException;
 }
