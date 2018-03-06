fe44b1ceadf9821ad2320ee47b8f3b29a1a0409c
Florian Lacreuse <florian.lacreuse@kobalt-si.fr>
2018-03-06 09:52:49 +0100

    ReferenceData search: use code value filter.

--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/referencedata/model/AbstractLocalizedReferenceDataDataProvider.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/referencedata/model/AbstractLocalizedReferenceDataDataProvider.java
@@ -25,7 +25,8 @@ public abstract class AbstractLocalizedReferenceDataDataProvider<T extends Local
 	@Override
 	public ILocalizedReferenceDataSearchQuery<T, S, ?> getSearchQuery() {
 		return ((ILocalizedReferenceDataSearchQuery<T, S, ?>) super.getSearchQuery())
-				.label(labelModel.getObject());
+				.label(labelModel.getObject())
+				.code(codeModel.getObject());
 	}
 
 	public IModel<String> getLabelModel() {
