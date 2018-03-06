0603cff3eadc73065b8168ca7a4be619a8a6cbe8
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-03-06 11:53:39 +0100

    add igloo profile definition to init scripts

--- basic-application/basic-application-init/src/main/java/org/iglooproject/basicapp/init/BasicApplicationInitFromExcelMain.java
+++ basic-application/basic-application-init/src/main/java/org/iglooproject/basicapp/init/BasicApplicationInitFromExcelMain.java
@@ -2,18 +2,17 @@ package org.iglooproject.basicapp.init;
 
 import java.io.IOException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import de.schlichtherle.truezip.file.TFile;
 import org.iglooproject.basicapp.init.config.spring.BasicApplicationInitConfig;
 import org.iglooproject.basicapp.init.util.SpringContextWrapper;
 import org.iglooproject.jpa.exception.SecurityServiceException;
 import org.iglooproject.jpa.exception.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public final class BasicApplicationInitFromExcelMain {
+import de.schlichtherle.truezip.file.TFile;
+
+public final class BasicApplicationInitFromExcelMain extends AbstractBasicApplicationMain {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(BasicApplicationInitFromExcelMain.class);
 
@@ -21,10 +20,11 @@ public final class BasicApplicationInitFromExcelMain {
 	}
 
 	public static void main(String[] args) throws ServiceException, SecurityServiceException, IOException {
-		ConfigurableApplicationContext context = null;
-		try {
-			context = new AnnotationConfigApplicationContext(BasicApplicationInitConfig.class);
-			
+		new BasicApplicationInitFromExcelMain().run();
+	}
+
+	protected void run() {
+		try (AnnotationConfigApplicationContext context = startContext("development", BasicApplicationInitConfig.class)) {
 			SpringContextWrapper contextWrapper = context.getBean("springContextWrapper", SpringContextWrapper.class);
 			
 			contextWrapper.openEntityManager();
@@ -38,10 +38,6 @@ public final class BasicApplicationInitFromExcelMain {
 		} catch (Throwable e) { // NOSONAR We just want to log the Exception/Error, no error handling here.
 			LOGGER.error("Error during initialization", e);
 			throw new IllegalStateException(e);
-		} finally {
-			if (context != null) {
-				context.close();
-			}
 		}
 	}
 
--- basic-application/basic-application-init/src/main/java/org/iglooproject/basicapp/init/BasicApplicationSqlUpdateScriptMain.java
+++ basic-application/basic-application-init/src/main/java/org/iglooproject/basicapp/init/BasicApplicationSqlUpdateScriptMain.java
@@ -1,30 +1,28 @@
 package org.iglooproject.basicapp.init;
 
-import java.io.IOException;
-
 import org.iglooproject.basicapp.init.config.spring.BasicApplicationInitConfig;
-import org.iglooproject.jpa.exception.SecurityServiceException;
-import org.iglooproject.jpa.exception.ServiceException;
 import org.iglooproject.jpa.migration.SqlUpdateScript;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public final class BasicApplicationSqlUpdateScriptMain {
+public final class BasicApplicationSqlUpdateScriptMain extends AbstractBasicApplicationMain {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(BasicApplicationSqlUpdateScriptMain.class);
 
 	private BasicApplicationSqlUpdateScriptMain() {
 	}
+	
+	public static void main(String[] args) {
+		new BasicApplicationSqlUpdateScriptMain().run(args);
+	}
+
 	/*
 	 * This script uses Hibernate's hbm2ddl to write in an sql file either the script which will create the jpa model
 	 * in the database or the differences between the jpa model and the existing database.
 	 */
-	public static void main(String[] args) throws ServiceException, SecurityServiceException, IOException {
-		ConfigurableApplicationContext context = null;
-		try {
-			context = new AnnotationConfigApplicationContext(BasicApplicationInitConfig.class);
+	protected void run(String[] args) {
+		try (AnnotationConfigApplicationContext context = startContext("development", BasicApplicationInitConfig.class)) {
 			String fileName;
 			String action;
 			
@@ -41,11 +39,6 @@ public final class BasicApplicationSqlUpdateScriptMain {
 			LOGGER.info("Initialization complete");
 		} catch (Throwable e) { // NOSONAR We just want to log the Exception/Error, no error handling here.
 			LOGGER.error("Error during initialization", e);
-			throw new IllegalStateException(e);
-		} finally {
-			if (context != null) {
-				context.close();
-			}
 		}
 	}
 
