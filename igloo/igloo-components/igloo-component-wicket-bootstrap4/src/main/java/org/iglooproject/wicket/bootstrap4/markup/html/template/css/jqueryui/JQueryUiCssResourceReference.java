90e6472516c6de9ff61975a51a6eeb77826630ab
Florian Lacreuse <florian.lacreuse@kobalt-si.fr>
2018-03-07 17:58:29 +0100

    Update to jQuery UI 1.10.4 for Bootstrap 4.

--- igloo/igloo-components/igloo-component-wicket-bootstrap4/src/main/java/org/iglooproject/wicket/bootstrap4/markup/html/template/css/jqueryui/JQueryUiCssResourceReference.java
+++ igloo/igloo-components/igloo-component-wicket-bootstrap4/src/main/java/org/iglooproject/wicket/bootstrap4/markup/html/template/css/jqueryui/JQueryUiCssResourceReference.java
@@ -3,15 +3,17 @@ package org.iglooproject.wicket.bootstrap4.markup.html.template.css.jqueryui;
 import org.apache.wicket.request.resource.CssResourceReference;
 
 public final class JQueryUiCssResourceReference extends CssResourceReference {
+
 	private static final long serialVersionUID = -4192693085575758769L;
-	
+
 	private static final JQueryUiCssResourceReference INSTANCE = new JQueryUiCssResourceReference();
-	
+
 	private JQueryUiCssResourceReference() {
-		super(JQueryUiCssResourceReference.class, "jquery-ui-1.8.23.custom.css");
+		super(JQueryUiCssResourceReference.class, "jquery-ui-1.10.4.custom.css");
 	}
-	
+
 	public static JQueryUiCssResourceReference get() {
 		return INSTANCE;
 	}
+
 }
