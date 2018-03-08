270b84d94426b1d878af74b8a083a6be55f03671
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-03-08 12:05:35 +0100

    allure: try to use history

--- ci/artifacts-push.sh
+++ ci/artifacts-push.sh
@@ -4,7 +4,7 @@ ARTIFACTS=artifacts
 
 mkdir -p "${ARTIFACTS}"
 
-for result in surefire-reports allure-results jacoco.exec; do
+for result in surefire-reports allure-maven-plugin jacoco.exec; do
   find '(' -name "$result" -a '!' -wholename -a '!' -wholename "./""${ARTIFACTS}""/*" ')' \
        -exec cp -ar --parents {} "${ARTIFACTS}/" \;
 done
