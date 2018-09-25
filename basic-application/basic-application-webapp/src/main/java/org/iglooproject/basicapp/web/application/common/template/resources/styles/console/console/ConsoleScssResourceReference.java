9d32403c8d8b80a0e2b9215b6261d9595e6dc47a
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2018-09-25 17:35:57 +0200

    Fix SCSS variables files imports to avoid scope import and !global.

--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/common/template/resources/styles/console/console/ConsoleScssResourceReference.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/common/template/resources/styles/console/console/ConsoleScssResourceReference.java
@@ -1,4 +1,4 @@
-package org.iglooproject.basicapp.web.application.common.template.resources.styles.console;
+package org.iglooproject.basicapp.web.application.common.template.resources.styles.console.console;
 
 import java.util.List;
 
