64904dfbf5a526b9d56bdf9bea65e449bc1b4d3d
Julien Gonzalez <julien.gonzalez@kobalt.fr>
2018-10-10 18:29:37 +0200

    JPA · Testing combinations of @Basic.optional and @Column.nullable

---
+++ igloo/igloo-components/igloo-component-jpa-test/src/test/java/org/iglooproject/test/business/nullables/service/IEntityNullableService.java
@@ -0,0 +1,8 @@
+package org.iglooproject.test.business.nullables.service;
+
+import org.iglooproject.jpa.business.generic.service.IGenericEntityService;
+import org.iglooproject.test.business.nullables.model.EntityNullable;
+
+public interface IEntityNullableService extends IGenericEntityService<Long, EntityNullable> {
+
+}
