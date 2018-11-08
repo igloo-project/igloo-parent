0172b94452d511df30b7b02267d099f08ded7c4f
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2018-11-08 09:56:43 +0100

    Add missing BS Util js dependency for BS Modal js.

--- igloo/igloo-components/igloo-component-wicket-bootstrap4/src/main/java/org/iglooproject/wicket/bootstrap4/markup/html/template/js/bootstrap/modal/BootstrapModalJavaScriptResourceReference.java
+++ igloo/igloo-components/igloo-component-wicket-bootstrap4/src/main/java/org/iglooproject/wicket/bootstrap4/markup/html/template/js/bootstrap/modal/BootstrapModalJavaScriptResourceReference.java
@@ -1,15 +1,30 @@
 package org.iglooproject.wicket.bootstrap4.markup.html.template.js.bootstrap.modal;
 
+import java.util.List;
+
+import org.apache.wicket.markup.head.HeaderItem;
+import org.iglooproject.functional.SerializableSupplier2;
+import org.iglooproject.wicket.bootstrap4.markup.html.template.js.bootstrap.util.BootstrapUtilJavaScriptResourceReference;
 import org.iglooproject.wicket.more.markup.html.template.js.jquery.plugins.util.WebjarsJQueryPluginResourceReference;
+import org.iglooproject.wicket.more.webjars.WebjarUtil;
 
 public final class BootstrapModalJavaScriptResourceReference extends WebjarsJQueryPluginResourceReference {
 
 	private static final long serialVersionUID = -8799742276479282371L;
 
+	private static final SerializableSupplier2<List<HeaderItem>> DEPENDENCIES = WebjarUtil.memoizeHeaderItemsforReferences(
+			BootstrapUtilJavaScriptResourceReference.get()
+	);
+
 	private static final BootstrapModalJavaScriptResourceReference INSTANCE = new BootstrapModalJavaScriptResourceReference();
 
 	private BootstrapModalJavaScriptResourceReference() {
 		super("bootstrap-override/current/js/dist/modal.js");
+	}
+
+	@Override
+	public List<HeaderItem> getDependencies() {
+		return DEPENDENCIES.get();
 	}
 
 	public static BootstrapModalJavaScriptResourceReference get() {
