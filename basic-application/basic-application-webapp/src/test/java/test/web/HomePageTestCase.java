2e9890c03827b48f3b98e0298a54e3dedcbf74a9
Margot Piva <margot.piva@kobalt.fr>
2018-12-07 11:41:13 +0100

    Refactore navBar construction test

--- basic-application/basic-application-webapp/src/test/java/test/web/HomePageTestCase.java
+++ basic-application/basic-application-webapp/src/test/java/test/web/HomePageTestCase.java
@@ -2,19 +2,28 @@ package test.web;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 
-import org.apache.wicket.Component;
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.util.tester.TagTester;
-import org.iglooproject.basicapp.web.application.administration.page.AdministrationAnnouncementListPage;
+import org.apache.wicket.util.visit.IVisitor;
 import org.iglooproject.basicapp.web.application.common.template.theme.basic.NavbarPanel;
 import org.iglooproject.basicapp.web.application.navigation.page.HomePage;
+import org.iglooproject.functional.Predicates2;
 import org.iglooproject.jpa.exception.SecurityServiceException;
 import org.iglooproject.jpa.exception.ServiceException;
 import org.iglooproject.jpa.security.business.authority.util.CoreAuthorityConstants;
-import org.iglooproject.wicket.bootstrap4.console.maintenance.search.page.ConsoleMaintenanceSearchPage;
 import org.iglooproject.wicket.markup.html.basic.CoreLabel;
+import org.iglooproject.wicket.more.markup.html.template.model.NavigationMenuItem;
 import org.junit.Test;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.ImmutableList;
 
 public class HomePageTestCase extends AbstractBasicApplicationWebappTestCase {
 
@@ -66,85 +75,136 @@ public class HomePageTestCase extends AbstractBasicApplicationWebappTestCase {
 	@Test
 	public void navbarUserAuthenticated() throws ServiceException, SecurityServiceException {
 		authenticateUser(utilisateur);
-		navBarComponents();
+		
+		tester.startPage(HomePage.class);
+		tester.assertRenderedPage(HomePage.class);
+		
+		navBar(1);
 	}
 
 	@Test
 	public void navbarUserAdmin() throws ServiceException, SecurityServiceException {
 		authenticateUser(administrateur);
-		navBarComponents();
-	}
-
-	@Test
-	public void navBarNavigationMenu() throws ServiceException, SecurityServiceException {
-		authenticateUser(administrateur);
 		
 		tester.startPage(HomePage.class);
 		tester.assertRenderedPage(HomePage.class);
 		
-		Component consoleNavItem = tester.getComponentFromLastRenderedPage("navbar:mainNav:" + NavbarItem.CONSOLE.getOrder());
-		tester.clickLink(consoleNavItem.getPageRelativePath() + ":navLink");
-		tester.assertRenderedPage(ConsoleMaintenanceSearchPage.class);
+		navBar(8);
 	}
 
-	@Test
-	public void navBarNavigationSubMenu() throws ServiceException, SecurityServiceException {
-		authenticateUser(administrateur);
+	private void navBar(int nbExpectedItems) {
+		tester.assertVisible("navbar", NavbarPanel.class);
 		
-		tester.startPage(HomePage.class);
-		tester.assertRenderedPage(HomePage.class);
+		final MutableInt countVisibleItems = new MutableInt(0);
+		tester.assertVisible("navbar:mainNav", ListView.class);
+		@SuppressWarnings("unchecked")
+		ListView<NavigationMenuItem> menuItems = (ListView<NavigationMenuItem>) tester.getComponentFromLastRenderedPage("navbar:mainNav");
 		
-		Component announcementNavItem = tester.getComponentFromLastRenderedPage("navbar:mainNav:" + NavbarItem.ADMINISTRATION.getOrder() + ":subNavContainer:subNav:3");
-		tester.clickLink(announcementNavItem.getPageRelativePath() + ":navLink");
-		tester.assertRenderedPage(AdministrationAnnouncementListPage.class);
+		menuItems.visitChildren(ListItem.class, (IVisitor<ListItem<NavigationMenuItem>, Void>) (object, visit) -> {
+			if (!(object.getModelObject() instanceof NavigationMenuItem)) {
+				return;
+			}
+			
+			if (object.determineVisibility()) {
+				countVisibleItems.increment();
+			}
+			
+			if (object.getModelObject().getSubMenuItems().isEmpty()) {
+				visit.dontGoDeeper();
+			}
+		});
+		
+		int countAccessibleItems = navBarComponents(NavbarItem.menu(), "navbar:mainNav");
+		assertEquals(nbExpectedItems, countAccessibleItems, 0);
+		assertEquals(nbExpectedItems, countVisibleItems.getValue(), 0);
 	}
 
-	private void navBarComponents() throws ServiceException, SecurityServiceException {
-		tester.startPage(HomePage.class);
-		tester.assertRenderedPage(HomePage.class);
+	private int navBarComponents(List<NavbarItem> menu, String pathToMenu) {
+		int nbItems = 0;
 		
-		tester.assertComponent("navbar", NavbarPanel.class);
-		
-		for (NavbarItem navbarItem : NavbarItem.values()) {
-			if (authenticationService.hasRole(navbarItem.getAuthority())) {
-				tester.assertVisible("navbar:mainNav:" + navbarItem.getOrder());
-				tester.assertEnabled("navbar:mainNav:" + navbarItem.getOrder() + ":navLink");
-				tester.assertLabel("navbar:mainNav:" + navbarItem.getOrder() + ":navLink:label", navbarItem.getLabel());
-			} else {
-				tester.assertInvisible("navbar:mainNav:" + navbarItem.getOrder());
+		for (NavbarItem menuItem : menu) {
+			boolean hasRoleMenu = false;
+			for (String authority : menuItem.getAuthorities()) {
+				if (authenticationService.hasRole(authority)) {
+					hasRoleMenu = true;
+				}
+			}
+			if (hasRoleMenu) {
+				tester.assertVisible(pathToMenu);
+				tester.assertUsability(pathToMenu + ":" + menuItem.getOrder() + ":navLink");
+				tester.assertEscapeLabel(pathToMenu + ":" + menuItem.getOrder() + ":navLink:label", menuItem.getLabel());
+				nbItems ++;
+				
+				//SubMenu
+				
+				String subMenuPath = pathToMenu + ":" + menuItem.getOrder() + ":subNavContainer:subNav";
+				nbItems += navBarComponents(NavbarItem.submenu(menuItem), subMenuPath);
 			}
 		}
+		return nbItems;
 	}
 
 	private enum NavbarItem {
-
-		HOME(localize("navigation.home"), CoreAuthorityConstants.ROLE_AUTHENTICATED, 0),
-		REFERENCE_DATA(localize("navigation.referenceData"), CoreAuthorityConstants.ROLE_ADMIN, 1),
-		ADMINISTRATION(localize("navigation.administration"), CoreAuthorityConstants.ROLE_ADMIN, 2),
-		CONSOLE(localize("navigation.console"), CoreAuthorityConstants.ROLE_ADMIN, 3);
+		// Menu
+		HOME(null, localize("navigation.home"), 0, CoreAuthorityConstants.ROLE_AUTHENTICATED),
+		REFERENCE_DATA(null, localize("navigation.referenceData"), 1, CoreAuthorityConstants.ROLE_ADMIN),
+		ADMINISTRATION(null, localize("navigation.administration"), 2, CoreAuthorityConstants.ROLE_ADMIN),
+		CONSOLE(null, localize("navigation.console"), 3, CoreAuthorityConstants.ROLE_ADMIN),
+		
+		// SubMenu
+		ADMINISTRATION_BASIC_USER(ADMINISTRATION, localize("navigation.administration.user.basic"), 0, CoreAuthorityConstants.ROLE_ADMIN),
+		ADMINISTRATION_TECHNICAL_USER(ADMINISTRATION, localize("navigation.administration.user.technical"), 1, CoreAuthorityConstants.ROLE_ADMIN),
+		ADMINISTRATION_USERGROUP(ADMINISTRATION, localize("navigation.administration.userGroup"), 2, CoreAuthorityConstants.ROLE_ADMIN),
+		ADMINISTRATION_ANNOUNCEMENT(ADMINISTRATION, localize("navigation.administration.announcement"), 3, CoreAuthorityConstants.ROLE_ADMIN);
 		
 		private String label;
 		
-		private String authority;
+		private String[] authorities;
 		
 		private int order;
 		
-		private NavbarItem(String label, String authority, int order) {
+		private NavbarItem parent;
+		
+		private NavbarItem(NavbarItem parent, String label, int order, String ... authorities) {
+			this.parent = parent;
 			this.label = label;
-			this.authority = authority;
+			this.authorities = authorities;
 			this.order = order;
+		}
+		
+		public NavbarItem getParent() {
+			return parent;
 		}
 
 		public String getLabel() {
 			return label;
 		}
-
-		public String getAuthority() {
-			return authority;
+		
+		public String[] getAuthorities() {
+			return authorities;
 		}
-
+		
 		public int getOrder() {
 			return order;
 		}
+		
+		public static List<NavbarItem> menu() {
+			return Arrays.stream(values())
+				.filter(Predicates2.compose(Predicates.isNull(), NavbarItem::getParent))
+				.sorted(Comparator.comparing(NavbarItem::getOrder))
+				.collect(ImmutableList.toImmutableList());
+		}
+		
+		public static List<NavbarItem> submenu(NavbarItem parent) {
+			if (parent == null) {
+				return ImmutableList.of();
+			}
+			
+			return Arrays.stream(values())
+				.filter(Predicates2.compose(Predicates2.equalTo(parent), NavbarItem::getParent))
+				.sorted(Comparator.comparing(NavbarItem::getOrder))
+				.collect(ImmutableList.toImmutableList());
+		}
 	}
+
 }
