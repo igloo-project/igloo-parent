40832558733bfe39fb352810bedf6a13b44da4be
Mathieu Palley <mathieu.palley@kobalt.fr>
2019-03-20 17:56:17 +0100

    add flyway-common.properties and the associated constant

--- igloo/igloo-components/igloo-component-config-test/src/main/java/org/iglooproject/test/config/spring/ConfigurationPropertiesUrlConstants.java
+++ igloo/igloo-components/igloo-component-config-test/src/main/java/org/iglooproject/test/config/spring/ConfigurationPropertiesUrlConstants.java
@@ -3,6 +3,7 @@ package org.iglooproject.test.config.spring;
 public class ConfigurationPropertiesUrlConstants {
 
 	public static final String JPA_COMMON = "classpath:/configuration/jpa-common.properties";
+	public static final String FLYWAY_COMMON = "classpath:/configuration/flyway-common.properties";
 	public static final String JPA_EHCACHE_COMMON = "classpath:/configuration/jpa-ehcache-common.properties";
 	public static final String JPA_SEARCH_LUCENE_COMMON = "classpath:/configuration/jpa-search-lucene-common.properties";
 	public static final String JPA_SECURITY_COMMON = "classpath:/configuration/jpa-security-common.properties";
