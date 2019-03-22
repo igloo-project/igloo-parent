34e925f864f97a47de3fe95519c20ac37f6715b0
Mathieu Palley <mathieu.palley@kobalt.fr>
2019-03-22 18:10:50 +0100

    add autoconfigure taskmanagement and test

--- igloo/igloo-components/igloo-spring-autoconfigure/src/test/java/test/specific/TaskManagementAutoConfigurationTestCase.java
+++ igloo/igloo-components/igloo-spring-autoconfigure/src/test/java/test/specific/TaskManagementAutoConfigurationTestCase.java
@@ -5,33 +5,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.igloo.spring.autoconfigure.EnableIglooAutoConfiguration;
 import org.igloo.spring.autoconfigure.IglooAutoConfigurationImportSelector;
 import org.igloo.spring.autoconfigure.bootstrap.IglooBootstrap3AutoConfiguration;
-import org.iglooproject.jpa.search.service.IHibernateSearchService;
+import org.iglooproject.jpa.more.business.task.service.QueuedTaskHolderManagerImpl;
 import org.junit.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Base class used to check that {@link EnableIglooAutoConfiguration} triggers IglooJpaAutoConfiguration properly. 
+ * Base class used to check that {@link EnableIglooAutoConfiguration} triggers IglooTaskManagerAutoConfiguration properly. 
  * 
  * This class uses ApplicationContextRunner to initialize contexts with suitable configurations,
  * which are declared at the bottom of the file.
  *  
  */
-public class HibernateSearchAutoConfigurationTestCase {
+public class TaskManagementAutoConfigurationTestCase {
 
 	/**
-	 * Check that autoconfiguration from {@link IHibernateSearchService} is triggered with EnableIglooAutoConfiguration
+	 * Check that autoconfiguration from {@link QueuedTaskHolderManagerImpl} is triggered with EnableIglooAutoConfiguration
 	 */
 	@Test
-	public void testIglooHibernateSearchAutoConfigure() {
+	public void testIglooTaskManagementAutoConfigure() {
 		new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(TestConfig.class))
 			.withPropertyValues(String.format("%s=%s",
 					IglooAutoConfigurationImportSelector.PROPERTY_NAME_AUTOCONFIGURE_EXCLUDE,
 					IglooBootstrap3AutoConfiguration.class.getName()))
 			.run(
-				(context) -> { assertThat(context).hasSingleBean(IHibernateSearchService.class); }
+				(context) -> { assertThat(context).hasSingleBean(QueuedTaskHolderManagerImpl.class); }
 			);
 	}
 	
