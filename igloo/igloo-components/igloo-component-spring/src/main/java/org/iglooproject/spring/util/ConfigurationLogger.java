80bc4f2f0134cc922a8aa5946146321c2205eb3b
Laurent Almeras <laurent.almeras@kobalt.fr>
2019-06-27 18:54:08 +0200

    Logging mechanism for PropertySources
    This will be used to ensure that property source resolution is not
    broken when we migrate from old system to vanilla Spring
    @PropertySource.
    
    PropertySourcesPlaceholderConfigurer is a component initialized with
    environment that loads properties from @ConfigurationLocations and
    bootstrap configuration.
    
    It is needed to dump this configuration in pre-@PropertySource igloo.
    
    Javadoc for ConfigurationLogger.java translated in english.

--- igloo/igloo-components/igloo-component-spring/src/main/java/org/iglooproject/spring/util/ConfigurationLogger.java
+++ igloo/igloo-components/igloo-component-spring/src/main/java/org/iglooproject/spring/util/ConfigurationLogger.java
@@ -2,6 +2,9 @@ package org.iglooproject.spring.util;
 
 import java.util.List;
 
+import org.iglooproject.spring.property.model.PropertyId;
+import org.iglooproject.spring.property.service.IConfigurablePropertyService;
+import org.iglooproject.spring.property.service.IPropertyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -12,30 +15,23 @@ import org.springframework.context.support.AbstractApplicationContext;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
-import org.iglooproject.spring.property.model.PropertyId;
-import org.iglooproject.spring.property.service.IConfigurablePropertyService;
-import org.iglooproject.spring.property.service.IPropertyService;
-
 /**
- * <p>Ce listener Spring permet de logguer la configuration Spring lors de l'émission
- * de l'événement REFRESH.</p>
+ * <p>This Spring listener allows to log PropertyId-s values on refresh event. It is triggered on by a root context
+ * event.</p>
  * 
- * <p>Ce listener n'est déclenché que pour les événements émis par un contexte racine.</p>
+ * <p>Logged properties are controlled by <i>propertyNamesForInfoLogLevel</i> attribute, that list PropertyId-s keys to
+ * log. These string values must match a registered PropertyId key.</p>
  * 
- * <p>La propriété <i>propertyNamesForInfoLogLevel</i> permet de spécifier, par une liste
- * de noms de propriétés, quels sont les éléments de configuration à logguer au
- * niveau INFO. De plus, elle permet de forcer le logging de propriétés ne
- * possédant pas d'accesseur (tels que les informations d'accès à la base de données.</p>
+ * <p>Logged values are sent to {@link org.iglooproject.spring.util.ConfigurationLogger} with an INFO level,
+ * and according to <i>propertyNamesForInfoLogLevel</i> list order.</p>
  * 
- * <p>Les propriétés listées dans <i>propertyNamesForInfoLogLevel</i> sont affichées
- * dans l'ordre de déclaration, avec un log level INFO, puis l'ensemble des
- * propriétés possédant un accesseur sont logguées avec un log level TRACE.</p>
+ * <p>If {@link org.iglooproject.spring.util.ConfigurationLogger} is set to TRACE level, all registered PropertyId-s
+ * are logged in a second phase.</p>
  * 
- * <p>Les autres éléments sont loggués au niveau TRACE.</p>
+ * <p>logPattern attribute allows to control the way key/value are logged.</p>
  * 
- * <p>La propriété <i>logPattern</i> permet de spécifier le formattage des messages
- * de log émis pour chaque item de configuration. Deux arguments, le nom de la propriété
- * et sa valeur, sont passés en paramètre de String.format sur ce pattern.</p>
+ * @see PropertyId
+ * @see IConfigurablePropertyService
  */
 public class ConfigurationLogger implements ApplicationListener<ContextRefreshedEvent> {
 	
@@ -55,67 +51,96 @@ public class ConfigurationLogger implements ApplicationListener<ContextRefreshed
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent refresh) {
-		
+		// run only once by context
 		if (refresh.getSource() instanceof AbstractApplicationContext &&
 				((AbstractApplicationContext) refresh.getSource()).getParent() == null) {
 			LOGGER.info("Configuration logging");
 			
+			// retrieve application context
 			ApplicationContext context = refresh.getApplicationContext();
-			String[] propertyServiceNames = context.getBeanNamesForType(IConfigurablePropertyService.class);
 			
-			if (propertyServiceNames.length > 0) {
-				String propertyServiceName = propertyServiceNames[0];
-				
-				if (propertyServiceNames.length > 1) {
-					LOGGER.warn(String.format("Multiple %1$s found. We only log the configuration of the first instance.", IPropertyService.class.getSimpleName()));
-				}
-				
+			// retrieve IConfigurablePropertyService
+			IConfigurablePropertyService propertyService = getPropertyServiceName(context);
+			if (propertyService != null) {
 				LOGGER.info("Configuration found, start logging");
-
-				IConfigurablePropertyService propertyService = (IConfigurablePropertyService) context.getBean(propertyServiceName);
 				
-				List<String> loggedProperties = Lists.newArrayList();
-				@SuppressWarnings("unchecked")
-				Iterable<PropertyId<?>> registeredPropertyIds =
-						(Iterable<PropertyId<?>>) (Object) Iterables.filter(propertyService.listRegistered(), PropertyId.class);
-				
-				/* On logge les informations qu'on a configurées dans le contexte Spring */
-				for (String propertyIdKey : propertyIdsKeysForInfoLogLevel) {
-					if (loggedProperties.contains(propertyIdKey)) {
-						continue;
-					}
-					
-					for (PropertyId<?> propertyId : registeredPropertyIds) {
-						if (propertyId.getKey().equals(propertyIdKey)) {
-							logPropertyAsInfo(propertyIdKey, propertyService.get(propertyId));
-							loggedProperties.add(propertyIdKey);
-							break;
-						}
-					}
-				}
-				
-				/* Si jamais on est en mode TRACE, on logge aussi les autres propriétés */
-				if (LOGGER.isTraceEnabled()) {
-					for (PropertyId<?> propertyId : registeredPropertyIds) {
-						if (!propertyIdsKeysForInfoLogLevel.contains(propertyId.getKey())) {
-							logPropertyAsTrace(propertyId.getKey(), propertyService.get(propertyId));
-						}
-					}
-				}
+				/* Log PropertyId-s  */
+				logConfigurationPropertiesIds(propertyService);
 			} else {
-				LOGGER.warn(String.format("No %1$s found. Unable to log the configuration.", IConfigurablePropertyService.class.getSimpleName()));
+				// warn if no IPropertyService found
+				if (LOGGER.isWarnEnabled()) {
+					LOGGER.warn(String.format("No %1$s found. Unable to log the configuration.", IConfigurablePropertyService.class.getSimpleName()));
+				}
 			}
 			
 			LOGGER.info("Configuration logging end");
 		}
 	}
+
+	/**
+	 * IConfigurablePropertyService should be unique. A warn log is thrown if multiple beans are found
+	 */
+	private IConfigurablePropertyService getPropertyServiceName(ApplicationContext context) {
+		String[] propertyServiceNames = context.getBeanNamesForType(IConfigurablePropertyService.class);
+		if (propertyServiceNames.length == 0) {
+			return null;
+		}
+		String propertyServiceName = propertyServiceNames[0];
+		if (propertyServiceNames.length > 1) {
+			if (LOGGER.isWarnEnabled()) { // NOSONAR (squid:S1066)
+				LOGGER.warn(String.format("Multiple %1$s found. We only log the configuration of the first instance.",
+						IPropertyService.class.getSimpleName()));
+			}
+		}
+		return (IConfigurablePropertyService) context.getBean(propertyServiceName);
+	}
+
+	/**
+	 * Log configured PropertyId-s values, according to configuration order, then, if TRACE mode enabled, log all
+	 * PropertyId-s values.
+	 */
+	private void logConfigurationPropertiesIds(IConfigurablePropertyService propertyService) {
+		// store logged properties to log each property only once
+		List<String> loggedProperties = Lists.newArrayList();
+		@SuppressWarnings("unchecked")
+		Iterable<PropertyId<?>> registeredPropertyIds =
+				(Iterable<PropertyId<?>>) (Object) Iterables.filter(propertyService.listRegistered(), PropertyId.class);
+		
+		// iterate on propertyIdsKeysForInfoLogLevel so that properties are logged according to configuration order.
+		for (String propertyIdKey : propertyIdsKeysForInfoLogLevel) {
+			if (loggedProperties.contains(propertyIdKey)) {
+				continue;
+			}
+			
+			for (PropertyId<?> propertyId : registeredPropertyIds) {
+				if (propertyId.getKey().equals(propertyIdKey)) {
+					logPropertyAsInfo(propertyIdKey, propertyService.get(propertyId));
+					loggedProperties.add(propertyIdKey);
+					break;
+				}
+			}
+		}
+		
+		/* If LOGGER configured at TRACE level, logging all PropertyId-s after the configured ones */
+		if (LOGGER.isTraceEnabled()) {
+			for (PropertyId<?> propertyId : registeredPropertyIds) {
+				if (!propertyIdsKeysForInfoLogLevel.contains(propertyId.getKey())) {
+					logPropertyAsTrace(propertyId.getKey(), propertyService.get(propertyId));
+				}
+			}
+		}
+	}
 	
 	private void logPropertyAsInfo(String propertyName, Object value) {
-		LOGGER.info(String.format(logPattern, propertyName, value));
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info(String.format(logPattern, propertyName, value));
+		}
 	}
 	
 	private void logPropertyAsTrace(String propertyName, Object value) {
-		LOGGER.trace(String.format(logPattern, propertyName, value));
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.trace(String.format(logPattern, propertyName, value));
+		}
 	}
 
 }
