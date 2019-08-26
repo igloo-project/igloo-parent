aa91af1bcbb0340fa974e8413356a4af30b54d56
Laurent Almeras <laurent.almeras@kobalt.fr>
2019-08-26 18:30:00 +0200

    gitlab-ci: dependencies, fix bash script

--- ci/maven-dependencies.sh
+++ ci/maven-dependencies.sh
@@ -26,7 +26,7 @@ chmod 644 ~/.ssh/known_hosts
 
 mvn site:stage-deploy \
 	-DstagingSiteURL="scp://${SYNC_TEST_REPORTS_USER}@${SYNC_TEST_REPORTS_HOST}/${SYNC_TEST_REPORTS_DOCUMENT_ROOT}/${CI_PROJECT_NAME}-dependencies/${CI_COMMIT_REF_NAME}/${SYNC_DATE_COMMIT}" \
-	-DtopSiteURL=https://reports.tools.kobalt.fr/${CI_PROJECT_NAME}-dependencies/${CI_COMMIT_REF_NAME}/${SYNC_DATE_COMMIT}"
+	-DtopSiteURL="https://reports.tools.kobalt.fr/${CI_PROJECT_NAME}-dependencies/${CI_COMMIT_REF_NAME}/${SYNC_DATE_COMMIT}"
 
 if [ $TEST_RESULT -ne 0 ]; then
   echo "Dependency job ends with some test failures"
