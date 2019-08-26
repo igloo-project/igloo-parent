94f6fac98e42d1e0d4560605ff0b4e4ce453288f
Laurent Almeras <lalmeras@gmail.com>
2019-08-27 00:11:00 +0200

    gitlab-ci: dependencies, wagon config for deploy

--- ci/maven-dependencies.sh
+++ ci/maven-dependencies.sh
@@ -25,7 +25,7 @@ echo "$SYNC_TEST_REPORTS_SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 
 mvn site:stage-deploy \
-	-DstagingSiteURL="scp://${SYNC_TEST_REPORTS_USER}@${SYNC_TEST_REPORTS_HOST}/${SYNC_TEST_REPORTS_DOCUMENT_ROOT}/${CI_PROJECT_NAME}-dependencies/${CI_COMMIT_REF_NAME}/${SYNC_DATE_COMMIT}" \
+	-DstagingSiteURL="scp://${SYNC_TEST_REPORTS_USER}@${SYNC_TEST_REPORTS_HOST}${SYNC_TEST_REPORTS_DOCUMENT_ROOT}/${CI_PROJECT_NAME}-dependencies/${CI_COMMIT_REF_NAME}/${SYNC_DATE_COMMIT}" \
 	-DtopSiteURL="https://reports.tools.kobalt.fr/${CI_PROJECT_NAME}-dependencies/${CI_COMMIT_REF_NAME}/${SYNC_DATE_COMMIT}"
 
 if [ $TEST_RESULT -ne 0 ]; then
