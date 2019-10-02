01735b71bfd166e9d66dce25ddede8bacbd903ae
Laurent Almeras <laurent.almeras@kobalt.fr>
2019-10-02 17:56:39 +0200

    *PanelRendererServiceImpl: add renderPage method

--- igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/notification/service/AbstractWicketRendererServiceImpl.java
+++ igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/notification/service/AbstractWicketRendererServiceImpl.java
@@ -3,6 +3,7 @@ package org.iglooproject.wicket.more.notification.service;
 import java.util.Locale;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.Page;
 import org.iglooproject.functional.SerializableSupplier2;
 import org.iglooproject.jpa.security.service.IRunAsSystemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,13 @@ public abstract class AbstractWicketRendererServiceImpl extends AbstractOfflineP
 	protected String renderComponent(final SerializableSupplier2<Component> componentSupplier, final Locale locale, final String variation) {
 		return securityService.runAsSystem(
 			() -> AbstractWicketRendererServiceImpl.super.renderComponent(componentSupplier, locale, variation)
+		);
+	}
+	
+	@Override
+	protected String renderPage(final SerializableSupplier2<? extends Page> pageSupplier, final Locale locale, final String variation) {
+		return securityService.runAsSystem(
+			() -> AbstractWicketRendererServiceImpl.super.renderPage(pageSupplier, locale, variation)
 		);
 	}
 }
