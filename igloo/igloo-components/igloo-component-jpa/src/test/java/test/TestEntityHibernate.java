0244cc5d3b1e12f0d62f5e269453fa222e217f8c
Laurent Almeras <laurent.almeras@kobalt.fr>
2019-10-31 15:45:22 +0100

    jpa: working GenericEntity without hibernate
    equals implementation is based on statically defined lambda so that
    equals/toString can be used without hibernate.

---
+++ igloo/igloo-components/igloo-component-jpa/src/test/java/test/TestEntityHibernate.java
@@ -0,0 +1,60 @@
+package test;
+
+import org.hibernate.Hibernate;
+import org.iglooproject.jpa.business.generic.model.GenericEntity;
+
+public class TestEntityHibernate extends GenericEntity<Long, TestEntityHibernate> {
+
+	private static final long serialVersionUID = 5968050124765460930L;
+
+	public TestEntityHibernate() {}
+
+	private Long id;
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String getNameForToString() {
+		return String.valueOf(id);
+	}
+
+	@Override
+	public String getDisplayName() {
+		return getNameForToString();
+	}
+
+	/**
+	 * This method is the old equals implementation from GenericEntity, without lambda static switching.
+	 */
+	@Override
+	public boolean equals(Object object) {
+		if (object == null) {
+			return false;
+		}
+		if (object == this) {
+			return true;
+		}
+		
+		// process class comparison; old code without lambda-controlled static switch
+		if (Hibernate.getClass(object) != Hibernate.getClass(this)) {
+			return false;
+		}
+
+		TestEntityHibernate entity = (TestEntityHibernate) object; 
+		Long id = getId();
+
+		if (id == null) {
+			return false;
+		}
+
+		return id.equals(entity.getId());
+	}
+}
---
+++ igloo/igloo-components/igloo-component-jpa/src/test/java/test/TestEntitySimple.java
@@ -0,0 +1,59 @@
+package test;
+
+import org.iglooproject.jpa.business.generic.model.GenericEntity;
+
+public class TestEntitySimple extends GenericEntity<Long, TestEntitySimple> {
+
+	private static final long serialVersionUID = 5968050124765460930L;
+
+	public TestEntitySimple() {}
+
+	private Long id;
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String getNameForToString() {
+		return String.valueOf(id);
+	}
+
+	@Override
+	public String getDisplayName() {
+		return getNameForToString();
+	}
+
+	/**
+	 * This method is the old equals implementation from GenericEntity, without lambda static switching.
+	 */
+	@Override
+	public boolean equals(Object object) {
+		if (object == null) {
+			return false;
+		}
+		if (object == this) {
+			return true;
+		}
+		
+		// process class comparison; old code without hibernate and without lambda-controlled static switch
+		if (object.getClass() != this.getClass()) {
+			return false;
+		}
+
+		TestEntitySimple entity = (TestEntitySimple) object; 
+		Long id = getId();
+
+		if (id == null) {
+			return false;
+		}
+
+		return id.equals(entity.getId());
+	}
+}
