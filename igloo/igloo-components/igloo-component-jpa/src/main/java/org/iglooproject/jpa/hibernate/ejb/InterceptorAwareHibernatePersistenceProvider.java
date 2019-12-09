9ba8c7b57d77c3931ba5d1bba72e87b432b02d40
Mathieu Palley <mathieu.palley@kobalt.fr>
2019-12-09 17:41:26 +0100

    update maven dependencies and plugins versions
    spring 5.2.0 -> 5.2.2
    spring-security 5.1.4 -> 5.2.1
    hibernate 5.4.3 -> 5.4.10
    hibernate-search 5.11.1 -> 5.11.4

--- igloo/igloo-components/igloo-component-jpa/src/main/java/org/iglooproject/jpa/hibernate/ejb/InterceptorAwareHibernatePersistenceProvider.java
+++ igloo/igloo-components/igloo-component-jpa/src/main/java/org/iglooproject/jpa/hibernate/ejb/InterceptorAwareHibernatePersistenceProvider.java
@@ -40,8 +40,8 @@ public class InterceptorAwareHibernatePersistenceProvider extends HibernatePersi
 	public EntityManagerFactory createContainerEntityManagerFactory(PersistenceUnitInfo info, Map properties) {
 		return new EntityManagerFactoryBuilderImpl(new PersistenceUnitInfoDescriptor(info), properties) {
 			@Override
-			protected void populate(SessionFactoryBuilder sfBuilder, StandardServiceRegistry ssr) {
-				super.populate(sfBuilder, ssr);
+			protected void populateSfBuilder(SessionFactoryBuilder sfBuilder, StandardServiceRegistry ssr) {
+				super.populateSfBuilder(sfBuilder, ssr);
 				
 				if (InterceptorAwareHibernatePersistenceProvider.this.interceptor != null) {
 					LOGGER.warn("Installing our Spring managed interceptor.");
