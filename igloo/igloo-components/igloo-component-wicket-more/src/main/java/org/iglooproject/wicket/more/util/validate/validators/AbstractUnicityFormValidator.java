40cb2e04a76a7241b736386cc0a68057c8f31ab4
Margot Piva <margot.piva@kobalt.fr>
2020-05-05 11:14:11 +0200

    Improve FormComponents' feedbacks for AbstractUnicityFormValidator on error

--- igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/util/validate/validators/AbstractUnicityFormValidator.java
+++ igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/util/validate/validators/AbstractUnicityFormValidator.java
@@ -1,5 +1,7 @@
 package org.iglooproject.wicket.more.util.validate.validators;
 
+import java.util.stream.Stream;
+
 import javax.annotation.Nonnull;
 
 import org.apache.wicket.Component;
@@ -7,8 +9,6 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.validation.AbstractFormValidator;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.validation.ValidationError;
-
 import org.iglooproject.commons.util.CloneUtils;
 
 public abstract class AbstractUnicityFormValidator<T> extends AbstractFormValidator {
@@ -47,16 +47,15 @@ public abstract class AbstractUnicityFormValidator<T> extends AbstractFormValida
 		T matchingObject = getByUniqueField();
 		if (matchingObject != null && !isSame(matchingObject, mainObject)) {
 			error(formComponent1, errorKey);
+			
+			Stream.of(components)
+				.skip(1)
+				.forEach(this::error);
 		}
 	}
 	
 	protected boolean isSame(@Nonnull T matchingObject, T mainObject) {
 		return matchingObject.equals(mainObject);
-	}
-
-	protected ValidationError decorate(ValidationError error) {
-		// Does nothing by default
-		return error;
 	}
 
 	protected abstract T getByUniqueField();
