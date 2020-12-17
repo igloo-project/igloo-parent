a137ffb0001971abe6752213ac66fe5d25fa89cf
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2020-12-17 17:33:25 +0100

    Minor fixes on properties console page

--- igloo/igloo-components/igloo-component-wicket-bootstrap4/src/main/java/org/iglooproject/wicket/bootstrap4/console/maintenance/properties/page/ConsoleMaintenancePropertiesPage.java
+++ igloo/igloo-components/igloo-component-wicket-bootstrap4/src/main/java/org/iglooproject/wicket/bootstrap4/console/maintenance/properties/page/ConsoleMaintenancePropertiesPage.java
@@ -1,6 +1,5 @@
 package org.iglooproject.wicket.bootstrap4.console.maintenance.properties.page;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +11,6 @@ import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.iglooproject.spring.property.model.IPropertyRegistryKey;
 import org.iglooproject.spring.property.model.PropertyId;
 import org.iglooproject.spring.property.service.IPropertyRegistry;
 import org.iglooproject.spring.property.service.IPropertyService;
@@ -22,66 +20,58 @@ import org.iglooproject.wicket.markup.html.basic.CoreLabel;
 import org.iglooproject.wicket.more.condition.Condition;
 import org.iglooproject.wicket.more.markup.html.basic.PlaceholderContainer;
 import org.iglooproject.wicket.more.markup.html.template.model.BreadCrumbElement;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableList;
 
 public class ConsoleMaintenancePropertiesPage extends ConsoleMaintenanceTemplate {
 
 	private static final long serialVersionUID = -6149952103369498125L;
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(ConsoleMaintenancePropertiesPage.class);
 
 	@SpringBean
 	private IPropertyRegistry propertyRegistry;
 
 	@SpringBean
 	private IPropertyService propertyService;
-	
+
 	@SpringBean
 	private PropertySourceLogger propertySourceLogger;
-	
+
 	public ConsoleMaintenancePropertiesPage(PageParameters parameters) {
 		super(parameters);
 		
 		addBreadCrumbElement(new BreadCrumbElement(new ResourceModel("console.maintenance.properties")));
 		
-		IModel<List<IPropertyRegistryKey<?>>> propertyIdsModel = new LoadableDetachableModel<List<IPropertyRegistryKey<?>>>() {
+		IModel<List<PropertyId<?>>> propertyIdsModel = new LoadableDetachableModel<List<PropertyId<?>>>() {
 			private static final long serialVersionUID = 1L;
 			@Override
-			protected List<IPropertyRegistryKey<?>> load() {
-				return new ArrayList<>(propertyRegistry.listRegistered());
+			protected List<PropertyId<?>> load() {
+				return propertyRegistry.listRegistered().stream()
+					.filter(PropertyId.class::isInstance)
+					.map(PropertyId.class::cast)
+					.collect(ImmutableList.toImmutableList());
 			}
 		};
-		ListView<IPropertyRegistryKey<?>> propertyIdsListView = new ListView<IPropertyRegistryKey<?>>("propertyIds", propertyIdsModel) {
+		
+		ListView<PropertyId<?>> propertyIdsListView = new ListView<PropertyId<?>>("propertyIds", propertyIdsModel) {
 			private static final long serialVersionUID = 1L;
 			@Override
-			protected void populateItem(ListItem<IPropertyRegistryKey<?>> item) {
-				if (item.getModelObject() instanceof PropertyId) {
-					IModel<PropertyId<Object>> propertyIdModel = new LoadableDetachableModel<PropertyId<Object>>() {
-						private static final long serialVersionUID = 1L;
-						@SuppressWarnings("unchecked")
-						@Override
-						protected PropertyId<Object> load() {
-							return (PropertyId<Object>) item.getModelObject();
-						}
-					};
-					IModel<Object> propertyIdValueModel = new LoadableDetachableModel<Object>() {
-						private static final long serialVersionUID = 1L;
-						@Override
-						protected Object load() {
-							return propertyService.get(propertyIdModel.getObject());
-						}
-					};
-					item.setVisible(true);
-					item.add(
-						new CoreLabel("name", propertyIdModel.map(PropertyId::getKey)),
-						new CoreLabel("value", propertyIdValueModel)
-					);
-				} else {
-					item.setVisible(false);
-				}
+			protected void populateItem(ListItem<PropertyId<?>> item) {
+				IModel<PropertyId<?>> propertyIdModel = item.getModel();
+				IModel<Object> propertyIdValueModel = new LoadableDetachableModel<Object>() {
+					private static final long serialVersionUID = 1L;
+					@Override
+					protected Object load() {
+						return propertyService.get(propertyIdModel.getObject());
+					}
+				};
+				item.setVisible(true);
+				item.add(
+					new CoreLabel("name", propertyIdModel.map(PropertyId::getKey)),
+					new CoreLabel("value", propertyIdValueModel)
+				);
 			}
 		};
+		
 		add(
 			propertyIdsListView
 				.add(Condition.collectionModelNotEmpty(propertyIdsModel).thenShow()),
@@ -93,9 +83,10 @@ public class ConsoleMaintenancePropertiesPage extends ConsoleMaintenanceTemplate
 			private static final long serialVersionUID = 1L;
 			@Override
 			protected List<Map.Entry<Object, Object>> load() {
-				return new ArrayList<>(propertySourceLogger.listProperties().entrySet());
+				return ImmutableList.copyOf(propertySourceLogger.listProperties().entrySet());
 			}
 		};
+		
 		ListView<Map.Entry<Object, Object>> propertiesListView = new ListView<Map.Entry<Object, Object>>("properties", propertiesModel) {
 			private static final long serialVersionUID = 1L;
 			@Override
@@ -106,6 +97,7 @@ public class ConsoleMaintenancePropertiesPage extends ConsoleMaintenanceTemplate
 				);
 			}
 		};
+		
 		add(
 			propertiesListView
 				.add(Condition.collectionModelNotEmpty(propertiesModel).thenShow()),
