d57328bd92884b66870b3f3d1ad06da2984318a0
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2021-01-04 10:55:55 +0100

    BasicApp: clean up user password recovery notification panel

--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/notification/component/UserPasswordRecoveryRequestHtmlNotificationPanel.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/notification/component/UserPasswordRecoveryRequestHtmlNotificationPanel.java
@@ -10,7 +10,6 @@ import org.iglooproject.basicapp.core.util.ResourceKeyGenerator;
 import org.iglooproject.basicapp.core.util.binding.Bindings;
 import org.iglooproject.wicket.markup.html.basic.CoreLabel;
 import org.iglooproject.wicket.more.link.descriptor.generator.ILinkGenerator;
-import org.iglooproject.wicket.more.markup.html.link.InvisibleLink;
 import org.iglooproject.wicket.more.model.BindingModel;
 
 public class UserPasswordRecoveryRequestHtmlNotificationPanel<T extends User> extends AbstractHtmlNotificationPanel<T> {
@@ -30,7 +29,6 @@ public class UserPasswordRecoveryRequestHtmlNotificationPanel<T extends User> ex
 			ILinkGenerator linkGenerator) {
 		super(id, objectModel);
 		
-		// Intro
 		StringResourceModel descriptionTextModel = new StringResourceModel(resourceKeyGenerator.resourceKey("text"), objectModel)
 			.setParameters(dateModel, authorModel)
 			.setDefaultValue(
@@ -40,31 +38,26 @@ public class UserPasswordRecoveryRequestHtmlNotificationPanel<T extends User> ex
 		
 		add(new CoreLabel("intro", descriptionTextModel).setEscapeModelStrings(false));
 		
-		// Main link
-		if (linkGenerator != null) {
-			StringResourceModel linkIntroModel = new StringResourceModel(resourceKeyGenerator.resourceKey("link.intro"), objectModel)
-				.setParameters(dateModel, authorModel)
-				.setDefaultValue(
-					new StringResourceModel(defaultResourceKeyGenerator.resourceKey("link.intro"), objectModel)
-						.setParameters(dateModel, authorModel)
-				);
-			
-			StringResourceModel linkLabelModel = new StringResourceModel(resourceKeyGenerator.resourceKey("link.label"), objectModel)
-				.setParameters(dateModel, authorModel)
-				.setDefaultValue(
-					new StringResourceModel(defaultResourceKeyGenerator.resourceKey("link.label"), objectModel)
-						.setParameters(dateModel, authorModel)
-				);
-			
-			add(
-				new CoreLabel("linkIntro", linkIntroModel),
-				linkGenerator.link("mainLink")
-					.setAbsolute(true)
-					.setBody(linkLabelModel)
+		StringResourceModel linkIntroModel = new StringResourceModel(resourceKeyGenerator.resourceKey("link.intro"), objectModel)
+			.setParameters(dateModel, authorModel)
+			.setDefaultValue(
+				new StringResourceModel(defaultResourceKeyGenerator.resourceKey("link.intro"), objectModel)
+					.setParameters(dateModel, authorModel)
 			);
-		} else {
-			add(new InvisibleLink<>("mainLink"));
-		}
+		
+		StringResourceModel linkLabelModel = new StringResourceModel(resourceKeyGenerator.resourceKey("link.label"), objectModel)
+			.setParameters(dateModel, authorModel)
+			.setDefaultValue(
+				new StringResourceModel(defaultResourceKeyGenerator.resourceKey("link.label"), objectModel)
+					.setParameters(dateModel, authorModel)
+			);
+		
+		add(
+			new CoreLabel("linkIntro", linkIntroModel),
+			linkGenerator.link("mainLink")
+				.setAbsolute(true)
+				.setBody(linkLabelModel)
+		);
 		
 		add(
 			new WebMarkupContainer("helpUsername")
