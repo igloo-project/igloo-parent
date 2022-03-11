b6ace4c0e6506e37d500d98aa027bf456fea18ce
Chloe Baffert Bui Van <chloe.baffert-buivan@kobalt.fr>
2022-03-11 17:14:26 +0100

    BasicApp : Spring security Java config

--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/config/spring/BasicApplicationWebappConfig.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/config/spring/BasicApplicationWebappConfig.java
@@ -23,8 +23,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 @Import({
 	BasicApplicationCoreCommonConfig.class,
 	BasicApplicationWebappSecurityConfig.class,
--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/config/spring/BasicApplicationWebappSecurityConfig.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/config/spring/BasicApplicationWebappSecurityConfig.java
@@ -1,12 +1,114 @@
 package org.iglooproject.basicapp.web.application.config.spring;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
+import org.iglooproject.jpa.security.business.authority.util.CoreAuthorityConstants;
+import org.iglooproject.web.security.access.expression.method.CoreWebSecurityExpressionHandler;
 import org.iglooproject.web.security.config.spring.AbstractWebappSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
 @Configuration
-@ImportResource({ "classpath:spring/security-web-context.xml" })
 public class BasicApplicationWebappSecurityConfig extends AbstractWebappSecurityConfig {
+
+	@Configuration
+	@Order(1)
+	public static class ConsoleWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.regexMatcher("^/console/.*")
+				.headers().disable()
+				.csrf().disable()
+				.formLogin()
+					.loginPage("/console/login/")
+					.defaultSuccessUrl("/console/login/success/", true)
+					.failureUrl("/console/login/failure/").and()
+				.anonymous().authorities(CoreAuthorityConstants.ROLE_ANONYMOUS).and()
+				.exceptionHandling().accessDeniedPage("/console/access-denied/").and()
+				.authorizeRequests()
+					.regexMatchers(
+						"/console/login/.*",
+						"/console/access-denied/.*").permitAll()
+					.regexMatchers("/console/.*").hasAnyAuthority(CoreAuthorityConstants.ROLE_ADMIN);
+		}
+	}
+
+	@Configuration
+	@Order(2)
+	public static class ResourcesWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.regexMatcher("^/wicket/resource/.*")
+				.headers().disable()
+				.csrf().disable()
+				.formLogin().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
+				.exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint()).and()
+				.authorizeRequests()
+					.regexMatchers(
+						"^/wicket/resource/org.iglooproject.showcase.web.application.common.template.resources.js.[^/]+.*",
+						"^/wicket/resource/org.iglooproject.showcase.web.application.common.template.resources.styles.[^/]+.*",
+						"^/wicket/resource/org.iglooproject.showcase.web.application.common.template.resources.images.[^/]+.*").hasAnyAuthority(CoreAuthorityConstants.ROLE_ANONYMOUS)
+					.regexMatchers("^/wicket/resource/org.iglooproject.showcase.web.application.[^/]+.*").hasAnyAuthority(CoreAuthorityConstants.ROLE_AUTHENTICATED)
+					.regexMatchers("^/wicket/resource/.*").hasAnyAuthority(CoreAuthorityConstants.ROLE_ANONYMOUS);
+		}
+	}
+
+	@Configuration
+	@Order(3)
+	public static class BasicApplicationWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		@Autowired
+		private CoreWebSecurityExpressionHandler webSecurityExpressionHandler;
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.headers().disable()
+				.csrf().disable()
+				.formLogin()
+					.loginPage("/login/")
+					.defaultSuccessUrl("/login/success/", true)
+					.failureUrl("/login/failure/").and()
+				.anonymous().authorities(CoreAuthorityConstants.ROLE_ANONYMOUS).and()
+				.exceptionHandling().accessDeniedPage("/access-denied/").and()
+				.authorizeRequests()
+					.antMatchers(
+						"/login/",
+						"/login/failure/",
+						"/access-denied/",
+						"/security/password/recovery/request/creation/",
+						"/security/password/recovery/request/reset/",
+						"/security/password/creation/",
+						"/security/password/reset/",
+						"/maintenance/"
+					).permitAll()
+					.antMatchers("/**").hasAnyAuthority(CoreAuthorityConstants.ROLE_AUTHENTICATED);
+		}
+
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web
+				.expressionHandler(webSecurityExpressionHandler)
+				// ignore urls
+				.ignoring().antMatchers(
+					"/static/**",
+					"/monitoring/**",
+					"/webjars/**",
+					"/android-chrome-192x192.png",
+					"/android-chrome-256x256.png",
+					"/apple-touch-icon.png",
+					"/browserconfig.xml",
+					"/favicon.ico",
+					"/favicon-16x16.png",
+					"/favicon-32x32.png",
+					"/mstile-150x150.png",
+					"/safari-pinned-tab.svg",
+					"/site.webmanifest");
+		}
+	}
 
 }
