a2b2daacc25827ac4f35256ed3296e0fdb866174
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2022-03-15 11:31:39 +0100

    BasicApp: announcement content multilines

--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/common/component/AnnouncementMessagePanel.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/common/component/AnnouncementMessagePanel.java
@@ -2,9 +2,13 @@ package org.iglooproject.basicapp.web.application.common.component;
 
 import org.apache.wicket.model.IModel;
 import org.iglooproject.basicapp.core.business.announcement.model.Announcement;
+import org.iglooproject.basicapp.core.business.announcement.model.atomic.AnnouncementType;
+import org.iglooproject.basicapp.core.business.announcement.predicate.AnnouncementPredicates;
 import org.iglooproject.basicapp.web.application.common.renderer.AnnouncementRenderer;
 import org.iglooproject.wicket.markup.html.basic.CoreLabel;
 import org.iglooproject.wicket.markup.html.panel.GenericPanel;
+import org.iglooproject.wicket.more.condition.Condition;
+import org.iglooproject.wicket.more.markup.html.basic.EnclosureContainer;
 
 public class AnnouncementMessagePanel extends GenericPanel<Announcement> {
 
@@ -15,8 +19,21 @@ public class AnnouncementMessagePanel extends GenericPanel<Announcement> {
 		setOutputMarkupId(true);
 		
 		add(
-			new CoreLabel("title", AnnouncementRenderer.title().asModel(announcementModel)),
-			new CoreLabel("description", AnnouncementRenderer.description().asModel(announcementModel))
+			new EnclosureContainer("serviceInterruptionContainer")
+				.condition(Condition.predicate(announcementModel, AnnouncementPredicates.type(AnnouncementType.SERVICE_INTERRUPTION)))
+				.add(
+					new CoreLabel("title", AnnouncementRenderer.title().asModel(announcementModel)),
+					new CoreLabel("description", AnnouncementRenderer.description().asModel(announcementModel))
+						.hideIfEmpty()
+				),
+			new EnclosureContainer("otherContainer")
+				.condition(Condition.predicate(announcementModel, AnnouncementPredicates.type(AnnouncementType.SERVICE_INTERRUPTION)).negate())
+				.add(
+					new CoreLabel("title", AnnouncementRenderer.title().asModel(announcementModel)),
+					new CoreLabel("description", AnnouncementRenderer.description().asModel(announcementModel))
+						.multiline()
+						.hideIfEmpty()
+				)
 		);
 	}
 
