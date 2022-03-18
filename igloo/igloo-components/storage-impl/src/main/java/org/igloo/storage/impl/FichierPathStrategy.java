73ef77a0f4bc9db5cf2a53ed38068a9d46d03777
Margot Piva <margot.piva@kobalt.fr>
2022-03-18 15:45:45 +0100

    storage: FichierPathStrategy + test

--- igloo/igloo-components/storage-impl/src/main/java/org/igloo/storage/impl/FichierPathStrategy.java
+++ igloo/igloo-components/storage-impl/src/main/java/org/igloo/storage/impl/FichierPathStrategy.java
@@ -1,4 +1,35 @@
 package org.igloo.storage.impl;
 
-public class FichierPathStrategy {
+import com.google.common.base.Splitter;
+import com.google.common.hash.Hashing;
+import org.igloo.storage.model.Fichier;
+import org.igloo.storage.model.atomic.IFichierPathStrategy;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.UUID;
+
+public class FichierPathStrategy implements IFichierPathStrategy {
+
+	private final int hashSizeInBytes;
+
+	public FichierPathStrategy(int hashSizeInBytes) {
+		this.hashSizeInBytes = hashSizeInBytes;
+	}
+
+	@Override
+	public String getPath(Fichier fichier) {
+		String digest = fichier.getUuid().toString().replace("-", "");
+
+		Path path = Splitter.fixedLength(2)
+			.splitToStream(digest)
+			.skip(1)
+			.limit(hashSizeInBytes)
+			.map(Path::of)
+			.reduce(Path.of(""), (a, b) -> a.resolve(b));
+		// TODO: gestion extension
+		return Path.of(fichier.getFichierType().getPath(), path.toString(), fichier.getUuid().toString()).toString();
+	}
+
 }
--- igloo/igloo-components/storage-impl/src/test/java/test/model/StorageUnitType.java
+++ igloo/igloo-components/storage-impl/src/test/java/test/model/StorageUnitType.java
@@ -2,6 +2,7 @@ package test.model;
 
 import java.util.Set;
 
+import org.igloo.storage.impl.FichierPathStrategy;
 import org.igloo.storage.impl.LegacyFichierPathStrategy;
 import org.igloo.storage.model.atomic.IFichierPathStrategy;
 import org.igloo.storage.model.atomic.IFichierType;
@@ -9,7 +10,7 @@ import org.igloo.storage.model.atomic.IStorageUnitType;
 
 public enum StorageUnitType implements IStorageUnitType {
 
-	TYPE_1(Set.of(FichierType1.CONTENT1, FichierType1.CONTENT2, FichierType2.CONTENT4), new LegacyFichierPathStrategy(1)),
+	TYPE_1(Set.of(FichierType1.CONTENT1, FichierType1.CONTENT2, FichierType2.CONTENT4), new FichierPathStrategy(1)),
 	TYPE_2(Set.of(FichierType2.CONTENT3), new LegacyFichierPathStrategy(1));
 
 	private final Set<IFichierType> acceptedFichierTypes;
