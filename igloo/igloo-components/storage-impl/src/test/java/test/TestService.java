097eb9f4dec3bd9512d9155b23ba3f9b47cde03b
Margot Piva <margot.piva@kobalt.fr>
2022-03-21 11:53:28 +0100

    storage: TU Service + Mockito

--- igloo/igloo-components/storage-impl/src/test/java/test/TestService.java
+++ igloo/igloo-components/storage-impl/src/test/java/test/TestService.java
@@ -3,9 +3,7 @@ package test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -21,6 +19,7 @@ import javax.persistence.EntityManagerFactory;
 
 import org.apache.commons.io.IOUtils;
 import org.igloo.storage.api.IStorageService;
+import org.igloo.storage.impl.StorageEvent;
 import org.igloo.storage.impl.StorageOperations;
 import org.igloo.storage.impl.StorageService;
 import org.igloo.storage.model.Fichier;
@@ -31,6 +30,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.mockito.AdditionalAnswers;
+import org.mockito.Answers;
 import org.mockito.Mockito;
 import org.springframework.orm.jpa.EntityManagerFactoryUtils;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -46,10 +46,10 @@ class TestService extends AbstractTest {
 	private static final String FILE_CONTENT = "blabla";
 	// checksum from 'echo -n "blabla" | sha256sum'
 	private static final String FILE_CHECKSUM_SHA_256 = "ccadd99b16cd3d200c22d6db45d8b6630ef3d936767127347ec8a76ab992c2ea";
-	private static final long FILE_SIZE = 6l;
+	private static final long FILE_SIZE = 6L;
 	private IStorageService storageService;
 	private TransactionTemplate transactionTemplate;
-	private StorageOperations operations = mock(StorageOperations.class);
+	private final StorageOperations operations = mock(StorageOperations.class);
 	private Path tempDir;
 
 	@BeforeEach
@@ -58,20 +58,20 @@ class TestService extends AbstractTest {
 		this.tempDir = tempDir;
 		PlatformTransactionManager transactionManager = new JpaTransactionManager(entityManagerFactory);
 		transactionTemplate = new TransactionTemplate(transactionManager, writeTransactionAttribute());
-		transactionTemplate.execute((t) -> {
-			EntityManager em = EntityManagerFactoryUtils.getTransactionalEntityManager(entityManagerFactory);
-			return storageService.createStorageUnit(StorageUnitType.TYPE_1);
-		});
+		transactionTemplate.executeWithoutResult((t) -> storageService.createStorageUnit(StorageUnitType.TYPE_1));
 	}
 
 	@Test
-	void testAdd(EntityManagerFactory entityManagerFactory) throws IOException {
+	void testAdd() throws IOException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		doAnswer(AdditionalAnswers.<InputStream, Path>answerVoid((a, b) -> { IOUtils.copy(a, baos); }))
 			.when(operations).copy(any(), any());
-		Fichier fichier = createFichier(entityManagerFactory, "filename.txt", FichierType1.CONTENT1, FILE_CONTENT, () -> {});
+		Fichier fichier = createFichier("filename.txt", FichierType1.CONTENT1, FILE_CONTENT, () -> {});
+
 		verify(operations).copy(any(), Mockito.eq(Path.of(fichier.getStorageUnit().getPath(), fichier.getRelativePath())));
-		
+		verify(operations, times(1)).copy(any(), any());
+		verifyNoMoreInteractions(operations);
+
 		assertThat(baos.toByteArray()).isEqualTo(FILE_CONTENT.getBytes(StandardCharsets.UTF_8)).hasSize((int) FILE_SIZE);
 		assertThat(fichier.getChecksum()).isEqualTo(FILE_CHECKSUM_SHA_256);
 		assertThat(fichier.getSize()).isEqualTo(FILE_SIZE);
@@ -79,61 +79,74 @@ class TestService extends AbstractTest {
 	}
 
 	@Test
-	void testAddRollback(EntityManagerFactory entityManagerFactory) {
+	void testAddRollback() throws IOException {
 		Runnable action = () -> {
-			String fileContent = FILE_CONTENT;
-			FichierType1 type = FichierType1.CONTENT1;
-			createFichier(entityManagerFactory, "filename", type, fileContent, () -> { throw new IllegalStateException("Triggered rollback"); });
+			createFichier("filename", FichierType1.CONTENT1, FILE_CONTENT, () -> { throw new IllegalStateException("Triggered rollback"); });
 		};
 
 		assertThatCode(action::run).as("Fichier add must throw an exception").isInstanceOf(IllegalStateException.class).hasMessageContaining("Triggered rollback");
-		assertThat(Path.of(tempDir.toString(), "relative-path")).doesNotExist();
+		verify(operations, times(1)).copy(any(), any());
+		verify(operations, times(1)).removePhysicalFile(any(), any());
+		verifyNoMoreInteractions(operations);
 	}
 
 	@Test
-	void testGet(EntityManagerFactory entityManagerFactory) throws IOException {
-		Fichier fichier = createFichier(entityManagerFactory, "filename", FichierType1.CONTENT1, FILE_CONTENT, () -> {});
+	void testGet() throws IOException {
+		Fichier fichier = createFichier("filename", FichierType1.CONTENT1, FILE_CONTENT, () -> {});
+		Mockito.reset(operations); // Get rid of creation operation
 
 		storageService.getFile(fichier);
+		verify(operations, times(1)).getFile(any());
+		verifyNoMoreInteractions(operations);
 
 		verify(operations).getFile(Mockito.eq(Path.of(fichier.getStorageUnit().getPath(), fichier.getRelativePath())));
 	}
 
 	@Test
-	void testRemove(EntityManagerFactory entityManagerFactory) {
-		String fileContent = FILE_CONTENT;
-		FichierType1 type = FichierType1.CONTENT1;
-
+	void testRemove() {
 		transactionTemplate.executeWithoutResult((t) -> {
-			Fichier fichier = createFichier(entityManagerFactory, "filename", type, fileContent, () -> {});
+			Fichier fichier = createFichier("filename", FichierType1.CONTENT1, FILE_CONTENT, () -> {});
+			Mockito.reset(operations); // Get rid of creation operation
 			storageService.removeFichier(fichier);
 		});
 
-		assertThat(Path.of(tempDir.toString(), "relative-path")).doesNotExist();
+		verify(operations, times(1)).removePhysicalFile(any(), any());
+		verifyNoMoreInteractions(operations);
+	}
+
+	// TODO MPI : à vérifier par LAL
+	@Test
+	void testRemoveRollback(EntityManagerFactory entityManagerFactory) throws IOException {
+		Runnable action = () -> {
+			Fichier fichier = transactionTemplate.execute((t) -> createFichier("filename", FichierType1.CONTENT1, FILE_CONTENT, () -> {}));
+
+			transactionTemplate.executeWithoutResult((t) -> {
+				EntityManager em = EntityManagerFactoryUtils.getTransactionalEntityManager(entityManagerFactory);
+				storageService.removeFichier(em.find(Fichier.class, fichier.getId()));
+				throw new IllegalStateException("Triggered rollback");
+			});
+		};
+
+		assertThatCode(action::run).as("Fichier remove must throw an exception").isInstanceOf(IllegalStateException.class).hasMessageContaining("Triggered rollback");
+		verify(operations, times(1)).copy(any(), any());
+		verifyNoMoreInteractions(operations); // method removePhysicalFile should not be called
 	}
 
 	@Test
-	void testRemoveRollback(EntityManagerFactory entityManagerFactory) {
-		// TODO
-	}
-
-	@Test
-	void testInvalidate(EntityManagerFactory entityManagerFactory) {
-		String fileContent = FILE_CONTENT;
-		FichierType1 type = FichierType1.CONTENT1;
-
+	void testInvalidate() {
 		Fichier fichier = transactionTemplate.execute((t) -> {
-			Fichier fichierToInvalidate = createFichier(entityManagerFactory, "filename", type, fileContent, () -> {});
+			Fichier fichierToInvalidate = createFichier("filename", FichierType1.CONTENT1, FILE_CONTENT, () -> {});
 			Mockito.reset(operations); // Get rid of creation operation
 			storageService.invalidateFichier(fichierToInvalidate);
 			return fichierToInvalidate;
 		});
 
+		assertThat(fichier).isNotNull();
 		assertThat(fichier.getStatus()).isEqualTo(FichierStatus.INVALIDATED);
 		Mockito.verifyNoInteractions(operations);
 	}
 
-	private Fichier createFichier(EntityManagerFactory entityManagerFactory, String filename, IFichierType fichierType, String fileContent, Runnable postCreationAction) {
+	private Fichier createFichier(String filename, IFichierType fichierType, String fileContent, Runnable postCreationAction) {
 		return transactionTemplate.execute((t) -> {
 			try (InputStream bais = new ByteArrayInputStream(fileContent.getBytes(StandardCharsets.UTF_8))) {
 				return storageService.addFichier(filename, fichierType, bais);
@@ -150,4 +163,5 @@ class TestService extends AbstractTest {
 		ta.setReadOnly(false);
 		return ta;
 	}
+
 }
