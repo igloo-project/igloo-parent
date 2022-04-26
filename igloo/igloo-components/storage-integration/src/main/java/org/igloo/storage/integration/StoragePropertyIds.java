769d17c22b7079cddecc934c2c458b723a8985d6
Laurent Almeras <lalmeras@gmail.com>
2022-04-26 19:20:12 +0200

    storage: storage unit types config

--- igloo/igloo-components/storage-integration/src/main/java/org/igloo/storage/integration/StoragePropertyIds.java
+++ igloo/igloo-components/storage-integration/src/main/java/org/igloo/storage/integration/StoragePropertyIds.java
@@ -1,5 +1,8 @@
 package org.igloo.storage.integration;
 
+import java.util.Set;
+
+import org.igloo.storage.model.atomic.IStorageUnitType;
 import org.iglooproject.spring.property.model.AbstractPropertyIds;
 import org.iglooproject.spring.property.model.ImmutablePropertyId;
 
@@ -9,5 +12,6 @@ public class StoragePropertyIds extends AbstractPropertyIds {
 	public static final ImmutablePropertyId<Integer> TRANSACTION_SYNCHRONIZATION_ORDER = immutable("storage.transactionSynchronizationOrder");
 	public static final ImmutablePropertyId<String> DB_FICHIER_SEQUENCE_NAME = immutable("storage.db.fichierSequenceName");
 	public static final ImmutablePropertyId<String> DB_STORAGE_UNIT_SEQUENCE_NAME = immutable("storage.db.storageUnitSequenceName");
+	public static final ImmutablePropertyId<Set<IStorageUnitType>> STORAGE_UNIT_TYPE_CANDIDATES = immutable("storage.storageUnitTypeCandidates");
 
 }
--- igloo/igloo-components/storage-integration/src/main/java/org/igloo/storage/integration/StorageSpringAutoConfiguration.java
+++ igloo/igloo-components/storage-integration/src/main/java/org/igloo/storage/integration/StorageSpringAutoConfiguration.java
@@ -3,10 +3,14 @@ package org.igloo.storage.integration;
 import static org.igloo.storage.integration.StoragePropertyIds.DB_FICHIER_SEQUENCE_NAME;
 import static org.igloo.storage.integration.StoragePropertyIds.DB_STORAGE_UNIT_SEQUENCE_NAME;
 import static org.igloo.storage.integration.StoragePropertyIds.PATH;
+import static org.igloo.storage.integration.StoragePropertyIds.STORAGE_UNIT_TYPE_CANDIDATES;
 import static org.igloo.storage.integration.StoragePropertyIds.TRANSACTION_SYNCHRONIZATION_ORDER;
 
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManagerFactory;
 
@@ -22,6 +26,8 @@ import org.iglooproject.jpa.config.spring.provider.JpaPackageScanProvider;
 import org.iglooproject.spring.config.spring.IPropertyRegistryConfig;
 import org.iglooproject.spring.property.service.IPropertyRegistry;
 import org.iglooproject.spring.property.service.IPropertyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -32,17 +38,19 @@ import org.springframework.core.Ordered;
 @ConditionalOnProperty(name = "igloo-ac.storage.disabled", havingValue = "false", matchIfMissing = true)
 public class StorageSpringAutoConfiguration implements IPropertyRegistryConfig {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(StorageSpringAutoConfiguration.class);
+
 	@Bean
 	@ConditionalOnMissingBean
 	public IStorageService storageService(EntityManagerFactory entityManagerFactory, IPropertyService propertyService,
-			Set<IStorageUnitType> storageUnitTypes, IMimeTypeResolver mimetypeResolver) {
+			IMimeTypeResolver mimetypeResolver) {
 		return new StorageService(
-				propertyService.get(StoragePropertyIds.TRANSACTION_SYNCHRONIZATION_ORDER),
+				propertyService.get(TRANSACTION_SYNCHRONIZATION_ORDER),
 				new DatabaseOperations(
 						entityManagerFactory,
-						propertyService.get(StoragePropertyIds.DB_FICHIER_SEQUENCE_NAME),
-						propertyService.get(StoragePropertyIds.DB_STORAGE_UNIT_SEQUENCE_NAME)),
-				storageUnitTypes,
+						propertyService.get(DB_FICHIER_SEQUENCE_NAME),
+						propertyService.get(DB_STORAGE_UNIT_SEQUENCE_NAME)),
+				propertyService.get(STORAGE_UNIT_TYPE_CANDIDATES),
 				new StorageOperations(),
 				() -> Path.of(propertyService.get(StoragePropertyIds.PATH)),
 				mimetypeResolver);
@@ -65,6 +73,40 @@ public class StorageSpringAutoConfiguration implements IPropertyRegistryConfig {
 		registry.registerString(PATH);
 		registry.registerString(DB_FICHIER_SEQUENCE_NAME, "fichier_id_seq");
 		registry.registerString(DB_STORAGE_UNIT_SEQUENCE_NAME, "storageUnit_id_seq");
+		// retrieve enum values from a ',' separated list. Each item can be an enum type OR a enum value
+		// enum value must be written package.enumType.ENUM_VALUE
+		registry.register(StoragePropertyIds.STORAGE_UNIT_TYPE_CANDIDATES, s -> {
+			Set<IStorageUnitType> types = new HashSet<>();
+			Set<String> values = Arrays.stream(s.split(",")).collect(Collectors.toUnmodifiableSet());
+			for (String value : values) {
+				value = value.strip();
+				try {
+					types.addAll(resolveIStorageUnitType(value));
+				} catch (ClassNotFoundException e) {
+					LOGGER.debug("{} cannot be resolved to a class. Resolving as an enum value.", value);
+					if (value.contains(".")) {
+						try {
+							String className = value.substring(0, value.lastIndexOf("."));
+							String enumName = value.substring(value.lastIndexOf(".") + 1);
+							resolveIStorageUnitType(className).stream().filter(i -> enumName.equals(i.getName()))
+								.findFirst().ifPresentOrElse(types::add, () -> { throw new IllegalStateException(); });
+						} catch (ClassNotFoundException|IllegalStateException e1) {
+							throw new IllegalStateException(String.format("%s cannot be resolved to a value. Check value.", value));
+						}
+					}
+				}
+			}
+			return types;
+		});
+	}
+
+	private Set<IStorageUnitType> resolveIStorageUnitType(String className) throws ClassNotFoundException {
+		Class<?> clazz = Class.forName(className);
+		if (IStorageUnitType.class.isAssignableFrom(clazz) && Enum.class.isAssignableFrom(clazz)) {
+			return Set.of(clazz.getEnumConstants()).stream().map(IStorageUnitType.class::cast).collect(Collectors.toUnmodifiableSet());
+		} else {
+			throw new ClassNotFoundException(String.format("Values from %s are not Enum&IStorageUnitType", className));
+		}
 	}
 
 }
