3c416abf0ed9dfbd693e7115d6c39ab5af0d7acd
Laurent Almeras <lalmeras@gmail.com>
2022-05-19 16:56:52 +0200

    junit migration fix: restore a discarded test-suite

--- igloo/igloo-components/igloo-component-wicket-more-test/src/test/java/test/wicket/more/model/TestSuiteGenericEntityCollectionModel.java
+++ igloo/igloo-components/igloo-component-wicket-more-test/src/test/java/test/wicket/more/model/TestSuiteGenericEntityCollectionModel.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.stream.Stream;
 
 import org.apache.wicket.model.IModel;
 import org.iglooproject.functional.SerializableSupplier2;
@@ -11,26 +12,52 @@ import org.iglooproject.wicket.more.model.GenericEntityCollectionModel;
 import org.iglooproject.wicket.more.model.threadsafe.SessionThreadSafeGenericEntityArrayListModel;
 import org.iglooproject.wicket.more.model.threadsafe.SessionThreadSafeGenericEntityHashSetModel;
 import org.iglooproject.wicket.more.model.threadsafe.SessionThreadSafeGenericEntityTreeSetModel;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
+import com.google.common.base.Equivalence;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import test.wicket.more.business.person.model.Person;
 import test.wicket.more.business.person.model.PersonComparator;
+import test.wicket.more.model.TestSuiteGenericEntityCollectionModel.TestGenericEntityCollectionModel;
+import test.wicket.more.model.TestSuiteGenericEntityCollectionModel.TestSessionThreadSafeGenericEntityArrayListModel;
+import test.wicket.more.model.TestSuiteGenericEntityCollectionModel.TestSessionThreadSafeGenericEntityHashSetModel;
+import test.wicket.more.model.TestSuiteGenericEntityCollectionModel.TestSessionThreadSafeGenericEntityTreeSetModel;
 
+@Suite
+@SelectClasses({
+	TestGenericEntityCollectionModel.class,
+	TestSessionThreadSafeGenericEntityArrayListModel.class,
+	TestSessionThreadSafeGenericEntityHashSetModel.class,
+	TestSessionThreadSafeGenericEntityTreeSetModel.class
+})
+/**
+ * For these tests, we perform a parameterized run, but supplier is null (as createModel, ...) do not use its value
+ * and equivalence is either equal a custom ordered set implementation.
+ */
 public class TestSuiteGenericEntityCollectionModel {
 	
 	public static class TestGenericEntityCollectionModel extends AbstractTestGenericEntityCollectionModel<Collection<Person>> {
+		public static Stream<Arguments> data() {
+			ImmutableList.Builder<Arguments> builder = ImmutableList.builder();
+			builder.add(Arguments.of(null, Equivalence.equals()));
+			return builder.build().stream();
+		}
+		
 		@Override
 		protected IModel<Collection<Person>> createModel(SerializableSupplier2<? extends Collection<Person>> supplier) {
 			return GenericEntityCollectionModel.of(Person.class);
 		}
-
+		
 		@Override
 		protected Collection<Person> createCollection(SerializableSupplier2<? extends Collection<Person>> supplier, Person... person) {
 			return Lists.newArrayList(person);
 		}
-
+		
 		@Override
 		protected Collection<Person> clone(SerializableSupplier2<? extends Collection<Person>> supplier, Collection<Person> collection) {
 			return Lists.newArrayList(collection);
@@ -38,16 +65,22 @@ public class TestSuiteGenericEntityCollectionModel {
 	}
 	
 	public static class TestSessionThreadSafeGenericEntityArrayListModel extends AbstractTestGenericEntityCollectionModel<List<Person>> {
+		public static Stream<Arguments> data() {
+			ImmutableList.Builder<Arguments> builder = ImmutableList.builder();
+			builder.add(Arguments.of(null, Equivalence.equals()));
+			return builder.build().stream();
+		}
+
 		@Override
 		protected IModel<List<Person>> createModel(SerializableSupplier2<? extends List<Person>> supplier) {
 			return SessionThreadSafeGenericEntityArrayListModel.of(Person.class);
 		}
-
+		
 		@Override
 		protected List<Person> createCollection(SerializableSupplier2<? extends List<Person>> supplier, Person... person) {
 			return Lists.newArrayList(person);
 		}
-
+		
 		@Override
 		protected List<Person> clone(SerializableSupplier2<? extends List<Person>> supplier, List<Person> collection) {
 			return Lists.newArrayList(collection);
@@ -55,16 +88,22 @@ public class TestSuiteGenericEntityCollectionModel {
 	}
 	
 	public static class TestSessionThreadSafeGenericEntityHashSetModel extends AbstractTestGenericEntityCollectionModel<Set<Person>> {
+		public static Stream<Arguments> data() {
+			ImmutableList.Builder<Arguments> builder = ImmutableList.builder();
+			builder.add(Arguments.of(null, ORDERED_SET_EQUIVALENCE));
+			return builder.build().stream();
+		}
+		
 		@Override
 		protected IModel<Set<Person>> createModel(SerializableSupplier2<? extends Set<Person>> supplier) {
 			return SessionThreadSafeGenericEntityHashSetModel.of(Person.class);
 		}
-
+		
 		@Override
 		protected Set<Person> createCollection(SerializableSupplier2<? extends Set<Person>> supplier, Person... person) {
 			return Sets.newHashSet(person);
 		}
-
+		
 		@Override
 		protected Set<Person> clone(SerializableSupplier2<? extends Set<Person>> supplier, Set<Person> collection) {
 			return Sets.newHashSet(collection);
@@ -72,18 +111,24 @@ public class TestSuiteGenericEntityCollectionModel {
 	}
 	
 	public static class TestSessionThreadSafeGenericEntityTreeSetModel extends AbstractTestGenericEntityCollectionModel<SortedSet<Person>> {
+		public static Stream<Arguments> data() {
+			ImmutableList.Builder<Arguments> builder = ImmutableList.builder();
+			builder.add(Arguments.of(null, ORDERED_SET_EQUIVALENCE));
+			return builder.build().stream();
+		}
+		
 		@Override
 		protected IModel<SortedSet<Person>> createModel(SerializableSupplier2<? extends SortedSet<Person>> supplier) {
 			return SessionThreadSafeGenericEntityTreeSetModel.of(Person.class, PersonComparator.get());
 		}
-
+		
 		@Override
 		protected SortedSet<Person> createCollection(SerializableSupplier2<? extends SortedSet<Person>> supplier, Person... person) {
 			SortedSet<Person> result = Sets.newTreeSet(PersonComparator.get());
 			result.addAll(Lists.newArrayList(person));
 			return result;
 		}
-
+		
 		@Override
 		protected SortedSet<Person> clone(SerializableSupplier2<? extends SortedSet<Person>> supplier, SortedSet<Person> collection) {
 			SortedSet<Person> result = Sets.newTreeSet(PersonComparator.get());
