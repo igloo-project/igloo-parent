be9163e2538e8a742d21018bf080f1473eccc069
Laurent Almeras <lalmeras@gmail.com>
2022-07-11 17:21:36 +0200

    spring-boot 2.7 update: ImportSelector behavior change
    For Igloo auto-configuration we do not want to load spring-boot
    autoconfig, only Igloo ones.

--- igloo/igloo-components/igloo-spring-autoconfigure/src/main/java/org/igloo/spring/autoconfigure/IglooAutoConfigurationImportSelector.java
+++ igloo/igloo-components/igloo-spring-autoconfigure/src/main/java/org/igloo/spring/autoconfigure/IglooAutoConfigurationImportSelector.java
@@ -1,12 +1,17 @@
 package org.igloo.spring.autoconfigure;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
 import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.Assert;
 
 public class IglooAutoConfigurationImportSelector extends AutoConfigurationImportSelector {
 
@@ -32,6 +37,22 @@ public class IglooAutoConfigurationImportSelector extends AutoConfigurationImpor
 		String[] excludes = getEnvironment()
 				.getProperty(PROPERTY_NAME_AUTOCONFIGURE_EXCLUDE, String[].class);
 		return (excludes != null) ? Arrays.asList(excludes) : Collections.emptyList();
+	}
+
+	/**
+	 * With spring-boot 2.7, default implementation automatically add spring-boot autoconfig. We want only igloo
+	 * auto-configuration !
+	 */
+	@Override
+	protected List<String> getCandidateConfigurations(AnnotationMetadata metadata, AnnotationAttributes attributes) {
+		List<String> configurations = new ArrayList<>(
+				SpringFactoriesLoader.loadFactoryNames(getSpringFactoriesLoaderFactoryClass(), getBeanClassLoader()));
+		// We skip spring-boot autoconfiguration loading
+		//ImportCandidates.load(AutoConfiguration.class, getBeanClassLoader()).forEach(configurations::add);
+		Assert.notEmpty(configurations,
+				"No auto configuration classes found in META-INF/spring.factories nor in META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports. If you "
+						+ "are using a custom packaging, make sure that file is correct.");
+		return configurations;
 	}
 
 }
