4e13e6e33b855c2e92b225165c5f461401e277cd
Florian Lacreuse <lacreuse.florian@gmail.com>
2022-09-16 17:07:13 +0200

    Split application and console environment header panel

--- basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/console/common/component/ConsoleAccessEnvironmentPanel.java
+++ basic-application/basic-application-webapp/src/main/java/org/iglooproject/basicapp/web/application/console/common/component/ConsoleAccessEnvironmentPanel.java
@@ -1,19 +1,11 @@
 package org.iglooproject.basicapp.web.application.console.common.component;
 
-import org.iglooproject.basicapp.web.application.common.component.EnvironmentPanel;
-
-public class ConsoleAccessEnvironmentPanel extends EnvironmentPanel {
+public class ConsoleAccessEnvironmentPanel extends ConsoleEnvironmentPanel {
 
 	private static final long serialVersionUID = 5325046851722206296L;
 
 	public ConsoleAccessEnvironmentPanel(String id) {
 		super(id);
-	}
-
-	@Override
-	protected void onConfigure() {
-		super.onConfigure();
-		setVisible(true);
 	}
 
 }
