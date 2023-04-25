61aca3e97e310f979025d38e6c5ac410b23a02b1
Laurent Almeras <lalmeras@gmail.com>
2023-04-25 19:40:13 +0200

    igloo-boot: restore SecurityUtils

---
+++ igloo/igloo-components/igloo-spring-autoconfigure/src/main/java/org/igloo/spring/autoconfigure/security/util/SecurityUtils.java
@@ -0,0 +1,49 @@
+package org.igloo.spring.autoconfigure.security.util;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.iglooproject.jpa.security.business.authority.util.CoreAuthorityConstants;
+import org.iglooproject.jpa.security.model.CorePermissionConstants;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+
+public class SecurityUtils {
+
+	public static String defaultPermissionHierarchyAsString() {
+		return hierarchyAsStringFromMap(ImmutableMultimap.<String, String>builder()
+				.put(CorePermissionConstants.ADMINISTRATION, CorePermissionConstants.WRITE)
+				.put(CorePermissionConstants.WRITE, CorePermissionConstants.READ)
+				.build()
+		);
+	}
+
+	public static String defaultRoleHierarchyAsString() {
+		return hierarchyAsStringFromMap(ImmutableMultimap.<String, String>builder()
+				.put(CoreAuthorityConstants.ROLE_SYSTEM, CoreAuthorityConstants.ROLE_ADMIN)
+				.put(CoreAuthorityConstants.ROLE_ADMIN, CoreAuthorityConstants.ROLE_AUTHENTICATED)
+				.put(CoreAuthorityConstants.ROLE_AUTHENTICATED, CoreAuthorityConstants.ROLE_ANONYMOUS)
+				.build()
+		);
+	}
+
+	public static String hierarchyAsStringFromMap(Multimap<String, String> multimap) {
+		return hierarchyAsStringFromMap(multimap.asMap());
+	}
+
+	public static String hierarchyAsStringFromMap(Map<String, ? extends Collection<String>> map) {
+		StringBuilder builder = new StringBuilder();
+		for (Map.Entry<String, ? extends Collection<String>> entry : map.entrySet()) {
+			String parent = entry.getKey();
+			for (String child : entry.getValue()) {
+				builder.append(parent).append(" > ").append(child).append("\n");
+			}
+		}
+		return builder.toString();
+	}
+
+	private SecurityUtils() {
+	}
+
+}
