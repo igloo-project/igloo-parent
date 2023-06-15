0b2320487f2719caed6837d176db5d87389312cf
Laurent Almeras <lalmeras@gmail.com>
2023-06-15 18:26:59 +0200

    fix igloo console page

---
+++ igloo/igloo-components/igloo-cache/src/main/java/igloo/cache/autoconfigure/CacheAutoConfiguration.java
@@ -0,0 +1,65 @@
+package igloo.cache.autoconfigure;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import igloo.cache.monitor.CacheManagerWrapper;
+import igloo.cache.spring.CacheManagerRegistry;
+import igloo.cache.spring.ICacheManagerRegistry;
+
+/**
+ * <p>Configure {@link Cacheable} annotation, spring caching subsystem and register a cache registry needed by cache
+ * console page.</p>
+ * 
+ * <p>Cache configuration is done with spring boot <code>spring.cache</code> properties. Please note that hibernate
+ * second-level cache is configured and initialized independantly.</p>
+ * 
+ * <p>Igloo supports ehcache2 (deprecated), caffeine and caffeine + jcache caches.</p>
+ * 
+ * <p>A not customized initialization use a <code>cacheManager</code> initialized by spring/spring-boot subsystem
+ * (used for spring usage - {@link Cacheable} proxies, ...) and a <code>jpaCacheManager</code> (used for second-level
+ * cachine) initialized by Hibernate and exposed as a bean for monitoring purpose.</p>
+ * 
+ * @see IglooJpaCacheConfig
+ */
+@Configuration
+@ConditionalOnClass({ CacheManagerWrapper.class, CacheManager.class, ICacheManagerRegistry.class })
+@AutoConfiguration(before = org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration.class)
+// needed to ensure that cacheManager is already defined; else @Autowired @Qualifier("cacheManager") #cacheManager may fail. 
+@Import(org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration.class)
+@EnableCaching
+public class CacheAutoConfiguration implements CachingConfigurer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CacheAutoConfiguration.class);
+
+	@Bean
+	public ICacheManagerRegistry cacheManagerRegistry(Map<String, CacheManager> cms) {
+		if (LOGGER.isInfoEnabled()) {
+			LOGGER.info("Registering cache managers: {}", cms.keySet().stream().collect(Collectors.joining(", ")));
+		}
+		return new CacheManagerRegistry(cms);
+	}
+
+	@Qualifier("cacheManager")
+	@Autowired
+	private CacheManager cacheManager;
+
+	@Override
+	public CacheManager cacheManager() {
+		return cacheManager;
+	}
+}
--- igloo/igloo-components/igloo-component-jpa/src/main/java/org/iglooproject/jpa/autoconfigure/JpaCacheAutoConfiguration.java
+++ igloo/igloo-components/igloo-component-jpa/src/main/java/org/iglooproject/jpa/autoconfigure/JpaCacheAutoConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.github.benmanes.caffeine.jcache.spi.CaffeineCachingProvider;
 
@@ -15,33 +16,19 @@ import jakarta.persistence.EntityManagerFactory;
  * Expose second-level cache a spring bean. This is done to allow to build a common console page for monitoring
  * spring and hibernate cache.
  */
+@Configuration
 @ConditionalOnExpression("'${spring.jpa.properties.hibernate.javax.cache.uri:none}' != 'none'")
 public class JpaCacheAutoConfiguration {
 
 	@Bean
-	public JpaCacheManagerWrapper jpaCacheManager(
+	public CacheManager jpaCacheManager(
 			EntityManagerFactory entityManagerFactory,
 			@Value("${spring.jpa.properties.hibernate.javax.cache.provider}") String cacheProvider,
 			@Value("${spring.jpa.properties.hibernate.javax.cache.uri}") String locationUri) throws URISyntaxException {
 		if (CaffeineCachingProvider.class.getName().equals(cacheProvider)) {
-			return new JpaCacheManagerWrapper(org.iglooproject.jpa.autoconfigure.JCacheLookup.lookup(locationUri));
+			return org.iglooproject.jpa.autoconfigure.JCacheLookup.lookup(locationUri);
 		} else {
 			throw new IllegalStateException();
-		}
-	}
-
-	/**
-	 * Hide cacheManager in a wrapper (as spring-boot is difficult to configure with multiple {@link CacheManager} beans). 
-	 */
-	public class JpaCacheManagerWrapper {
-		private final CacheManager cacheManager;
-		
-		public JpaCacheManagerWrapper(CacheManager cacheManager) {
-			this.cacheManager = cacheManager;
-		}
-		
-		public CacheManager getCacheManager() {
-			return cacheManager;
 		}
 	}
 }
