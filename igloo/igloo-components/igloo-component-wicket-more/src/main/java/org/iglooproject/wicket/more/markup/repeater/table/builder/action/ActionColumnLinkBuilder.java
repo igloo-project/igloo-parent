ecd241b7e9a9fdfa6f2d1d30be5ebbb1e4dc626f
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2023-11-03 11:46:53 +0100

    DataTableBuilder: fix actions column html tags

--- igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/repeater/table/builder/action/ActionColumnLinkBuilder.java
+++ igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/repeater/table/builder/action/ActionColumnLinkBuilder.java
@@ -1,12 +1,16 @@
 package org.iglooproject.wicket.more.markup.repeater.table.builder.action;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.model.IModel;
 import org.iglooproject.wicket.more.link.descriptor.AbstractDynamicBookmarkableLink;
 import org.iglooproject.wicket.more.link.descriptor.generator.ILinkGenerator;
 import org.iglooproject.wicket.more.link.descriptor.mapper.ILinkDescriptorMapper;
 import org.iglooproject.wicket.more.markup.html.factory.ComponentFactories;
+import org.iglooproject.wicket.more.markup.repeater.table.column.CoreActionColumnLinkPanel;
 
 import igloo.bootstrap.renderer.IBootstrapRenderer;
+import igloo.wicket.component.PlaceholderContainer;
 
 public class ActionColumnLinkBuilder<T>
 		extends AbstractActionColumnElementBuilder<T, AbstractDynamicBookmarkableLink, ActionColumnLinkBuilder<T>> {
@@ -19,7 +23,26 @@ public class ActionColumnLinkBuilder<T>
 			ILinkDescriptorMapper<? extends ILinkGenerator, ? super IModel<T>> mapper) {
 		super(renderer, ComponentFactories.fromLinkDescriptorMapper(mapper));
 	}
-	
+
+	@Override
+	public final MarkupContainer create(String wicketId, IModel<T> rowModel) {
+		return new CoreActionColumnLinkPanel<T>(wicketId, rowModel) {
+			private static final long serialVersionUID = 1L;
+			@Override
+			protected Component getLink(String string, IModel<T> rowModel) {
+				AbstractDynamicBookmarkableLink link = getFactory().create("link", rowModel);
+				decorateLink(link, rowModel);
+				return link;
+			}
+			@Override
+			protected PlaceholderContainer getPlaceholder(String string, IModel<T> rowModel) {
+				PlaceholderContainer placeholder = new PlaceholderContainer("linkPlaceholder");
+				decoratePlaceholder(placeholder, rowModel);
+				return placeholder;
+			}
+		};
+	}
+
 	@Override
 	protected void decorateLink(AbstractDynamicBookmarkableLink link, IModel<T> rowModel) {
 		super.decorateLink(link, rowModel);
--- igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/repeater/table/builder/action/ActionColumnSimpleElementBuilder.java
+++ igloo/igloo-components/igloo-component-wicket-more/src/main/java/org/iglooproject/wicket/more/markup/repeater/table/builder/action/ActionColumnSimpleElementBuilder.java
@@ -1,9 +1,13 @@
 package org.iglooproject.wicket.more.markup.repeater.table.builder.action;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.link.AbstractLink;
 import org.apache.wicket.model.IModel;
+import org.iglooproject.wicket.more.markup.repeater.table.column.CoreActionColumnSimpleElementPanel;
 
 import igloo.bootstrap.renderer.IBootstrapRenderer;
+import igloo.wicket.component.PlaceholderContainer;
 import igloo.wicket.factory.IOneParameterComponentFactory;
 
 public class ActionColumnSimpleElementBuilder<T, L extends AbstractLink>
@@ -14,6 +18,25 @@ public class ActionColumnSimpleElementBuilder<T, L extends AbstractLink>
 	public ActionColumnSimpleElementBuilder(IBootstrapRenderer<? super T> renderer,
 			IOneParameterComponentFactory<? extends L, IModel<T>> factory) {
 		super(renderer, factory);
+	}
+
+	@Override
+	public final MarkupContainer create(String wicketId, IModel<T> rowModel) {
+		return new CoreActionColumnSimpleElementPanel<T>(wicketId, rowModel) {
+			private static final long serialVersionUID = 1L;
+			@Override
+			protected Component getLink(String string, IModel<T> rowModel) {
+				L link = getFactory().create("link", rowModel);
+				decorateLink(link, rowModel);
+				return link;
+			}
+			@Override
+			protected PlaceholderContainer getPlaceholder(String string, IModel<T> rowModel) {
+				PlaceholderContainer placeholder = new PlaceholderContainer("linkPlaceholder");
+				decoratePlaceholder(placeholder, rowModel);
+				return placeholder;
+			}
+		};
 	}
 
 }
