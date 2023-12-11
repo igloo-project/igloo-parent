0ed8317cc827ffdd719b07992fb93825f52a912b
Aurelien JOLIVET <aurelien.jolivet@kobalt.fr>
2023-12-11 13:10:04 +0100

    Suppression ehcache

--- igloo/igloo-components/igloo-component-config-test/src/main/java/org/iglooproject/test/config/spring/ConfigurationPropertiesUrlConstants.java
+++ igloo/igloo-components/igloo-component-config-test/src/main/java/org/iglooproject/test/config/spring/ConfigurationPropertiesUrlConstants.java
@@ -4,7 +4,6 @@ public class ConfigurationPropertiesUrlConstants {
 
 	public static final String JPA_COMMON = "classpath:/configuration/jpa-common.properties";
 	public static final String FLYWAY_COMMON = "classpath:/configuration/flyway-common.properties";
-	public static final String JPA_EHCACHE_COMMON = "classpath:/configuration/jpa-ehcache-common.properties";
 	public static final String JPA_SEARCH_LUCENE_COMMON = "classpath:/configuration/jpa-search-lucene-common.properties";
 	public static final String JPA_SECURITY_COMMON = "classpath:/configuration/jpa-security-common.properties";
 	public static final String JERSEY_MOCK_COMMON = "classpath:/configuration/jersey-mock-common.properties";
