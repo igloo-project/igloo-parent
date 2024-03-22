8aaa2ec0983361182172b0c439fcf5ffdf938905
Laurent Almeras <lalmeras@gmail.com>
2024-03-22 10:06:12 +0100

    hibernate-search refactor (igloo 6.x)
    * ReferenceDataSearchQueryImpl -> OldReferenceDataSearchQueryImpl: uses
      old matchIf... helpers
    * new ReferenceDataSearchQueryImpl: prototype (old-style bean) with
      modern hibernate search DSL queries

--- igloo/igloo-components/igloo-component-jpa-more/src/main/java/org/iglooproject/jpa/more/business/referencedata/search/GenericReferenceDataSearchQueryImpl.java
+++ igloo/igloo-components/igloo-component-jpa-more/src/main/java/org/iglooproject/jpa/more/business/referencedata/search/GenericReferenceDataSearchQueryImpl.java
@@ -6,31 +6,19 @@ import org.apache.lucene.search.SortField;
 import org.iglooproject.commons.util.exception.IllegalSwitchValueException;
 import org.iglooproject.jpa.more.business.generic.model.search.EnabledFilter;
 import org.iglooproject.jpa.more.business.referencedata.model.GenericReferenceData;
-import org.iglooproject.jpa.more.business.search.query.OldAbstractHibernateSearchSearchQuery;
+import org.iglooproject.jpa.more.business.search.query.AbstractHibernateSearchSearchQuery;
 import org.iglooproject.jpa.more.business.sort.ISort;
 import org.iglooproject.jpa.more.business.sort.ISort.SortOrder;
 
-public abstract class GenericReferenceDataSearchQueryImpl
-		<
-		T extends GenericReferenceData<? super T, ?>,
-		S extends ISort<SortField>,
-		Q extends IGenericReferenceDataSearchQuery<T, S, Q>
-		>
-		extends OldAbstractHibernateSearchSearchQuery<T, S> implements IGenericReferenceDataSearchQuery<T, S, Q> {
-
+public class GenericReferenceDataSearchQueryImpl<
+			T extends GenericReferenceData<? super T, ?>,
+			S extends ISort<SortField>,
+			Q extends IGenericReferenceDataSearchQuery<T, S, Q>>
+		extends AbstractHibernateSearchSearchQuery<T, S> implements IGenericReferenceDataSearchQuery<T, S, Q> {
+	
 	@SafeVarargs
-	public GenericReferenceDataSearchQueryImpl(Class<? extends T> clazz, S... defaultSorts) {
+	protected GenericReferenceDataSearchQueryImpl(Class<T> clazz, S ... defaultSorts) {
 		super(clazz, defaultSorts);
-	}
-
-	@SafeVarargs
-	public GenericReferenceDataSearchQueryImpl(Class<? extends T>[] classes, S... defaultSorts) {
-		super(classes, defaultSorts);
-	}
-
-	@SuppressWarnings("unchecked")
-	protected final Q thisAsQ() {
-		return (Q) this;
 	}
 
 	@Override
@@ -38,16 +26,21 @@ public abstract class GenericReferenceDataSearchQueryImpl
 		if (enabledFilter != null && !EnabledFilter.ALL.equals(enabledFilter)) {
 			switch (enabledFilter) {
 			case ENABLED_ONLY:
-				must(matchIfGiven(GenericReferenceData.ENABLED, Boolean.TRUE));
+				must(p -> p.match().field(GenericReferenceData.ENABLED).matching(true));
 				break;
 			case DISABLED_ONLY:
-				must(matchIfGiven(GenericReferenceData.ENABLED, Boolean.FALSE));
+				must(p -> p.match().field(GenericReferenceData.ENABLED).matching(false));
 				break;
 			default:
 				throw new IllegalSwitchValueException(enabledFilter);
 			}
 		} 
 		return thisAsQ();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected final Q thisAsQ() {
+		return (Q) this;
 	}
 
 	@Override
