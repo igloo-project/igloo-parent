7924799a41e5e8317e8ebef24e66efd739ae5562
Roland Foucher <roland.foucher@kobalt.fr>
2024-07-03 15:14:15 +0200

    remove igloo-component-wicket-more-jqplot

--- igloo/igloo-components/igloo-component-wicket-more-jqplot/src/main/java/org/iglooproject/wicket/more/jqplot/util/LabelledSeries.java
+++
@@ -1,7 +0,0 @@
-package org.iglooproject.wicket.more.jqplot.util;
-
-import nl.topicus.wqplot.data.AbstractSeries;
-
-public class LabelledSeries<K, V> extends AbstractSeries<K, V, LabelledSeriesEntry<K, V>> {
-
-}
