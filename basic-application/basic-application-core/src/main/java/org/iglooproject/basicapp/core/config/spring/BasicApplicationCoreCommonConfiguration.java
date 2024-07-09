c7a41e365c547ed0230e9f5dd1cce78053028e91
Laurent Almeras <lalmeras@gmail.com>
2024-07-09 17:18:01 +0200

    remove useless warnings

--- basic-application/basic-application-core/src/main/java/org/iglooproject/basicapp/core/config/spring/BasicApplicationCoreCommonConfiguration.java
+++ basic-application/basic-application-core/src/main/java/org/iglooproject/basicapp/core/config/spring/BasicApplicationCoreCommonConfiguration.java
@@ -4,6 +4,8 @@ import org.iglooproject.basicapp.core.BasicApplicationCorePackage;
 import org.iglooproject.config.bootstrap.spring.annotations.ManifestPropertySource;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +13,18 @@ import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {
+	FreeMarkerAutoConfiguration.class, // Not used and trigger a warning at startup
+	// trigger warnings at startup
+	// ... Is this bean getting eagerly injected into a currently created BeanPostProcessor
+	// [projectingArgumentResolverBeanPostProcessor] ...
+	// https://github.com/spring-projects/spring-data-jpa/issues/3244
+	// https://github.com/spring-projects/spring-batch/issues/4489
+	// https://github.com/spring-projects/spring-boot/issues/38558
+	// https://github.com/spring-projects/spring-security/issues/14209
+	// https://github.com/spring-projects/spring-ws/issues/1391
+	SpringDataWebAutoConfiguration.class
+})
 @ManifestPropertySource(prefix = "basic-application.core")
 @Import({
 	BasicApplicationJpaModelConfiguration.class,
