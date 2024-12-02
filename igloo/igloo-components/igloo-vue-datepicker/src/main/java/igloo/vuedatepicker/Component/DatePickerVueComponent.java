ac268cb1d14c3af5107389af25bffcb5891c3ba6
Roland Foucher <roland.foucher@kobalt.fr>
2024-12-02 11:29:48 +0100

    test Abstract + multiples apps

--- igloo/igloo-components/igloo-vue-datepicker/src/main/java/igloo/vuedatepicker/Component/DatePickerVueComponent.java
+++ igloo/igloo-components/igloo-vue-datepicker/src/main/java/igloo/vuedatepicker/Component/DatePickerVueComponent.java
@@ -1,23 +1,15 @@
 package igloo.vuedatepicker.Component;
 
 import igloo.bootstrap.jsmodel.JsHelpers;
-import igloo.vuedatepicker.behavior.IJsDatePicker;
 import igloo.vuedatepicker.behavior.JsDatePicker;
-import igloo.vuedatepicker.behavior.VueBehavior;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.function.Consumer;
 import org.apache.commons.lang3.ArrayUtils;
-import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.html.form.AbstractTextComponent;
 import org.apache.wicket.model.IModel;
 
-public class DatePickerVueComponent extends AbstractTextComponent<LocalDate> {
+public class DatePickerVueComponent extends AbstractDatePickerVueComponent<LocalDate> {
 
   private static final long serialVersionUID = 1L;
-
-  private final VueBehavior vueBehavior;
 
   public DatePickerVueComponent(String id, IModel<LocalDate> model) {
     this(id, model, builder -> {});
@@ -25,52 +17,24 @@ public class DatePickerVueComponent extends AbstractTextComponent<LocalDate> {
 
   public DatePickerVueComponent(
       String id, IModel<LocalDate> model, Consumer<JsDatePicker.Builder> jsDatePickerConsumer) {
-    super(id, model);
-    setType(Date.class);
-
-    JsDatePicker.Builder builder =
-        JsDatePicker.builder()
-            .dateModel(
-                JsHelpers.ofLocalDateModel(getModel())) // TODO of(Temporal) puis check type ?
-            .autoApply(JsHelpers.of(true))
-            .enableTimePicker(JsHelpers.of(false))
-            .format(JsHelpers.of("dd/MM/yyyy"))
-            .textInput(JsHelpers.of(true));
-    jsDatePickerConsumer.accept(builder);
-
-    vueBehavior = new VueBehavior(builder.build());
-    add(vueBehavior);
+    super(id, model, jsDatePickerConsumer);
   }
 
   @Override
   public void convertInput() {
-    IJsDatePicker jsDatePicker = vueBehavior.getJsDatePicker();
     String[] value = getInputAsArray();
     if (ArrayUtils.isNotEmpty(value)) {
-      DateTimeFormatter format =
-          DateTimeFormatter.ofPattern(
-              (jsDatePicker == null || jsDatePicker.format() == null)
-                  ? "MM/dd/yyyy, HH:mm"
-                  : jsDatePicker.format().render().replace("\"", ""));
-
-      setConvertedInput(LocalDate.parse(value[0], format));
+      setConvertedInput(LocalDate.parse(value[0], getFormat()));
     }
   }
 
   @Override
-  protected void onComponentTag(final ComponentTag tag) {
-    // Must be attached to an vue-date-picker tag
-    checkComponentTag(tag, "vue-date-picker");
-
-    // Default handling for component tag
-    super.onComponentTag(tag);
-  }
-
-  public String getVModelVarName() {
-    return vueBehavior.getVmodelVarName(this);
-  }
-
-  public String getVueOnChangeVarName() {
-    return vueBehavior.getVueOnChangeVarName(this);
+  protected JsDatePicker.Builder getDefaultJsDatePickerBuilder() {
+    return JsDatePicker.builder()
+        .dateModel(JsHelpers.ofLocalDateModel(getModel()))
+        .autoApply(JsHelpers.of(true))
+        .enableTimePicker(JsHelpers.of(false))
+        .format(JsHelpers.of("dd/MM/yyyy"))
+        .textInput(JsHelpers.of(true));
   }
 }
