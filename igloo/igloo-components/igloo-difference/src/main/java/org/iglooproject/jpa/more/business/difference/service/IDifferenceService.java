421e5302b57f9184251c471a75978cfd88810f01
Roland Foucher <roland.foucher@kobalt.fr>
2025-02-05 15:32:36 +0100

    clean IDifferenceService#getMinimalDifferenceGenerator not used in new AbstractConfiguredDifferenceServiceImpl pattern.
    - use multiple different services if multiple use cases needed
    - create a local interface if use old pattern and need abstract methode

--- igloo/igloo-components/igloo-difference/src/main/java/org/iglooproject/jpa/more/business/difference/service/IDifferenceService.java
+++ igloo/igloo-components/igloo-difference/src/main/java/org/iglooproject/jpa/more/business/difference/service/IDifferenceService.java
@@ -9,10 +9,4 @@ public interface IDifferenceService<T> extends IHistoryDifferenceGenerator<T> {
    * @return A difference generator to be used in most cases.
    */
   IDifferenceFromReferenceGenerator<T> getMainDifferenceGenerator();
-
-  /**
-   * @return A difference generator to use when the diff must be made on part of the properties (for
-   *     instance for the creation of an object).
-   */
-  IDifferenceFromReferenceGenerator<T> getMinimalDifferenceGenerator();
 }
