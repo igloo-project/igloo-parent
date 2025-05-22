e5c980338dbc81bb3315887b2d9d8e98fc963739
Florian Lacreuse <florian.lacreuse@kobalt.fr>
2025-05-22 13:54:41 +0200

    BasicApp: use jakarta InternetAddress to validate email address

--- basic-application/basic-application-front/src/main/java/basicapp/front/common/validator/EmailAddressValueValidator.java
+++ basic-application/basic-application-front/src/main/java/basicapp/front/common/validator/EmailAddressValueValidator.java
@@ -1,5 +1,7 @@
 package basicapp.front.common.validator;
 
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidator;
 import org.apache.wicket.validation.ValidationError;
@@ -8,21 +10,25 @@ public class EmailAddressValueValidator implements IValidator<String> {
 
   private static final long serialVersionUID = 1L;
 
-  private static final EmailAddressValidator VALIDATOR = new EmailAddressValidator();
-
   private static final EmailAddressValueValidator INSTANCE = new EmailAddressValueValidator();
 
   public static EmailAddressValueValidator get() {
     return INSTANCE;
   }
 
-  protected EmailAddressValueValidator() {
-    super();
-  }
-
   @Override
   public void validate(IValidatable<String> validatable) {
-    if (!VALIDATOR.isValid(validatable.getValue())) {
+    String value = validatable.getValue();
+
+    if (!EmailAddressValidator.getInstance().isValid(value)) {
+      validatable.error(new ValidationError().addKey("common.validator.emailAddress"));
+      return;
+    }
+
+    try {
+      InternetAddress internetAddress = new InternetAddress(value);
+      internetAddress.validate();
+    } catch (AddressException e) {
       validatable.error(new ValidationError().addKey("common.validator.emailAddress"));
     }
   }
