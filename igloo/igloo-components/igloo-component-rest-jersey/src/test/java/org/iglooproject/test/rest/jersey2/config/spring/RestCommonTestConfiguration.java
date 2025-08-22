6a305d469273a6686e3d12af60603f908a0179be
Laurent Almeras <laurent.almeras@kobalt.fr>
2025-08-22 18:53:41 +0200

    igloo-component-rest-jersey: drop grizzly, use testcontainers
    Use @SpringBootTest for tests (same spring context used for client and
    server). Use testcontainers for database.

--- igloo/igloo-components/igloo-component-rest-jersey/src/test/java/org/iglooproject/test/rest/jersey2/config/spring/RestCommonTestConfiguration.java
+++ igloo/igloo-components/igloo-component-rest-jersey/src/test/java/org/iglooproject/test/rest/jersey2/config/spring/RestCommonTestConfiguration.java
@@ -1,29 +1,41 @@
 package org.iglooproject.test.rest.jersey2.config.spring;
 
-import org.iglooproject.config.bootstrap.spring.annotations.IglooPropertySourcePriority;
-import org.iglooproject.test.config.spring.ConfigurationPropertiesUrlConstants;
+import igloo.test.listener.postgresql.PsqlTestContainerConfiguration;
+import java.util.List;
+import org.glassfish.jersey.servlet.ServletContainer;
 import org.iglooproject.test.jpa.junit.JpaOnlyTestConfiguration;
 import org.iglooproject.test.rest.jersey2.business.RestTestBusinessPackage;
+import org.iglooproject.test.rest.jersey2.client.RestClientPackage;
+import org.iglooproject.test.rest.jersey2.server.SimpleRestApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
 @EnableAutoConfiguration
-@Import({JpaOnlyTestConfiguration.class})
+@Configuration
+@Import({JpaOnlyTestConfiguration.class, PsqlTestContainerConfiguration.class})
 @EntityScan(basePackageClasses = RestTestBusinessPackage.class)
-@ComponentScan(basePackageClasses = {RestTestBusinessPackage.class})
-@PropertySource(
-    name = IglooPropertySourcePriority.APPLICATION,
-    value = {
-      ConfigurationPropertiesUrlConstants.JPA_COMMON,
-      ConfigurationPropertiesUrlConstants.JERSEY_MOCK_COMMON,
-      "classpath:rest-common.properties"
-    },
-    encoding = "UTF-8")
+@ComponentScan(basePackageClasses = {RestTestBusinessPackage.class, RestClientPackage.class})
 @EnableTransactionManagement
-public class RestCommonTestConfiguration {}
+public class RestCommonTestConfiguration {
+
+  @Configuration
+  public static class WebXmlConfiguration {
+
+    @Bean
+    public ServletRegistrationBean<ServletContainer> restServlet() {
+      ServletContainer servletContainer = new ServletContainer(new SimpleRestApplication());
+      ServletRegistrationBean<ServletContainer> bean = new ServletRegistrationBean<>();
+      bean.setServlet(servletContainer);
+      bean.setName("restServlet");
+      bean.setUrlMappings(List.of("/*"));
+
+      return bean;
+    }
+  }
+}
