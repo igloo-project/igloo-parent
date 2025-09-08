984eca837782f79f3603525c0188c6ad2843a610
Roland Foucher <roland.foucher@kobalt.fr>
2025-09-08 08:58:51 +0200

    remove ICoreUserService - make userService more abstract

--- igloo/igloo-components/igloo-component-jpa-security/src/main/java/org/iglooproject/jpa/security/business/user/service/ICoreUserService.java
+++
@@ -1,8 +0,0 @@
-package org.iglooproject.jpa.security.business.user.service;
-
-import org.iglooproject.jpa.business.generic.model.GenericEntity;
-import org.iglooproject.jpa.business.generic.service.IGenericEntityService;
-import org.iglooproject.jpa.security.business.user.model.IUser;
-
-public interface ICoreUserService<U extends GenericEntity<Long, U> & IUser>
-    extends IGenericEntityService<Long, U>, ICoreUserSecurityService<U> {}
