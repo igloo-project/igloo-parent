7a0b1e533e19838faedf2270c2807636abd94532
Roland Foucher <roland.foucher@kobalt.fr>
2025-09-09 10:16:31 +0200

    add jpa and criteria sorts utils

--- igloo/igloo-components/igloo-component-jpa-more/src/main/java/org/iglooproject/jpa/more/util/jparepository/JpaRepositoryUtils.java
+++ igloo/igloo-components/igloo-component-jpa-more/src/main/java/org/iglooproject/jpa/more/util/jparepository/JpaRepositoryUtils.java
@@ -1,5 +1,8 @@
 package org.iglooproject.jpa.more.util.jparepository;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
 import java.util.Map;
 import org.iglooproject.jpa.more.business.sort.ISort;
 import org.iglooproject.jpa.more.business.sort.ISort.SortOrder;
@@ -10,12 +13,25 @@ import org.springframework.data.domain.Sort.Order;
 public class JpaRepositoryUtils {
   public static PageRequest createPageRequest(
       Map<? extends ISort<Order>, SortOrder> sorts, int offset, int limit) {
-    return PageRequest.of(
-        offset != 0 ? offset / limit : 0,
-        limit,
-        Sort.by(
-            sorts.entrySet().stream()
-                .flatMap(entry -> entry.getKey().getSortFields(entry.getValue()).stream())
-                .toList()));
+    return PageRequest.of(offset != 0 ? offset / limit : 0, limit, createSort(sorts));
+  }
+
+  public static Sort createSort(Map<? extends ISort<Order>, SortOrder> sorts) {
+    return Sort.by(
+        sorts.entrySet().stream()
+            .flatMap(entry -> entry.getKey().getSortFields(entry.getValue()).stream())
+            .toList());
+  }
+
+  public static List<jakarta.persistence.criteria.Order> createCriteriaOrders(
+      Map<? extends ISort<Order>, SortOrder> sorts, CriteriaBuilder cb, Root<?> root) {
+    return sorts.entrySet().stream()
+        .flatMap(entry -> entry.getKey().getSortFields(entry.getValue()).stream())
+        .map(
+            o ->
+                o.getDirection().isAscending()
+                    ? cb.asc(root.get(o.getProperty()))
+                    : cb.desc(root.get(o.getProperty())))
+        .toList();
   }
 }
